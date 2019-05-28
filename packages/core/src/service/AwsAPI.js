e024f39b0bcb04e582a2f466fe2713ddfd481515
LL\shosarin <shobhit.sarin@sapient.com>
2019-05-28 18:15:04 +0530

    Aws credentials update

--- packages/core/src/service/AwsAPI.js
+++ packages/core/src/service/AwsAPI.js
@@ -1,23 +1,28 @@
 
 import AWSAppSyncClient from 'aws-appsync';
-import {awsConfig} from './aws-cred-exports';
+// import { fetch } from "node-fetch";
+import { awsConfig } from './aws-cred-exports';
 
+// if(!process.browser) {
+//     global.fetch = fetch;
+// }
 
 
 export default class AwsAppSyncClient {
     static getClient() {
-        if(AwsAppSyncClient._client) return AwsAppSyncClient._client;
+        if(AwsAppSyncClient.client) return AwsAppSyncClient.client;
         return AwsAppSyncClient.createClient();
     }
+
     static createClient() {
-    AwsAppSyncClient._client = new AWSAppSyncClient({
+    AwsAppSyncClient.client = new AWSAppSyncClient({
             url: awsConfig.aws_appsync_graphqlEndpoint,
             region: awsConfig.aws_appsync_region,
             auth: {
               type: awsConfig.aws_appsync_authenticationType,
               apiKey:awsConfig.aws_appsync_apiKey,
-            },
+            }
           });
-          return AwsAppSyncClient._client;
+          return AwsAppSyncClient.client;
     }
 }
