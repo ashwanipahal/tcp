51602f0670f6e4b6bbcef75994f908a9277da71e
Alvaro Bueno <alvaro.bueno@publicissapient.com>
2019-06-04 16:24:23 -0400

    [WIP] module D grid issues

---
+++ packages/web/src/components/common/molecules/moduleD/ModuleD.js
@@ -0,0 +1,75 @@
+// any molecule will come here
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { PropTypes } from 'prop-types';
+import Grid from '@tcp/core/src/components/common/molecules/Grid';
+import Col from '@tcp/core/src/components/common/atoms/Col';
+import Row from '@tcp/core/src/components/common/atoms/Row';
+import theme from '@tcp/web/Styles/themes/primary';
+
+const ModuleDBorder = styled.div`
+  border: 1px solid #979797;
+`;
+
+const StyledH2 = styled.h2`
+  font-family: 'Montserrat';
+  font-weight: 900;
+  text-transform: uppercase;
+  font-size: 48px;
+  text-align: center;
+`;
+
+const StyledLink = styled.a`
+  font-family: Nunito;
+  font-size: 15px;
+  color: #575757;
+  margin-top: 10px;
+  display: block;
+  text-align: center;
+`;
+
+const StyledButton = styled.button`
+  font-family: 'Nunito';
+  font-weight: 600;
+  text-transform: uppercase;
+  font-size: 16px;
+  border: 1px solid #979797;
+  color: #575757;
+  padding: 19px 80px;
+`;
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const colSize = {
+  small: 3,
+  medium: 2,
+  large: 3,
+  xlarge: 2,
+};
+
+const ModuleD = ({ assets }) => (
+  <Grid style={ModuleDBorder} theme={theme}>
+    <StyledH2>Mini me shop</StyledH2>
+    <Row>
+      {assets &&
+        assets.map((item, i) => (
+          <Col key={i} colSize={colSize}>
+            <img src={item.url} />
+            <StyledLink href="{item.link}">{item.text}</StyledLink>
+          </Col>
+        ))}
+    </Row>
+
+    <Row>
+      <StyledButton>Shop all</StyledButton>
+    </Row>
+  </Grid>
+);
+ModuleD.propTypes = {
+  assets: PropTypes.array.isRequired,
+};
+
+export default ModuleD;
--- packages/web/src/components/features/content/HomePage/views/HomePage.view.jsx
+++ packages/web/src/components/features/content/HomePage/views/HomePage.view.jsx
@@ -4,6 +4,7 @@ import Col from '@tcp/core/src/components/common/atoms/Col';
 import Grid from '@tcp/core/src/components/common/molecules/Grid';
 import { PropTypes } from 'prop-types';
 import { NavBar, Test } from '../molecules';
+import ModuleD from '@tcp/web/src/components/common/molecules/moduleD/moduleD';
 
 // colCount is the number of columns the component needs to cover in each of the viewport
 const colSize = {
@@ -43,10 +44,33 @@ const offsetLeft2 = {
   medium: 2,
 };
 
+const moduleDAssets = [
+  {
+    url: 'https://via.placeholder.com/330',
+    text: 'mom & me tees 1',
+    link: 'http://www.google.com',
+  },
+  {
+    url: 'https://via.placeholder.com/330',
+    text: 'mom & me tees 2',
+    link: 'http://www.google.com',
+  },
+  {
+    url: 'https://via.placeholder.com/330',
+    text: 'mom & me tees 3',
+    link: 'http://www.google.com',
+  },
+  {
+    url: 'https://via.placeholder.com/330',
+    text: 'mom & me tees 4',
+    link: 'http://www.google.com',
+  },
+];
+
 const HomePageView = ({ links }) => (
   <Fragment>
     <NavBar links={links} />
-    <Test className="test" />
+    <ModuleD assets={moduleDAssets} />
     <Grid>
       <Row noFlex>
         <Col isColInlineBlock colSize={colSize} offsetRight={offsetRight}>
