8fdf6a16dda8fa680420e3b7e5abb6dc361fb42d
LL\shosarin <shobhit.sarin@sapient.com>
2019-06-09 01:34:30 +0530

    next config for flow

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,4 +1,5 @@
 const withTM = require('next-transpile-modules');
+const path = require('path');
 
 module.exports = withTM({
   transpileModules: ['@tcp'],
@@ -6,6 +7,13 @@ module.exports = withTM({
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
+    });
+    config.module.rules.forEach(rule => {
+      const newRule = rule;
+      if (newRule.use && newRule.use.loader === 'next-babel-loader') {
+        newRule.use.options.configFile = path.resolve('./.babelrc');
+      }
+      return newRule;
     });
 
     return config;
