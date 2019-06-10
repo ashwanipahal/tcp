c64cfac70a44218a641ea01b911101077edbcf3b
Raghav <raghasb@gmail.com>
2019-06-10 18:20:37 +0530

    Added dummy data to header

--- packages/web/src/components/common/organisms/Header/Header.style.js
+++ packages/web/src/components/common/organisms/Header/Header.style.js
@@ -4,11 +4,42 @@ export default css`
   .header-topnav {
     background-color: ${props => props.theme.colors.PRIMARY.PALEGRAY};
     height: 45px;
+    text-transform: uppercase;
   }
   .header-topnav__promo-area {
     text-align: center;
   }
   .header-topnav__track-order {
     text-align: center;
+  }
+  .header-brand {
+    box-sizing: border-box;
+    height: 129px;
+    padding: 50px 0px;
+    text-align: center;
+  }
+  .dummy-nav {
+    color: ${props => props.theme.colors.PRIMARY.DARK};
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    text-transform: uppercase;
+    padding: 10px 0;
+  }
+  .header-promo {
+    background-color: ${props => props.theme.colors.PRIMARY.PALEGRAY};
+    border-top: 1px solid ${props => props.theme.colors.PRIMARY.LIGHTGRAY};
+    box-sizing: border-box;
+    height: 60px;
+    padding: 20px 0;
+    text-align: center;
+    text-transform: uppercase;
+  }
+  .header-loyalty {
+    background-color: ${props => props.theme.colors.PRIMARY.LIGHTGRAY};
+    padding: 36px 0;
+    text-align: center;
+    text-transform: uppercase;
   }
 `;
--- packages/web/src/components/common/organisms/Header/views/Header.view.jsx
+++ packages/web/src/components/common/organisms/Header/views/Header.view.jsx
@@ -24,7 +24,7 @@ const Header = ({ className }) => (
           colSize={{
             large: 8,
             medium: 4,
-            small: 3,
+            small: 0,
           }}
         >
           Promo area
@@ -38,6 +38,73 @@ const Header = ({ className }) => (
           }}
         >
           Track order
+        </Col>
+      </Row>
+    </div>
+    <div className="header-brand">
+      <Row>
+        <Col
+          className="header-brand__home-logo"
+          colSize={{
+            large: 12,
+            medium: 8,
+            small: 6,
+          }}
+        >
+          BRAND HOME LOGO
+        </Col>
+      </Row>
+    </div>
+    <div className="header-nav">
+      <Row>
+        <Col
+          className="header-nav__nav-row"
+          colSize={{
+            large: 12,
+            medium: 0,
+            small: 0,
+          }}
+        >
+          <div className="dummy-nav">
+            <div>Girl</div>
+            <div>Toddler Girl</div>
+            <div>Boy</div>
+            <div>Toddler Boy</div>
+            <div>Baby</div>
+            <div>Shoes</div>
+            <div>Accessories</div>
+            <div>Trending</div>
+            <div>Gift Cards</div>
+            <div>Clearance</div>
+          </div>
+        </Col>
+      </Row>
+    </div>
+    <div className="header-promo">
+      <Row>
+        <Col
+          className="header-promo__promo-banner"
+          colSize={{
+            large: 12,
+            medium: 8,
+            small: 6,
+          }}
+        >
+          Promo banners
+        </Col>
+      </Row>
+    </div>
+    <div className="header-loyalty">
+      <Row>
+        <Col
+          className="header-loyalty__promo-loyalty"
+          colSize={{
+            large: 12,
+            medium: 8,
+            small: 6,
+          }}
+        >
+          Loyalty Promo banners
         </Col>
       </Row>
     </div>
