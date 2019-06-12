85783be7693186f4f203763430b518c186821226
aditya.vashist <aditya.vashist.sd@gmail.com>
2019-06-12 13:11:57 +0530

    unwanted file removed

--- packages/core/styles/globalStyles/index.js
+++ packages/core/styles/globalStyles/index.js
@@ -1,11 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 import globalStyles from './commonStyles';
 import fonts from './fonts';
-import icons from './icons';
 
 // eslint-disable-next-line no-unused-expressions
 export default createGlobalStyle`
   ${fonts}
   ${globalStyles}
-  ${icons}
 `;
