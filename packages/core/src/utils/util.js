e5fc548c480ef91b5cf3ca49c8c64da630e9aae4
LL\shosarin <shobhit.sarin@sapient.com>
2019-06-13 14:07:28 +0530

    review comments of react-axe

--- packages/core/src/utils/util.js
+++ packages/core/src/utils/util.js
@@ -1,10 +1,10 @@
-import { ENV_PRODUCTION } from '../constants/env.config';
+import { ENV_PRODUCTION, ENV_DEVELOPMENT } from '../constants/env.config';
 
 module.exports = {
   isProduction() {
     return process.env.NODE_ENV === ENV_PRODUCTION;
   },
   isDevelopment() {
-    return process.env.NODE_ENV !== ENV_PRODUCTION;
+    return process.env.NODE_ENV === ENV_DEVELOPMENT;
   },
 };
