3c701ed9f594efaeab34adde2875c63eba67974b
shobhitsarin <shobhit.sarin@sapient.com>
2019-06-18 13:47:58 +0530

    Reselect in store view

--- packages/core/src/components/features/browse/ProductListingPage/container/ProductListingPage.container.js
+++ packages/core/src/components/features/browse/ProductListingPage/container/ProductListingPage.container.js
@@ -1,33 +1,34 @@
 import React from 'react';
-import { connect } from "react-redux";
-import { getPlpProducts } from "./ProductListingPage.actions";
-import { ProductListView } from "../views/ProductListingPage.view";
+import { connect } from 'react-redux';
+import { getPlpProducts } from './ProductListingPage.actions';
+import { ProductListView } from '../views/ProductListingPage.view';
+import getExpensivePlpProducts from './ProductListingPage.storeview';
 
 class ProductListingPageContainer extends React.Component {
-
   componentDidMount() {
     this.props.getProducts();
   }
 
   render() {
-    return (
-        <ProductListView data={this.props.products}/>
-    );
+    return <ProductListView data={this.props.products} />;
   }
 }
 
 function mapStateToProps(state) {
- return {
-  products: state.ProductListingPageReducer.products
- };
-};
+  return {
+    products: getExpensivePlpProducts(state),
+  };
+}
 
 function mapDispatchToProps(dispatch) {
-  return { 
-  getProducts: () => {
-    dispatch(getPlpProducts());
-  }
+  return {
+    getProducts: () => {
+      dispatch(getPlpProducts());
+    },
+  };
 }
-};
 
-export default connect(mapStateToProps,mapDispatchToProps)(ProductListingPageContainer);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProductListingPageContainer);
