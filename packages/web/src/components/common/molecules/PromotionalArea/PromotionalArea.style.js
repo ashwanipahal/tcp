6c07ed09c9eb59ef875352237a11efad4f852a9f
Raghav <raghasb@gmail.com>
2019-06-18 13:36:48 +0530

    Updated mock data

--- packages/web/src/components/common/molecules/PromotionalArea/PromotionalArea.style.js
+++ packages/web/src/components/common/molecules/PromotionalArea/PromotionalArea.style.js
@@ -27,4 +27,12 @@ export default css`
       font-size: ${props => props.theme.fonts.fontSize.body.bodytext.copy4}px;
     }
   }
+
+  a {
+    color: ${props => props.theme.colors.WHITE};
+
+    @media ${props => props.theme.mediaQuery.medium} {
+      color: ${props => props.theme.colors.BLACK};
+    }
+  }
 `;
