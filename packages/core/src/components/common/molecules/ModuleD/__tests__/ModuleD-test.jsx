d337bfedc7c327e1f0666f3bc1220c7176e06b0c
Raghav <raghasb@gmail.com>
2019-06-24 13:59:56 +0530

    updated moduleD test

--- packages/core/src/components/common/molecules/ModuleD/__tests__/ModuleD-test.jsx
+++ packages/core/src/components/common/molecules/ModuleD/__tests__/ModuleD-test.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ModuleD from '../ModuleD';
-import mock from '../mock';
 
 describe('ModuleD component', () => {
   it('renders correctly', () => {
-    const props = {
-      data: mock,
-    };
+    const props = {};
 
     const wrapper = shallow(<ModuleD {...props} />);
     expect(wrapper).toMatchSnapshot();
