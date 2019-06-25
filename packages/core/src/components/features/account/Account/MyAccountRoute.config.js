865fea14c108c4213c972a2b682b59d2c61db87f
priya <pkumari12@sapient.com>
2019-06-25 18:20:19 +0530

    updated snapshot

--- packages/core/src/components/features/account/Account/MyAccountRoute.config.js
+++ packages/core/src/components/features/account/Account/MyAccountRoute.config.js
@@ -2,52 +2,52 @@
  * The following data is just for the purpose of painting the left nav in account section
  * This will change once we integrate it with cms labels
  */
-
+// will uncomment once we have nav data story in place
 const navData = [
-  {
-    id: 'accountOverview',
-    url: '/account',
-    displayName: 'Account Overview',
-    component: 'accountOverview',
-    href: '/account?id=accountOverview',
-  },
-  {
-    id: 'myPlaceRewards',
-    url: '/account/place-rewards',
-    displayName: 'My Place Rewards',
-    component: 'myPlaceRewards',
-    href: '/account?id=myPlaceRewards',
-    subSections: [
-      {
-        id: 'pointsHistory',
-        url: '/account/place-rewards/points-history',
-        displayName: 'Points History',
-        component: 'pointsHistoryPage',
-        href: '/account?id=pointsHistoryPage',
-      },
-    ],
-  },
-  {
-    id: 'myWallet',
-    url: '/account/wallet',
-    displayName: 'My Wallet',
-    component: 'myWalletPage',
-    href: '/account?id=myWalletPage',
-  },
-  {
-    id: 'earnExtraPoints',
-    url: '/account/extra-points',
-    displayName: 'Earn Extra Points',
-    component: 'earnExtraPointsPage',
-    href: '/account?id=earnExtraPointsPage',
-  },
-  {
-    id: 'profileInformation',
-    url: '/account/profile',
-    displayName: 'Profile Information',
-    component: 'profileInformation',
-    href: '/account?id=profileInformation',
-  },
+  // {
+  //   id: 'accountOverview',
+  //   url: '/account',
+  //   displayName: 'Account Overview',
+  //   component: 'accountOverview',
+  //   href: '/account?id=accountOverview',
+  // },
+  // {
+  //   id: 'myPlaceRewards',
+  //   url: '/account/place-rewards',
+  //   displayName: 'My Place Rewards',
+  //   component: 'myPlaceRewards',
+  //   href: '/account?id=myPlaceRewards',
+  //   subSections: [
+  //     {
+  //       id: 'pointsHistory',
+  //       url: '/account/place-rewards/points-history',
+  //       displayName: 'Points History',
+  //       component: 'pointsHistoryPage',
+  //       href: '/account?id=pointsHistoryPage',
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 'myWallet',
+  //   url: '/account/wallet',
+  //   displayName: 'My Wallet',
+  //   component: 'myWalletPage',
+  //   href: '/account?id=myWalletPage',
+  // },
+  // {
+  //   id: 'earnExtraPoints',
+  //   url: '/account/extra-points',
+  //   displayName: 'Earn Extra Points',
+  //   component: 'earnExtraPointsPage',
+  //   href: '/account?id=earnExtraPointsPage',
+  // },
+  // {
+  //   id: 'profileInformation',
+  //   url: '/account/profile',
+  //   displayName: 'Profile Information',
+  //   component: 'profileInformation',
+  //   href: '/account?id=profileInformation',
+  // },
   {
     id: 'addressBook',
     url: '/account/address-book',
@@ -55,27 +55,27 @@ const navData = [
     component: 'addressBook',
     href: '/account?id=addressBook',
   },
-  {
-    id: 'paymentGiftCards',
-    url: '/account/payment',
-    displayName: 'Payment & Gift Cards',
-    component: 'paymentGiftCardsPage',
-    href: '/account?id=paymentGiftCardsPage',
-  },
-  {
-    id: 'myPreferences',
-    url: '/account/my-preference',
-    displayName: 'My Preferences',
-    component: 'myPreferencePage',
-    href: '/account?id=myPreferencePage',
-  },
-  {
-    id: 'myPlaceRewardsCC',
-    url: '/account/rewardsCreditCard',
-    displayName: 'My Place Rewards Credit Card',
-    component: 'myPlaceRewardsCCPage',
-    href: '/account?id=myPlaceRewardsCCPage',
-  },
+  // {
+  //   id: 'paymentGiftCards',
+  //   url: '/account/payment',
+  //   displayName: 'Payment & Gift Cards',
+  //   component: 'paymentGiftCardsPage',
+  //   href: '/account?id=paymentGiftCardsPage',
+  // },
+  // {
+  //   id: 'myPreferences',
+  //   url: '/account/my-preference',
+  //   displayName: 'My Preferences',
+  //   component: 'myPreferencePage',
+  //   href: '/account?id=myPreferencePage',
+  // },
+  // {
+  //   id: 'myPlaceRewardsCC',
+  //   url: '/account/rewardsCreditCard',
+  //   displayName: 'My Place Rewards Credit Card',
+  //   component: 'myPlaceRewardsCCPage',
+  //   href: '/account?id=myPlaceRewardsCCPage',
+  // },
 ];
 
 export default navData;
