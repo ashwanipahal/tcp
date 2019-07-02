2789498a0b08430c7969fcb682f1551c5e844a6d
Prasoon <prasoon.singh@publicissapient.com>
2019-07-02 18:48:17 +0530

    fix-issues

--- packages/core/src/components/common/atoms/Button/__tests__/Button.native-test.jsx
+++ packages/core/src/components/common/atoms/Button/__tests__/Button.native-test.jsx
@@ -3,22 +3,27 @@ import { shallow } from 'enzyme';
 import Button from '../views/Button.native';
 
 describe('Button', () => {
+  const props = {
+    id: 'btn',
+    name: 'button test',
+    buttonVariation: 'fixed-width',
+  };
+
+  let component = null;
+
+  beforeEach(() => {
+    component = shallow(<Button {...props} />);
+  });
+
   it('should be defined', () => {
     expect(Button).toBeDefined();
   });
 
   it('should render correctly', () => {
-    const tree = shallow(<Button name="button test" />);
-    expect(tree).toMatchSnapshot();
+    expect(component).toMatchSnapshot();
   });
 
-  it('renders correctly', () => {
-    const props = {
-      title: 'Hello',
-      id: 'abc',
-      buttonVariation: 'fixed-width',
-    };
-    const component = shallow(<Button {...props} />);
-    expect(component.find('#abc')).toHaveLength(1);
+  it('should return abc component value one', () => {
+    expect(component.find('#btn')).toHaveLength(1);
   });
 });
