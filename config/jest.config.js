cd84e8a613f7f02e41b000d004b0daf34c7c93f4
Siddhartha Nanda <sidnanda@publicisgroupe.net>
2019-07-02 11:34:58 +0530

    added jest coverageThreshold 50%

--- config/jest.config.js
+++ config/jest.config.js
@@ -47,4 +47,12 @@ module.exports = {
   ],
   coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
 };
