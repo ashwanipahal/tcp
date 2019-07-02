535531010454dff43d4b532e39ea69e805dff6ce
Siddhartha Nanda <sidnanda@publicisgroupe.net>
2019-07-02 11:23:44 +0530

    test case added for BrandTabs

--- packages/web/src/components/common/molecules/BrandTabs/__tests__/BrandTabs-test.jsx
+++ packages/web/src/components/common/molecules/BrandTabs/__tests__/BrandTabs-test.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import BrandTabs from '../views';
+import { BrandTabsVanilla } from '../views/BrandTabs';
 import mockData from './mock';
 
 describe('BrandTabs component', () => {
+  it('renders correctly with tcp logoClass', () => {
+    const props = {
+      className: 'brand-tabs',
+      data: mockData.brand_tabs,
+      logoClass: 'header__brand-tab--tcp',
+    };
+    const component = shallow(<BrandTabsVanilla {...props}>Brand Tabs with TCP</BrandTabsVanilla>);
+    expect(component).toMatchSnapshot();
+  });
+
   it('renders correctly', () => {
     const props = {
       className: 'brand-tabs',
       data: mockData.brand_tabs,
+      logoClass: 'header__brand-tab--gymboree',
     };
-    const component = shallow(<BrandTabs {...props} />);
+    const component = shallow(
+      <BrandTabsVanilla {...props}>Brand Tabs with Gymboree</BrandTabsVanilla>
+    );
     expect(component).toMatchSnapshot();
   });
 });
