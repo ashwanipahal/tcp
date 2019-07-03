b01805aa244ac56dbfa33107232328abdf35896c
Siddhartha Nanda <sidnanda@publicisgroupe.net>
2019-07-03 10:06:26 +0530

    review comments incorporated

--- packages/web/src/components/common/molecules/BrandTabs/__tests__/BrandTabs-test.jsx
+++ packages/web/src/components/common/molecules/BrandTabs/__tests__/BrandTabs-test.jsx
@@ -4,25 +4,13 @@ import { BrandTabsVanilla } from '../views/BrandTabs';
 import mockData from './mock';
 
 describe('BrandTabs component', () => {
-  it('renders correctly with tcp logoClass', () => {
+  it('renders correctly with brand-tabs className', () => {
     const props = {
       className: 'brand-tabs',
       data: mockData.brand_tabs,
-      logoClass: 'header__brand-tab--tcp',
     };
     const component = shallow(<BrandTabsVanilla {...props}>Brand Tabs with TCP</BrandTabsVanilla>);
     expect(component).toMatchSnapshot();
-  });
-
-  it('renders correctly', () => {
-    const props = {
-      className: 'brand-tabs',
-      data: mockData.brand_tabs,
-      logoClass: 'header__brand-tab--gymboree',
-    };
-    const component = shallow(
-      <BrandTabsVanilla {...props}>Brand Tabs with Gymboree</BrandTabsVanilla>
-    );
-    expect(component).toMatchSnapshot();
+    expect(component.find('.brand-tabs')).toHaveLength(1);
   });
 });
