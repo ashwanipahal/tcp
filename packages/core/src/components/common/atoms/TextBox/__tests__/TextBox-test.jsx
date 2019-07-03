c1475de28be18a3f3728a81a60938a1b7effa285
sachin rawal <srawal.sapient@outlook.com>
2019-07-03 16:04:27 +0530

    update changes

--- packages/core/src/components/common/atoms/TextBox/__tests__/TextBox-test.jsx
+++ packages/core/src/components/common/atoms/TextBox/__tests__/TextBox-test.jsx
@@ -9,6 +9,7 @@ describe('Textbox component', () => {
       id: 'abcd',
       className: 'asdfasdf',
       meta: 'sdfdsf',
+      input: { value: 'hello' },
     };
     const component = shallow(<TextBoxVanilla {...props} />);
     expect(component).toMatchSnapshot();
