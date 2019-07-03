2ca5268bc65d22846ceee20a5f20c1c7758fb4c0
Siddhartha Nanda <sidnanda@publicisgroupe.net>
2019-07-03 16:00:52 +0530

    review comments incorporated

--- packages/core/src/components/common/molecules/Modal/__tests__/ModalCloseIcon-test.jsx
+++ packages/core/src/components/common/molecules/Modal/__tests__/ModalCloseIcon-test.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import ModalCloseIcon from '../view/ModalCloseIcon';
+import { ModalCloseIconVanilla } from '../view/ModalCloseIcon';
 
 describe('Modal Close Icon', () => {
   it('renders correctly with props', () => {
     const props = {
+      className: 'modal-close-button',
       closeFunc: jest.fn(),
     };
     const component = shallow(
-      <ModalCloseIcon {...props}>Modal Close Icon with props</ModalCloseIcon>
+      <ModalCloseIconVanilla {...props}>Modal Close Icon with props</ModalCloseIconVanilla>
     );
     expect(component).toMatchSnapshot();
+  });
+
+  it('renders correctly with function call', () => {
+    const props = {
+      closeFunc: jest.fn(),
+      className: 'modal-close-button',
+    };
+    const component = shallow(
+      <ModalCloseIconVanilla {...props}>Modal Close Icon with function call</ModalCloseIconVanilla>
+    );
+    component.find('button').simulate('click', props.closeFunc);
+    expect(component.find('.modal-close-button')).toHaveLength(1);
+    expect(props.closeFunc).toBeCalled();
   });
 });
