b7061df16e1f90c3613bb64d16ce98fd3245562a
naman jain <njain63@sapient.com>
2019-07-04 12:26:15 +0530

    test cases in progress

--- packages/core/src/components/features/account/AddressVerification/views/__tests__/AddressVerification.view-test.jsx
+++ packages/core/src/components/features/account/AddressVerification/views/__tests__/AddressVerification.view-test.jsx
@@ -1,34 +1,49 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { AddressVerification } from '../AddressVerification.view';
-import CONSTANTS from '../../AddressVerification.constants';
 
 describe('AddressVerification component', () => {
-  it('should renders modal component if verification result is not valid', () => {
-    const props = {
-      heading: 'Add Address',
-      userAddress: {
-        firstName: 'test',
-        lastName: 'test',
-        addressLine: ['test line 1', 'test line 2', ''],
-        city: 'test city',
-        state: 'test state',
-        zipCode: '11111'
-      },
-      suggestedAddress: {
-        firstName: 'suggested firstName',
-        lastName: 'suggested lastName',
-        addressLine: ['test suggested','',  ''],
-        city: 'test suggested city',
-        state: 'test suggested state',
-        zipCode: '11111'
-      },
-      verificationResult: {
-        result: CONSTANTS.VERIFY_ADDRESS_RESULT.INVALID_ERROR,
-        status: 'AE'
-      }
+  describe('for valid address', () => {
+    let component;
+    let onSuccessSpy;
+    let resetVerifyAddressActionSpy;
+    const userAddress = {
+      firstName: 'test',
+      lastName: 'test',
+      addressLine: ['test line 1', 'test line 2', ''],
+      city: 'test city',
+      state: 'test state',
+      zipCode: '11111',
     };
-    const component = shallow(<AddressVerification {...props} />);
-    expect(component).toMatchSnapshot();
+
+    beforeEach(() => {
+      onSuccessSpy = jest.fn();
+      resetVerifyAddressActionSpy = jest.fn();
+      const props = {
+        heading: 'Add Address',
+        userAddress,
+        suggestedAddress: userAddress,
+        verificationResult: '',
+        labels: {},
+        onSuccess: onSuccessSpy,
+        resetVerifyAddressAction: resetVerifyAddressActionSpy,
+      };
+      component = shallow(<AddressVerification {...props} />);
+      component.setProps({
+        verificationResult: 'AS01',
+      });
+    });
+
+    it('should not render anything', () => {
+      expect(component.isEmptyRender()).toBeTruthy();
+    });
+
+    it('should call onSuccess prop with formatted payload', () => {
+      expect(onSuccessSpy.mock.calls[0][0].contact).toHaveLength(1);
+    });
+
+    it('should call resetVerifyAddressAction props', () => {
+      expect(resetVerifyAddressActionSpy).toBeCalled();
+    });
   });
 });
