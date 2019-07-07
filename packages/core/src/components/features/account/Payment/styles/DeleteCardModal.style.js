6012108d04327d6e092c671609a33101e0abcf34
sachin rawal <srawal.sapient@outlook.com>
2019-07-07 09:37:51 +0530

    update styling

--- packages/core/src/components/features/account/Payment/styles/DeleteCardModal.style.js
+++ packages/core/src/components/features/account/Payment/styles/DeleteCardModal.style.js
@@ -4,28 +4,51 @@ const deleteCardModalStyle = css`
   @media ${props => props.theme.mediaQuery.medium} {
     margin: 0 80px;
   }
+  @media ${props => props.theme.mediaQuery.smallOnly} {
+    .deleteCardModal_card {
+      display: block;
+    }
+  }
   .deleteCardModal_cardInfo {
     float: left;
     width: 80%;
     padding-left: 20px;
+    @media ${props => props.theme.mediaQuery.smallOnly} {
+      width: 75%;
+    }
   }
   .deleteCardModal_expiry {
     float: right;
+    @media ${props => props.theme.mediaQuery.smallOnly} {
+      float: none;
+    }
   }
   .deleteCardModal_btnWrapper {
+    text-align: center;
     padding-top: 80px;
     clear: both;
     button.deleteCardModal_btn {
       display: inline-block;
       font-weight: ${props => props.theme.fonts.fontWeight.normal};
-      float: right;
-      width: 40%;
+      float: none;
+      width: 60%;
+      @media ${props => props.theme.mediaQuery.medium} {
+        float: right;
+        width: 40%;
+      }
     }
     button.deleteCardModal_deleteCancel {
-      float: left;
+      float: none;
+      @media ${props => props.theme.mediaQuery.medium} {
+        float: left;
+      }
     }
   }
   .deleteCardModal_desc {
+    @media ${props => props.theme.mediaQuery.smallOnly} {
+      margin: 0 auto;
+      width: 75%;
+    }
     .deleteCardModal_img {
       width: 15%;
       float: left;
