c65f15741a6d82210884054b4ec9aaa5013d7341
Abhishek Jha <abhishekjha@Abhisheks-MacBook-Pro.local>
2019-07-08 19:12:51 +0530

    Basic Animation Achieved

--- packages/mobileapp/screens/AnimationIconPage.js
+++ packages/mobileapp/screens/AnimationIconPage.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import { Button, StyleSheet, UIManager, LayoutAnimation, Image, View } from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  UIManager,
+  LayoutAnimation,
+  Image,
+  View,
+  TouchableOpacity,
+} from 'react-native';
 import jake from './jake.png';
 
 UIManager.setLayoutAnimationEnabledExperimental &&
@@ -8,32 +16,49 @@ UIManager.setLayoutAnimationEnabledExperimental &&
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { position: 'flex-end' };
+    this.state = { openSwitch: false };
   }
 
-  changePosition = position => {
+  changePosition = () => {
     LayoutAnimation.spring();
     this.setState({
-      position,
+      openSwitch: !this.state.openSwitch,
     });
   };
 
   render() {
     return (
-      <View style={[styles.container, { justifyContent: this.state.position }]}>
-        <Image source={jake} />
-        <View style={styles.buttonsContainer}>
-          <Button
-            style={styles.button}
-            title="Middle"
-            onPress={() => this.changePosition('center')}
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={() => this.changePosition()}
+          style={{
+            flex: 1,
+            justifyContent: this.state.openSwitch ? 'center' : 'flex-end',
+            alignItems: this.state.openSwitch ? 'flex-start' : 'flex-end',
+          }}
+        >
+          <Image source={jake} style={{ height: 70, width: 70 }} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => this.changePosition()}
+          style={[
+            styles.secondIcon,
+            {
+              flex: 1,
+              backgroundColor: this.state.openSwitch ? '#00000070' : '#ff00ff',
+              justifyContent: this.state.openSwitch ? 'center' : 'flex-end',
+              alignItems: this.state.openSwitch ? 'center' : 'flex-start',
+            },
+          ]}
+        >
+          <Image
+            source={jake}
+            style={{
+              height: this.state.openSwitch ? 70 : 0,
+              width: this.state.openSwitch ? 70 : 0,
+            }}
           />
-          <Button
-            style={styles.button}
-            title="Bottom"
-            onPress={() => this.changePosition('flex-end')}
-          />
-        </View>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -41,11 +66,18 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    paddingBottom: 32,
+    flexDirection: 'row',
+    height: 200,
+    position: 'absolute',
+    bottom: 0,
+    backgroundColor: '#F00',
+    width: '100%',
+  },
+  secondIcon: {
+    flex: 1,
+    // alignItems: "flex-end",
   },
   buttonsContainer: {
     flexDirection: 'row',
