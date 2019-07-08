43b89ebf155d6f9c2b6d4da48eda7da34ece45e0
sachin rawal <srawal.sapient@outlook.com>
2019-07-08 13:11:51 +0530

    delete saga

---
+++ packages/core/src/components/features/account/Payment/container/__tests__/DeleteCard.saga-test.js
@@ -0,0 +1,60 @@
+import { takeLatest, put } from 'redux-saga/effects';
+import PAYMENT_CONSTANTS from '../../Payment.constants';
+import { DeleteCardSaga, deleteCard } from '../DeleteCard.saga';
+import {
+  updateCardCardListonDelete,
+  updateCardListonDeleteErr,
+  setDeleteModalMountedState,
+} from '../Payment.actions';
+
+describe('DeleteAddressSaga', () => {
+  describe('deleteAddress', () => {
+    let deleteCardGen;
+    const payload = {
+      creditCardId: '123456',
+      action: 'D',
+    };
+    beforeEach(() => {
+      deleteCardGen = deleteCard({ payload });
+      deleteCardGen.next();
+    });
+    it('should dispatch updateCardCardListonDelete action for success response', () => {
+      const response = {
+        statusCode: 200,
+        body: { addressId: '12345' },
+      };
+      const putDescriptor = deleteCardGen.next(response).value;
+      expect(putDescriptor).toEqual(put(updateCardCardListonDelete(response.body)));
+      expect(deleteCardGen.next().value).toEqual(put(setDeleteModalMountedState({ state: false })));
+    });
+    it('should dispatch updateCardCardListonDelete action for success response if body is not present', () => {
+      const response = {
+        statusCode: 200,
+      };
+      const putDescriptor = deleteCardGen.next(response).value;
+      expect(putDescriptor).toEqual(put(updateCardCardListonDelete('')));
+      expect(deleteCardGen.next().value).toEqual(put(setDeleteModalMountedState({ state: false })));
+    });
+    it('should dispatch updateCardCardListonDelete action for error response', () => {
+      const response = {
+        error: 'error in API',
+      };
+      const putDescriptor = deleteCardGen.next(response).value;
+      expect(putDescriptor).toEqual(put(updateCardListonDeleteErr(response.error)));
+    });
+    it('should dispatch updateCardCardListonDelete action when api fails', () => {
+      const response = {
+        error: 'error in API',
+      };
+      const putDescriptor = deleteCardGen.throw(response).value;
+      expect(putDescriptor).toEqual(put(updateCardListonDeleteErr(response)));
+    });
+  });
+  describe('deleteAddressSaga', () => {
+    it('should return correct takeLatest effect', () => {
+      const generator = DeleteCardSaga();
+      const takeLatestDescriptor = generator.next().value;
+      expect(takeLatestDescriptor).toEqual(takeLatest(PAYMENT_CONSTANTS.DELETE_CARD, deleteCard));
+    });
+  });
+});
