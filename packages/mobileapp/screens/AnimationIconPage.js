a0b900d456b4880152936e4eefb9a41ad8c82387
Abhishek Jha <abhishekjha@Abhisheks-MacBook-Pro.local>
2019-07-08 17:12:46 +0530

    Added Animation

---
+++ packages/mobileapp/screens/AnimationIconPage.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Button, StyleSheet, UIManager, LayoutAnimation, Image, View } from 'react-native';
+import jake from './jake.png';
+
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { position: 'flex-end' };
+  }
+
+  changePosition = position => {
+    LayoutAnimation.spring();
+    this.setState({
+      position,
+    });
+  };
+
+  render() {
+    return (
+      <View style={[styles.container, { justifyContent: this.state.position }]}>
+        <Image source={jake} />
+        <View style={styles.buttonsContainer}>
+          <Button
+            style={styles.button}
+            title="Middle"
+            onPress={() => this.changePosition('center')}
+          />
+          <Button
+            style={styles.button}
+            title="Bottom"
+            onPress={() => this.changePosition('flex-end')}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    paddingBottom: 32,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    position: 'absolute',
+    bottom: 16,
+    width: '100%',
+  },
+  button: {
+    width: 100,
+  },
+});
