19b7b8908fe94a8f1b4a02d28a79b02ca623e4f1
Ajay Saini <ajay.saini@publicissapient.com>
2019-07-09 12:13:42 +0530

    Add text

--- packages/core/src/components/features/account/AddressBook/views/AddressBook.view.native.jsx
+++ packages/core/src/components/features/account/AddressBook/views/AddressBook.view.native.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import ParentContainer from '../styles/AddressBook.style.native'; //eslint-disable-line
+import { ParentContainer, StyledText } from '../styles/AddressBook.style.native'; //eslint-disable-line
+import { Text } from 'react-native'; //eslint-disable-line
 
 export default class AddressBook extends React.PureComponent<Props> {
   render() {
-    return <ParentContainer />;
+    return (
+      <ParentContainer>
+        <StyledText>Address Book</StyledText>
+      </ParentContainer>
+    );
   }
 }
