5c64e5803a43a983284ae8a307c89372237a8064
aditya.vashist <aditya.vashist.sd@gmail.com>
2019-07-10 15:31:50 +0530

    graphql client pointed to UAT

--- packages/core/src/services/config.js
+++ packages/core/src/services/config.js
@@ -10,12 +10,12 @@ export const awsAppSync = {
   aws_project_region: 'us-east-1',
   aws_appsync_region: 'us-east-1',
   aws_appsync_authenticationType: 'API_KEY',
-  aws_appsync_apiKey: graphQLAPIKey.dev,
+  aws_appsync_apiKey: graphQLAPIKey.uat,
   aws_content_delivery_bucket: 'ppync-20190316024210-hostingbucket-windows',
   aws_content_delivery_bucket_region: 'us-east-1',
   aws_content_delivery_url:
     'http://ppync-20190316024210-hostingbucket-windows.s3-website-us-east-1.amazonaws.com',
-  aws_appsync_graphqlEndpoint: endpoints.graphQL.dev,
+  aws_appsync_graphqlEndpoint: endpoints.graphQL.uat,
 };
 
 export const graphQLClient = 'graphQL';
