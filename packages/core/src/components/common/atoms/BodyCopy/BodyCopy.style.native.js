8812e9ec42baa4256f5ed0ab0ad117e8b47e6f04
Prasoon <prasoon.singh@publicissapient.com>
2019-07-11 15:57:03 +0530

    update the code

--- packages/core/src/components/common/atoms/BodyCopy/BodyCopy.style.native.js
+++ packages/core/src/components/common/atoms/BodyCopy/BodyCopy.style.native.js
@@ -11,11 +11,3 @@ const BodyCopyStyles = css`
 `;
 
 export default BodyCopyStyles;
-
-// font-family : ${props => props.fontFamily || "Montserrat"};
-//    line-height: ${props => props.lineHeight || 60};
-//    letter-spacing : ${props => props.letterSpacing || 1 };
-//    text-align : ${props => props.textAlign || 'center'};
-//    color : ${props => props.color || 'red'};
-//    font-size : ${ props => props.fontSize || '20px'};
-//    font-weight :${props => props.fontWeight || '200'};
