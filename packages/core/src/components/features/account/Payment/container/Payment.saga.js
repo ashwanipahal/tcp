0fac7008f390dbc1faf82f825046fb4cf44713f1
priya <pkumari12@sapient.com>
2019-07-11 18:16:38 +0530

    review comments

--- packages/core/src/components/features/account/Payment/container/Payment.saga.js
+++ packages/core/src/components/features/account/Payment/container/Payment.saga.js
@@ -2,14 +2,14 @@ import { call, takeLatest, put } from 'redux-saga/effects';
 import { validateReduxCache } from '../../../../../utils/cache.util';
 import PAYMENT_CONSTANTS from '../Payment.constants';
 import fetchData from '../../../../../service/API';
-import { setCardList, getCardListErr, setLoader } from './Payment.actions';
+import { setCardList, getCardListErr, showLoader } from './Payment.actions';
 import endpoints from '../../../../../service/endpoint';
 
 export function* getCardList() {
   try {
     const { relURI, method } = endpoints.getCardList;
     const baseURI = endpoints.getCardList.baseURI || endpoints.global.baseURI;
-    yield put(setLoader());
+    yield put(showLoader());
     const res = yield call(
       fetchData,
       baseURI,
