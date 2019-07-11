4b7a3d3238b64ddc1fc776a6e2c7aaf6723ab982
schauhan2@sapient.com <sanjeev.chauhan@publicissapient.com>
2019-07-11 10:35:53 +0000

    Revert "Grid Framework optimization | PLP page base structure (pull request #195)"
    This reverts pull request #195.
    
    > Grid Framework optimization | PLP page base structure

--- packages/core/src/components/common/atoms/Row/views/Row.jsx
+++ packages/core/src/components/common/atoms/Row/views/Row.jsx
@@ -3,21 +3,15 @@ import { PropTypes } from 'prop-types';
 import withStyles from '../../../hoc/withStyles';
 import StyledRow from '../Row.style';
 
+// @flow
+
 // An additonal prop 'fullBleed' is added.
 // This property ignores the offset of the row and spans across the space of the grid.
-const Row = ({ className, children, tagName }) => {
-  const CustomTag = `${tagName}`;
-  return <CustomTag className={className}>{children}</CustomTag>;
-};
+const Row = ({ className, children }) => <div className={className}>{children}</div>;
 
 Row.propTypes = {
   children: PropTypes.string.isRequired,
   className: PropTypes.string.isRequired,
-  tagName: PropTypes.string,
-};
-
-Row.defaultProps = {
-  tagName: 'div',
 };
 
 export default withStyles(Row, StyledRow);
--- packages/core/src/components/features/browse/ProductListingPage/views/ProductListingPage.view.js
+++ packages/core/src/components/features/browse/ProductListingPage/views/ProductListingPage.view.js
@@ -1,29 +1,21 @@
 /* eslint-disable */
 import React from 'react';
 import ProductListingPageStyle from '../styles/ProductListingPage.style';
-import Grid from '@tcp/core/src/components/common/molecules/Grid';
-import Row from '@tcp/core/src/components/common/atoms/Row';
-import Col from '@tcp/core/src/components/common/atoms/Col';
 
-export const ProductListView = ({ data, className }) => {
+export const ProductListView = ({ data }) => {
   return (
-    <ProductListingPageStyle>
+    <React.Fragment>
       <h1>PLP Page</h1>
-      <Row tagName="ul" className={className}>
+      <ProductListingPageStyle className="product-wrapper">
         {data &&
           data.map(item => (
-            <Col
-              tagName="li"
-              key={item.productid}
-              className="product-item"
-              colSize={{ small: 2, medium: 3, large: 4 }}
-            >
+            <li key={item.productid} className="product-item">
               <p className="product-name">{item.product_name}</p>
               <p className="product-disc-price">{item.min_offer_price}</p>
               <p className="product-original-price">{`Was ${item.min_list_price}`}</p>
-            </Col>
+            </li>
           ))}
-      </Row>
-    </ProductListingPageStyle>
+      </ProductListingPageStyle>
+    </React.Fragment>
   );
 };
