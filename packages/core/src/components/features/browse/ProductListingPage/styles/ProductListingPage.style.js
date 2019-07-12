4d27340aad9c3e0eb607341f512d8d883762e5ed
Himalaya <himalaya.batra@publicissapient.com>
2019-07-12 11:53:21 +0530

    RWD-856 Cart & Checkout | Added to bag template - Web

--- packages/core/src/components/features/browse/ProductListingPage/styles/ProductListingPage.style.js
+++ packages/core/src/components/features/browse/ProductListingPage/styles/ProductListingPage.style.js
@@ -1,3 +1,4 @@
+/* stylelint-disable */
 import styled from 'styled-components';
 
 export default styled.div`
@@ -6,6 +7,34 @@ export default styled.div`
     margin-right: 0;
     @media ${props => props.theme.mediaQuery.large} {
       margin-right: 0;
+    }
+  }
+  .product-disc-price {
+    @media ${props => props.theme.mediaQuery.large} {
+      text-align: center;
+      padding: 1%;
+      color: #900;
+    }
+  }
+  .product-original-price {
+    @media ${props => props.theme.mediaQuery.large} {
+      text-align: center;
+      padding: 1%;
+      color: #999;
+    }
+  }
+  .product-item {
+    @media ${props => props.theme.mediaQuery.large} {
+      width: 29%;
+      border: solid 0.5px black;
+      border-radius: 4px;
+      padding: 1%;
+      margin-bottom: 1%;
+      font-family: Lato-Bold;
+      font-style: normal;
+      font-stretch: normal;
+      font-size: 15px;
+      background-color: #eeeeef;
     }
   }
 `;
