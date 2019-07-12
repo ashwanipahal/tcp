159f7907dfeee2552f4d15deda655a4d2fbbb242
Prasoon <prasoon.singh@publicissapient.com>
2019-07-12 14:16:24 +0530

    Add Heading Atom

--- packages/core/src/components/common/atoms/index.native.js
+++ packages/core/src/components/common/atoms/index.native.js
@@ -3,5 +3,6 @@ import RichText from './RichText';
 import Image from './Image';
 import Anchor from './Anchor';
 import BodyCopy from './BodyCopy';
+import Heading from './Heading';
 
-export { Button, Image, RichText, Anchor, BodyCopy };
+export { Button, Image, RichText, Anchor, BodyCopy, Heading };
