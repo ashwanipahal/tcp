d2db3c22f667cfb0f29ae68fa3f28f52d908d772
priya <pkumari12@sapient.com>
2019-07-13 22:40:32 +0530

    added payment reducer

--- packages/mobileapp/src/reduxStore/reducers/reducers.js
+++ packages/mobileapp/src/reduxStore/reducers/reducers.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import { createFilteredReducer } from '@tcp/core/src/utils/redux.util';
 import LoginPageReducer from '@tcp/core/src/components/features/account/LoginPage/container/LoginPage.reducer';
+import PaymentReducer from '@tcp/core/src/components/features/account/Payment/container/Payment.reducer';
 import { ProductListingPageReducer } from '@tcp/core/src/components/features/browse/ProductListingPage/container/ProductListingPage.reducer';
 import LabelReducer from '@tcp/core/src/reduxStore/reducers/labels';
 import LayoutReducer from '@tcp/core/src/reduxStore/reducers/layout';
@@ -10,6 +11,7 @@ import {
   LAYOUT_REDUCER_KEY,
   LOGINPAGE_REDUCER_KEY,
   PRODUCTLISTINGPAGE_REDUCER_KEY,
+  PAYMENT_REDUCER_KEY,
 } from '@tcp/core/src/constants/reducer.constants';
 import HeaderReducer from '@tcp/core/src/components/common/organisms/Header/container/Header.reducer';
 
@@ -24,4 +26,5 @@ export default combineReducers({
   [LAYOUT_REDUCER_KEY]: LayoutReducer,
   [PRODUCTLISTINGPAGE_REDUCER_KEY]: filteredProductListingPageReducer,
   [LOGINPAGE_REDUCER_KEY]: LoginPageReducer,
+  [PAYMENT_REDUCER_KEY]: PaymentReducer,
 });
