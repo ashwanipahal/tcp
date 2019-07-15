b10c37ec8d44389cdaf96cbd58d1aa812e202463
Poorva <poorva.singh@publicissapient.com>
2019-07-15 12:56:07 +0530

    add configuration for multiple apps in ios and android

---
+++ packages/mobileapp/ios/mobileapp/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  tcp
+//
+//  Created by Poorva Singh on 15/07/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import UIKit
+
+class AppDelegate: NSObject {
+
+}
