13edc0999c1d6ddea17852bacc9af7c3ee8c8a14
Poorva <poorva.singh@publicissapient.com>
2019-07-15 13:17:04 +0530

    update schemes

--- packages/mobileapp/ios/mobileapp/AppDelegate.swift
+++ packages/mobileapp/ios/mobileapp/AppDelegate.swift
@@ -8,6 +8,41 @@
 
 import UIKit
 
-class AppDelegate: NSObject {
-
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  var bridge: RCTBridge!
+  
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    // Get current app environment from info.plist and send it as initial property to react-native code
+    var props: [String: Any]? = nil
+    if let appName = Bundle.main.infoDictionary?["CFBundleDisplayName"] {
+      props = ["appType": appName]
+    }
+    
+    print("appName")
+    
+    // Override point for customization after application launch.
+    guard let jsCodeLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index", fallbackResource:nil) else {
+      return false
+    }
+    
+    let rootView = RCTRootView(bundleURL:jsCodeLocation, moduleName: "mobileapp", initialProperties: props, launchOptions:launchOptions)
+    self.bridge = rootView?.bridge
+    
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    let rootViewController = UIViewController()
+    
+    rootViewController.view = rootView
+    
+    self.window!.rootViewController = rootViewController;
+    self.window!.makeKeyAndVisible()
+    
+    return true
+    
+  }
+  
 }
