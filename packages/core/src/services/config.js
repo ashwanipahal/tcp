5e29d420cf353c23cf16f104b04e1c147885af38
aditya.vashist <aditya.vashist.sd@gmail.com>
2019-07-16 15:08:02 +0530

    minor issue resolved

--- packages/core/src/services/config.js
+++ packages/core/src/services/config.js
@@ -11,12 +11,12 @@ export const awsAppSync = {
   aws_project_region: 'us-east-1',
   aws_appsync_region: 'us-east-1',
   aws_appsync_authenticationType: 'API_KEY',
-  aws_appsync_apiKey: graphQLAPIKey.sandbox,
+  aws_appsync_apiKey: graphQLAPIKey.uat,
   aws_content_delivery_bucket: 'ppync-20190316024210-hostingbucket-windows',
   aws_content_delivery_bucket_region: 'us-east-1',
   aws_content_delivery_url:
     'http://ppync-20190316024210-hostingbucket-windows.s3-website-us-east-1.amazonaws.com',
-  aws_appsync_graphqlEndpoint: endpoints.graphQL.sandbox,
+  aws_appsync_graphqlEndpoint: endpoints.graphQL.uat,
 };
 
 // TODO - to move it in env config file
