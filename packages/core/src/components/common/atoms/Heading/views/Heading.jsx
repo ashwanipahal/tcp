f77193fcaecbf246a53a91ee0bc45d89c14bd45b
Siddhartha Nanda <sidnanda@publicisgroupe.net>
2019-07-16 12:30:19 +0530

    test cases updated

--- packages/core/src/components/common/atoms/Heading/views/Heading.jsx
+++ packages/core/src/components/common/atoms/Heading/views/Heading.jsx
@@ -5,7 +5,7 @@ import withStyles from '../../../hoc/withStyles';
 import styles from '../Heading.style';
 
 const headingVariants = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'nav', 'listMenu'];
-const Heading = ({ children, className, component, variant, dataLocator, ...others }) => {
+const Heading = ({ children, className, component, variant, ...others }) => {
   let componentVariant = headingVariants.indexOf(variant) !== -1 ? variant : 'h1';
   componentVariant = componentVariant === 'listMenu' ? 'li' : componentVariant;
   const Component = component || componentVariant;
@@ -22,6 +22,7 @@ Heading.defaultProps = {
   children: null,
   variant: 'h1',
   color: 'text.primary',
+  textAlign: 'left',
 };
 
 Heading.propTypes = {
