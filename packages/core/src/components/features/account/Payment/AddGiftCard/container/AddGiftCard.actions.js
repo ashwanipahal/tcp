5485f89ff7521167324a870e6b013e1e55fc3a65
Ajay Saini <ajay.saini@publicissapient.com>
2019-07-17 17:34:57 +0530

    Add flow check in addGiftCardSuccess

--- packages/core/src/components/features/account/Payment/AddGiftCard/container/AddGiftCard.actions.js
+++ packages/core/src/components/features/account/Payment/AddGiftCard/container/AddGiftCard.actions.js
@@ -15,7 +15,7 @@ export const addGiftCardFailure = (payload: {}) => {
   };
 };
 
-export const addGiftCardSuccess = payload => {
+export const addGiftCardSuccess = (payload: {}) => {
   return {
     type: ADD_GIFT_CARD_CONSTANTS.ADD_GIFT_CARD_SUCCESS,
     payload,
