92b31d3413725fa797da7b80cdcb247b5a37dc94
Furkan Ali <furali@publicisgroupe.net>
2019-07-22 12:55:12 +0530

    review comment solved

--- packages/core/src/components/features/account/Payment/views/CreditCardModalInfo.view.jsx
+++ packages/core/src/components/features/account/Payment/views/CreditCardModalInfo.view.jsx
@@ -27,12 +27,12 @@ const CreditCardModalInfo = ({
 }: Props) => {
   return (
     <div className={className}>
-      <Row fullBleed className="elem-mb-MED">
+      <Row fullBleed className="elem-mb-LRG elem-mt-MED">
         <Col
           colSize={{
-            small: 12,
+            small: 6,
             large: 12,
-            medium: 12,
+            medium: 10,
           }}
         >
           <BodyCopy fontSize="fs18" fontFamily="secondary" fontWeight="extrabold">
@@ -41,7 +41,7 @@ const CreditCardModalInfo = ({
         </Col>
       </Row>
 
-      <Row fullBleed>
+      <Row fullBleed className="elem-mt-XS">
         <Col
           colSize={{
             small: 2,
@@ -95,24 +95,28 @@ const CreditCardModalInfo = ({
               </BodyCopy>
             </Col>
           </Row>
-        </Col>
-      </Row>
-      <Row fullBleed className="elem-mb-MED">
-        <Col
-          colSize={{
-            small: 4,
-            large: 7,
-            medium: 8,
-          }}
-          offsetLeft={{
-            small: 2,
-            medium: 2,
-            large: 3,
-          }}
-        >
-          <BodyCopy fontSize="fs14">
-            <Address address={address} fontWeight="regular" showCountry={false} showPhone={false} />
-          </BodyCopy>
+
+          <Row fullBleed className="elem-mb-MED elem-mt-MED">
+            <Col
+              colSize={{
+                small: 6,
+                large: 12,
+                medium: 6,
+              }}
+              offsetLeft={{
+                medium: 2,
+              }}
+            >
+              <BodyCopy fontSize="fs14">
+                <Address
+                  address={address}
+                  fontWeight="regular"
+                  showCountry={false}
+                  showPhone={false}
+                />
+              </BodyCopy>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </div>
