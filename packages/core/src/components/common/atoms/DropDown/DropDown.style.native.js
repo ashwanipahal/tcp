83dd7abfc1f48121525712e90cba340562648db6
Ajay Saini <ajay.saini@publicissapient.com>
2019-07-22 15:20:24 +0530

    Add drop down

---
+++ packages/core/src/components/common/atoms/DropDown/DropDown.style.native.js
@@ -0,0 +1,27 @@
+import styled, { css } from 'styled-components';
+
+const style = css`
+  border: 1px solid #9c9c9c;
+  background-color: #eeeeee;
+  height: 42px;
+  align-items: center;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const StyledText = styled.Text`
+  color: ${props => props.theme.colorPalette.gray[800]};
+  font-family: ${props => props.theme.typography.fonts.secondary};
+  font-size: ${props => props.theme.typography.fontSizes.fs13};
+  font-weight: ${props => props.theme.typography.fontWeights.black};
+  font-style: normal;
+  margin-right: ${props => props.theme.spacing.ELEM_SPACING.SM};
+`;
+
+const Row = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export { style, StyledText, Row };
