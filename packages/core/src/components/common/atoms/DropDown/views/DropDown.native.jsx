e330cdf97c9999fa273398b502a5893a0cb99307
Ajay Saini <ajay.saini@publicissapient.com>
2019-07-22 18:25:53 +0530

    Add overlay

--- packages/core/src/components/common/atoms/DropDown/views/DropDown.native.jsx
+++ packages/core/src/components/common/atoms/DropDown/views/DropDown.native.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Image, View } from 'react-native';
+import { Image, View, TouchableHighlight, FlatList, TouchableWithoutFeedback } from 'react-native';
 import withStyles from '../../../hoc/withStyles.native';
-import { style, StyledText, Row } from '../DropDown.style.native';
+import { style, StyledText, Row, OverLayView } from '../DropDown.style.native';
 
 const icon = require('../../../../../assets/carrot-small-down.png');
 
@@ -10,17 +10,59 @@ type Props = {
   title: string,
 };
 
-const DropDown = (props: Props) => {
-  const { title } = props;
-  return (
-    <View accessibilityTraits="none" accessibilityComponentType="none" {...props}>
-      <Row>
-        <StyledText>{title}</StyledText>
-        <Image source={icon} />
-      </Row>
-    </View>
-  );
-};
+class DropDown extends React.PureComponent<Props> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dropDownIsOpen: false,
+    };
+  }
+
+  openDropDown = () => {
+    this.setState({
+      dropDownIsOpen: true,
+    });
+  };
+
+  dropDownLayout = ({ item }) => {
+    return (
+      <TouchableWithoutFeedback accessibilityTraits="none" accessibilityComponentType="none">
+        <View>
+          <StyledText>{item.key}</StyledText>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  };
+
+  render() {
+    const { title } = this.props;
+    const { dropDownIsOpen } = this.state;
+    return (
+      <View>
+        <TouchableHighlight
+          accessibilityTraits="none"
+          accessibilityComponentType="none"
+          onPress={this.openDropDown}
+        >
+          <Row {...this.props}>
+            <StyledText>{title}</StyledText>
+            <Image source={icon} />
+          </Row>
+        </TouchableHighlight>
+        <OverLayView>
+          {dropDownIsOpen && (
+            <FlatList
+              data={[{ key: 'hello hello hello' }, { key: 'hey hey hey' }]}
+              renderItem={item => this.dropDownLayout(item)}
+            />
+          )}
+        </OverLayView>
+        <StyledText>STATESTATESTATESTATESTATESTATE</StyledText>
+      </View>
+    );
+  }
+}
 
 export default withStyles(DropDown, style);
 export { DropDown as DropDownVanilla };
