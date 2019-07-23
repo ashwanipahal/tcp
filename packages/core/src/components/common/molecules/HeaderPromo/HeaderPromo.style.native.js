a9f634b893481e2c5022ac7cca6cc6ccb7b7253f
Prasoon <prasoon.singh@publicissapient.com>
2019-07-23 11:19:04 +0530

    update code

--- packages/core/src/components/common/molecules/HeaderPromo/HeaderPromo.style.native.js
+++ packages/core/src/components/common/molecules/HeaderPromo/HeaderPromo.style.native.js
@@ -1,29 +1,31 @@
 import styled from 'styled-components/native';
 
-export const Wrapper = styled.View`
+export const Container = styled.View`
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
   height: 42px;
   background: ${props => props.theme.colors.PRIMARY.COLOR3};
 `;
-export const WrapperView = styled.TouchableOpacity``;
+export const ChildContainer = styled.TouchableOpacity`
+  align-items: center;
+`;
 
-export const CenterView = styled.TouchableOpacity`
+export const MessageContainer = styled.TouchableOpacity`
   flex-direction: row;
   align-items: center;
 `;
 
-export const PromoImage = styled.Image`
-  width: ${props => props.width || '6px'};
-  height: ${props => props.height || '10px'};
+export const Image = styled.Image`
+  width: 6px;
+  height: 10px;
   margin-left: ${props => props.theme.spacing.LAYOUT_SPACING.XXS};
   margin-right: ${props => props.theme.spacing.LAYOUT_SPACING.XXS};
 `;
 
 export default {
-  Wrapper,
-  WrapperView,
-  PromoImage,
-  CenterView,
+  Container,
+  ChildContainer,
+  Image,
+  MessageContainer,
 };
--- packages/core/src/components/common/molecules/HeaderPromo/views/HeaderPromo.native.jsx
+++ packages/core/src/components/common/molecules/HeaderPromo/views/HeaderPromo.native.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { BodyCopy } from '@tcp/core/src/components/common/atoms';
 import Carousel from 'react-native-snap-carousel';
-import { Wrapper, WrapperView, PromoImage, CenterView } from '../HeaderPromo.style.native';
+import { Container, ChildContainer, Image, MessageContainer } from '../HeaderPromo.style.native';
 import colors from '../../../../../../styles/themes/TCP/colors';
 import { getScreenWidth } from '../../../../../utils/utils.native';
 
 const bannerTextFirst = 'EARN PLACE CASH!';
 const bannerTextSecond = 'Buy Online, pickup in store';
-
-const rightIcon = require('../../../../../assets/carrot-large-right.png');
-const leftIcon = require('../../../../../assets/carrot-large-left.png');
 
 /**
  * Module height and width.
@@ -20,7 +17,13 @@ const MODULE_HEIGHT = 42;
 const MODULE_WIDTH = getScreenWidth();
 const autoplay = true;
 
-/* TODO - Remove the style1, style2, style3 when the styles start coming up from CMS */
+/**
+ * Next & Prev icons listing.
+ */
+const prevIcon = require('../../../../../assets/carrot-large-right.png');
+const nextIcon = require('../../../../../assets/carrot-large-left.png');
+
+/* TODO - To use the style1, style2, style3 when the styles start coming up from CMS */
 
 const manageTextStyles = style => {
   if (style === 'style1') {
@@ -87,14 +90,15 @@ const dataObject = [
 
 /**
  * @desc Returns updated Banner text details with styles.
+ * Content render on the basis of style type .
  */
 const renderView = () => {
   return (
-    <Wrapper>
-      <CenterView>
+    <Container>
+      <MessageContainer>
         <BodyCopy
           fontFamily="secondary"
-          fontSize="fs14"
+          fontSize="fs12"
           textAlign="center"
           fontWeight="black"
           text={bannerTextFirst}
@@ -102,18 +106,18 @@ const renderView = () => {
         />
         <BodyCopy
           fontFamily="secondary"
-          fontSize="fs14"
+          fontSize="fs12"
           textAlign="center"
           color="black"
           fontWeight="regular"
           text={bannerTextSecond}
         />
-      </CenterView>
-    </Wrapper>
+      </MessageContainer>
+    </Container>
   );
 };
 
-class HeaderPromo extends React.PureComponent {
+class HeaderPromo extends React.PureComponent<props> {
   constructor(props) {
     super(props);
     this.carousel = null;
@@ -131,10 +135,10 @@ class HeaderPromo extends React.PureComponent {
 
   render() {
     return (
-      <Wrapper>
-        <WrapperView onPress={() => this.manageSlide('next')}>
-          <PromoImage source={leftIcon} />
-        </WrapperView>
+      <Container>
+        <ChildContainer onPress={() => this.manageSlide('next')}>
+          <Image source={nextIcon} />
+        </ChildContainer>
         <Carousel
           data={dataObject}
           renderItem={renderView}
@@ -147,10 +151,10 @@ class HeaderPromo extends React.PureComponent {
             this.carousel = c;
           }}
         />
-        <WrapperView onPress={() => this.manageSlide('prev')}>
-          <PromoImage source={rightIcon} />
-        </WrapperView>
-      </Wrapper>
+        <ChildContainer onPress={() => this.manageSlide('prev')}>
+          <Image source={prevIcon} />
+        </ChildContainer>
+      </Container>
     );
   }
 }
