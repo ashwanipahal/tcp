fd2660e4c8d262919d8d237c7c0fa7780f597231
Siddhartha Nanda <sidnanda@publicisgroupe.net>
2019-07-23 12:53:18 +0530

    config updated for Module K

--- packages/core/src/components/common/molecules/ImageGrid/config.js
+++ packages/core/src/components/common/molecules/ImageGrid/config.js
@@ -1,5 +1,7 @@
+const cropParam = 'ar_1:1,q_80';
+
 export default {
   IMG_DATA: {
-    imgConfig: ['ar_1:1,q_0.8,w_164', 'ar_1:1,q_0.8,w_162', 'ar_1:1,q_0.8,w_210'],
+    imgConfig: [`${cropParam},w_164`, `${cropParam},w_162`, `${cropParam},w_210`],
   },
 };
