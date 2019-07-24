4e51b95aace5ae8074a75b7263f174f510ed0459
Raghav <raghasb@gmail.com>
2019-07-24 19:16:48 +0530

    code cleanup

--- packages/web/src/components/features/content/HomePage/container/HomePage.container.js
+++ packages/web/src/components/features/content/HomePage/container/HomePage.container.js
@@ -10,7 +10,6 @@ HomePageView.getInitActions = () => initActions;
 const mapStateToProps = state => {
   const homepageSlots = state.Layouts.homepage.slots;
   const newObj = {};
-
   homepageSlots.forEach(slotItem => {
     newObj[slotItem.name] = state.Modules[slotItem.contentId];
     newObj[slotItem.name].name = slotItem.moduleName;
