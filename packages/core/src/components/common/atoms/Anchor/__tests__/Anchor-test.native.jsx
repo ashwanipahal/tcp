5282732d94863ed7c6b13735f75124650ad2cc76
Abhishek Jha <abhishekjha@Abhisheks-MacBook-Pro.local>
2019-07-24 17:34:11 +0530

    Added test case coverage for anchor native and also worked on comments

--- packages/core/src/components/common/atoms/Anchor/__tests__/Anchor-test.native.jsx
+++ packages/core/src/components/common/atoms/Anchor/__tests__/Anchor-test.native.jsx
@@ -2,26 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { AnchorVanilla } from '../views/Anchor.native';
 
-describe('HeadingVanilla', () => {
-  let component;
-
-  beforeEach(() => {
-    component = shallow(<AnchorVanilla />);
-  });
-
-  it('should be defined', () => {
-    expect(component).toBeDefined();
-  });
-
-  it('should render correctly', () => {
-    expect(component).toMatchSnapshot();
-  });
-
-  it('should return abc component value one', () => {
-    expect(component.find('Text')).toHaveLength(1);
-  });
-});
-
 describe('Anchor Native', () => {
   let component;
   const navigate = jest.fn();
@@ -39,8 +19,44 @@ describe('Anchor Native', () => {
     );
   });
 
+  it('should be defined', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('should render correctly', () => {
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should return abc component value one', () => {
+    expect(component.find('Text')).toHaveLength(1);
+  });
+
   it('should call parseUrl', () => {
-    component.simulate('click');
-    expect(navigate.mock.calls.length).toEqual(0);
+    component.props().onPress();
+    expect(navigate).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call onPress for external url handler', () => {
+    component.setProps({ external: true, onPress: navigate });
+    component.props().onPress();
+    expect(navigate).toHaveBeenCalledTimes(2);
+  });
+
+  it('should call for plp page', () => {
+    component.setProps({ external: false, url: 'https://www.google.com/c/test' });
+    component.props().onPress();
+    expect(navigate).toHaveBeenCalledTimes(3);
+  });
+
+  it('should return null', () => {
+    component.setProps({ external: false, url: 'https://www.google.com/test' });
+    component.props().onPress();
+    expect(navigate).toHaveBeenCalledTimes(3);
+  });
+
+  it('should return navigation to default browser', () => {
+    component.setProps({ external: true, onPress: null });
+    component.props().onPress();
+    expect(navigate).toHaveBeenCalledTimes(3);
   });
 });
--- packages/core/src/components/common/atoms/Anchor/views/Anchor.native.jsx
+++ packages/core/src/components/common/atoms/Anchor/views/Anchor.native.jsx
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Linking } from 'react-native';
 import withStyles from '../../../hoc/withStyles.native';
 import { AnchorStyles, AnchorView, AnchorIcon } from '../Anchor.style.native';
 
@@ -11,37 +11,65 @@ type Props = {
 };
 
 /**
- * @param {string} url
- * @param {function} navigation
- * Returns navigation to the parsed URL based on  the url param
+ *
+ * @param {url} string
  */
-const parseUrl = (url, navigation) => {
-  const { navigate } = navigation;
+const categorizeUrl = url => {
   if (url.includes('/p/')) {
-    /**
-     * /p/Rainbow--The-Birthday-Girl--Graphic-Tee-2098277-10
-     * If url starts with “/p” → Create and navigate to a page in stack for Products (Blank page with a Text - “Product List”)
-     */
-    return navigate('ProductList', { product: 'New Product Nike' });
+    return '/p/';
   }
   if (url.includes('/c/')) {
-    /**
-     * /c/* - If url starts with “/c” (* can be anything in url) → Select “Shop” tab in tabbar and Open Shop page
-     */
-    return navigate('Plp');
+    return '/p/';
   }
   return null;
 };
 
+/**
+ * @param {string} url
+ * @param {function} navigation
+ * Returns navigation to the parsed URL based on  the url param
+ */
+const navigateToUrl = (url, navigation) => {
+  const { navigate } = navigation;
+  const urlType = {
+    1: '/p/',
+    2: '/c/',
+  };
+  const category = categorizeUrl(url);
+  switch (category) {
+    case urlType[1]:
+      /**
+       * /p/Rainbow--The-Birthday-Girl--Graphic-Tee-2098277-10
+       * If url starts with “/p” → Create and navigate to a page in stack for Products (Blank page with a Text - “Product List”)
+       */
+      return navigate('ProductList', { product: 'New Product Nike' });
+    case urlType[2]:
+      /**
+       * /c/* - If url starts with “/c” (* can be anything in url) → Select “Shop” tab in tabbar and Open Shop page
+       */
+      return navigate('Plp');
+    default:
+      return null;
+  }
+};
+
 const Icon = require('../../../../../assets/carrot-small-rights.png');
 
+const openUrlInBrowser = url => {
+  Linking.canOpenURL(url).then(supported => {
+    if (supported) {
+      Linking.openURL(url);
+    }
+  });
+};
+
 const Anchor = ({ anchorVariation, text, visible, ...otherProps }: Props) => {
-  const { url, external, navigation } = otherProps;
+  const { url, external, navigation, onPress } = otherProps;
+
+  const openUrlInExternalBrowser = onPress || (() => openUrlInBrowser(url));
+  const openUrl = external ? openUrlInExternalBrowser : () => navigateToUrl(url, navigation);
   return (
-    <AnchorView
-      accessibilityRole="button"
-      onPress={() => (external ? url : parseUrl(url, navigation))}
-    >
+    <AnchorView accessibilityRole="button" onPress={openUrl}>
       <Text anchorVariation={anchorVariation} {...otherProps}>
         {text}
       </Text>
