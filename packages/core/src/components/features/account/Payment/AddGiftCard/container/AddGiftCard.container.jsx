722a13c470e28a902d2a098834ca02cbdb055265
Ajay Saini <ajay.saini@publicissapient.com>
2019-07-24 13:33:30 +0530

    Update state

--- packages/core/src/components/features/account/Payment/AddGiftCard/container/AddGiftCard.container.jsx
+++ packages/core/src/components/features/account/Payment/AddGiftCard/container/AddGiftCard.container.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import AddGiftCardComponent from '../views/AddGiftCard.view';
-import { addGiftCardRequest } from './AddGiftCard.actions';
+import { addGiftCardRequest, resetShowNotification } from './AddGiftCard.actions';
 import { getAddGiftCardResponse } from './AddGiftCard.selector';
 import labels from './AddGiftCard.labels';
 import Router from 'next/router'; //eslint-disable-line
@@ -11,31 +11,44 @@ import Router from 'next/router'; //eslint-disable-line
 type Props = {
   onAddGiftCardClick: Function,
   addGiftCardResponse: String,
+  resetNotificationStateAction: Function,
 };
 
-export const AddGiftCardContainer = ({ onAddGiftCardClick, addGiftCardResponse }: Props) => {
-  const goBackToPayment = () => {
+export class AddGiftCardContainer extends React.Component<Props> {
+  componentWillUnmount() {
+    const { resetNotificationStateAction } = this.props;
+    resetNotificationStateAction();
+  }
+
+  goBackToPayment = () => {
     Router.push('/account?id=payment', '/account/payment');
     return null;
   };
 
-  if (addGiftCardResponse === 'success') {
-    return goBackToPayment();
+  render() {
+    const { onAddGiftCardClick, addGiftCardResponse } = this.props;
+
+    if (addGiftCardResponse === 'success') {
+      return this.goBackToPayment();
+    }
+    return (
+      <AddGiftCardComponent
+        onAddGiftCardClick={onAddGiftCardClick}
+        labels={labels}
+        addGiftCardResponse={addGiftCardResponse}
+        goBackToPayment={this.goBackToPayment}
+      />
+    );
   }
-  return (
-    <AddGiftCardComponent
-      onAddGiftCardClick={onAddGiftCardClick}
-      labels={labels}
-      addGiftCardResponse={addGiftCardResponse}
-      goBackToPayment={goBackToPayment}
-    />
-  );
-};
+}
 
 export const mapDispatchToProps = (dispatch: ({}) => void) => {
   return {
     onAddGiftCardClick: (payload: {}) => {
       dispatch(addGiftCardRequest(payload));
+    },
+    resetNotificationStateAction: () => {
+      dispatch(resetShowNotification());
     },
   };
 };
