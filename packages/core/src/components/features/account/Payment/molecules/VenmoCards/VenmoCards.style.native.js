47ae1a7c3f8ee023b30c144eea699f49a7d26622
Ajay Saini <ajay.saini@publicissapient.com>
2019-07-25 14:52:14 +0530

    Update css

--- packages/core/src/components/features/account/Payment/molecules/VenmoCards/VenmoCards.style.native.js
+++ packages/core/src/components/features/account/Payment/molecules/VenmoCards/VenmoCards.style.native.js
@@ -4,7 +4,6 @@ const ParentContainerStyle = css``;
 
 const HeadingTextStyle = styled.Text`
   height: ${props => props.theme.spacing.ELEM_SPACING.XXL};
-  top: ${props => props.theme.spacing.ELEM_SPACING.MED};
   font-size: ${props => props.theme.typography.fontSizes.fs16};
   font-weight: 600;
 `;
