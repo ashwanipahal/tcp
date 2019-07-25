47fd68d7daa144188350e71e850cfc2416057aaa
“Vijay <vijdadhi@publicisgroupe.net>
2019-07-25 16:33:02 +0530

    update icon constatnt

--- packages/core/src/components/common/atoms/Icon/Icon.constants.js
+++ packages/core/src/components/common/atoms/Icon/Icon.constants.js
@@ -3,6 +3,14 @@ export const ICON_FONT_CLASS = {
 };
 
 export const ICON_NAME = {
+  chevronRight: 'chevron-right',
+  chevronLeft: 'chevron-left',
+  chevronUp: 'chevron-up',
+  chevronDown: 'chevron-down',
+  arrowRight: 'arrow-right',
+  arrowLeft: 'arrow-left',
+  arrowUp: 'arrow-up',
+  arrowDown: 'arrow-down',
   Facebook: 'facebook',
 };
 
