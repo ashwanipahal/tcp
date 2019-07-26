b7258d167972a0d12cb94a100362cda919020326
Abhishek Jha <abhishekjha@Abhisheks-MacBook-Pro.local>
2019-07-26 20:16:45 +0530

    WIP Labels loaded

--- packages/core/src/components/features/account/AddressBook/styles/AddressBook.style.native.js
+++ packages/core/src/components/features/account/AddressBook/styles/AddressBook.style.native.js
@@ -1,7 +1,41 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
 const StyledText = styled.Text`
   padding: 8px;
 `;
 
-export default StyledText;
+const ParentContainer = css`
+  margin-bottom: 120px;
+`;
+
+const StyledHeading = styled.Text`
+  padding: ${props => props.theme.spacing.ELEM_SPACING.XL} 0
+    ${props => props.theme.spacing.ELEM_SPACING.SM};
+`;
+const UnderlineStyle = styled.View`
+  height: 3px;
+  background-color: ${props => props.theme.colorPalette.black};
+  margin-bottom: ${props => props.theme.spacing.ELEM_SPACING.XXL};
+`;
+
+const UnderlineStyleLight = styled.View`
+  height: 1px;
+  background-color: ${props => props.theme.colors.FOOTER.DIVIDER};
+`;
+
+const InputField = styled.View`
+  height: 41px;
+  width: 347px;
+  margin-bottom: 32px;
+  padding-top: 16px;
+  padding-bottom: 7px;
+`;
+
+export {
+  StyledText,
+  ParentContainer,
+  StyledHeading,
+  UnderlineStyle,
+  UnderlineStyleLight,
+  InputField,
+};
--- packages/core/src/components/features/account/AddressBook/views/AddressBook.view.native.jsx
+++ packages/core/src/components/features/account/AddressBook/views/AddressBook.view.native.jsx
@@ -1,8 +1,73 @@
 import React from 'react';
-import StyledText from '../styles/AddressBook.style';
+import { View, ScrollView } from 'react-native';
+import {
+  StyledText,
+  StyledHeading,
+  ParentContainer,
+  UnderlineStyle,
+  UnderlineStyleLight,
+  InputField,
+} from '../styles/AddressBook.style';
+import BodyCopy from '../../../../common/atoms/BodyCopy';
+import withStyles from '../../../../common/hoc/withStyles.native';
+import { TEXT_ALIGNS } from '../../../../../../styles/themes/typography.constants';
+import colors from '../../../../../../styles/themes/TCP/colors';
+import { TextInput } from 'react-native-gesture-handler';
 
-export default class AddressBook extends React.PureComponent<Props> {
-  render() {
-    return <StyledText>Address Book</StyledText>;
-  }
-}
+// @flow
+type Props = {
+  labels: Object,
+};
+
+const inputBox = text => {
+  return (
+    <InputField>
+      <TextInput placeholder={text} />
+      <UnderlineStyleLight />
+    </InputField>
+  );
+};
+
+const loadAddressInfo = labels => {
+  return labels.map(labelElement => {
+    return inputBox(labelElement);
+  });
+};
+
+const constructLabelsObject = (labels, labelsToLoad) => {
+  return labelsToLoad.map(e => labels[e]);
+};
+
+const AddressBook = (props: Props) => {
+  const { labels } = props;
+  const labelsToLoad = [
+    'acc_lbl_first_name',
+    'acc_lbl_last_name',
+    'acc_lbl_address_line1',
+    'acc_lbl_address_line2',
+    'acc_lbl_city',
+    'acc_lbl_state',
+    'acc_lbl_zip_code',
+    'acc_lbl_country',
+    'acc_lbl_phone_number',
+  ];
+  const loadedLabels = constructLabelsObject(labels, labelsToLoad);
+  return (
+    <View {...props}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <StyledHeading>
+          <BodyCopy
+            fontSize="fs16"
+            fontWeight="extrabold"
+            text={labels.acc_lbl_add_address_form_heading}
+          />
+        </StyledHeading>
+        <UnderlineStyle />
+        {loadAddressInfo(loadedLabels)}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default withStyles(AddressBook, ParentContainer);
+export { AddressBook as AddressBookVanilla };
