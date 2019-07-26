992e82f08e8bce7ffeb3ac0401e257fd71f155f4
Deependra Sankhala <deependra.sankhala@gmail.com>
2019-07-26 16:37:27 +0530

    resolve eslint issue

---
+++ packages/core/src/components/features/account/AddressBook/views/__tests__/AddressBook.view-test.native.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { List } from 'immutable';
+import AddressBook from '../AddressView/views/Address.view.native';
+// import EmptyAddressListComponent from '../EmptyAddressList.view';
+// import AddressListComponent from '../AddressList.view';
+
+describe('Native AddressBook component', () => {
+  it('should renders correctly when addresses are not present', () => {
+    const props = {
+      addresses: new List(),
+      labels: {},
+    };
+    const component = shallow(<AddressBook {...props} />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should renders correctly when addresses are present', () => {
+    const props = {
+      addresses: new List([
+        {
+          firstName: 'test',
+          lastName: 'test',
+          addressLine: ['addressline 1', 'addressline 2'],
+          city: 'test city',
+          country: 'test country',
+          phone1: '1234567890',
+        },
+      ]),
+      labels: {},
+    };
+    const component = shallow(<AddressBook {...props} />);
+    expect(component).toMatchSnapshot();
+  });
+});
---
+++ packages/core/src/components/features/account/AddressBook/views/__tests__/AddressTile.view-test.native.jsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { AddressBookTileVanilla } from '../AddressTile.view.native';
+import Badge from '../../../../../common/atoms/Badge';
+
+const address = {
+  firstName: 'test',
+  lastName: 'test',
+  addressLine: ['addressline 1', 'addressline 2'],
+  city: 'test city',
+  country: 'test country',
+  phone1: '1234567890',
+  primary: 'true',
+};
+
+const labels = {
+  defaultShipping: 'DEFAULT SHIPPING',
+  defaultBilling: 'DEFAULT BILLING',
+  shipping: 'SHIPPING',
+  billing: 'BILLING',
+  makeDefault: 'Make Default',
+};
+
+describe('Native AddressTile component', () => {
+  it('should renders correctly', () => {
+    const props = {
+      address,
+      labels,
+    };
+    const component = shallow(<AddressBookTileVanilla {...props} />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should renders default Shipping badge if address is primary', () => {
+    const props = {
+      address,
+      labels,
+    };
+    const component = shallow(<AddressBookTileVanilla {...props} />);
+    expect(component.find(Badge).text()).toBe(labels.defaultShipping);
+  });
+
+  it('should renders shipping badge if xcont_isShippingAddress is true and address is not primary', () => {
+    const newAddress = Object.assign({}, address, {
+      primary: 'false',
+      xcont_isShippingAddress: 'true',
+    });
+    const props = {
+      address: newAddress,
+      labels,
+    };
+    const component = shallow(<AddressBookTileVanilla {...props} />);
+    expect(component.find(Badge).text()).toBe(labels.shipping);
+  });
+
+  it('should render default shipping badge only if xcont_isShippingAddress is true and address is also primary', () => {
+    const newAddress = Object.assign({}, address, {
+      primary: 'true',
+      xcont_isShippingAddress: 'true',
+    });
+    const props = {
+      address: newAddress,
+      labels,
+    };
+    const component = shallow(<AddressBookTileVanilla {...props} />);
+    expect(component.find(Badge)).toHaveLength(1);
+    expect(component.find(Badge).text()).toBe(labels.defaultShipping);
+  });
+
+  it('should renders default Billing badge if xcont_isDefaultBilling is true', () => {
+    const newAddress = Object.assign({}, address, {
+      primary: 'false',
+      xcont_isDefaultBilling: 'true',
+    });
+    const props = {
+      address: newAddress,
+      labels,
+    };
+    const component = shallow(<AddressBookTileVanilla {...props} />);
+    expect(component.find(Badge).text()).toBe(labels.defaultBilling);
+  });
+
+  it('should not render default Billing badge only if xcont_isDefaultBilling is true and xcont_isBillingAddress is also true', () => {
+    const newAddress = Object.assign({}, address, {
+      primary: 'false',
+      xcont_isDefaultBilling: 'true',
+      xcont_isBillingAddress: 'true',
+    });
+    const props = {
+      address: newAddress,
+      labels,
+    };
+    const component = shallow(<AddressBookTileVanilla {...props} />);
+    expect(component.find(Badge)).toHaveLength(1);
+    expect(component.find(Badge).text()).toBe(labels.defaultBilling);
+  });
+
+  it('should renders billing badge if xcont_isBillingAddress is true and address is not default ', () => {
+    const newAddress = Object.assign({}, address, {
+      xcont_isDefaultBilling: 'false',
+      xcont_isBillingAddress: 'true',
+    });
+    const props = {
+      address: newAddress,
+      labels,
+    };
+    const component = shallow(<AddressBookTileVanilla {...props} />);
+    expect(component.findWhere(com => com.is(Badge) && com.text() === labels.billing)).toHaveLength(
+      1
+    );
+  });
+});
