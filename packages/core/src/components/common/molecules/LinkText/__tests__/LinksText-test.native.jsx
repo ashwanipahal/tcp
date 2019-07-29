cd4ba12852255a5957f26bd0aaab1a16ca6d2b87
Ipsita Basak <ibasak@sapient.com>
2019-07-29 23:15:11 +0530

    Fixing failing app test case

--- packages/core/src/components/common/molecules/LinkText/__tests__/LinksText-test.native.jsx
+++ packages/core/src/components/common/molecules/LinkText/__tests__/LinksText-test.native.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import mock from '../../../../../services/abstractors/common/moduleK/mock';
-import LinkText from '../LinkText.native';
+import LinkText from '../index';
 
 let LinkTextComp;
 
