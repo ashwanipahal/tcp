0d95dbc960438f3bdddd1fc8f8a355e61fa07899
priya <pkumari12@sapient.com>
2019-07-29 12:00:00 +0530

    added payment native file

---
+++ packages/core/src/components/features/account/Payment/container/Payment.container.native.jsx
@@ -0,0 +1,138 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import {
+  getCardList,
+  setDeleteModalMountedState,
+  deleteCard,
+  checkBalance,
+  setDefaultPayment,
+  setPaymentNotification,
+} from './Payment.actions';
+import {
+  getCreditDebitCards,
+  getCardListFetchingState,
+  getShowNotificationState,
+  deleteModalOpenState,
+  showUpdatedNotificationOnModalState,
+  getGiftCards,
+  getVenmoCards,
+  getCardListState,
+  checkbalanceValue,
+  getShowNotificationCaptchaState,
+} from './Payment.selectors';
+import labels from './Payment.labels';
+import PaymentView from '../views/PaymentView';
+
+// @flow
+type Props = {
+  getCardListAction: Function,
+  showNotification: any,
+  deleteModalMountedState: boolean,
+  setDeleteModalMountState: Function,
+  onDeleteCard: Function,
+  showUpdatedNotificationOnModal: any,
+  creditCardList: List<any>,
+  venmoCardList: List<any>,
+  giftCardList: List<any>,
+  cardList: List<any>,
+  onGetBalanceCard: Function,
+  checkbalanceValueInfo: any,
+  setDefaultPaymentMethod: Function,
+  showNotificationCaptcha: boolean,
+  clearPaymentNotification: () => void,
+};
+
+export class PaymentContainer extends React.Component<Props> {
+  componentDidMount() {
+    const { getCardListAction } = this.props;
+    getCardListAction();
+  }
+
+  componentWillUnmount() {
+    const { clearPaymentNotification } = this.props;
+    clearPaymentNotification();
+  }
+
+  render() {
+    const {
+      showNotification,
+      setDeleteModalMountState,
+      deleteModalMountedState,
+      onDeleteCard,
+      showUpdatedNotificationOnModal,
+      creditCardList,
+      giftCardList,
+      venmoCardList,
+      cardList,
+      onGetBalanceCard,
+      checkbalanceValueInfo,
+      setDefaultPaymentMethod,
+      showNotificationCaptcha,
+    } = this.props;
+    return (
+      <PaymentView
+        deleteModalMountedState={deleteModalMountedState}
+        setDeleteModalMountState={setDeleteModalMountState}
+        showNotification={showNotification}
+        showNotificationCaptcha={showNotificationCaptcha}
+        onDeleteCard={onDeleteCard}
+        showUpdatedNotificationOnModal={showUpdatedNotificationOnModal}
+        labels={labels}
+        creditCardList={creditCardList}
+        giftCardList={giftCardList}
+        venmoCardList={venmoCardList}
+        cardList={cardList}
+        onGetBalanceCard={onGetBalanceCard}
+        checkbalanceValueInfo={checkbalanceValueInfo}
+        setDefaultPaymentMethod={setDefaultPaymentMethod}
+      />
+    );
+  }
+}
+
+export const mapDispatchToProps = (dispatch: ({}) => void) => {
+  return {
+    getCardListAction: () => {
+      dispatch(getCardList());
+    },
+    setDeleteModalMountState: payload => {
+      dispatch(setDeleteModalMountedState(payload));
+    },
+    onDeleteCard: payload => {
+      dispatch(deleteCard(payload));
+    },
+    onGetBalanceCard: payload => {
+      dispatch(checkBalance(payload));
+    },
+    setDefaultPaymentMethod: payload => {
+      dispatch(setDefaultPayment(payload));
+    },
+    clearPaymentNotification: () => {
+      dispatch(
+        setPaymentNotification({
+          status: '',
+        })
+      );
+    },
+  };
+};
+
+const mapStateToProps = state => {
+  return {
+    cardList: getCardListState(state),
+    creditCardList: getCreditDebitCards(state),
+    giftCardList: getGiftCards(state),
+    venmoCardList: getVenmoCards(state),
+    isFetching: getCardListFetchingState(state),
+    showNotification: getShowNotificationState(state),
+    showNotificationCaptcha: getShowNotificationCaptchaState(state),
+    deleteModalMountedState: deleteModalOpenState(state),
+    showUpdatedNotificationOnModal: showUpdatedNotificationOnModalState(state),
+    checkbalanceValueInfo: checkbalanceValue(state),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PaymentContainer);
