eb4792c7b9e8431f678d8dcbe8b836a1392a4f55
Furkan Ali <furali@publicisgroupe.net>
2019-07-30 10:13:28 +0530

    action paylaod change

--- packages/core/src/components/features/account/Account/container/Account.reducer.js
+++ packages/core/src/components/features/account/Account/container/Account.reducer.js
@@ -22,7 +22,7 @@ const AccountReducer = (state = initialState, action) => {
       return state.set('isFetching', true);
     case ACCOUNT_CONSTANTS.SET_ACCOUNT_NAVIGATION_LIST:
       return state
-        .set('accountNavigation', action.accountNav)
+        .set('accountNavigation', action)
         .set(DEFAULT_REDUCER_KEY, setCacheTTL(ACCOUNT_CONSTANTS.GET_ACCOUNT_NAV_LIST_TTL));
     default:
       return getDefaultState(state);
