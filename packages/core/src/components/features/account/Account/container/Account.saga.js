fa647577553310c0e176dfb5450b1102ace44490
Furkan Ali <furali@publicisgroupe.net>
2019-07-31 19:16:20 +0530

    US CA navigation apply

--- packages/core/src/components/features/account/Account/container/Account.saga.js
+++ packages/core/src/components/features/account/Account/container/Account.saga.js
@@ -1,15 +1,17 @@
 import { call, takeLatest, put } from 'redux-saga/effects';
 import ACCOUNT_CONSTANTS from '../Account.constants';
 import { validateReduxCache } from '../../../../../utils/cache.util';
+import utils from '../../../../../utils';
 import { setAccountNavigationList, showLoader } from './Account.actions';
 import accountNavigationAbstractor from '../../../../../services/abstractors/account/AccountNavigation/index';
 
 export function* getAccountNavigationList() {
+  const siteId = utils.getSiteId();
   try {
     yield put(showLoader());
     const accountNav = yield call(accountNavigationAbstractor.getData, 'AccountNavigation', {
       brand: 'TCP',
-      country: 'USA',
+      country: `${siteId}`,
       channel: 'Desktop',
     });
     yield put(setAccountNavigationList(accountNav));
