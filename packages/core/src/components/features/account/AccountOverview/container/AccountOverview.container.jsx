26a07308c79e89ca7182769a37c7c9e2d54c08bd
naman jain <njain63@sapient.com>
2019-08-05 16:02:21 +0530

    remove hardcoded labels

--- packages/core/src/components/features/account/AccountOverview/container/AccountOverview.container.jsx
+++ packages/core/src/components/features/account/AccountOverview/container/AccountOverview.container.jsx
@@ -4,17 +4,7 @@ import { connect } from 'react-redux';
 import AccountOverviewComponent from '../views/AccountOverview.view';
 
 const getAccountOverviewLabels = labels => {
-  return (
-    (labels && labels.accountOverview) || {
-      lbl_overview_heading: 'ACCOUNT OVERVIEW',
-      lbl_overview_addressBookHeading: 'Address Book',
-      lbl_overview_addressBookCTA: 'VIEW ADDRESS BOOK',
-      lbl_overview_paymentHeading: 'Payment & Gift Cards',
-      lbl_overview_paymentCTA: 'VIEW PAYMENT & GIFT CARDS',
-      lbl_overview_myPlaceRewardsHeading: 'My Place Rewards',
-      lbl_overview_myPlaceRewardsCTA: 'VIEW ALL REWARDS',
-    }
-  );
+  return (labels && labels.accountOverview) || {};
 };
 
 export const AccountOverviewContainer = ({ labels, ...otherProps }) => {
