1ae25b26be17fcca941be626e4fbd39aea9a8b73
Furkan Ali <furali@publicisgroupe.net>
2019-08-06 16:26:09 +0530

    work in progress

--- packages/core/src/components/features/account/common/organism/AddressOverviewTile/container/AddressOverviewTile.container.jsx
+++ packages/core/src/components/features/account/common/organism/AddressOverviewTile/container/AddressOverviewTile.container.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import AddressOverviewTileComponent from '../views';
+import {getAddressListState } from '../../../../AddressBook/container/AddressBook.selectors';
+import {
+  getAddressList
+} from '../../../../AddressBook/container/AddressBook.actions';
 
-export const AddressOverviewTile = ({ ...props }) => <AddressOverviewTileComponent {...props} />;
 
-const mapStateToProps = () => ({});
+// export const AddressOverviewTile = ({ ...props }, addressList) =>
 
-export default connect(mapStateToProps)(AddressOverviewTile);
+export class AddressOverviewTile extends React.Component{
+  componentDidMount() {
+    const { addressList } = this.props;
+    if(!addressList){
+      const { getAddressListAction } = this.props;
+      getAddressListAction();
+    }
+  }
+
+  render(){
+    const { addressList,labels } = this.props;
+    return(
+      <AddressOverviewTileComponent addressList={addressList} labels={labels}  />
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    addressList: getAddressListState(state),
+  };
+};
+
+
+export const mapDispatchToProps = dispatch => {
+  return {
+    getAddressListAction: () => {
+      dispatch(getAddressList());
+    },
+  };
+};
+
+
+AddressOverviewTile.propTypes = {
+  getAddressListAction: PropTypes.func.isRequired,
+  labels: PropTypes.shape({}),
+  addressList:PropTypes.shape({}),
+};
+
+AddressOverviewTile.defaultProps = {
+  labels: PropTypes.shape({ addressBook: {}, labels: {} }),
+  addressList: {}
+};
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AddressOverviewTile);
--- packages/core/src/components/features/account/common/organism/AddressOverviewTile/views/AddressOverviewTile.view.jsx
+++ packages/core/src/components/features/account/common/organism/AddressOverviewTile/views/AddressOverviewTile.view.jsx
@@ -1,15 +1,157 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Heading } from '@tcp/core/styles/themes/TCP/typotheme';
+import {Row, Col, BodyCopy} from '../../../../../../common/atoms';
+import withStyles from '../../../../../../common/hoc/withStyles';
+import Address from '../../../../../../common/molecules/Address';
+import styles from '../container/styles/AddressOverviewTile.style'
 
 import AccountOverviewTile from '../../../../../../common/molecules/AccountOverviewTile';
 
-export const AddressOverviewTile = ({ labels }) => {
+export const AddressOverviewTile = ({ labels, addressList }) => {
+
+
+const isShippingAddress = [];
+const isBillingAddress = [];
+
+if(addressList){
+  addressList.map(addr => addr).forEach(item => {
+    if(item.xcont_isShippingAddress === 'true'){
+      isShippingAddress.push(item);
+    }
+    if(item.xcont_isBillingAddress === 'true'){
+      isBillingAddress.push(item);
+    }
+  })
+  };
+
   return (
     <AccountOverviewTile
       title={labels.lbl_overview_addressBookHeading}
       ctaTitle={labels.lbl_overview_addressBookCTA}
     >
-      <p>Dummy Address book</p>
+
+      <BodyCopy component="div" className="elem-pt-LRG elem-pb-XL elem-mb-LRG heading">
+        <Row fullBleed>
+          <Col
+            colSize={{
+              small: 6,
+              large: 12,
+              medium: 8,
+            }}
+          >
+            <Row fullBleed>
+              <Col
+                colSize={{
+                  small: 5,
+                  large: 10,
+                  medium: 7,
+                }}
+              >
+
+                <Heading
+                  fontFamily="secondaryFontFamily"
+                  tag="h4"
+                >
+                  Default Shiping Address
+                </Heading>
+
+              </Col>
+              <Col
+                className="elem-mt-LRG"
+                colSize={{
+                  small: 1,
+                  large: 2,
+                  medium: 1,
+                }}
+              >
+                {isShippingAddress && isShippingAddress.length
+                  ? 'Edit'
+                  : 'Add'
+                }
+              </Col>
+            </Row>
+            <Row fullBleed className="elem-mb-XL">
+              <Col
+                colSize={{
+                  small: 5,
+                  large: 10,
+                  medium: 7,
+                }}
+              >
+                {isShippingAddress && isShippingAddress.length
+                  ? <Address address={isShippingAddress[0]} showPhone={false} showCountry={false} />
+                  : <p>You have not added an address yet.</p>
+                }
+              </Col>
+            </Row>
+
+          </Col>
+        </Row>
+      </BodyCopy>
+
+
+      <div>
+        <Row fullBleed>
+          <Col
+            colSize={{
+              small: 6,
+              large: 12,
+              medium: 8,
+            }}
+          >
+
+
+            <Row fullBleed>
+              <Col
+                colSize={{
+                  small: 5,
+                  large: 10,
+                  medium: 7,
+                }}
+              >
+
+                <Heading
+                  fontFamily="secondaryFontFamily"
+                  HeadingLarge="six"
+                  tag="h4"
+                  className="addressBook__separator"
+                >
+                  Default Billing Address
+                </Heading>
+
+              </Col>
+              <Col
+                className="elem-mt-LRG"
+                colSize={{
+                  small: 1,
+                  large: 2,
+                  medium: 1,
+                }}
+              >
+                {isBillingAddress && isBillingAddress.length
+                ? 'Edit'
+                : 'Add'
+                }
+              </Col>
+            </Row>
+            <Row fullBleed className="elem-mb-XXL">
+              <Col
+                colSize={{
+                  small: 5,
+                  large: 10,
+                  medium: 7,
+                }}
+              >
+                {isBillingAddress && isBillingAddress.length
+                  ? <Address address={isBillingAddress[0]} showPhone={false} showCountry={false} />
+                  : <p>You have not added an address yet.</p>
+                }
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </div>
     </AccountOverviewTile>
   );
 };
@@ -19,6 +161,7 @@ AddressOverviewTile.propTypes = {
     lbl_overview_addressBookHeading: PropTypes.string,
     lbl_overview_addressBookCTA: PropTypes.string,
   }),
+  addressList: PropTypes.shape({}),
 };
 
 AddressOverviewTile.defaultProps = {
@@ -26,6 +169,7 @@ AddressOverviewTile.defaultProps = {
     lbl_overview_addressBookHeading: 'Address Book',
     lbl_overview_addressBookCTA: 'View Address Book',
   },
+  addressList:{}
 };
 
-export default AddressOverviewTile;
+export default withStyles(AddressOverviewTile, styles);
