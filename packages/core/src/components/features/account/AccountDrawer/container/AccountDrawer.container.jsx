931be5b19d306484a3ed0dcfa0581cb5ba2f7f1f
Harmeet Singh <harmeet.singh@sapient.com>
2019-08-07 09:34:38 +0530

    added comment

--- packages/core/src/components/features/account/AccountDrawer/container/AccountDrawer.container.jsx
+++ packages/core/src/components/features/account/AccountDrawer/container/AccountDrawer.container.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import AccountDrawerView from '../views/AccountDrawerView';
 import labels from '../AccountDrawer.labels';
 import { openOverlayModal } from '../../../OverlayModal/container/OverlayModal.actions';
 import { getUserName } from './AccountDrawer.selector';
 
-// @flow
-type Props = {
-  className: string,
-  userName: any,
-};
-
-export const AccountDrawerContainer = ({ className, userName }: Props) => {
+export const AccountDrawerContainer = ({ className, userName }) => {
   return <AccountDrawerView className={className} labels={labels} userName={userName} />;
 };
 
@@ -21,12 +16,30 @@ export const mapStateToProps = state => {
   };
 };
 
-export const mapDispatchToProps = (dispatch: ({}) => void) => {
+export const mapDispatchToProps = dispatch => {
   return {
     openOverlay: payload => {
       dispatch(openOverlayModal(payload));
     },
   };
+};
+
+AccountDrawerContainer.propTypes = {
+  className: PropTypes.string,
+  labels: PropTypes.shape({}),
+  userName: PropTypes.string,
+};
+
+AccountDrawerContainer.defaultProps = {
+  className: '',
+  labels: {
+    CREATE_ACC_MY_FAV: 'My Favorites',
+    CREATE_ACC_MY_PLACE_REWARDS_CC: 'My Place Rewards Credit Card',
+    CREATE_ACC_WALLET: 'Wallet',
+    CREATE_ACC_ORDERS: 'Orders',
+    CREATE_ACC_SIGN_OUT: 'Sign Out',
+  },
+  userName: '',
 };
 
 export default connect(
