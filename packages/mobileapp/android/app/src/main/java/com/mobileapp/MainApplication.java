a6be9bfca61c146f1c0c588e5a84b93ed0a018bb
sachin rawal <srawal.sapient@outlook.com>
2019-08-08 21:01:23 +0530

    push

--- packages/mobileapp/android/app/src/main/java/com/mobileapp/MainApplication.java
+++ packages/mobileapp/android/app/src/main/java/com/mobileapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.mobileapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfingerprint.FingerprintAuthPackage;
+import com.oblador.keychain.KeychainPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(new MainReactPackage(),
+            new FingerprintAuthPackage(),
+            new KeychainPackage(),
             new AsyncStoragePackage(), new VectorIconsPackage(),
           new RNGestureHandlerPackage());
     }
