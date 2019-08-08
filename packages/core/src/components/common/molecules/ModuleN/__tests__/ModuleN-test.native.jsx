9fab3c59f8926c981f8cd8f948f4e1282022872c
Prasoon <prasoon.singh@publicissapient.com>
2019-08-08 09:51:17 +0530

    UPDATE

---
+++ packages/core/src/components/common/molecules/ModuleN/__tests__/ModuleN-test.native.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import mock from '../../../../../services/abstractors/common/moduleN/mock';
+import { ModuleNVanilla } from '../views/ModuleN.native';
+
+describe('ModuleNVanilla', () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<ModuleNVanilla {...mock.moduleN.composites} />);
+  });
+
+  it('ModuleN should be defined', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('ModuleN should render correctly', () => {
+    expect(component).toMatchSnapshot();
+  });
+});
