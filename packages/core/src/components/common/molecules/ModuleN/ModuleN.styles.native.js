e601452cf7a0936441d13023e0bcaaac8b346953
Prasoon <prasoon.singh@publicissapient.com>
2019-08-08 21:42:54 +0530

    update

--- packages/core/src/components/common/molecules/ModuleN/ModuleN.styles.native.js
+++ packages/core/src/components/common/molecules/ModuleN/ModuleN.styles.native.js
@@ -10,7 +10,7 @@ export const PromoTextBannerWrapper = styled.View`
   margin-bottom: ${props => props.theme.spacing.LAYOUT_SPACING.XXS};
 `;
 export const ButtonContainer = styled.View`
-  margin-bottom: ${props => props.theme.spacing.LAYOUT_SPACING.XXS};
+  margin-bottom: ${props => props.theme.spacing.ELEM_SPACING.SM};
 `;
 
 export default {
