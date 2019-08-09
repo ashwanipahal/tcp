d086e54b817c7f27615e5b8c8cf5ec62f9aedbf8
Prasoon <prasoon.singh@publicissapient.com>
2019-08-09 06:37:08 +0530

    update

--- packages/core/src/components/common/molecules/ButtonList/__tests__/ButtonList-test.native.jsx
+++ packages/core/src/components/common/molecules/ButtonList/__tests__/ButtonList-test.native.jsx
@@ -6,9 +6,6 @@ import { Container } from '../ButtonList.styles.native';
 
 describe('ButtonList', () => {
   let stackedCTAList;
-  let scrollCTAList;
-  let linkCTAList;
-  let imageCTAList;
 
   beforeEach(() => {
     stackedCTAList = shallow(
@@ -17,48 +14,18 @@ describe('ButtonList', () => {
         stackedCTAButtons={mock.moduleN.composites.stackedCTAButtons}
       />
     );
-
-    scrollCTAList = shallow(
-      <ButtonListVanilla
-        buttonListVariation="scrollCTAList"
-        scrollCTAButtons={mock.moduleN.composites.linkList}
-      />
-    );
-
-    linkCTAList = shallow(
-      <ButtonListVanilla
-        buttonListVariation="linkCTAList"
-        linkList={mock.moduleN.composites.linkList}
-      />
-    );
-
-    imageCTAList = shallow(
-      <ButtonListVanilla
-        buttonListVariation="imageCTAList"
-        divImageCTACarousel={mock.moduleN.composites.divImageCTACarousel}
-      />
-    );
   });
 
   it('ButtonList should be defined', () => {
     expect(stackedCTAList).toBeDefined();
-    expect(scrollCTAList).toBeDefined();
-    expect(linkCTAList).toBeDefined();
-    expect(imageCTAList).toBeDefined();
   });
 
   it('ButtonList should render correctly', () => {
     expect(stackedCTAList).toMatchSnapshot();
-    expect(scrollCTAList).toMatchSnapshot();
-    expect(linkCTAList).toMatchSnapshot();
-    expect(imageCTAList).toMatchSnapshot();
   });
 
   it('should render Container', () => {
     expect(stackedCTAList.find(Container)).toHaveLength(2);
-    expect(scrollCTAList.find(Container)).toHaveLength(1);
-    expect(linkCTAList.find(Container)).toHaveLength(1);
-    expect(imageCTAList.find(Container)).toHaveLength(1);
   });
 
   it('should render View', () => {
@@ -71,25 +38,5 @@ describe('ButtonList', () => {
 
   it('should render CustomButton', () => {
     expect(stackedCTAList.find('FlatList')).toHaveLength(1);
-  });
-
-  it('should render scrollCTAListContainer', () => {
-    expect(imageCTAList.find(Container)).toHaveLength(1);
-  });
-
-  it('should render scrollCTAList View', () => {
-    expect(imageCTAList.find('Styled(View)')).toHaveLength(1);
-  });
-
-  it('should render scrollCTAList CustomButton', () => {
-    expect(imageCTAList.find('Styled(CustomButton)')).toHaveLength(0);
-  });
-
-  it('should render scrollCTAList FlatList', () => {
-    expect(imageCTAList.find('FlatList')).toHaveLength(1);
-  });
-
-  it('should render linkCTAList Container', () => {
-    expect(linkCTAList.find(Container)).toHaveLength(1);
   });
 });
