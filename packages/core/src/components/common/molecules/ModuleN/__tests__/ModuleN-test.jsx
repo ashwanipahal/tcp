ef607478bf142c741a50da6a49fdd736f0657cad
Siddhartha Nanda <siddhartha.nanda@publicissapient.com>
2019-08-09 13:57:05 +0530

    added test cases for Module N and ButtonList

---
+++ packages/core/src/components/common/molecules/ModuleN/__tests__/ModuleN-test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import mock from '../../../../../services/abstractors/common/moduleD/mockN';
+import { ModuleNVanilla as ModuleN } from '../views/ModuleN';
+import { LinkText, PromoBanner } from '../..';
+
+describe('ModuleL component', () => {
+  let ModuleNComp;
+
+  beforeEach(() => {
+    ModuleNComp = shallow(<ModuleN {...mock.moduleD.composites} />);
+  });
+  it('renders correctly', () => {
+    expect(ModuleNComp).toMatchSnapshot();
+  });
+
+  it('should render header', () => {
+    expect(ModuleNComp.find(LinkText)).toHaveLength(4);
+  });
+
+  it('should render promo text banner', () => {
+    expect(ModuleNComp.find(PromoBanner)).toHaveLength(4);
+  });
+
+  it('Module has PromoBanner class', () => {
+    expect(ModuleNComp.find('.moduleN__promo-banner')).toHaveLength(4);
+  });
+});
