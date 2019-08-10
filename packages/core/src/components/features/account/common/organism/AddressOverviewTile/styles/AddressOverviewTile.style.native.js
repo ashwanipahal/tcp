c0366a219614ecc5564cbad887707898c7c8e332
Gagan Bhardwaj <gbhardwaj4@sapient.com>
2019-08-09 23:25:26 -0400

    RWD-3963: Address Overview Added

--- packages/core/src/components/features/account/common/organism/AddressOverviewTile/styles/AddressOverviewTile.style.native.js
+++ packages/core/src/components/features/account/common/organism/AddressOverviewTile/styles/AddressOverviewTile.style.native.js
@@ -7,14 +7,52 @@ const UnderlineStyle = styled.View`
   margin-top: ${props => props.theme.spacing.ELEM_SPACING.MED};
 `;
 
-const PaymentTileContainer = styled.View`
+const AddressTileContainer = styled.View`
   border: 1px solid ${props => props.theme.colorPalette.gray[700]};
   padding: ${props => props.theme.spacing.ELEM_SPACING.MED};
 `;
 
 const ButtonWrapperStyle = styled.View`
-  padding-top: ${props => props.theme.spacing.ELEM_SPACING.XXS};
+  margin-top: ${props => props.theme.spacing.ELEM_SPACING.LRG};
   margin-bottom: ${props => props.theme.spacing.ELEM_SPACING.XXS};
 `;
 
-export { UnderlineStyle, PaymentTileContainer, ButtonWrapperStyle };
+const TouchableLink = styled.TouchableHighlight.attrs({
+  underlayColor: props => props.theme.colors.BUTTON.WHITE.ALT_FOCUS,
+})`
+  margin-right: ${props => props.theme.spacing.ELEM_SPACING.XS};
+`;
+
+const AddressTypeContainer = styled.View`
+  height: 24px;
+  flex: 1;
+  flex-direction: row;
+`;
+
+const BodyCopyStyle = {
+  fontSize: 'fs13',
+};
+
+const LeftContainer = {
+  flex: 1.5,
+  flexDirection: 'row',
+  justifyContent: 'flex-start',
+};
+
+const RightContainer = {
+  flex: 0.5,
+  flexDirection: 'row',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+};
+
+export {
+  UnderlineStyle,
+  AddressTileContainer,
+  ButtonWrapperStyle,
+  TouchableLink,
+  BodyCopyStyle,
+  AddressTypeContainer,
+  LeftContainer,
+  RightContainer,
+};
--- packages/core/src/components/features/account/common/organism/AddressOverviewTile/views/AddressOverviewTile.view.native.jsx
+++ packages/core/src/components/features/account/common/organism/AddressOverviewTile/views/AddressOverviewTile.view.native.jsx
@@ -1,110 +1,149 @@
 import React from 'react';
+import { View } from 'react-native';
 import BodyCopy from '@tcp/core/src/components/common/atoms/BodyCopy';
-import { getIconCard } from '@tcp/core/src/utils/index.native';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import {
-  getCardList,
-  checkBalance,
-} from '@tcp/core/src/components/features/account/Payment/container/Payment.actions';
-import { cardIconMapping } from '@tcp/core/src/components/features/account/common/molecule/CardTile/views/CardTile.utils';
-import { getCardListState } from '@tcp/core/src/components/features/account/Payment/container/Payment.selectors';
-
-import AddressItem from '../../../molecule/AddressItem';
 import CustomButton from '../../../../../../common/atoms/Button';
 import {
   UnderlineStyle,
-  PaymentTileContainer,
+  AddressTileContainer,
   ButtonWrapperStyle,
+  BodyCopyStyle,
+  TouchableLink,
+  AddressTypeContainer,
+  LeftContainer,
+  RightContainer,
 } from '../styles/AddressOverviewTile.style.native';
+import Address from '../../../../../../common/molecules/Address';
 
 export class AddressOverviewTile extends React.PureComponent<Props> {
   static propTypes = {
-    getCardListAction: PropTypes.func,
-    cardList: PropTypes.arrayOf({}),
+    addressList: PropTypes.arrayOf({}),
     labels: PropTypes.shape({}),
     handleComponentChange: PropTypes.func.isRequired,
   };
 
   static defaultProps = {
-    getCardListAction: () => {},
-    cardList: [],
     labels: {},
+    addressList: [],
   };
-
-  componentDidMount() {
-    const { getCardListAction } = this.props;
-    getCardListAction();
-  }
-
-  getCreditCardView = (card, isAddVariation) => {
-    const { labels, handleComponentChange } = this.props;
-    const cardTileProps = {
-      title: labels.lbl_overview_defaultShipingAddress,
-      text: !isAddVariation
-        ? `${labels.lbl_overview_card_ending} ${card.accountNo.slice(-4)}`
-        : labels.lbl_overview_addressNotAdded,
-      subText: !isAddVariation
-        ? `${labels.lbl_overview_expires} ${card.expMonth.trim()}/${card.expYear}`
-        : '',
-      variation: !isAddVariation
-        ? labels.lbl_overview_addressBookEdit
-        : labels.lbl_overview_addressBookAdd,
-      icon: !isAddVariation ? getIconCard(cardIconMapping[card.ccBrand]) : '',
-    };
-    return (
-      <AddressItem paymentInfo={cardTileProps} handleComponentChange={handleComponentChange} />
-    );
-  };
-
-  getGiftCardView = (card, isAddVariation) => {
-    const { labels, handleComponentChange } = this.props;
-    const cardTileProps = {
-      title: labels.lbl_overview_defaultBillingAddress,
-      text: !isAddVariation
-        ? `${labels.lbl_overview_card_ending} ${card.accountNo.slice(-4)}`
-        : labels.lbl_overview_addressNotAdded,
-      subText: !isAddVariation ? `${labels.lbl_overview_remaining_balance}:` : '',
-      variation: !isAddVariation
-        ? labels.lbl_overview_addressBookEdit
-        : labels.lbl_overview_addressBookAdd,
-      icon: !isAddVariation ? getIconCard(cardIconMapping.GC) : '',
-    };
-    return (
-      <AddressItem paymentInfo={cardTileProps} handleComponentChange={handleComponentChange} />
-    );
-  };
-
-  getGiftCardList = cardList =>
-    cardList && cardList.size > 0 && cardList.filter(card => card.ccType === 'GiftCard');
-
-  getCreditCardList = cardList =>
-    cardList &&
-    cardList.size > 0 &&
-    cardList.filter(card => card.ccType !== 'GiftCard' && card.ccType !== 'VENMO');
 
   render() {
-    const { cardList, labels, handleComponentChange } = this.props;
-    const creditCardList = this.getCreditCardList(cardList);
-    const giftCardList = this.getGiftCardList(cardList);
+    const { addressList, labels, handleComponentChange } = this.props;
+    const defaultShippingAddress = [];
+    const defaultBillingAddress = [];
 
+    if (addressList) {
+      addressList
+        .map(addr => addr)
+        .forEach(item => {
+          if (item.primary === 'true') {
+            defaultShippingAddress.push(item);
+          }
+          if (item.xcont_isDefaultBilling === 'true') {
+            defaultBillingAddress.push(item);
+          }
+        });
+    }
     return (
-      <PaymentTileContainer>
+      <AddressTileContainer>
         <BodyCopy
           fontFamily="secondary"
           fontSize="fs16"
           text={labels.lbl_overview_addressBookHeading}
           color="black"
         />
-        <UnderlineStyle />
-        {creditCardList && creditCardList.size > 0
-          ? creditCardList.map(card => this.getCreditCardView(card, false))
-          : this.getCreditCardView(null, true)}
+
         <UnderlineStyle />
 
-        {giftCardList && giftCardList.size > 0
-          ? giftCardList.map(card => this.getGiftCardView(card, false))
-          : this.getGiftCardView(null, true)}
+        <AddressTypeContainer>
+          <View style={LeftContainer}>
+            <BodyCopy
+              fontFamily="secondary"
+              fontSize="fs16"
+              text={labels.lbl_overview_defaultShipingAddress}
+              color="black"
+            />
+          </View>
+          <View style={RightContainer}>
+            <TouchableLink
+              onPress={() => handleComponentChange('addressBookMobile')}
+              textDecorationLine="underline"
+            >
+              <BodyCopy
+                fontFamily="secondary"
+                fontSize="fs13"
+                fontWeight="regular"
+                text={
+                  defaultShippingAddress && defaultShippingAddress.length
+                    ? labels.lbl_overview_addressBookEdit
+                    : labels.lbl_overview_addressBookAdd
+                }
+                color="gray.900"
+              />
+            </TouchableLink>
+          </View>
+        </AddressTypeContainer>
+        {defaultShippingAddress && defaultShippingAddress.length ? (
+          <Address
+            address={defaultShippingAddress[0]}
+            showCountry={false}
+            customStyle={BodyCopyStyle}
+            showName={false}
+          />
+        ) : (
+          <BodyCopy
+            fontSize="fs13"
+            fontFamily="secondary"
+            fontWeight="regular"
+            text={labels.lbl_overview_addressNotAdded}
+          />
+        )}
+        <UnderlineStyle />
+
+        <AddressTypeContainer>
+          <View style={LeftContainer}>
+            <BodyCopy
+              fontFamily="secondary"
+              fontSize="fs16"
+              text={labels.lbl_overview_defaultBillingAddress}
+              color="black"
+            />
+          </View>
+          <View style={RightContainer}>
+            <TouchableLink
+              onPress={() => handleComponentChange('addressBookMobile')}
+              textDecorationLine="underline"
+            >
+              <BodyCopy
+                fontFamily="secondary"
+                fontSize="fs13"
+                fontWeight="regular"
+                text={
+                  defaultBillingAddress && defaultBillingAddress.length
+                    ? labels.lbl_overview_addressBookEdit
+                    : labels.lbl_overview_addressBookAdd
+                }
+                color="gray.900"
+              />
+            </TouchableLink>
+          </View>
+        </AddressTypeContainer>
+
+        {defaultBillingAddress && defaultBillingAddress.length ? (
+          <Address
+            address={defaultBillingAddress[0]}
+            showCountry={false}
+            customStyle={BodyCopyStyle}
+            showName={false}
+          />
+        ) : (
+          <BodyCopy
+            fontSize="fs13"
+            fontFamily="secondary"
+            fontWeight="regular"
+            text={labels.lbl_overview_addressNotAdded}
+          />
+        )}
 
         <ButtonWrapperStyle>
           <CustomButton
@@ -115,29 +154,9 @@ export class AddressOverviewTile extends React.PureComponent<Props> {
             onPress={() => handleComponentChange('addressBookMobile')}
           />
         </ButtonWrapperStyle>
-      </PaymentTileContainer>
+      </AddressTileContainer>
     );
   }
 }
 
-export const mapDispatchToProps = dispatch => {
-  return {
-    getCardListAction: () => {
-      dispatch(getCardList());
-    },
-    onGetBalanceCard: payload => {
-      dispatch(checkBalance(payload));
-    },
-  };
-};
-
-const mapStateToProps = state => {
-  return {
-    cardList: getCardListState(state),
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(AddressOverviewTile);
+export default AddressOverviewTile;
