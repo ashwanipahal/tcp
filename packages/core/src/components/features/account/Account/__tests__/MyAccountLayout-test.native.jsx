bba560139a8dc88f9c51c549ba9badfcd8784d3c
Gagan Bhardwaj <gbhardwaj4@sapient.com>
2019-08-10 19:42:36 -0400

    RWD-4829: Fixed  unit test case

--- packages/core/src/components/features/account/Account/__tests__/MyAccountLayout-test.native.jsx
+++ packages/core/src/components/features/account/Account/__tests__/MyAccountLayout-test.native.jsx
@@ -4,13 +4,26 @@ import { MyAccountLayoutViewVanilla } from '../views/MyAccountLayout.view.native
 import navData from '../MyAccountRoute.config';
 
 describe('My Account Layout View', () => {
+  const mainContent = jest.fn();
+  const handleComponentChange = jest.fn();
+  let props = {
+    labels: {},
+    className: 'MyAccountLayoutView',
+    handleComponentChange,
+  };
+
   it('should render MyAccountLayoutView Correctly', () => {
-    const mainContent = jest.fn();
+    props = {
+      labels: {},
+      className: 'MyAccountLayoutView',
+      handleComponentChange,
+    };
     const tree = shallow(
       <MyAccountLayoutViewVanilla
         mainContent={mainContent}
         navData={navData}
-        selectedComponent="accountOverview"
+        selectedComponent="addressBookMobile"
+        {...props}
       />
     );
     expect(tree).toMatchSnapshot();
