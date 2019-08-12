9e08aaf8fe3b3a8201ba08b97f211fc8d0ddd4e7
Mohit Jain <mohit.jain2@publicissapient.com>
2019-08-12 18:31:46 +0530

    RWD-4937 | FIX TO HIDE order ledger and coupon list in empty bag

--- packages/core/src/components/features/CnC/BagPage/container/BagPage.container.jsx
+++ packages/core/src/components/features/CnC/BagPage/container/BagPage.container.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import BagPageSelector from './BagPage.selectors';
 import BagPage from '../views/BagPage.view';
 import BAG_PAGE_ACTIONS from './BagPage.actions';
+import { getCartOrderList } from '../../CartItemTile/container/CartItemTile.selectors';
+
 // @flow
 // type Props = {
 //   closeModal: Function,
@@ -35,10 +37,12 @@ export const mapDispatchToProps = (dispatch: ({}) => void) => {
 };
 
 const mapStateToProps = state => {
+  const { size = 0 } = getCartOrderList(state) || {};
   return {
     labels: BagPageSelector.getBagPageLabels(state),
     totalCount: BagPageSelector.getTotalItems(state),
     productsTypes: BagPageSelector.getProductsTypes(state),
+    orderItemsCount: size,
   };
 };
 
