f75ee9f7560b7be81e24c5b584c14cf92b38bc89
Madan Joshi <mjoshi8@sapient.com>
2019-08-14 00:35:43 +0530

    Page Loader

---
+++ packages/web/src/components/features/content/Loader/Loader.style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const LoaderLoyalty = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 10099;
+  overflow-x: hidden;
+  overflow-y: auto;
+  text-align: center;
+  background: rgba(0, 0, 0, 0.5);
+  bottom: 0;
+`;
+
+const loaderStyles = {
+  LoaderLoyalty,
+};
+
+export default loaderStyles;
