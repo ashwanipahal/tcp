226c1573b698561e9d5adb2d7d668cb5ace9b944
Harmeet Singh <harmeet.singh@sapient.com>
2019-08-13 15:22:35 +0530

    added payment tiles

--- packages/core/src/components/features/account/common/organism/PaymentOverviewTile/container/PaymentOverviewTile.container.jsx
+++ packages/core/src/components/features/account/common/organism/PaymentOverviewTile/container/PaymentOverviewTile.container.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PaymentOverviewTileComponent from '../views';
+import {
+  getCreditCardDefault,
+  getGiftCards,
+  getVenmoCards,
+} from '../../../../Payment/container/Payment.selectors';
+import { getCardList } from '../../../../Payment/container/Payment.actions';
 
-export const PaymentOverviewTile = ({ ...props }) => <PaymentOverviewTileComponent {...props} />;
+export class PaymentOverviewTile extends React.Component<Props> {
+  componentDidMount() {
+    const { getCardListAction } = this.props;
+    getCardListAction();
+  }
 
-const mapStateToProps = () => ({});
+  render() {
+    const { creditCardDefault, giftCardList, venmoCardList, labels } = this.props;
+    const creditCardDefaultValue = creditCardDefault.get(0);
+    const giftCardListValue = giftCardList.get(0);
+    const venmoCardListValue = venmoCardList.get(0);
+    return (
+      <PaymentOverviewTileComponent
+        creditCardDefault={creditCardDefaultValue}
+        giftCardList={giftCardListValue}
+        venmoCardList={venmoCardListValue}
+        labels={labels}
+      />
+    );
+  }
+}
 
-export default connect(mapStateToProps)(PaymentOverviewTile);
+export const mapDispatchToProps = dispatch => {
+  return {
+    getCardListAction: () => {
+      dispatch(getCardList());
+    },
+  };
+};
+
+const mapStateToProps = state => {
+  return {
+    creditCardDefault: getCreditCardDefault(state),
+    giftCardList: getGiftCards(state),
+    venmoCardList: getVenmoCards(state),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PaymentOverviewTile);
