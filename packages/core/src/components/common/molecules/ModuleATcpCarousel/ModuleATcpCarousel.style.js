d7c67b4d38b5ed10124f6607f035c0378de90ad5
Raghav <raghasb@gmail.com>
2019-08-13 20:29:54 +0530

    fixed border for scrollCTA

--- packages/core/src/components/common/molecules/ModuleATcpCarousel/ModuleATcpCarousel.style.js
+++ packages/core/src/components/common/molecules/ModuleATcpCarousel/ModuleATcpCarousel.style.js
@@ -70,9 +70,14 @@ const style = css`
 
   .button-list-container.stackedCTAList,
   .button-list-container.scrollCTAList {
+    margin-bottom: ${props => props.theme.spacing.ELEM_SPACING.XXXL};
+    margin-top: ${props => props.theme.spacing.ELEM_SPACING.XL};
+  }
+
+  .button-list-container.stackedCTAList {
     border-left: 1px solid ${props => props.theme.colors.BORDER.NORMAL};
     border-top: 1px solid ${props => props.theme.colors.BORDER.NORMAL};
-    margin-bottom: ${props => props.theme.spacing.ELEM_SPACING.XXXL};
+    margin-top: 0;
 
     @media ${props => props.theme.mediaQuery.medium} {
       border-left: 0;
