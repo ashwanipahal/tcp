2ece0997fa7bff336475cccc4a361c9c11369860
Amit Chawla <amit.chawla2@sapient.com>
2019-08-13 10:48:28 +0530

    Jest Config Updated

--- config/jest.config.js
+++ config/jest.config.js
@@ -54,10 +54,10 @@ module.exports = {
   coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
   coverageThreshold: {
     global: {
-      statements: 70,
-      branches: 55.5,
-      functions: 62,
-      lines: 70,
+      statements: 69,
+      branches: 55,
+      functions: 61,
+      lines: 69,
     },
   },
   snapshotSerializers: ['enzyme-to-json/serializer'],
--- config/jest.config.native.js
+++ config/jest.config.native.js
@@ -50,9 +50,9 @@ module.exports = {
   coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
   coverageThreshold: {
     global: {
-      statements: 62,
-      branches: 43.5,
-      functions: 52.5,
+      statements: 63,
+      branches: 44,
+      functions: 53,
       lines: 70,
     },
   },
