5c66d4c0f19def8d5f755c48ed71ceb083e2638b
furkan <furkan.ali@publicissapient.com>
2019-08-18 22:28:22 +0530

    functional task done and test case pending

--- packages/mobileapp/android/app/src/main/java/com/mobileapp/MainApplication.java
+++ packages/mobileapp/android/app/src/main/java/com/mobileapp/MainApplication.java
@@ -3,11 +3,16 @@ package com.mobileapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfingerprint.FingerprintAuthPackage;
+import com.oblador.keychain.KeychainPackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,10 +27,12 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNGestureHandlerPackage()
-      );
+      return Arrays.<ReactPackage>asList(new MainReactPackage(),
+            new FingerprintAuthPackage(),
+            new KeychainPackage(),
+            new NetInfoPackage(),
+            new AsyncStoragePackage(), new VectorIconsPackage(),
+          new RNGestureHandlerPackage());
     }
 
     @Override
