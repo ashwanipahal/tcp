08c2fc962ae727e0435b5c85ae64b2ee729914e8
Gagan Bhardwaj <gbhardwaj4@sapient.com>
2019-08-19 23:05:00 -0400

    RWD-724: Unit test added

---
+++ packages/core/src/components/features/account/AddEditCreditCard/views/__tests__/AddEditCreditCard.view-test.native.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { AddEditCreditCard } from '../AddEditCreditCard.view.native';
+
+describe('AddEditCreditCard component', () => {
+  it('should renders correctly', () => {
+    const props = {
+      labels: {
+        paymentGC: {},
+        common: {},
+      },
+    };
+    const component = shallow(<AddEditCreditCard {...props} />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should renders correctly with error message', () => {
+    const props = {
+      labels: {
+        paymentGC: {},
+        common: {},
+      },
+      isEdit: true,
+      errorMessage: 'There is some error',
+    };
+    const component = shallow(<AddEditCreditCard {...props} />);
+    expect(component).toMatchSnapshot();
+  });
+});
