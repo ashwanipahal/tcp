71c922e280381f207dc00cb264dced82f81017b3
Mayank <mayank.rastogi@publicissapient.com>
2019-08-20 11:26:46 +0530

    Review comments incorporation

--- packages/core/src/components/features/account/common/molecule/CouponTile/views/CouponTile.view.jsx
+++ packages/core/src/components/features/account/common/molecule/CouponTile/views/CouponTile.view.jsx
@@ -3,27 +3,24 @@ import PropTypes from 'prop-types';
 import BodyCopy from '../../../../../../common/atoms/BodyCopy';
 import withStyles from '../../../../../../common/hoc/withStyles';
 import styles from '../styles/CouponTile.style';
+import { COUPON_REDEMPTION_TYPE } from '../../../../../../../services/abstractors/CnC/CartItemTile';
 
 export const CouponTile = ({ className, coupon, labels }) => {
   let couponClass = '';
   let couponTextLabel = '';
-  let couponType = '';
+
   switch (coupon.offerType) {
-    case 'PC':
-    case 'PLACECASH':
+    case COUPON_REDEMPTION_TYPE.PLACECASH:
       couponClass = 'coupon-placecash';
       couponTextLabel = `${labels.lbl_overview_couponTypePlacecash}`;
-      couponType = 'placecash';
       break;
-    case 'rewards':
+    case COUPON_REDEMPTION_TYPE.REWARDS:
       couponClass = 'coupon-reward';
       couponTextLabel = `${labels.lbl_overview_couponTypeReward}`;
-      couponType = 'reward';
       break;
     default:
       couponClass = 'coupon-saving';
       couponTextLabel = `${labels.lbl_overview_couponTypeSaving}`;
-      couponType = 'savings';
   }
 
   return (
@@ -49,7 +46,7 @@ export const CouponTile = ({ className, coupon, labels }) => {
           {coupon.title}
         </BodyCopy>
 
-        {couponType === 'placecash' && (
+        {coupon.offerType === COUPON_REDEMPTION_TYPE.PLACECASH && (
           <BodyCopy fontSize="fs10" data-locator="accountoverview-myplacerewatdstile-rewarduseby">
             {`${labels.lbl_overview_couponValid} ${coupon.effectiveDate} - ${
               coupon.expirationDate
@@ -57,7 +54,7 @@ export const CouponTile = ({ className, coupon, labels }) => {
           </BodyCopy>
         )}
 
-        {couponType !== 'placecash' && (
+        {coupon.offerType !== COUPON_REDEMPTION_TYPE.PLACECASH && (
           <BodyCopy fontSize="fs10" data-locator="accountoverview-myplacerewatdstile-rewarduseby">
             {`${labels.lbl_overview_couponUseBy} ${coupon.expirationDate}`}
           </BodyCopy>
