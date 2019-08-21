4b9a4f7657d3a7eba6ec05d0d6bc7dbb26c06f75
Raghav <raghasb@gmail.com>
2019-08-21 22:55:51 +0530

    recovered deleted files

---
+++ packages/core/src/components/features/account/AddEditAddress/views/AddEditAddress.view.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Heading } from '@tcp/core/styles/themes/TCP/typotheme';
+import withStyles from '@tcp/core/src/components/common/hoc/withStyles';
+import Anchor from '../../../../common/atoms/Anchor';
+import styles from '../styles/AddEditAddress.style';
+
+import AddEditAddress from '../../../../common/organisms/AddEditAddress/container/AddEditAddress.container';
+
+const AddEditAddressContainer = ({ labels, isEdit, backToAddressBookClick, className }) => {
+  return (
+    <div className={className}>
+      <Anchor
+        className="addAddress__anchor__back"
+        fontSizeVariation="xlarge"
+        anchorVariation="secondary"
+        to="/account?id=address-book"
+        data-locator="addnewaddress-back"
+        asPath="/account/address-book"
+      >
+        {labels.common.lbl_common_backLink}
+      </Anchor>
+      <Heading
+        fontFamily="primaryFontFamily"
+        HeadingLarge="six"
+        tag="h4"
+        className="addAddress__separator"
+      >
+        {isEdit
+          ? labels.addressBook.ACC_LBL_EDIT_ADDRESS_FORM_HEADING
+          : labels.addressBook.ACC_LBL_ADD_ADDRESS_FORM_HEADING}
+      </Heading>
+      <AddEditAddress
+        backToAddressBookClick={backToAddressBookClick}
+        labels={labels}
+        isEdit={isEdit}
+      />
+    </div>
+  );
+};
+
+AddEditAddressContainer.propTypes = {
+  labels: PropTypes.shape({}),
+  isEdit: PropTypes.bool,
+  backToAddressBookClick: PropTypes.func,
+  className: PropTypes.string,
+};
+
+AddEditAddressContainer.defaultProps = {
+  labels: {
+    common: {
+      lbl_common_backLink: '',
+    },
+    addressBook: {
+      ACC_LBL_EDIT_ADDRESS_FORM_HEADING: '',
+      ACC_LBL_ADD_ADDRESS_FORM_HEADING: '',
+    },
+  },
+  isEdit: false,
+  backToAddressBookClick: () => {},
+  className: '',
+};
+
+export default withStyles(AddEditAddressContainer, styles);
