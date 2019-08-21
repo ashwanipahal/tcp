d89f6a1b9522a16dd8ea52dd28637643aff056e2
Gagan Bhardwaj <gbhardwaj4@sapient.com>
2019-08-21 15:56:22 -0400

    RWD-2001: Initial Commit

---
+++ packages/core/src/components/features/account/common/organism/MyPlaceRewardsOverviewTile/styles/MyPlaceRewardsOverviewTile.style.native.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components/native';
+
+const UnderlineStyle = styled.View`
+  height: 1px;
+  background-color: ${props => props.theme.colorPalette.gray[600]};
+  margin-bottom: ${props => props.theme.spacing.ELEM_SPACING.LRG};
+  margin-top: ${props => props.theme.spacing.ELEM_SPACING.LRG};
+`;
+
+const AddressTileContainer = styled.View`
+  border: 1px solid ${props => props.theme.colorPalette.gray[700]};
+  padding: ${props => props.theme.spacing.ELEM_SPACING.MED};
+`;
+
+const ButtonWrapperStyle = styled.View`
+  margin-top: ${props => props.theme.spacing.LAYOUT_SPACING.XL};
+  margin-bottom: ${props => props.theme.spacing.ELEM_SPACING.XXS};
+`;
+
+const TouchableLink = styled.TouchableHighlight.attrs({
+  underlayColor: props => props.theme.colors.BUTTON.WHITE.ALT_FOCUS,
+})`
+  margin-right: ${props => props.theme.spacing.ELEM_SPACING.XS};
+`;
+
+const AddressTypeContainer = styled.View`
+  height: 24px;
+  flex: 1;
+  flex-direction: row;
+`;
+
+const ShopAnchor = styled.View`
+  height: 42px;
+  border: 1px solid gray;
+  justify-content: center;
+  margin: 25px 0px;
+`;
+
+const BodyCopyStyle = {
+  fontSize: 'fs13',
+};
+
+const LeftContainer = {
+  flex: 1.5,
+  flexDirection: 'row',
+  justifyContent: 'flex-start',
+};
+
+const RightContainer = {
+  flex: 0.5,
+  flexDirection: 'row',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+};
+
+export {
+  UnderlineStyle,
+  AddressTileContainer,
+  ButtonWrapperStyle,
+  TouchableLink,
+  BodyCopyStyle,
+  AddressTypeContainer,
+  LeftContainer,
+  RightContainer,
+  ShopAnchor,
+};
---
+++ packages/core/src/components/features/account/common/organism/MyPlaceRewardsOverviewTile/views/MyPlaceRewardsOverviewTile.view.native.jsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import { View } from 'react-native';
+import PropTypes from 'prop-types';
+import BodyCopy from '@tcp/core/src/components/common/atoms/BodyCopy';
+import { navigateToNestedRoute } from '@tcp/core/src/utils/utils.app';
+import { withNavigation } from 'react-navigation';
+import CustomButton from '../../../../../../common/atoms/Button';
+import {
+  UnderlineStyle,
+  AddressTileContainer,
+  ButtonWrapperStyle,
+  BodyCopyStyle,
+  AddressTypeContainer,
+  LeftContainer,
+  RightContainer,
+  ShopAnchor,
+} from '../styles/MyPlaceRewardsOverviewTile.style.native';
+import Anchor from '../../../../../../common/atoms/Anchor';
+
+export class MyPlaceRewardsOverviewTile extends React.PureComponent<Props> {
+  render() {
+    const { className, labels, coupons, isBrierleyEnabled, handleComponentChange } = this.props;
+    const rewardCouponsCount = coupons && coupons.size;
+    let walletOverviewInfo = '';
+    let rewardDataLocator = '';
+
+    if (rewardCouponsCount) {
+      walletOverviewInfo = labels.lbl_overview_myPlaceRewardsAvailable.replace(
+        /\{0\}/,
+        rewardCouponsCount
+      );
+      rewardDataLocator = 'accountoverview-myplacerewatdstile-youhaverewardtext';
+    } else {
+      walletOverviewInfo = labels.lbl_overview_myPlaceRewardsDesc;
+      rewardDataLocator = 'accountoverview-myplacerewatdstile-startshoptext';
+    }
+    const { navigation } = this.props;
+    return (
+      <View>
+        <AddressTileContainer>
+          <BodyCopy
+            fontFamily="secondary"
+            fontSize="fs16"
+            text={labels.lbl_overview_myPlaceRewardsHeading}
+            color="black"
+          />
+
+          <UnderlineStyle />
+          {!rewardCouponsCount && (
+            <View>
+              <BodyCopy
+                fontFamily="secondary"
+                fontSize="fs13"
+                text={labels.lbl_overview_myPlaceRewardsDesc}
+                color="black"
+              />
+              <ShopAnchor>
+                <Anchor
+                  anchorVariation="button"
+                  buttonVariation="variable-width"
+                  fullWidth
+                  centered
+                  fill="WHITE"
+                  data-locator="accountoverview-myplacerewardstile-startshopbtn"
+                  accessibilityRole="link"
+                  accessibilityLabel={labels.lbl_overview_myPlaceRewardsShopNow}
+                  text={labels.lbl_overview_myPlaceRewardsShopNow}
+                  borderWidth="1"
+                  borderColor="black"
+                  onPress={() => navigateToNestedRoute(navigation, 'HomeStack', 'home')}
+                />
+              </ShopAnchor>
+            </View>
+          )}
+          <ButtonWrapperStyle>
+            <CustomButton
+              text={labels.lbl_overview_myPlaceRewardsCTA}
+              buttonVariation="variable-width"
+              fill="BLUE"
+              color="white"
+              onPress={() => handleComponentChange('myPlaceRewardsMobile')}
+            />
+          </ButtonWrapperStyle>
+        </AddressTileContainer>
+      </View>
+    );
+  }
+}
+
+MyPlaceRewardsOverviewTile.propTypes = {
+  labels: PropTypes.shape({
+    lbl_overview_myPlaceRewardsHeading: PropTypes.string.isRequired,
+    lbl_overview_myPlaceRewardsCTA: PropTypes.string.isRequired,
+    lbl_overview_myPlaceRewardsAvailable: PropTypes.string.isRequired,
+    lbl_overview_myPlaceRewardsDesc: PropTypes.string.isRequired,
+    lbl_overview_myPlaceRewardsShopNow: PropTypes.string.isRequired,
+  }).isRequired,
+  coupons: PropTypes.shape([]).isRequired,
+  isBrierleyEnabled: PropTypes.bool,
+  className: PropTypes.string,
+};
+
+MyPlaceRewardsOverviewTile.defaultProps = {
+  isBrierleyEnabled: true,
+  className: '',
+};
+
+export default withNavigation(MyPlaceRewardsOverviewTile);
