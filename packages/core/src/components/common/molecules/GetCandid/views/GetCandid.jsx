4947104db5bfa0f4d1d13fdf1f4d8c8e11c3854f
shobhitsarin <shobhit.sarin@sapient.com>
2019-08-24 16:59:11 +0530

    Adding getCandid as lazy loaded comp

--- packages/core/src/components/common/molecules/GetCandid/views/GetCandid.jsx
+++ packages/core/src/components/common/molecules/GetCandid/views/GetCandid.jsx
@@ -8,6 +8,7 @@ import style from '../styles/GetCandid.style';
 import withStyles from '../../../hoc/withStyles';
 import { getAPIConfig } from '../../../../../utils';
 import { PDP_PAGE_ID } from '../config';
+import withLazyLoad from '../../../hoc/withLazyLoad';
 
 class GetCandid extends React.PureComponent {
   static propTypes = {
@@ -170,5 +171,5 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(withStyles(GetCandid, style));
+export default connect(mapStateToProps)(withStyles(withLazyLoad(GetCandid), style));
 export { GetCandid as GetCandidVanilla };
