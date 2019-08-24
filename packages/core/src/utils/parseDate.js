e1ce0af61724e9b7a03f6aaabd963fa9763a828d
furkan <furkan.ali@publicissapient.com>
2019-08-24 23:21:39 +0530

    month issue fixed

--- packages/core/src/utils/parseDate.js
+++ packages/core/src/utils/parseDate.js
@@ -109,4 +109,9 @@ export const COMPLETE_MONTH = {
   11: 'DECEMBER',
 };
 
+export const getCapitalizeFirstLetter = string => {
+  const lowerString = string.toLowerCase();
+  return lowerString.charAt(0).toUpperCase() + lowerString.slice(1);
+};
+
 export { parseDate };
