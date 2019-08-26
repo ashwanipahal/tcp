fd7f1bdf4a66a7fd1fb68b533df7fe0ce18a3745
Erik Jung <erik.jung@sapientrazorfish.com>
2019-08-26 16:41:02 -0400

    RWD-5713 | Pass env vars into Next build config

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,9 +1,18 @@
+// This will only read from system vars and ./.env
+require('dotenv').config();
 const withTM = require('next-transpile-modules');
 const path = require('path');
 
 module.exports = withTM({
   transpileModules: ['@tcp'],
   useFileSystemPublicRoutes: false,
+  // This is to supply build-time environment vars to both server and client files:
+  //    https://nextjs.org/docs#build-time-configuration
+  // We can also use something like this if we need entire *.env file usage:
+  //    https://github.com/zeit/next.js/blob/canary/examples/with-dotenv/next.config.js
+  env: {
+    ANALYTICS: process.env.RWD_WEB_ANALYTICS,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
