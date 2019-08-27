ca58c43b55da97a0548b6361b1f3bb8171aa2c9a
Satyavan Dash <satyavan.dash@publicissapient.com>
2019-08-27 17:34:54 +0530

    fix() - error message

--- packages/core/src/components/features/account/TrackOrder/styles/TrackOrderModal.native.style.js
+++ packages/core/src/components/features/account/TrackOrder/styles/TrackOrderModal.native.style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const TrackOrderModalHeader = styled.View`
+export const TrackOrderModalHeader = styled.View`
   display: flex;
   justify-content: center;
   align-items: center;
