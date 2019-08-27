7952a0dcc4cdc28f58388a3b12fc9fd73782ce5f
Harminder Pal Singh <harminderpal.singh@publicissapient.com>
2019-08-28 00:03:13 +0530

    updated slot creation design

--- packages/core/src/components/common/molecules/HomePageSlots/HomePageSlots.jsx
+++ packages/core/src/components/common/molecules/HomePageSlots/HomePageSlots.jsx
@@ -2,37 +2,26 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 
 const HomePageSlots = props => {
-  const { slotMatcher, modules } = props;
-  const slotKeys = Object.keys(props)
-    .filter(key => slotMatcher.test(key))
-    .sort();
-  const componentNameMap = modules.reduce((componentMap, { name: moduleName, component }) => {
-    // eslint-disable-next-line no-param-reassign
-    componentMap[moduleName] = component;
-    return componentMap;
-  }, {});
+  const { slots = [], modules, ...others } = props;
 
-  return slotKeys.map(key => {
-    const slotData = props[key];
-    const { name: slotModuleName } = slotData;
-    const Component = componentNameMap[slotModuleName];
-
-    return Component && <Component key={slotModuleName} {...slotData} />;
-  });
-};
-
-HomePageSlots.defaultProps = {
-  slotMatcher: /^slot_/,
+  return slots
+    .sort((a, b) => (b.name > a.name ? -1 : 1))
+    .map(slot => {
+      const Module = modules[slot.moduleName];
+      return Module && slot.data && <Module key={slot.contentId} {...slot.data} {...others} />;
+    });
 };
 
 HomePageSlots.propTypes = {
-  modules: PropTypes.arrayOf(
+  modules: PropTypes.shape({}).isRequired,
+  slots: PropTypes.arrayOf(
     PropTypes.shape({
+      contentId: PropTypes.string,
+      data: PropTypes.shape({}),
+      moduleName: PropTypes.string,
       name: PropTypes.string,
-      component: PropTypes.elementType,
     })
   ).isRequired,
-  slotMatcher: PropTypes.instanceOf(RegExp),
 };
 
 export default HomePageSlots;
