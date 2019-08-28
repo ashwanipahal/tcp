8792a34533ce954f6a6ffa3f961e72070e2033e1
Poorva <poorva.singh@publicissapient.com>
2019-08-28 12:11:42 +0530

    review comments incorporated

--- packages/core/styles/globalStyles/StyledText.js
+++ packages/core/styles/globalStyles/StyledText.js
@@ -1,20 +1,17 @@
 import styled, { css } from 'styled-components/native';
-// eslint-disable-next-line import/no-unresolved
-import { Platform } from 'react-native';
+import { isAndroid } from '../../src/utils/index.native';
 
 // This css add font logic to android platform for different type of font families
 const androidFontStyles = css`
   ${props =>
-    Platform.OS === `android` && props.fontWeight && props.fontFamily
+    isAndroid() && props.fontWeight && props.fontFamily
       ? `
      font-family: ${props.theme.typography.androidFonts[props.fontFamily]}_${props.fontWeight};
      font-weight: normal;
      `
       : ''};
   ${props =>
-    Platform.OS === `android` &&
-    (!props.fontWeight || props.fontWeight === 'regular') &&
-    props.fontFamily
+    isAndroid() && (!props.fontWeight || props.fontWeight === 'regular') && props.fontFamily
       ? `
       font-family: ${props.theme.typography.androidFonts[props.fontFamily]};
       font-weight: normal;
