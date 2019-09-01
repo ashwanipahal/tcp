7c5f3ebef616a55a5d580f619821495b32d4e87d
Himalaya <himalaya.batra@publicissapient.com>
2019-09-02 00:23:30 +0530

    removing commented code

--- packages/mobileapp/src/pages/home.jsx
+++ packages/mobileapp/src/pages/home.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { createStackNavigator } from 'react-navigation';
 import ProductListingPageContainer from '@tcp/core/src/components/features/browse/ProductListingPage';
 import BagPage from '@tcp/core/src/components/features/CnC/BagPage';
-// import Checkout from '@tcp/core/src/components/features/CnC/Checkout';
-// import PickupPage from '@tcp/core/src/components/features/CnC/Checkout/organisms/PickupPage';
-// import ShippingPage from '@tcp/core/src/components/features/CnC/Checkout/organisms/ShippingPage';
 import LoginPageContainer from '@tcp/core/src/components/features/account/LoginPage';
 import Home from '../components/features/content/HomePage';
 import account from '../components/features/account/account';
@@ -24,9 +21,6 @@ const SubHomeStack = createStackNavigator(
     ProductListingPageContainer,
     BagPage,
     LoginPageContainer,
-    // Checkout,
-    // PickupPage,
-    // ShippingPage,
   },
   {
     defaultNavigationOptions: {
