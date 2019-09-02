8a1056983a85623c9f557f8c9722a98f88f9dc1e
Prasoon <prasoon.singh@publicissapient.com>
2019-09-02 13:40:17 +0530

    update

--- packages/core/src/components/common/atoms/Button/Button.style.native.js
+++ packages/core/src/components/common/atoms/Button/Button.style.native.js
@@ -8,7 +8,7 @@ const getShape = props => {
   const { isGymboree } = theme;
 
   if (isGymboree && !noShape) {
-    return `border-radius: 25px;`;
+    return `border-radius: 25.5px;`;
   }
   return `
   null
@@ -17,12 +17,10 @@ const getShape = props => {
 
 const style = css`
   flex-direction: row;
-  width: auto;
   align-items: center;
   justify-content: center;
   min-height: 42px;
-  flex-grow: 0;
-  border: 1px solid ${props => props.theme.colorPalette.secondary.black};
+  border: 1px solid ${props => props.theme.colorPalette.gray[600]};
   ${props =>
     props.buttonVariation === 'fixed-width'
       ? `
@@ -37,13 +35,13 @@ const style = css`
    height: ${props.height};
    ${props.fill === 'RED' ? `  background: ${props.theme.colorPalette.secondary.dark};  ` : ''};
    ${props.fill === 'BLUE' ? ` background: ${props.theme.colorPalette.blue[700]};` : ''};
-   ${props.fill === 'BLACK' ? ` background: ${props.theme.colorPalette.black};` : ''};
+   ${props.fill === 'BLACK' ? ` background: ${props.theme.colorPalette.gray[700]};` : ''};
 
    ${
      props.fill === 'RED' ? `  border: 1px solid ${props.theme.colorPalette.secondary.dark};  ` : ''
    };
    ${props.fill === 'BLUE' ? ` border: 1px solid ${props.theme.colorPalette.blue[700]};` : ''};
-   ${props.fill === 'BLACK' ? ` border: 1px solid ${props.theme.colorPalette.black};` : ''};
+   ${props.fill === 'BLACK' ? ` border: 1px solid ${props.theme.colorPalette.gray[600]};` : ''};
    ${getShape(props)}
    `
       : ''};
@@ -68,14 +66,14 @@ export const CustomStyleText = styled(StyledText)`
   text-align: center;
   letter-spacing: 0.93px;
   opacity: ${props => (props.disableButton ? props.theme.opacity.opacity.medium : '1')};
-  font-size: ${props => props.theme.typography.fontSizes.fs14};
+  font-size: ${props => props.theme.typography.fontSizes.fs13};
   font-family: ${props => props.theme.typography.fonts.secondary};
   font-weight: ${props => props.theme.typography.fontWeights.extrabold};
 
   ${props =>
     props.buttonVariation === 'variable-width'
       ? `
-  color: ${props.theme.colorPalette[props.color || 'black']};
+  color: ${props.color || props.theme.colorPalette.gray[700]};
   padding: 12px 32px;
   `
       : ''};
@@ -83,7 +81,7 @@ export const CustomStyleText = styled(StyledText)`
   ${props =>
     props.buttonVariation === 'fixed-width'
       ? `
-  color: ${props.theme.colorPalette[props.color || 'black']};
+  color: ${props.color || props.theme.colorPalette.gray[700]};
   padding: 4px 20px;
   `
       : ''};
