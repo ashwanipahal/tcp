28e01306c61a0cd64e815dbf1e6413d450f1a71c
Vinit Kumar <vinit.k.85@gmail.com>
2019-09-02 15:31:15 +0530

    Added test cases

--- packages/core/src/components/features/account/AddEditCreditCard/container/AddEditCreditCard.reducer.js
+++ packages/core/src/components/features/account/AddEditCreditCard/container/AddEditCreditCard.reducer.js
@@ -4,7 +4,6 @@ import constants from './AddEditCreditCard.constants';
 const initialState = fromJS(null);
 
 const AddEditCreditCardReducer = (state = initialState, action) => {
-  console.log(action.payload);
   switch (action.type) {
     case constants.ADD_CREDIT_CARD_SUCCESS:
       return fromJS(action.payload);
