c1a7004ec3107490e34f0072c0aabfdcee6a1a1f
Himalaya <himalaya.batra@publicissapient.com>
2019-09-03 19:34:38 +0530

    RWD-7370: app crash for bagpage fixed

---
+++ packages/core/src/components/features/CnC/AddedToBagActions/views/AddedToBagActions.view.native.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import BodyCopy from '../../../../common/atoms/BodyCopy';
+// import OpenLoginModal from '../../../account/LoginPage/views/LoginModal.native';
+import {
+  ButtonWrapper,
+  ActionsWrapper,
+  ViewBagButton,
+  CheckoutButton,
+} from '../styles/AddedToBagActions.style.native';
+import CheckoutConstants from '../../Checkout/Checkout.constants';
+
+class AddedToBagActions extends React.PureComponent<Props> {
+  routeToCheckout = e => {
+    const { navigation, closeCheckoutModalMountState } = this.props;
+    if (e) {
+      e.preventDefault();
+    }
+    navigation.navigate(CheckoutConstants.CHECKOUT_PAGES_NAMES.CHECKOUT, {
+      nextToRoot: CheckoutConstants.CHECKOUT_PAGES_NAMES.PICKUP,
+    });
+    closeCheckoutModalMountState({ state: false });
+  };
+
+  render() {
+    const {
+      labels,
+      showAddTobag,
+      // checkoutModalMountedState,
+      // handleCartCheckout,
+      // navigation,
+      // closeCheckoutModalMountState,
+      isUserLoggedIn,
+    } = this.props;
+    return (
+      <ActionsWrapper>
+        {showAddTobag && (
+          <ButtonWrapper>
+            <ViewBagButton>
+              <BodyCopy
+                textTransform="uppercase"
+                color="white"
+                fontWeight="extrabold"
+                fontFamily="secondary"
+                fontSize="fs13"
+                text={labels.viewBag && labels.viewBag.toUpperCase()}
+              />
+            </ViewBagButton>
+          </ButtonWrapper>
+        )}
+        <ButtonWrapper>
+          <CheckoutButton>
+            {!isUserLoggedIn && (
+              <BodyCopy
+                color="white"
+                fontWeight="extrabold"
+                fontFamily="secondary"
+                fontSize="fs13"
+                text={labels.checkout && labels.checkout.toUpperCase()}
+                onPress={this.routeToCheckout}
+              />
+            )}
+
+            {/* {!isUserLoggedIn && (
+              <BodyCopy
+                color="white"
+                fontWeight="extrabold"
+                fontFamily="secondary"
+                fontSize="fs13"
+                text={labels.checkout && labels.checkout.toUpperCase()}
+                // onPress={handleCartCheckout}
+                onPress={() => {
+                  navigateToNestedRoute(navigation, 'HomeStack', 'Checkout');
+                }}
+              />
+            )} */}
+          </CheckoutButton>
+        </ButtonWrapper>
+        {/* <OpenLoginModal
+          variation="checkout"
+          openState={checkoutModalMountedState}
+          setLoginModalMountState={closeCheckoutModalMountState}
+          handleContinueAsGuest={this.routeToCheckout}
+          handleAfterLogin={this.routeToCheckout}
+        /> */}
+      </ActionsWrapper>
+    );
+  }
+}
+
+AddedToBagActions.propTypes = {
+  labels: PropTypes.shape.isRequired,
+  showAddTobag: PropTypes.shape,
+  navigation: PropTypes.shape({}).isRequired,
+};
+
+AddedToBagActions.defaultProps = {
+  showAddTobag: true,
+};
+
+export default AddedToBagActions;
