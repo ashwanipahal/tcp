578688c4add7337d6b6dfc732e9e04a5f7ea7cf2
sachin rawal <srawal.sapient@outlook.com>
2019-09-03 09:03:02 +0530

    code push

--- packages/core/src/components/features/account/LoginPage/views/LoginModal.jsx
+++ packages/core/src/components/features/account/LoginPage/views/LoginModal.jsx
@@ -50,7 +50,7 @@ class OpenLoginModal extends React.Component<Props> {
         overlayClassName="TCPModal__Overlay"
         className={`TCPModal__Content, ${className}`}
         maxWidth={variation === 'checkout' ? '616px' : '450px'}
-        minHeight="500px"
+        heightConfig={{ minHeight: '500px', height: '550px', maxHeight: '550px' }}
       >
         {component === 'login' ? (
           <LoginPageContainer
