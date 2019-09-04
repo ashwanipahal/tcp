a0211abb438a2fa4e86e00e77e3a7c83392442c7
Lalit guglani <lguglani@sapient.com>
2019-09-04 15:53:12 +0530

    Stack navigation fix

--- packages/mobileapp/src/navigation/AppNavigator.jsx
+++ packages/mobileapp/src/navigation/AppNavigator.jsx
@@ -1,5 +1,6 @@
 import {
   createAppContainer,
+  createStackNavigator,
   createSwitchNavigator,
   createBottomTabNavigator,
 } from 'react-navigation';
@@ -11,23 +12,43 @@ import AccountStack from '../pages/account';
 import WalletStack from '../pages/login';
 import BrandSwitchStack from '../pages/brandSwitch';
 import Header from '../components/common/molecules/Header';
+import CheckoutStack from '../pages/checkout';
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    HomeStack,
+    PlpStack,
+    BrandSwitchStack,
+    AccountStack,
+    WalletStack,
+  },
+  {
+    header: props => <Header {...props} />,
+    tabBarComponent: NavBar,
+  }
+);
+
+const RootStack = createStackNavigator(
+  {
+    Main: {
+      screen: TabNavigator,
+    },
+    Checkout: {
+      screen: CheckoutStack,
+    },
+  },
+  {
+    mode: 'modal',
+    headerMode: 'none',
+  }
+);
 
 export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    Main: createBottomTabNavigator(
-      {
-        HomeStack,
-        PlpStack,
-        BrandSwitchStack,
-        AccountStack,
-        WalletStack,
-      },
-      {
-        header: props => <Header {...props} />,
-        tabBarComponent: NavBar,
-      }
-    ),
+    Main: {
+      screen: RootStack,
+    },
   })
 );
