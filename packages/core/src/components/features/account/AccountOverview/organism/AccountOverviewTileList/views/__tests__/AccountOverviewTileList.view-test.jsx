114cc564e31d0a63fefa414c259a09aff1761603
naman jain <njain63@sapient.com>
2019-09-04 13:44:21 +0530

    fix RWD-7249 by setting gutter based on component index

--- packages/core/src/components/features/account/AccountOverview/organism/AccountOverviewTileList/views/__tests__/AccountOverviewTileList.view-test.jsx
+++ packages/core/src/components/features/account/AccountOverview/organism/AccountOverviewTileList/views/__tests__/AccountOverviewTileList.view-test.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { AccountOverviewTileList } from '../AccountOverviewTileList.view';
+import {
+  AccountOverviewTileList,
+  COMPONENTS_US,
+  COMPONENTS_CA,
+} from '../AccountOverviewTileList.view';
+import { isCanada } from '../../../../../../../../utils';
+import { Col } from '../../../../../../../common/atoms';
+
+jest.mock('../../../../../../../../utils', () => {
+  const originalModule = jest.requireActual('../../../../../../../../utils');
+  return {
+    __esModule: true,
+    ...originalModule,
+    isCanada: jest.fn(() => false),
+  };
+});
 
 describe('AccountOverviewTileList component', () => {
   it('should render correctly', () => {
     const component = shallow(<AccountOverviewTileList />);
     expect(component).toMatchSnapshot();
+  });
+
+  it('should render components as per the US component list', () => {
+    const component = shallow(<AccountOverviewTileList />);
+    expect(component.find(Col)).toHaveLength(COMPONENTS_US.length);
+  });
+
+  it('should render components as per the CA component list', () => {
+    isCanada.mockImplementationOnce(() => true);
+    const component = shallow(<AccountOverviewTileList />);
+    expect(component.find(Col)).toHaveLength(COMPONENTS_CA.length);
   });
 });
