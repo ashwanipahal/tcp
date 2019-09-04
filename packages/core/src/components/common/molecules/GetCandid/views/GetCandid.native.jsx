b5870a097eb650b8c71ca682af376a8d3cfc5596
Raghav <raghasb@gmail.com>
2019-09-04 16:28:52 +0530

    implemented getCandid APP

--- packages/core/src/components/common/molecules/GetCandid/views/GetCandid.native.jsx
+++ packages/core/src/components/common/molecules/GetCandid/views/GetCandid.native.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import { FlatList } from 'react-native';
-import { Wrapper, ImageGridItem, Touchable } from '../styles/GetCandid.style.native';
+
+import Anchor from '../../../atoms/Anchor';
+import BodyCopy from '../../../atoms/BodyCopy';
+import {
+  ImageGridItem,
+  ImageWrapper,
+  Title,
+  Touchable,
+  Wrapper,
+} from '../styles/GetCandid.style.native';
 
 class GetCandid extends React.PureComponent {
   static propTypes = {
@@ -38,25 +47,67 @@ class GetCandid extends React.PureComponent {
     const image = LowResolution;
     return (
       <Touchable accessibilityRole="image">
-        <ImageGridItem key={index.toString()} url={image.Url} height="103px" width="103px" />
+        <ImageGridItem
+          key={index.toString()}
+          index={index}
+          url={image.Url}
+          height="103px"
+          width="103px"
+        />
       </Touchable>
     );
   };
 
   render() {
-    const { candidData } = this.props;
+    const { candidData, labels } = this.props;
     const data = candidData && candidData.Views;
+
     return (
-      data && (
-        <Wrapper>
-          <FlatList
-            numColumns={3}
-            data={data}
-            keyExtractor={this.keyExtractor}
-            renderItem={this.renderItem}
+      <Wrapper>
+        {labels && (
+          <>
+            <Title
+              mobileFontFamily="primary"
+              fontSize="fs20"
+              fontWeight="semibold"
+              color="gray.900"
+              marginBottom="12px"
+              text={labels.title}
+              textAlign="center"
+              letterSpacing="ls167"
+            />
+            <BodyCopy
+              mobileFontFamily="primary"
+              fontSize="fs14"
+              fontWeight="regular"
+              color="gray.900"
+              text={labels.titleDescription}
+              textAlign="center"
+            />
+          </>
+        )}
+        {data && (
+          <ImageWrapper>
+            <FlatList
+              numColumns={3}
+              data={data.slice(0, 9)}
+              keyExtractor={this.keyExtractor}
+              renderItem={this.renderItem}
+            />
+          </ImageWrapper>
+        )}
+        {labels && (
+          <Anchor
+            fontFamily="secondary"
+            color="gray.900"
+            anchorVariation="primary"
+            noLink
+            dataLocator=""
+            text={labels.btnSeeMore}
+            visible
           />
-        </Wrapper>
-      )
+        )}
+      </Wrapper>
     );
   }
 }
@@ -66,6 +117,12 @@ GetCandid.defaultProps = {
   candidData: {
     Settings: {},
     Views: [],
+    Tags: [],
+  },
+  labels: {
+    title: 'title',
+    titleDescripton: 'titleDescripton',
+    btnSeeMore: 'btnSeeMore',
   },
   fetchCandidData: () => {},
 };
@@ -74,6 +131,7 @@ GetCandid.propTypes = {
   apiConfig: PropTypes.shape({}),
   candidData: PropTypes.shape({}),
   fetchCandidData: PropTypes.func,
+  labels: PropTypes.shape({}),
 };
 
 export default GetCandid;
