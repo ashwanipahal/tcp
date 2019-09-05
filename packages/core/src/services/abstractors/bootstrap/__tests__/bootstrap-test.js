7eb42eeb7011e949fd622170a0b21f78dfa88423
Abhiraj <aku332@sapient.com>
2019-09-05 18:43:37 +0530

    test cases completion

--- packages/core/src/services/abstractors/bootstrap/__tests__/bootstrap-test.js
+++ packages/core/src/services/abstractors/bootstrap/__tests__/bootstrap-test.js
@@ -9,23 +9,35 @@ import LabelsAbstractor from '../labels';
 jest.mock('../layout/layout');
 jest.mock('../../../handler/handler');
 
-it('abstractor - bootstrap', () => {
-  return bootstrap(['homepage'], null, {}).then(data => {
-    expect(data.homepage.items[0].layout.slots[0]).toHaveProperty(
-      'name',
-      'moduleName',
-      'contentId'
-    );
-    expect(data.homepage.items[0].layout.slots[1]).toHaveProperty(
-      'name',
-      'moduleName',
-      'contentId'
-    );
-    expect(data.homepage.items[0].layout.slots[0].moduleName).toEqual('moduleD');
-    expect(data.homepage.items[0].layout.slots[1].moduleName).toEqual('moduleH');
-    expect(data.labels).toMatchObject(LabelsAbstractor.processData(labelsMock));
-    expect(data.header).toMatchObject(HeaderAbstractor.processData(headerMock));
-    expect(data.footer).toMatchObject(FooterAbstractor.processData(footerMock));
+describe('abstractor - bootstrap', () => {
+  const abstractorBootstrap = () =>
+    bootstrap(['homepage'], null, {}).then(data => {
+      expect(data.homepage.items[0].layout.slots[0]).toHaveProperty(
+        'name',
+        'moduleName',
+        'contentId'
+      );
+      expect(data.homepage.items[0].layout.slots[1]).toHaveProperty(
+        'name',
+        'moduleName',
+        'contentId'
+      );
+      expect(data.homepage.items[0].layout.slots[0].moduleName).toEqual('moduleD');
+      expect(data.homepage.items[0].layout.slots[1].moduleName).toEqual('moduleH');
+      expect(data.labels).toMatchObject(LabelsAbstractor.processData(labelsMock));
+      expect(data.header).toMatchObject(HeaderAbstractor.processData(headerMock));
+      expect(data.footer).toMatchObject(FooterAbstractor.processData(footerMock));
+    });
+
+  it('bootstrap - redis disabled', () => {
+    return abstractorBootstrap();
+  });
+
+  it('bootstrap - redis enabled', () => {
+    global.redisClient = {
+      connected: true,
+    };
+    return abstractorBootstrap();
   });
 });
 
