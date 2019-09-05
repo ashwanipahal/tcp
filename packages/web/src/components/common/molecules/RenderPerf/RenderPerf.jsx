8d52bfb0d25ba4ff18d85a6c1fb7c8063f7bf096
Erik Jung <erik.jung@sapientrazorfish.com>
2019-09-05 09:44:48 -0400

    RWD-2558 | Combine Perf and RenderPerf components

--- packages/web/src/components/common/molecules/RenderPerf/RenderPerf.jsx
+++ packages/web/src/components/common/molecules/RenderPerf/RenderPerf.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
+import Safe from 'react-safe';
+import { string } from 'prop-types';
 import ServerOnly from '../../atoms/ServerOnly';
-import Perf from '../../atoms/Perf';
 
-export function Mark(props) {
+export function Mark({ name }) {
   return process.env.RWD_WEB_PERF_TIMING ? (
     <ServerOnly>
-      <Perf.Mark {...props} />
+      <Safe.script>{`performance.mark("${name}")`}</Safe.script>
     </ServerOnly>
   ) : null;
 }
 
-export function Measure(props) {
+Mark.propTypes = {
+  name: string.isRequired,
+};
+
+export function Measure({ name, start, end }) {
+  const startMark = start ? `"${start}"` : undefined;
+  const endMark = end ? `"${end}"` : undefined;
   return process.env.RWD_WEB_PERF_TIMING ? (
     <ServerOnly>
-      <Perf.Measure {...props} />
+      <Safe.script>{`performance.measure("${name}", ${startMark}, ${endMark})`}</Safe.script>
     </ServerOnly>
   ) : null;
 }
+
+Measure.propTypes = {
+  name: string.isRequired,
+  start: string,
+  end: string,
+};
+
+Measure.defaultProps = {
+  start: '',
+  end: '',
+};
 
 export default {
   Mark,
