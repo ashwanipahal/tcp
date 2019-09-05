5c8912c44a59b469c2008bf696e7baaa17844430
furkan <furkan.ali@publicissapient.com>
2019-09-05 10:01:10 +0530

    bug fixed

--- packages/core/src/components/features/account/common/molecule/CouponIcon/styles/CouponIcon.style.native.js
+++ packages/core/src/components/features/account/common/molecule/CouponIcon/styles/CouponIcon.style.native.js
@@ -6,10 +6,18 @@ const IconText = styled.View`
   align-items: center;
   height: ${props => props.theme.spacing.ELEM_SPACING.XXL};
   padding: ${props => props.theme.spacing.ELEM_SPACING.XS};
-  background-color: ${props =>
+  ${props =>
     props.type === COUPON_REDEMPTION_TYPE.PLACECASH
-      ? props.theme.colorPalette.green[300]
-      : props.theme.colorPalette.orange[800]};
+      ? ` background: ${props.theme.colorPalette.green[300]};`
+      : ''};
+  ${props =>
+    props.type === COUPON_REDEMPTION_TYPE.SAVING
+      ? ` background: ${props.theme.colorPalette.purple};`
+      : ''};
+  ${props =>
+    props.type === COUPON_REDEMPTION_TYPE.REWARDS
+      ? ` background: ${props.theme.colorPalette.orange[800]};`
+      : ''};
 `;
 
 export default IconText;
