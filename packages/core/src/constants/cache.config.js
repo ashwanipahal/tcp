222455b99500a370de988cd01f5c9a5a5adcf391
Abhiraj <aku332@sapient.com>
2019-09-05 18:59:15 +0530

    todo - comment added

--- packages/core/src/constants/cache.config.js
+++ packages/core/src/constants/cache.config.js
@@ -1,6 +1,7 @@
 import { LABELS } from '../reduxStore/constants';
 
 const CACHED_KEYS = {
+  // @TODO: replace LABELS.labels with LABELS.global after global label separation
   [`${LABELS.labels}:${LABELS.global}`]: LABELS.labels,
 };
 
