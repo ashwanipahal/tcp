acc6e96295a480298fe579598d54ace8e23ebaac
furkan <furkan.ali@publicissapient.com>
2019-09-07 11:25:12 +0530

    working progress

--- packages/core/src/components/features/account/MyProfile/organism/PersonalInformation/views/PersonalInformation.view.native.jsx
+++ packages/core/src/components/features/account/MyProfile/organism/PersonalInformation/views/PersonalInformation.view.native.jsx
@@ -1,51 +1,80 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ModalNative from '@tcp/core/src/components/common/molecules/Modal';
+import CreateAccount from '@tcp/core/src/components/features/account/CreateAccount';
 import PersonalInformationDisplay from '../../../molecules/PersonalInformationDisplay';
 import Address from '../../../../../../common/molecules/Address';
 
 import MyProfileTile from '../../../../../../common/molecules/MyProfileTile';
 
-export const PersonalInformation = ({
-  labels,
-  handleComponentChange,
-  profileInfoTile,
-  userEmail,
-  userBirthday,
-  userFullName,
-  userPhoneNumber,
-  airMiles,
-  myPlaceNumber,
-}) => {
-  const { address } = profileInfoTile;
-  return (
-    <>
-      <MyProfileTile
-        title={labels.lbl_profile_personal_information}
-        ctaTitle={labels.lbl_profile_edit_personal_info}
-        handleComponentChange={() => handleComponentChange('accountOverviewMobile')}
-      >
-        <PersonalInformationDisplay
-          labels={labels}
-          userEmail={userEmail}
-          userBirthday={userBirthday}
-          userFullName={userFullName}
-          userPhoneNumber={userPhoneNumber}
-          airMiles={airMiles}
-          myPlaceNumber={myPlaceNumber}
-        />
-      </MyProfileTile>
-      {address && address.isComplete && (
+
+class PersonalInformation extends React.PureComponent{
+  constructor() {
+    super();
+    this.state = { isOpenBool: false };
+  }
+
+  openModal = () => {
+    const { isOpenBool } = this.state;
+    this.setState({
+      isOpenBool: !isOpenBool,
+    });
+  };
+
+
+  render(){
+    const {
+      labels,
+      handleComponentChange,
+      profileInfoTile,
+      userEmail,
+      userBirthday,
+      userFullName,
+      userPhoneNumber,
+      airMiles,
+      myPlaceNumber,
+    } = this.props;
+
+    const { address } = profileInfoTile;
+    const {isOpenBool} = this.state;
+    return (
+      <>
         <MyProfileTile
-          title={labels.lbl_profile_mailing_address}
-          ctaTitle={labels.lbl_profile_edit_mailing_info}
-          handleComponentChange={() => handleComponentChange('accountOverviewMobile')}
+          title={labels.lbl_profile_personal_information}
+          ctaTitle={labels.lbl_profile_edit_personal_info}
+          handleComponentChange={this.openModal}
         >
-          <Address address={address} dataLocatorPrefix="profileinfo-editmailing" showName={false} />
+          <PersonalInformationDisplay
+            labels={labels}
+            userEmail={userEmail}
+            userBirthday={userBirthday}
+            userFullName={userFullName}
+            userPhoneNumber={userPhoneNumber}
+            airMiles={airMiles}
+            myPlaceNumber={myPlaceNumber}
+          />
         </MyProfileTile>
-      )}
-    </>
-  );
-};
+        {address && address.isComplete && (
+          <MyProfileTile
+            title={labels.lbl_profile_mailing_address}
+            ctaTitle={labels.lbl_profile_edit_mailing_info}
+            handleComponentChange={() => handleComponentChange('accountOverviewMobile')}
+          >
+            <Address address={address} dataLocatorPrefix="profileinfo-editmailing" showName={false} />
+          </MyProfileTile>
+        )}
+
+        <ModalNative
+          isOpen={isOpenBool}
+          onRequestClose={this.openModal}
+          heading={labels.lbl_profile_personal_information}
+        >
+          <CreateAccount />
+        </ModalNative>
+      </>
+    );
+  }
+}
 
 PersonalInformation.propTypes = {
   labels: PropTypes.shape({
