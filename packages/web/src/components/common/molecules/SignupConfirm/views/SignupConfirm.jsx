369ea845b04f746017498267cee2d219de24d3bb
Satyavan Dash <satyavan.dash@publicissapient.com>
2019-09-09 15:22:51 +0530

    fix - review comments

--- packages/web/src/components/common/molecules/SignupConfirm/views/SignupConfirm.jsx
+++ packages/web/src/components/common/molecules/SignupConfirm/views/SignupConfirm.jsx
@@ -6,68 +6,70 @@ import withStyles from '@tcp/core/src/components/common/hoc/withStyles';
 import { isGymboree, getIconPath } from '@tcp/core/src/utils';
 import SignupConfirmStyle from '../SignupConfirm.style';
 
-const emailIcon = `dot-email-${isGymboree() ? 'orange' : 'blue'}`;
-const chatIcon = `chat${isGymboree() ? '-orange' : '-blue'}`;
+const SignupConfirm = ({ className, formViewConfig, susbscriptionType }) => {
+  const emailIcon = `dot-email-${isGymboree() ? 'orange' : 'blue'}`;
+  const chatIcon = `chat${isGymboree() ? '-orange' : '-blue'}`;
 
-const SignupConfirm = ({ className, formViewConfig, susbscriptionType }) => (
-  <div className={className} id="sign-up-modal-confirm-view">
-    <BodyCopy
-      fontSize={['fs28', 'fs28', 'fs38']}
-      fontFamily="primary"
-      fontWeight="black"
-      textAlign="center"
-      className="thank-you__label"
-      data-locator={`${susbscriptionType}_thank_you_message`}
-    >
-      {formViewConfig.thankYouTextLabel}
-    </BodyCopy>
-    <BodyCopy
-      fontSize={['fs18', 'fs18', 'fs22']}
-      fontFamily="secondary"
-      textAlign="center"
-      className="confirmation-label"
-      data-locator={`${susbscriptionType}_copy_text_01`}
-    >
-      {formViewConfig.joiningTextLabel}
-    </BodyCopy>
-    <Image
-      src={susbscriptionType === 'email' ? getIconPath(emailIcon) : getIconPath(chatIcon)}
-      alt={`${susbscriptionType}-icon`}
-      className="confirmation-image"
-      aria-hidden="true"
-      data-locator={susbscriptionType === 'email' ? 'e-mail_icon' : 'sms_icon'}
-    />
-    <BodyCopy
-      fontSize={['fs22', 'fs22', 'fs28']}
-      fontFamily="secondary"
-      textAlign="center"
-      fontWeight="semibold"
-      color="primary.dark"
-      className="first-label"
-      data-locator={`${susbscriptionType}_copy_text_02`}
-    >
-      {formViewConfig.confirmationMsgReceiveLabel}
-    </BodyCopy>
-    <BodyCopy
-      fontSize={['fs16', 'f16', 'fs18']}
-      fontFamily="secondary"
-      textAlign="center"
-      className="redeem-label"
-      data-locator={`${susbscriptionType}_copy_text_03`}
-    >
-      {formViewConfig.extraMessageLabel}
-    </BodyCopy>
-    <BodyCopy
-      fontSize="fs10"
-      fontFamily="secondary"
-      textAlign="center"
-      className="tnc-label"
-      data-locator={`${susbscriptionType}_copy_text_04`}
-    >
-      {formViewConfig.footerTextLabel}
-    </BodyCopy>
-  </div>
-);
+  return (
+    <div className={className} id="sign-up-modal-confirm-view">
+      <BodyCopy
+        fontSize={['fs28', 'fs28', 'fs38']}
+        fontFamily="primary"
+        fontWeight="black"
+        textAlign="center"
+        className="thank-you__label"
+        data-locator={`${susbscriptionType}_thank_you_message`}
+      >
+        {formViewConfig.thankYouTextLabel}
+      </BodyCopy>
+      <BodyCopy
+        fontSize={['fs18', 'fs18', 'fs22']}
+        fontFamily="secondary"
+        textAlign="center"
+        className="confirmation-label"
+        data-locator={`${susbscriptionType}_copy_text_01`}
+      >
+        {formViewConfig.joiningTextLabel}
+      </BodyCopy>
+      <Image
+        src={susbscriptionType === 'email' ? getIconPath(emailIcon) : getIconPath(chatIcon)}
+        alt={`${susbscriptionType}-icon`}
+        className="confirmation-image"
+        aria-hidden="true"
+        data-locator={susbscriptionType === 'email' ? 'e-mail_icon' : 'sms_icon'}
+      />
+      <BodyCopy
+        fontSize={['fs22', 'fs22', 'fs28']}
+        fontFamily="secondary"
+        textAlign="center"
+        fontWeight="semibold"
+        color="primary.dark"
+        className="first-label"
+        data-locator={`${susbscriptionType}_copy_text_02`}
+      >
+        {formViewConfig.confirmationMsgReceiveLabel}
+      </BodyCopy>
+      <BodyCopy
+        fontSize={['fs16', 'f16', 'fs18']}
+        fontFamily="secondary"
+        textAlign="center"
+        className="redeem-label"
+        data-locator={`${susbscriptionType}_copy_text_03`}
+      >
+        {formViewConfig.extraMessageLabel}
+      </BodyCopy>
+      <BodyCopy
+        fontSize="fs10"
+        fontFamily="secondary"
+        textAlign="center"
+        className="tnc-label"
+        data-locator={`${susbscriptionType}_copy_text_04`}
+      >
+        {formViewConfig.footerTextLabel}
+      </BodyCopy>
+    </div>
+  );
+};
 
 SignupConfirm.propTypes = {
   className: PropTypes.string,
