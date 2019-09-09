29391a48047ef8285e33eddcb11e8a5b23c8e86e
Vinit Kumar <vinit.k.85@gmail.com>
2019-09-09 12:01:25 +0530

    view coupon details app

--- packages/core/src/components/features/CnC/common/organism/CouponAndPromos/styles/CouponDetailModal.style.native.js
+++ packages/core/src/components/features/CnC/common/organism/CouponAndPromos/styles/CouponDetailModal.style.native.js
@@ -1,2 +1,27 @@
+import styled from 'styled-components/native';
+
 const ModalHeaderStyle = { maxHeight: 50 };
-export default ModalHeaderStyle;
+
+const StyledModalWrapper = styled.View`
+  align-items: center;
+  margin: ${props => props.theme.spacing.ELEM_SPACING.LRG};
+`;
+
+const Horizontal = styled.View`
+  height: 1px;
+  background-color: ${props => props.theme.colors.PRIMARY.LIGHTGRAY};
+  margin: ${props => props.theme.spacing.ELEM_SPACING.LRG} 0;
+  flex-direction: row;
+  width: 100%;
+`;
+
+const PrivacyContent = styled.View`
+  text-align: left;
+  justify-content: flex-start;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-top: ${props => props.theme.spacing.ELEM_SPACING.XL};
+  margin-bottom: ${props => props.theme.spacing.ELEM_SPACING.SM};
+`;
+
+export { StyledModalWrapper, Horizontal, PrivacyContent, ModalHeaderStyle };
