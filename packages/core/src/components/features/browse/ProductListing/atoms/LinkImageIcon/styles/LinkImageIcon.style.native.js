5440f1d846e2ed94135ff4447de81ec091302582
“Vijay <vijdadhi@publicisgroupe.net>
2019-09-09 14:39:29 +0530

    fixed android color switch border issue

--- packages/core/src/components/features/browse/ProductListing/atoms/LinkImageIcon/styles/LinkImageIcon.style.native.js
+++ packages/core/src/components/features/browse/ProductListing/atoms/LinkImageIcon/styles/LinkImageIcon.style.native.js
@@ -1,21 +1,36 @@
 import styled from 'styled-components/native';
 
 const getImageStyle = props => {
-  const { theme, selected, width, height, resizeMode, borderWidth, borderRadius } = props;
-  const { colorPalette } = theme;
-  const borderColor = selected ? colorPalette.gray[900] : colorPalette.gray[1100];
+  const { width, height, resizeMode, borderRadius } = props;
   return `
     width: ${width};
     height: ${height};
     resize-mode: ${resizeMode};
-    border-color: ${borderColor};
-    border-width: ${borderWidth};
+    border-width: 0;
     border-radius: ${borderRadius};
   `;
 };
+const getImageBorderStyle = props => {
+  const { theme, selected, width, height, borderWidth, borderRadius } = props;
+  const { colorPalette } = theme;
+  const borderColor = selected ? colorPalette.gray[900] : colorPalette.gray[1100];
+  return `
+    align-items: center;
+    justify-content: center;
+    width: ${width + 1};
+    height: ${height + 1};
+    border-color: ${borderColor};
+    border-width: ${borderWidth};
+    border-radius: ${borderRadius + 1};
+  `;
+};
+
+const ImageTouchableOpacity = styled.TouchableOpacity`
+  ${getImageBorderStyle}
+`;
 
 const ImageComp = styled.Image`
   ${getImageStyle}
 `;
 
-export default ImageComp;
+export { ImageComp, ImageTouchableOpacity };
