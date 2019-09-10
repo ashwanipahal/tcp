1e09ff48618f369c4873e6d22e189f2f0e7d9b2d
Ayush Kumar <ayush.kumar@publicissapient.com>
2019-09-10 23:13:20 +0530

    RWD-2216 dropdown changes

--- packages/core/src/components/features/CnC/Checkout/organisms/ShippingPage/molecules/GiftServices/styles/GiftServices.style.js
+++ packages/core/src/components/features/CnC/Checkout/organisms/ShippingPage/molecules/GiftServices/styles/GiftServices.style.js
@@ -16,12 +16,16 @@ export default css`
     border: 1px solid;
     height: 146px;
     padding: 0;
+    width: 99%;
   }
   .addReceipt {
     padding-top: 10px;
     margin-left: 35px;
   }
   .giftServicesContainer {
+    p {
+      width: 100%;
+    }
     margin-left: 35px;
   }
   .messageTextWrapper {
@@ -35,18 +39,16 @@ export default css`
     display: inline-block;
     width: 142px;
   }
-  .dropdownDivOverFlow {
-    width: 100%;
-  }
+
   .dropdownliBottomBorder {
-    width: 97%;
     height: 71px;
     cursor: pointer;
-    padding-top: 0px;
+    padding-top: 2px;
     padding-bottom: 0px;
   }
   .price {
     float: right;
+    margin-right: 5px;
   }
   .longDesc {
     font-size: 12px;
@@ -57,18 +59,58 @@ export default css`
     padding-top: 3px;
   }
 
-  .custom-select {
-    width: 100%;
+  .phone-field-wrapper {
+    width: auto;
+    margin-left: -12px;
+    padding-right: 15px;
   }
 
-  .dropdownUlBorder {
-    width: 100%;
+  .normal-select-box {
+    padding-top: 5px;
   }
+
+  .custom-select {
+    width: 100%;
+    font-family: ${props => props.theme.fonts.secondaryFontFamily};
+    > span {
+      display: none;
+    }
+  }
+
   .giftServicesModal {
     @media ${props => props.theme.mediaQuery.large} {
       height: 90%;
     }
   }
+
+  .customSelectTitle {
+    font-family: ${props => props.theme.fonts.secondaryFontFamily};
+    font-size: ${props => props.theme.fonts.fontSize.body.bodytext.copy3}px;
+  }
+
+  .dropdownDivOverFlow {
+    div {
+      font-family: ${props => props.theme.fonts.secondaryFontFamily};
+    }
+    .dropdownUlBorder {
+      width: auto;
+      border: 1px solid ${props => props.theme.colors.PRIMARY.DARK};
+    }
+    .dropdownActiveClass {
+      background-color: ${props => props.theme.colors.PRIMARY.DARK};
+      div {
+        color: ${props => props.theme.colors.WHITE};
+        font-family: ${props => props.theme.fonts.secondaryFontFamily};
+      }
+      span {
+        font-family: ${props => props.theme.fonts.secondaryFontFamily};
+      }
+      .dropdownActiveIcon {
+        display: none;
+      }
+    }
+  }
+
   .gymImage {
     @media ${props => props.theme.mediaQuery.small} {
       margin-top: 8px;
