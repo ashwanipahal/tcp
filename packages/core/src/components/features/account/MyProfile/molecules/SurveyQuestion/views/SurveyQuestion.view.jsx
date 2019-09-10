f31c704265d196f4d2c9e8fc8b4329941be7ec90
Gagan Bhardwaj <gbhardwaj4@sapient.com>
2019-09-10 11:58:38 -0400

    RWD-7890: Option select and scroll view for android issue

--- packages/core/src/components/features/account/MyProfile/molecules/SurveyQuestion/views/SurveyQuestion.view.jsx
+++ packages/core/src/components/features/account/MyProfile/molecules/SurveyQuestion/views/SurveyQuestion.view.jsx
@@ -3,7 +3,8 @@ import withStyles from '@tcp/core/src/components/common/hoc/withStyles';
 import PropTypes from 'prop-types';
 import { BodyCopy, Row } from '@tcp/core/src/components/common/atoms';
 import TileOption from '@tcp/core/src/components/features/account/MyProfile/atoms/TileOption';
-
+import { getLabelValue } from '@tcp/core/src/utils/utils';
+import constants from '@tcp/core/src/components/features/account/MyProfile/molecules/AboutYouSurvey/container/AboutYouSurvey.utils';
 import styles from '../styles/SurveyQuestion.style';
 
 export const SurveyQuestion = ({
@@ -35,7 +36,9 @@ export const SurveyQuestion = ({
           fontFamily="secondary"
           className="question-text"
         >
-          {labels.lbl_profile_survey_select_one}
+          {questionId === constants.QUESTION1
+            ? getLabelValue(labels, 'lbl_profile_survey_select_one')
+            : getLabelValue(labels, 'lbl_profile_survey_select_all')}
         </BodyCopy>
       </Row>
       <div className="options__container">
