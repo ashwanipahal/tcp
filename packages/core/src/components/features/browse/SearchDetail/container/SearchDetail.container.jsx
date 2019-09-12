2ccdcd0610bb23eef78533be6a7cf96bfba6650b
Siddhartha Nanda <sidnanda@publicisgroupe.net>
2019-09-12 21:45:27 +0530

    fetching data from query string to display searched products

--- packages/core/src/components/features/browse/SearchDetail/container/SearchDetail.container.jsx
+++ packages/core/src/components/features/browse/SearchDetail/container/SearchDetail.container.jsx
@@ -1,21 +1,56 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'next/router'; //eslint-disable-line
+import { PropTypes } from 'prop-types';
 import SearchDetail from '../views/SearchDetail.view';
+import getSearchedResult from './SearchDetail.selectors';
+import { getSearchResult } from '../../../../../../../web/src/components/features/content/Header/molecules/SearchBar/SearchBar.actions';
 
 class SearchDetailContainer extends React.PureComponent {
+  componentDidMount() {
+    const {
+      router: {
+        query: { sq },
+      },
+      searchResult,
+    } = this.props;
+    searchResult(sq);
+  }
+
   render() {
-    return <SearchDetail />;
+    const { searchedResult } = this.props;
+    return <SearchDetail searchedResult={searchedResult} />;
   }
 }
 
-function mapStateToProps() {}
+function mapStateToProps(state) {
+  return {
+    searchedResult: getSearchedResult(state),
+  };
+}
 
-function mapDispatchToProps() {}
+function mapDispatchToProps(dispatch) {
+  return {
+    searchResult: sq => {
+      dispatch(getSearchResult(sq));
+    },
+  };
+}
 
-SearchDetailContainer.propTypes = {};
+SearchDetailContainer.propTypes = {
+  router: PropTypes.shape({
+    query: PropTypes.shape({
+      sq: PropTypes.string,
+    }),
+  }).isRequired,
+  searchResult: PropTypes.string,
+  searchedResult: PropTypes.arrayOf(PropTypes.shape({})),
+};
 
-SearchDetailContainer.defaultProps = {};
+SearchDetailContainer.defaultProps = {
+  searchResult: '',
+  searchedResult: {},
+};
 
 export default withRouter(
   connect(
--- packages/core/src/components/features/browse/SearchDetail/views/SearchDetail.view.jsx
+++ packages/core/src/components/features/browse/SearchDetail/views/SearchDetail.view.jsx
@@ -4,109 +4,67 @@ import withStyles from '../../../../common/hoc/withStyles';
 import SearchListingStyle from '../SearchDetail.style';
 import { Row, Col } from '../../../../common/atoms';
 
-let productDetails = [];
-productDetails = [
-  {
-    id: 70,
-    color: 'red',
-    size: 'M',
-    product: 'polo',
-  },
-  {
-    id: 10,
-    color: 'blue',
-    size: 'M',
-    product: 'jeans',
-  },
-  {
-    id: 30,
-    color: 'green',
-    size: 'M',
-    product: 'top',
-  },
-  {
-    id: 40,
-    color: 'pink',
-    size: 'M',
-    product: 'jeggings',
-  },
-  {
-    id: 60,
-    color: 'red',
-    size: 'S',
-    product: 'jeans',
-  },
-  {
-    id: 20,
-    color: 'tan',
-    size: 'M',
-    product: 'top',
-  },
-  {
-    id: 40,
-    color: 'orange',
-    size: 'L',
-    product: 'polo',
-  },
-  {
-    id: 30,
-    color: 'red',
-    size: 'L',
-    product: 'jeggings',
-  },
-];
-
-const searchProductsList = productDetails.map(item => {
-  return (
-    <div className="product-tile">
-      <div>
-        <p>{item.product}</p>
-        <p>{item.id}</p>
-        <p>{item.color}</p>
-        <p>{item.size}</p>
+class SearchListingView extends React.PureComponent {
+  render() {
+    const searchProductsList = searchedResult => {
+      return (
+        searchedResult &&
+        searchedResult.map(item => {
+          return (
+            <div className="product-tile">
+              <div>
+                <img src={item.imageUrl} alt={item.product_name} />
+                {item.min_list_price && <p>{`$ ${item.min_list_price}`}</p>}
+                <p>{item.id}</p>
+                <p>{item.color}</p>
+                <p>{item.product_name}</p>
+              </div>
+            </div>
+          );
+        })
+      );
+    };
+    const { className, searchedResult } = this.props;
+    return (
+      <div className={className}>
+        <Row className="placeholder">
+          <Col colSize={{ small: 6, medium: 8, large: 12 }}>
+            <div className="promo-area-1">You Searched For</div>
+            {/* {breadCrumbs && <FixedBreadCrumbs crumbs={breadCrumbs} separationChar=">" />} */}
+          </Col>
+        </Row>
+        <Row className="placeholder">
+          <Col colSize={{ small: 3, medium: 4, large: 6 }}>
+            <div className="filter">FILTERS</div>
+          </Col>
+          <Col colSize={{ small: 3, medium: 4, large: 6 }}>
+            <div className="sort-by">SORT BY</div>
+          </Col>
+        </Row>
+        <Row className="placeholder">
+          <Col colSize={{ small: 6, medium: 8, large: 12 }}>
+            <div className="showing-results">Showing X Results</div>
+          </Col>
+        </Row>
+        <Row className="placeholder">
+          <Col colSize={{ small: 6, medium: 8, large: 12 }}>
+            <div className="search-list-page">Search Listing Page</div>
+          </Col>
+        </Row>
+        <div className="product-tile-wrapper">{searchProductsList(searchedResult)}</div>
       </div>
-    </div>
-  );
-});
-
-const SearchListingView = ({ className }) => {
-  return (
-    <div className={className}>
-      <Row className="placeholder">
-        <Col colSize={{ small: 6, medium: 8, large: 12 }}>
-          <div className="promo-area-1">You Searched For</div>
-          {/* {breadCrumbs && <FixedBreadCrumbs crumbs={breadCrumbs} separationChar=">" />} */}
-        </Col>
-      </Row>
-      <Row className="placeholder">
-        <Col colSize={{ small: 3, medium: 4, large: 6 }}>
-          <div className="filter">FILTERS</div>
-        </Col>
-        <Col colSize={{ small: 3, medium: 4, large: 6 }}>
-          <div className="sort-by">SORT BY</div>
-        </Col>
-      </Row>
-      <Row className="placeholder">
-        <Col colSize={{ small: 6, medium: 8, large: 12 }}>
-          <div className="showing-results">Showing X Results</div>
-        </Col>
-      </Row>
-      <Row className="placeholder">
-        <Col colSize={{ small: 6, medium: 8, large: 12 }}>
-          <div className="search-list-page">Search Listing Page</div>
-        </Col>
-      </Row>
-      <div className="product-tile-wrapper">{searchProductsList}</div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 SearchListingView.propTypes = {
   className: PropTypes.string,
+  searchedResult: PropTypes.arrayOf(PropTypes.shape({})),
 };
 
 SearchListingView.defaultProps = {
   className: '',
+  searchedResult: {},
 };
 
 export default withStyles(SearchListingView, SearchListingStyle);
