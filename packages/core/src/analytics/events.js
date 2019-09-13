e53a7505675a0ebe5f964ab6e5bff7d6f5fc9998
Erik Jung <erik.jung@sapientrazorfish.com>
2019-09-13 18:33:59 -0400

    RWD-5713 | WIP add analytics event definitions

---
+++ packages/core/src/analytics/events.js
@@ -0,0 +1,59 @@
+/**
+ * @module analytics/events
+ *
+ * @description These definitions represent the specific payloads
+ * for each tracking call. The payload shapes should match what
+ * the analytics library needs to send, and can optionally be derived
+ * from the current action and previous/next states. The logic
+ * for deriving the payloads can live here or within the event
+ * mapper logic (or in both places) for flexibility.
+ *
+ * @example
+ * trackClick(action => ({
+ *   name: action.payload.thingThatWasClicked
+ * }))
+ *
+ * @example
+ * trackPageView((action, prevState) => ({
+ *   name: action.payload.routePath,
+ *   prevPage: prevState.router.path
+ * }))
+ *
+ * @see https://rangle.gitbook.io/redux-beacon/index-1/event-definition
+ * @see https://rangle.gitbook.io/redux-beacon/index-1/events-mapper
+ */
+
+import TrackingEvent from './TrackingEvent';
+
+export function trackPageView(eventDef) {
+  return (...args) => {
+    const { name } = eventDef(...args);
+    return TrackingEvent({
+      hitType: 'pageView',
+      name,
+      // TODO: Fill in rest of payload
+    });
+  };
+}
+
+export function trackClick(eventDef) {
+  return (...args) => {
+    const { name } = eventDef(...args);
+    return TrackingEvent({
+      hitType: 'click',
+      name,
+      // TODO: Fill in rest of payload
+    });
+  };
+}
+
+export function trackServiceResponse(eventDef) {
+  return (...args) => {
+    const { name } = eventDef(...args);
+    return TrackingEvent({
+      hitType: 'serviceResponse',
+      name,
+      // TODO: Fill in rest of payload
+    });
+  };
+}
