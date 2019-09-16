060f0b73955f5bf6701e4e8aecb0c62cf771b97a
Prasoon <prasoon.singh@publicissapient.com>
2019-09-16 09:21:41 +0530

    update

--- packages/core/src/components/common/molecules/ImageGrid/ImageGrid.native.jsx
+++ packages/core/src/components/common/molecules/ImageGrid/ImageGrid.native.jsx
@@ -5,7 +5,6 @@ import { Wrapper, ImageGridContainer, ImageGridItem } from './ImageGrid.style.na
 
 const ImageGrid = props => {
   const { mediaList, imgWidth, column, gutter, imgHeight, dataLocator, navigation } = props;
-
   return (
     <Wrapper>
       <ImageGridContainer width={imgWidth * column + gutter}>
