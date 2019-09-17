014c32c1961ad34e8ee3bd3219a05d20d272ca1e
Vinit Kumar <vinit.k.85@gmail.com>
2019-09-17 09:52:42 +0530

    Tnc and program details link

--- packages/core/src/components/features/account/PointHistory/views/PointsHistoryPage.view.jsx
+++ packages/core/src/components/features/account/PointHistory/views/PointsHistoryPage.view.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import BodyCopy from '@tcp/core/src/components/common/atoms/BodyCopy';
 import Anchor from '@tcp/core/src/components/common/atoms/Anchor';
+import Row from '@tcp/core/src/components/common/atoms/Row';
+import Col from '@tcp/core/src/components/common/atoms/Col';
 import withStyles from '@tcp/core/src/components/common/hoc/withStyles';
 import styles from '../styles/PointsHistoryPage.view.style';
-import PageHeadingWithLinks from '../../common/molecule/PageHeadingWithLinks';
+import FormPageHeading from '../../common/molecule/FormPageHeading';
 import PointsHistoryList from '../../common/organism/PointsHistory';
 import internalEndpoints from '../../common/internalEndpoints';
+import externalEndpoints from '../../common/externalEndpoints';
 import HistoryPointsRichTextComponent from './HistoryPointsRichText.view';
 
 /**
@@ -29,31 +32,65 @@ export const PointsHistoryPageView = props => {
           {labels.lbl_common_backLink}
         </Anchor>
       </BodyCopy>
-      <PageHeadingWithLinks
+      <FormPageHeading
+        className="elem-mb-XL"
         heading={labels.lbl_common_points_history_heading}
-        programDetailsCta={labels.lbl_points_history_program_details}
-        termsConditionCta={labels.lbl_common_tnc}
-      >
-        <BodyCopy component="div" className="elem-pb-XXL">
-          <BodyCopy component="div" className="elem-mb-XXL elem-mt-XXL elem-pb-MED">
-            <BodyCopy
-              component="p"
-              fontFamily="secondary"
-              fontSize="fs16"
-              fontWeight="extrabold"
-              className="hide-on-desktop hide-on-tablet elem-mb-LRG"
-            >
-              {labels.lbl_common_points_history}
-            </BodyCopy>
-            <PointsHistoryList view="edit" labels={labels} showFullHistory />
-          </BodyCopy>
-          <BodyCopy
-            component="div"
-            className="history-section-separator hide-on-mobile elem-mb-XXL elem-mt-XXL"
-          />
-          <HistoryPointsRichTextComponent content={richTextContent} />
+        data-locator="historyPointsLbl"
+      />
+      <BodyCopy component="div" className="elem-mb-XXL elem-mt-XXL elem-pb-MED">
+        <BodyCopy
+          component="p"
+          fontFamily="secondary"
+          fontSize="fs16"
+          fontWeight="extrabold"
+          className="hide-on-desktop hide-on-tablet elem-mb-LRG"
+        >
+          {labels.lbl_common_points_history}
         </BodyCopy>
-      </PageHeadingWithLinks>
+        <PointsHistoryList view="edit" labels={labels} showFullHistory />
+      </BodyCopy>
+      <BodyCopy
+        component="div"
+        className="history-section-separator hide-on-mobile elem-mb-XXL elem-mt-XXL"
+      />
+      <HistoryPointsRichTextComponent content={richTextContent} />
+      <Row className="elem-pt-XXL">
+        <Col
+          colSize={{
+            small: 6,
+            large: 12,
+            medium: 8,
+          }}
+          offsetLeft={{
+            small: 1,
+            large: 4,
+            medium: 2,
+          }}
+          className="anchor-wrapper"
+        >
+          <Anchor
+            fontSizeVariation="large"
+            underline
+            noLink
+            href={externalEndpoints.myPlaceRewardsPage}
+            anchorVariation="primary"
+            dataLocator="program_details_cta"
+          >
+            {labels.lbl_points_history_program_details}
+          </Anchor>
+          <Anchor
+            fontSizeVariation="large"
+            underline
+            noLink
+            href={externalEndpoints.termsAndConditionsPage}
+            anchorVariation="primary"
+            dataLocator="terms_condition_cta"
+            className="elem-ml-XL"
+          >
+            {labels.lbl_common_tnc}
+          </Anchor>
+        </Col>
+      </Row>
     </BodyCopy>
   );
 };
