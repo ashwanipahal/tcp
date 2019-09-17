dd679afde59cc4e98a0187fddfbfe9fdfae89b50
Nisheed Jagadish <nisheed2016@gmail.com>
2019-09-17 14:44:23 +0530

    Fixed issue with storybook and updated generator

--- stories/moduleD.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-import { storiesOf } from '@storybook/react';
-import { ModuleD } from '../.storybook/storybook-components/ModuleD';
-import Fragment from 'react';
-storiesOf('ModuleD', module).add('with 2 pictures', () => <Fragment />);
