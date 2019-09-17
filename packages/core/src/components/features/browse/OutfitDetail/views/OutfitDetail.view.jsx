68e685fad2577853b0d4aa6de9597e63e96c5c27
Siddharth <siddhartha.nanda@publicissapient.com>
2019-09-17 15:27:06 +0530

    Changes for Outfit label

--- packages/core/src/components/features/browse/OutfitDetail/views/OutfitDetail.view.jsx
+++ packages/core/src/components/features/browse/OutfitDetail/views/OutfitDetail.view.jsx
@@ -3,29 +3,18 @@ import PropTypes from 'prop-types';
 import { Row, Col } from '../../../../common/atoms';
 import withStyles from '../../../../common/hoc/withStyles';
 import OutfitDetailStyle from '../OutfitDetail.style';
-import config from '../OutfitDetail.config';
 
-const OutfitDetailView = ({ className }) => {
+const OutfitDetailView = ({ className, labels }) => {
   return (
     <div className={className}>
       <Row className="placeholder">
         <Col colSize={{ small: 6, medium: 8, large: 12 }}>
-          <div className="promo-area-0">{config.OUTFIT_TITLE}</div>
+          <div className="promo-area-0">{labels.lbl_outfit_title}</div>
         </Col>
       </Row>
       <Row className="placeholder">
         <Col colSize={{ small: 6, medium: 8, large: 12 }}>
-          <div className="promo-area-1">{config.OUTFIT_BREADCRUMB}</div>
-        </Col>
-      </Row>
-      <Row className="placeholder">
-        <Col colSize={{ small: 6, medium: 8, large: 12 }}>
-          <div className="promo-area-1">{config.OUTFIT_PROMO_AREA_1}</div>
-        </Col>
-      </Row>
-      <Row className="placeholder">
-        <Col colSize={{ small: 6, medium: 8, large: 12 }}>
-          <div className="product-detail-section">{config.OUTFIT_PROMO_AREA_2}</div>
+          <div className="promo-area-1">{labels.lbl_outfit_breadcrumb}</div>
         </Col>
       </Row>
     </div>
@@ -34,10 +23,12 @@ const OutfitDetailView = ({ className }) => {
 
 OutfitDetailView.propTypes = {
   className: PropTypes.string,
+  labels: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.string])),
 };
 
 OutfitDetailView.defaultProps = {
   className: '',
+  labels: {},
 };
 
 export default withStyles(OutfitDetailView, OutfitDetailStyle);
