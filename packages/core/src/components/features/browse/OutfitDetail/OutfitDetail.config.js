547e25b5cd3cfb2b7d0a71a1f45b629a82f603d2
Siddharth <siddhartha.nanda@publicissapient.com>
2019-09-17 15:51:39 +0530

    Deleting Old configuration

--- packages/core/src/components/features/browse/OutfitDetail/OutfitDetail.config.js
+++
@@ -1,7 +0,0 @@
-const outfitLabels = {
-  OUTFIT_TITLE: 'Outfit Detail Page',
-  OUTFIT_BREADCRUMB: 'OUTFIT BREADCRUMB',
-  OUTFIT_PROMO_AREA_1: 'PROMO AREA 1',
-  OUTFIT_PROMO_AREA_2: 'PROMO AREA 2',
-};
-export default outfitLabels;
