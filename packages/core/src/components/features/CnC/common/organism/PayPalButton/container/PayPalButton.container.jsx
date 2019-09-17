844b1941fb47c89d944ad9fa0d5060c9a55b29b0
deependra <deependra.sankhala@publicissapient.com>
2019-09-17 22:01:20 +0530

    added to globel

--- packages/core/src/components/features/CnC/common/organism/PayPalButton/container/PayPalButton.container.jsx
+++ packages/core/src/components/features/CnC/common/organism/PayPalButton/container/PayPalButton.container.jsx
@@ -5,24 +5,24 @@ import PayPalButton from '../organism/PaypalButton';
 import bagPageActions from '../../../../BagPage/container/BagPage.actions';
 import { getSetIsPaypalPaymentSettings } from '../../../../Checkout/container/Checkout.action';
 import { ServiceResponseError } from '../../../../../../../utils/errorMessage.util';
+import CONSTANTS from '../../../../Checkout/Checkout.constants';
 
 export class PayPalButtonContainer extends React.PureComponent<Props> {
   initalizePayPalButton = data => {
     const { startPaypalCheckout, paypalAuthorizationHandle, clearPaypalSettings } = this.props;
     const { containerId, height } = data;
     const options = {
-      locale: 'en_US',
+      locale: CONSTANTS.PAYPAL_LOCATE,
       style: {
         size: 'responsive',
         color: 'blue',
         shape: 'rect',
-        label: 'paypal',
+        label: CONSTANTS.PAYPAL_LABEL,
         tagline: false,
         height,
       },
       funding: {
-        // eslint-disable-next-line no-undef
-        disallowed: [paypal && paypal.FUNDING.CREDIT],
+        disallowed: [window.paypal && window.paypal.FUNDING.CREDIT],
       },
       env: 'sandbox',
       payment: () => {
