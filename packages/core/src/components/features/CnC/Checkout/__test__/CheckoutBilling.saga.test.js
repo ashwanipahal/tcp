cc3d1dbd0a595de6b7d1d3d724419ca459609bf5
Mohit Jain <mohit.jain2@publicissapient.com>
2019-09-18 16:24:05 +0530

    update

--- packages/core/src/components/features/CnC/Checkout/__test__/CheckoutBilling.saga.test.js
+++ packages/core/src/components/features/CnC/Checkout/__test__/CheckoutBilling.saga.test.js
@@ -1,103 +1,86 @@
-import { call, select } from 'redux-saga/effects';
-import CheckoutReview, {
-  submitOrderProcessing,
-  loadPersonalizedCoupons,
-} from '../container/CheckoutReview.saga';
-import { isGuest } from '../container/Checkout.selector';
-import { validateAndSubmitEmailSignup } from '../container/Checkout.saga.util';
-import { requestPersonalizedCoupons } from '../../../../../services/abstractors/CnC/index';
+import { call } from 'redux-saga/effects';
+import submitBilling, {
+  submitBillingData,
+  updatePaymentInstruction,
+} from '../container/CheckoutBilling.saga';
 
-describe('CheckoutReview saga', () => {
-  it('CheckoutReview', () => {
-    const CheckoutReviewSaga = CheckoutReview();
-    CheckoutReviewSaga.next();
-    CheckoutReviewSaga.next();
+describe('CheckoutBilling saga', () => {
+  it('CheckoutBilling', () => {
+    const CheckoutReviewSaga = submitBilling({ payload: { address: {} } });
     CheckoutReviewSaga.next();
     CheckoutReviewSaga.next();
 
-    expect(CheckoutReviewSaga.next().value).toEqual(
-      call(submitOrderProcessing, undefined, undefined, undefined)
+    expect(CheckoutReviewSaga.next(false).value).toEqual(
+      call(
+        submitBillingData,
+        { address: {}, phoneNumber: '' },
+        {
+          address1: undefined,
+          address2: undefined,
+          city: undefined,
+          country: undefined,
+          firstName: undefined,
+          lastName: undefined,
+          state: undefined,
+          zip: undefined,
+        },
+        undefined
+      )
     );
+    expect(CheckoutReviewSaga.next(false).value).toEqual(undefined);
   });
-});
+  it('submitBillingData', () => {
+    const CheckoutReviewSaga = submitBillingData({ address: { sameAsShipping: true } }, {});
+    CheckoutReviewSaga.next();
+    CheckoutReviewSaga.next();
+    CheckoutReviewSaga.next({ onFileAddressKey: '234' });
 
-const emailAddress = '123@123.com';
-describe('submitOrderProcessing saga', () => {
-  it('submitOrderProcessing review Page', () => {
-    const orderProcessing = submitOrderProcessing();
-    orderProcessing.next();
-    orderProcessing.next({ userDetails: { emailAddress } });
-    orderProcessing.next();
-
-    expect(orderProcessing.next().value).toEqual(select(isGuest));
-    expect(orderProcessing.next(true).value).toEqual(
-      call(validateAndSubmitEmailSignup, emailAddress, 'us_guest_checkout')
+    expect(CheckoutReviewSaga.next({ body: {} }).value).toEqual(
+      call(
+        updatePaymentInstruction,
+        { address: { sameAsShipping: true } },
+        undefined,
+        undefined,
+        {},
+        undefined
+      )
     );
+    expect(CheckoutReviewSaga.next(false).value).toEqual(undefined);
   });
-  it('submitOrderProcessing review Page with shipping email', () => {
-    const orderProcessing = submitOrderProcessing();
-    orderProcessing.next();
-    orderProcessing.next({ shipping: { emailAddress } });
-    orderProcessing.next();
-
-    expect(orderProcessing.next().value).toEqual(select(isGuest));
-    expect(orderProcessing.next(true).value).toEqual(
-      call(validateAndSubmitEmailSignup, emailAddress, 'us_guest_checkout')
+  it('updatePaymentInstruction', () => {
+    const func = () => {};
+    const CheckoutReviewSaga = updatePaymentInstruction(
+      { address: { sameAsShipping: true }, cardNumber: '1234' },
+      undefined,
+      undefined,
+      {},
+      func
     );
+    CheckoutReviewSaga.next();
+    CheckoutReviewSaga.next({ billing: {}, paymentId: '123' });
+    CheckoutReviewSaga.next();
+
+    expect(CheckoutReviewSaga.next({ body: {} }).value).toEqual(
+      call(func, false, true, true, false, false)
+    );
+    expect(CheckoutReviewSaga.next(false).value).toEqual(undefined);
   });
-});
-
-describe('loadPersonalizedCoupons saga', () => {
-  it('loadPersonalizedCoupons review Page', () => {
-    const orderProcessing = loadPersonalizedCoupons({
-      userDetails: { emailAddress },
-    });
-    orderProcessing.next();
-    orderProcessing.next(false);
-
-    expect(orderProcessing.next([]).value).toEqual(
-      call(requestPersonalizedCoupons, {
-        orderNumber: undefined,
-        emailAddress,
-        locationId: '0180',
-        couponList: [],
-        isElectiveBonus: undefined,
-        currencyCode: undefined,
-        payments: undefined,
-        cartItems: undefined,
-      })
+  it('updatePaymentInstruction', () => {
+    const func = () => {};
+    const CheckoutReviewSaga = updatePaymentInstruction(
+      { address: { sameAsShipping: true }, cardNumber: '1234' },
+      undefined,
+      undefined,
+      {},
+      func
     );
-    expect(
-      orderProcessing.next({
-        personalizedOffersResponse: {},
-        orderResponse: { pointsToNextReward: -1, userPoints: -1, earnedReward: true },
-      }).value
-    ).toEqual(undefined);
-  });
-  it('loadPersonalizedCoupons review Page for canada', () => {
-    const orderProcessing = loadPersonalizedCoupons({
-      userDetails: { emailAddress },
-    });
-    orderProcessing.next();
-    orderProcessing.next(true);
+    CheckoutReviewSaga.next();
+    CheckoutReviewSaga.next({ billing: {} });
+    CheckoutReviewSaga.next();
 
-    expect(orderProcessing.next([]).value).toEqual(
-      call(requestPersonalizedCoupons, {
-        orderNumber: undefined,
-        emailAddress,
-        locationId: '3180',
-        couponList: [],
-        isElectiveBonus: undefined,
-        currencyCode: undefined,
-        payments: undefined,
-        cartItems: undefined,
-      })
+    expect(CheckoutReviewSaga.next({ body: {} }).value).toEqual(
+      call(func, false, true, true, false, false)
     );
-    expect(
-      orderProcessing.next({
-        personalizedOffersResponse: {},
-        orderResponse: { pointsToNextReward: 12, userPoints: 15, earnedReward: true },
-      }).value
-    ).toEqual(undefined);
+    expect(CheckoutReviewSaga.next(false).value).toEqual(undefined);
   });
 });
