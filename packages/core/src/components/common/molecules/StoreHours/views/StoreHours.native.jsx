961d4d31e79e4eb51fe13c5b2c2c665f8e47e843
Nisheed Jagadish <nisheed2016@gmail.com>
2019-09-19 16:54:13 +0530

    Added native store hours component

--- packages/core/src/components/common/molecules/StoreHours/views/StoreHours.native.jsx
+++ packages/core/src/components/common/molecules/StoreHours/views/StoreHours.native.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import StoreHoursRoot from '../styles/StoreHours.style.native';
+import StoreHoursRoot, {
+  TimingsList,
+  TimingsItem,
+  TimingsText,
+  StoreHoursTitle,
+} from '../styles/StoreHours.style.native';
 
-const StoreHours = ({ children }) => <StoreHoursRoot>{children}</StoreHoursRoot>;
+const StoreHours = ({ children, title, storeTiming }) => {
+  return (
+    <StoreHoursRoot>
+      <StoreHoursTitle>{title}</StoreHoursTitle>
+      {storeTiming.length > 0 && (
+        <TimingsList>
+          {storeTiming.map(item => (
+            <TimingsItem key={item.id}>
+              <TimingsText textAlign="left">{item.label}</TimingsText>
+              <TimingsText textAlign="right">{item.value}</TimingsText>
+            </TimingsItem>
+          ))}
+        </TimingsList>
+      )}
+      {children}
+    </StoreHoursRoot>
+  );
+};
 
 StoreHours.propTypes = {
   children: PropTypes.arrayOf(PropTypes.node),
+  title: PropTypes.string.isRequired,
+  storeTiming: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      label: PropTypes.string,
+      value: PropTypes.string,
+    })
+  ),
 };
 
 StoreHours.defaultProps = {
   children: null,
+  storeTiming: [],
 };
 
 export default StoreHours;
