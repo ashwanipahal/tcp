b9de19121e310f5482837e9f097b533153ba9a5c
Raghav <raghasb@gmail.com>
2019-09-19 20:35:38 +0530

    Added check for gymboree

--- packages/web/src/components/features/content/Header/molecules/CountrySelector/container/CountrySelector.saga.js
+++ packages/web/src/components/features/content/Header/molecules/CountrySelector/container/CountrySelector.saga.js
@@ -9,6 +9,7 @@ import {
   getModifiedLanguageCode,
   siteRedirect,
   languageRedirect,
+  isGymboree,
 } from '@tcp/core/src/utils';
 import { API_CONFIG } from '@tcp/core/src/services/config';
 import endpoints from '@tcp/core/src/services/endpoints';
@@ -53,13 +54,18 @@ export function* submitCountrySelectionData({ payload: data }) {
   try {
     const { ca } = sites;
     const {
-      CATALOGID_CONFIG: { TCP },
+      CATALOGID_CONFIG: { TCP, Gymboree },
       CA_CONFIG_OPTIONS,
       US_CONFIG_OPTIONS,
     } = API_CONFIG;
     const storeId =
       data.country === ca.countryCode ? CA_CONFIG_OPTIONS.storeId : US_CONFIG_OPTIONS.storeId;
-    const catalogId = data.country === ca.countryCode ? TCP.Canada : TCP.USA;
+    let catalogId;
+    if (isGymboree()) {
+      catalogId = data.country === ca.countryCode ? Gymboree.Canada : Gymboree.USA;
+    } else {
+      catalogId = data.country === ca.countryCode ? TCP.Canada : TCP.USA;
+    }
     const { addShipToStore } = endpoints;
     const { sitesInfo } = API_CONFIG;
     const payload = {
