76c65bef46caf00ef9c0175c8348effb3dac5adb
Raghav <raghasb@gmail.com>
2019-09-20 18:41:12 +0530

    error handling for module K

--- packages/core/src/components/common/molecules/ModuleK/view/ModuleK.jsx
+++ packages/core/src/components/common/molecules/ModuleK/view/ModuleK.jsx
@@ -1,5 +1,6 @@
-// @flow
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { Col, Row, Button, BodyCopy, Anchor } from '../../../atoms';
 import { PromoBanner } from '../..';
 import { Carousel, LinkText, ImageGrid, style } from '../ModuleK.style';
@@ -8,25 +9,13 @@ import errorBoundary from '../../../hoc/withErrorBoundary';
 import config from '../config';
 import { getIconPath, getLocator } from '../../../../../utils';
 
-type Props = {
-  className: string,
-  headerText: Array<Object>,
-  promoTextBanner: Array<Object>,
-  masonryGrid: Object,
-};
-
-type State = {
-  current: number,
-  next: number,
-};
-
 /**
  * @class ModuleK - global reusable component will provide featured content module
  * with a composite background image and 2-6 CTAs
  * This component is plug and play at any given slot in layout by passing required data
  * @param {composites} composites the list of data for header texts, links and images for component
  */
-class ModuleK extends React.PureComponent<Props, State> {
+class ModuleK extends React.PureComponent {
   render() {
     const { headerText, masonryGrid, className } = this.props;
 
@@ -85,49 +74,52 @@ class ModuleK extends React.PureComponent<Props, State> {
                 customArrowRight: getIconPath('carousel-big-carrot'),
               }}
             >
-              {masonryGrid.map(({ promoBanner, mediaLinkedList, singleCTAButton }, index) => {
-                const checkPromo = promoBanner && promoBanner.length;
-                return (
-                  <React.Fragment>
-                    {checkPromo && (
-                      <PromoBanner
-                        promoBanner={promoBanner}
-                        className="moduleK__promoBanner"
-                        data-locator={`${getLocator('moduleK_promobanner_text')}${index + 1}`}
-                        fontSize="fs48"
-                      />
-                    )}
-                    <ImageGrid
-                      mediaLinkedList={mediaLinkedList}
-                      className="image-grid"
-                      colM={2}
-                      dataLocator={`${getLocator('moduleK_image')}${index + 1}`}
-                      dataLocatorContainer={`${getLocator('moduleK_image_set')}${index + 1}`}
-                      promo={checkPromo}
-                      imgConfigs={IMG_DATA.imgConfig}
-                    />
-                    <Col
-                      colSize={{
-                        small: 4,
-                        medium: 2,
-                        large: 3,
-                      }}
-                    >
-                      {singleCTAButton && (
-                        <Anchor {...singleCTAButton}>
-                          <Button
-                            buttonVariation="fixed-width"
-                            dataLocator={`${getLocator('moduleK_button_set')}${index + 1}`}
-                            className="carousal-cta"
-                          >
-                            {singleCTAButton.text}
-                          </Button>
-                        </Anchor>
+              {masonryGrid &&
+                masonryGrid.map(({ promoBanner, mediaLinkedList, singleCTAButton }, index) => {
+                  const checkPromo = promoBanner && promoBanner.length;
+                  return (
+                    <React.Fragment>
+                      {checkPromo && (
+                        <PromoBanner
+                          promoBanner={promoBanner}
+                          className="moduleK__promoBanner"
+                          data-locator={`${getLocator('moduleK_promobanner_text')}${index + 1}`}
+                          fontSize="fs48"
+                        />
                       )}
-                    </Col>
-                  </React.Fragment>
-                );
-              })}
+                      {mediaLinkedList && (
+                        <ImageGrid
+                          mediaLinkedList={mediaLinkedList}
+                          className="image-grid"
+                          colM={2}
+                          dataLocator={`${getLocator('moduleK_image')}${index + 1}`}
+                          dataLocatorContainer={`${getLocator('moduleK_image_set')}${index + 1}`}
+                          promo={checkPromo}
+                          imgConfigs={IMG_DATA.imgConfig}
+                        />
+                      )}
+                      <Col
+                        colSize={{
+                          small: 4,
+                          medium: 2,
+                          large: 3,
+                        }}
+                      >
+                        {singleCTAButton && (
+                          <Anchor {...singleCTAButton}>
+                            <Button
+                              buttonVariation="fixed-width"
+                              dataLocator={`${getLocator('moduleK_button_set')}${index + 1}`}
+                              className="carousal-cta"
+                            >
+                              {singleCTAButton.text}
+                            </Button>
+                          </Anchor>
+                        )}
+                      </Col>
+                    </React.Fragment>
+                  );
+                })}
             </Carousel>
           </Col>
         </Row>
@@ -135,6 +127,28 @@ class ModuleK extends React.PureComponent<Props, State> {
     );
   }
 }
+
+ModuleK.propTypes = {
+  className: PropTypes.string.isRequired,
+  headerText: PropTypes.arrayOf(
+    PropTypes.shape({
+      link: PropTypes.object,
+      textItems: PropTypes.array,
+    })
+  ).isRequired,
+  masonryGrid: PropTypes.arrayOf(
+    PropTypes.shape({
+      mediaLinkedList: PropTypes.arrayOf(PropTypes.shape({})),
+      promoBanner: PropTypes.arrayOf(
+        PropTypes.shape({
+          link: PropTypes.object,
+          textItems: PropTypes.array,
+        })
+      ),
+      singleCTAButton: PropTypes.shape({}),
+    })
+  ).isRequired,
+};
 
 export default withStyles(errorBoundary(ModuleK), style);
 export { ModuleK as ModuleKVanilla };
