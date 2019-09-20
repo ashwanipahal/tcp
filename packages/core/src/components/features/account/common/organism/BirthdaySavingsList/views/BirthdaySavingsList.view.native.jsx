43e753c902ac6fee03eb55c466504e005c367efe
Ajay Saini <ajay.saini@publicissapient.com>
2019-09-20 12:08:49 +0530

    Remove child feature

--- packages/core/src/components/features/account/common/organism/BirthdaySavingsList/views/BirthdaySavingsList.view.native.jsx
+++ packages/core/src/components/features/account/common/organism/BirthdaySavingsList/views/BirthdaySavingsList.view.native.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { List } from 'immutable';
 import { View } from 'react-native';
 import { BodyCopyWithSpacing } from '@tcp/core/src/components/common/atoms/styledWrapper';
+import Button from '@tcp/core/src/components/common/atoms/Button';
 import BirthdayCardComponent from '../../../molecule/BirthdayCard';
 import EmptyBirthdayCard from '../../../molecule/EmptyBirthdayCard';
 import constants from '../BirthdaySavingsList.constants';
@@ -42,9 +43,64 @@ InfoMessage.defaultProps = {
  * In case of view="edit", Add new Birthday Card will be rendered otherwise Empty space will be present.
  */
 class BirthdaySavingsList extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      removeModal: false,
+      activeChild: null,
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    const { status } = this.props;
+    if (status === 'success' && status !== prevProps.status) {
+      this.closeRemoveModal();
+    }
+  }
+
+  /**
+   * @function showRemoveModal
+   * @description This function will handle showing of remove Children Birthday Confirmation Modal
+   * @param {object} activeChild Current active children information to be removed
+   */
+  showRemoveModal = activeChild => {
+    this.setState({
+      removeModal: true,
+      activeChild,
+    });
+  };
+
+  /**
+   * @function closeRemoveModal
+   * @description This function will handle closing of remove Children Birthday Confirmation Modal
+   */
+  closeRemoveModal = () => {
+    this.setState({
+      removeModal: false,
+      activeChild: null,
+    });
+  };
+
+  /**
+   * @function removeBirthdayHandler
+   * @description This function will call removeBirthday prop with required params
+   * @param {object} activeChild Current active children information to be removed
+   */
+  removeBirthdayHandler = activeChild => {
+    const { removeBirthday } = this.props;
+    removeBirthday(activeChild);
+  };
+
   render() {
     const { labels, childrenBirthdays, view } = this.props;
+    const { removeModal, activeChild } = this.state;
     const isEditMode = view === 'edit';
+    const removeButtonStyle = {
+      marginTop: 59,
+    };
+    const cancelButtonStyle = {
+      marginTop: 20,
+    };
     if (isEditMode || (childrenBirthdays && childrenBirthdays.size > 0)) {
       const birthdays = childrenBirthdays
         ? childrenBirthdays.setSize(constants.MAX_BIRTHDAY_CARDS)
@@ -66,6 +122,7 @@ class BirthdaySavingsList extends PureComponent {
                       gender={birthday.get('gender')}
                       childId={birthday.get('childId')}
                       view={view}
+                      removeBirthday={this.showRemoveModal}
                     />
                   ) : (
                     <EmptyBirthdayCard labels={labels} view={view} />
@@ -74,6 +131,35 @@ class BirthdaySavingsList extends PureComponent {
               );
             })}
           </WrapLayout>
+
+          {removeModal && (
+            <View>
+              <BodyCopyWithSpacing
+                mobileFontFamily="secondary"
+                fontSize="fs14"
+                color="gray.900"
+                text={getLabelValue(labels, 'lbl_profile_removeInfoText').replace(
+                  /\$childName\$/,
+                  activeChild.childName
+                )}
+                spacingStyles="margin-top-MED"
+              />
+              <Button
+                buttonVariation="fixed-width"
+                fill="BLUE"
+                onPress={() => this.removeBirthdayHandler(activeChild)}
+                text={getLabelValue(labels, 'lbl_profile_removeCTA')}
+                style={removeButtonStyle}
+              />
+              <Button
+                buttonVariation="fixed-width"
+                fill="WHITE"
+                text={getLabelValue(labels, 'lbl_profile_removeCancelCTA')}
+                style={cancelButtonStyle}
+                onPress={this.closeRemoveModal}
+              />
+            </View>
+          )}
         </View>
       );
     }
@@ -85,10 +171,14 @@ BirthdaySavingsList.propTypes = {
   labels: PropTypes.shape({}).isRequired,
   childrenBirthdays: PropTypes.shape([]).isRequired,
   view: PropTypes.oneOf([constants.VIEW.READ, constants.VIEW.EDIT]),
+  removeBirthday: PropTypes.func,
+  status: PropTypes.string,
 };
 
 BirthdaySavingsList.defaultProps = {
   view: constants.VIEW.EDIT,
+  removeBirthday: () => {},
+  status: '',
 };
 
 export default BirthdaySavingsList;
