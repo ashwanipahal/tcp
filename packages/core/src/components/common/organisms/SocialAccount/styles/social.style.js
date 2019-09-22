0b9698e2be82a6156ab992999db7180eae9f609b
sachin rawal <srawal.sapient@outlook.com>
2019-09-22 14:59:03 +0530

    RWD-9640 - PIxel Issues | Facebook social Connect on My Preferences

--- packages/core/src/components/common/organisms/SocialAccount/styles/social.style.js
+++ packages/core/src/components/common/organisms/SocialAccount/styles/social.style.js
@@ -8,28 +8,31 @@ const socialStyle = css`
     padding: 15px 0;
   }
   .Facebook-icon--enable {
-    width: 50px;
-    height: 50px;
+    width: 62px;
+    height: 55px;
     background: url('/static/images/facebook@2x.png') no-repeat;
   }
   .Facebook-icon--disable {
-    width: 50px;
-    height: 50px;
+    width: 62px;
+    height: 55px;
     background: url('/static/images/facebook-fade@2x.png') no-repeat;
   }
 
   .social-accounts__align {
     display: flex;
     align-items: center;
-    padding-left: 20px;
+    padding-right: ${props => props.theme.spacing.LAYOUT_SPACING.XS};
   }
   .social_accounts_cross_plus-icon {
-    position: relative;
+    position: absolute;
     z-index: 1000;
-    width: 165px;
-    right: 160px;
-    text-align: right;
     cursor: pointer;
+    width: 215px;
+    height: 55px;
+    @media ${props => props.theme.mediaQuery.smallOnly} {
+      right: 120px;
+      width: 160px;
+    }
   }
   .social-account-icon {
     position: absolute;
