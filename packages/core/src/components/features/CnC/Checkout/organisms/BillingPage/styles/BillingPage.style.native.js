164173a0ba85fbbb0d9679f0f0fa9590a12bf7ed
Siddharth Pandey <siddharth.pandey@publicissapient.com>
2019-09-23 19:43:40 +0530

    RWD-8059

--- packages/core/src/components/features/CnC/Checkout/organisms/BillingPage/styles/BillingPage.style.native.js
+++ packages/core/src/components/features/CnC/Checkout/organisms/BillingPage/styles/BillingPage.style.native.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components/native';
 
 const Container = styled.View`
-  flex: 1;
   margin-left: ${props => props.theme.spacing.ELEM_SPACING.MED};
   margin-right: ${props => props.theme.spacing.ELEM_SPACING.MED};
 `;
