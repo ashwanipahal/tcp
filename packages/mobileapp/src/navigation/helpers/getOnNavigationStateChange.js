6c26be5dcb4ec10959881ae3c3d9378ffabbc5ba
Michael Edelman <michael@fabulas.io>
2019-09-24 11:40:03 -0400

    feat: integrates redux into screen/route tracker

--- packages/mobileapp/src/navigation/helpers/getOnNavigationStateChange.js
+++ packages/mobileapp/src/navigation/helpers/getOnNavigationStateChange.js
@@ -1,13 +1,17 @@
 import { getActiveRouteName } from './getActiveRouteName';
 /*eslint-disable */
 
-export function getOnNavigationStateChange() {
+export function getOnNavigationStateChange(store) {
   return {
     onNavigationStateChange: (prevState, currentState, action) => {
       const currentScreen = getActiveRouteName(currentState);
       const prevScreen = getActiveRouteName(prevState);
       if (prevScreen !== currentScreen) {
         // change the tracker here to use other Mobile analytics SDK.
+        store.dispatch({
+          type: 'analytics:TRACK_PAGE_VIEW',
+          payload: { previousScreen: prevScreen, currentScreen, appState: store.getState() },
+        });
         __DEV__
           ? console.info(`%cNow navigating to: ${currentScreen}`, 'background: #333; color: #fff')
           : null;
