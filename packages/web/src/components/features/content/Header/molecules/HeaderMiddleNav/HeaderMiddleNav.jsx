fe9d13fa7fa2966f0691485e2bd81dc172bec781
Raghav <raghasb@gmail.com>
2019-09-25 15:56:11 +0530

    udpated monor changes

--- packages/web/src/components/features/content/Header/molecules/HeaderMiddleNav/HeaderMiddleNav.jsx
+++ packages/web/src/components/features/content/Header/molecules/HeaderMiddleNav/HeaderMiddleNav.jsx
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Col, Row, Image, Anchor, BodyCopy } from '@tcp/core/src/components/common/atoms';
