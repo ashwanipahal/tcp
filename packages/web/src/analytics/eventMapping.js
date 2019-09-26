2849cfdd90f5decaac27120e6723afa495ff01a6
Erik Jung <erik.jung@sapientrazorfish.com>
2019-09-26 15:26:06 -0400

    RWD-5713 | Add new tracing event mapping for direct call example

--- packages/web/src/analytics/eventMapping.js
+++ packages/web/src/analytics/eventMapping.js
@@ -1,4 +1,5 @@
 import { TRACK_PAGE_VIEW, TRACK_CLICK, trackPageView, trackClick } from '@tcp/core/src/analytics';
+import ADDEDTOBAG_CONSTANTS from '@tcp/core/src/components/features/CnC/AddedToBag/AddedToBag.constants';
 
 /**
  * Analytics middleware event mapper.
@@ -27,11 +28,16 @@ import { TRACK_PAGE_VIEW, TRACK_CLICK, trackPageView, trackClick } from '@tcp/co
  */
 export default function(action) {
   switch (action.type) {
-    case TRACK_PAGE_VIEW: {
+    case TRACK_PAGE_VIEW:
       return trackPageView(/* (action, prevState, nextState) => ({}) */);
-    }
+
     case TRACK_CLICK:
       return trackClick(/* (action, prevState, nextState) => ({}) */);
+
+    // NOTE: Example of direct call tracking (opposed to page or click events)
+    case ADDEDTOBAG_CONSTANTS.ADD_TO_CART_ECOM:
+      return () => ({ eventName: 'api_addProductToCart' });
+
     default:
       return [];
   }
