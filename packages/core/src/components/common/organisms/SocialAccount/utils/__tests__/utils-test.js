187c14265a0c076435d74a1928aa135b4c1c5f2d
Ayush Kumar <ayush.kumar@publicissapient.com>
2019-09-30 13:08:34 +0530

    RWD-9851 Connected Social Account reward points message

--- packages/core/src/components/common/organisms/SocialAccount/utils/__tests__/utils-test.js
+++ packages/core/src/components/common/organisms/SocialAccount/utils/__tests__/utils-test.js
@@ -13,16 +13,16 @@ const titleFacebook = 'lbl_prefrence_social_points_text_2';
 const titleInstagram = 'lbl_prefrence_social_points_text_2_instagram';
 const titleTwitter = 'lbl_prefrence_social_points_text_2_twitter';
 
-describe('CTA Title for Default Store', () => {
-  it('should show correct labem if facebbok is linked', () => {
+describe('Reward points success message label', () => {
+  it('should show correct label if facebbok is linked', () => {
     const title = getLinkedSocialAccountLabel('AddFacebookAcct', labelState);
     expect(title).toBe(titleFacebook);
   });
-  it('should show correct labem if instagram is linked', () => {
+  it('should show correct label if instagram is linked', () => {
     const title = getLinkedSocialAccountLabel('AddInstagramAcct', labelState);
     expect(title).toBe(titleInstagram);
   });
-  it('should show correct labem if twitter is linked', () => {
+  it('should show correct label if twitter is linked', () => {
     const title = getLinkedSocialAccountLabel('AddTwitterAcct', labelState);
     expect(title).toBe(titleTwitter);
   });
--- packages/core/src/components/common/organisms/SocialAccount/utils/utils.js
+++ packages/core/src/components/common/organisms/SocialAccount/utils/utils.js
@@ -1,5 +1,11 @@
 import { getLabelValue } from '../../../../../utils';
 
+/**
+ * @function getLinkedSocialAccountLabel
+ * @param {string} socialAccount - social account which is being linked.
+ * @param {object} labels - label of linked social account
+ * @returns {string} Label - Added social account reward points label
+ */
 const getLinkedSocialAccountLabel = (socialAccount, labels) => {
   switch (socialAccount) {
     case 'AddFacebookAcct':
