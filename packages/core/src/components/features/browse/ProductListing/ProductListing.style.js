528c0726724213714e25d186a085d12ac334610f
Ipsita Basak <ibasak@sapient.com>
2019-09-30 17:54:01 +0530

    Removing the dummy styles of the template since it is interfering with the implemented styles.

--- packages/core/src/components/features/browse/ProductListing/ProductListing.style.js
+++ packages/core/src/components/features/browse/ProductListing/ProductListing.style.js
@@ -1,18 +1,6 @@
 import { css } from 'styled-components';
 
 export default css`
-  .bread-crumb,
-  .filter-area,
-  .product-list {
-    background: #d8d8d8;
-    padding: 20px 0 17px;
-    margin: 5px 0;
-  }
-  .bread-crumb,
-  .product-list {
-    background: white;
-    display: flex;
-  }
   @media ${props => props.theme.mediaQuery.large} {
     .show-count-web {
       display: none;
