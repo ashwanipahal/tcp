6c61fb971bb52e3b1c39d26ad11536f2e117f4e0
Himalaya <himalaya.batra@publicissapient.com>
2019-10-01 00:11:11 +0530

    RWD-9837: adding forgotten missing code

--- packages/core/src/components/features/CnC/CartItemTile/container/CartItemTile.selectors.js
+++ packages/core/src/components/features/CnC/CartItemTile/container/CartItemTile.selectors.js
@@ -82,6 +82,10 @@ export const getProductStore = product => {
   return product.getIn(['miscInfo', 'store']);
 };
 
+export const getProductStoreAddress = product => {
+  return product.getIn(['miscInfo', 'storeAddress']);
+};
+
 export const getProductOrderItemType = product => {
   return product.getIn(['miscInfo', 'orderItemType']);
 };
@@ -296,6 +300,7 @@ export const getProductDetails = tile => {
     miscInfo: {
       badge: getProductBadge(tile),
       store: getProductStore(tile),
+      storeAddress: getProductStoreAddress(tile),
       orderItemType: getProductOrderItemType(tile),
       bossStartDate: getBossStartDate(tile),
       bossEndDate: getBossEndDate(tile),
