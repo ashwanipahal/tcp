cbdf6592538289c071fa9f0bca92e05b7d81f2bf
Akshay Arora <akshay.arora2@publicissapient.com>
2019-10-01 16:46:55 +0530

    initial commit -- in progress

---
+++ packages/core/src/components/common/molecules/ModuleG/config.js
@@ -0,0 +1,49 @@
+import theme from '../../../../../styles/themes/TCP';
+
+const { breakpoints } = theme;
+
+export default {
+  TOTAL_IMAGES: 15,
+  CAROUSEL_OPTIONS: {
+    autoplay: false,
+    arrows: true,
+    autoplaySpeed: 2000,
+    centerMode: true,
+    fade: false,
+    speed: 1000,
+    dots: false,
+    swipe: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    initialSlide: 5,
+    responsive: [
+      {
+        breakpoint: parseInt(breakpoints.medium, 10) - 1,
+        settings: {
+          slidesToShow: 4,
+          arrows: false,
+          swipeToSlide: true,
+        },
+      },
+      {
+        breakpoint: parseInt(breakpoints.large, 10) - 1,
+        settings: {
+          slidesToShow: 5,
+          arrows: false,
+          swipeToSlide: true,
+        },
+      },
+    ],
+  },
+  IMG_DATA: {
+    productImgConfig: ['t_mod_J_product_m', 't_mod_J_img_product_t', 't_mod_J_img_product_d'],
+    promoImgConfig: ['t_mod_J_img_promo_m', 't_mod_J_img_promo_t', 't_mod_J_img_promo_d'],
+  },
+  PROMO_IMG_DATA: {
+    imgConfig: [
+      'c_fill,g_face:center,h_310,w_375',
+      'c_fill,g_face:center,h_158,w_177',
+      'c_fill,g_face:center,h_219,w_279',
+    ],
+  },
+};
---
+++ packages/core/src/components/common/molecules/ModuleG/styles/ModuleG.style.js
@@ -0,0 +1,44 @@
+import { css } from 'styled-components';
+
+export default css`
+  margin-top: ${props => props.theme.spacing.LAYOUT_SPACING.MED};
+  margin-bottom: ${props => props.theme.spacing.LAYOUT_SPACING.MED};
+
+  .moduleG__carousel-wrapper {
+    margin-top: ${props => props.theme.spacing.ELEM_SPACING.LRG};
+
+    &.moduleG__carousel-top {
+      margin-bottom: ${props => props.theme.spacing.LAYOUT_SPACING.MED};
+    }
+
+    .slick-list {
+      margin-right: -22%;
+
+      @media ${props => props.theme.mediaQuery.medium} {
+        margin-right: -15%;
+      }
+
+      @media ${props => props.theme.mediaQuery.large} {
+        margin-right: auto;
+      }
+    }
+  }
+
+  .image-link {
+    display: flex;
+    align-items: center;
+    width: 89px;
+    height: 110px;
+    margin: 0 auto;
+
+    @media ${props => props.theme.mediaQuery.medium} {
+      width: 146px;
+      height: 180px;
+    }
+
+    @media ${props => props.theme.mediaQuery.large} {
+      width: 190px;
+      height: 263px;
+    }
+  }
+`;
