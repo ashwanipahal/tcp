6d7314e270b1b29426b09b6f044f056d43a24cdd
Harminder Pal Singh <harminderpal.singh@publicissapient.com>
2019-10-01 12:23:32 +0530

    removed tests

--- packages/core/src/components/common/organisms/StyliticsProductTabList/__tests__/StyliticsProductTabList-test.native.jsx
+++ packages/core/src/components/common/organisms/StyliticsProductTabList/__tests__/StyliticsProductTabList-test.native.jsx
@@ -11,30 +11,6 @@ function getTimer(time) {
 }
 
 describe('StyliticsProductTabList', () => {
-  it('Should call dispatch function if product list category data is not available ', () => {
-    const getStyliticsProductTabListData = jest.fn();
-    shallow(
-      <StyliticsProductTabList
-        tabItems={[
-          {
-            text: {
-              text: 'test',
-            },
-            category: {
-              cat_id: '2',
-            },
-          },
-        ]}
-        getStyliticsProductTabListData={getStyliticsProductTabListData}
-      />
-    );
-
-    // 4002 because currently 4000 delay is being added componentDidMount to fetch data
-    return getTimer(4002).then(() => {
-      expect(getStyliticsProductTabListData).toBeCalledTimes(1);
-    });
-  });
-
   it('Should NOT call dispatch function if product list category data is  available ', () => {
     const getStyliticsProductTabListData = jest.fn();
     shallow(
@@ -81,33 +57,5 @@ describe('StyliticsProductTabList', () => {
     return getTimer(4004).then(() => {
       expect(getStyliticsProductTabListData).toBeCalledTimes(0);
     });
-  });
-
-  it('Should provide selected tab item on tab selection ', () => {
-    const onProductTabChangeMock = jest.fn();
-    const tabItems = [
-      {
-        text: {
-          text: 'test',
-        },
-        category: {
-          cat_id: '2',
-        },
-      },
-      {
-        text: {
-          text: 'test 2',
-        },
-        category: {
-          cat_id: '3',
-        },
-      },
-    ];
-
-    shallow(
-      <StyliticsProductTabList tabItems={tabItems} onProductTabChange={onProductTabChangeMock} />
-    );
-
-    expect(onProductTabChangeMock).toHaveBeenCalledWith('2', tabItems[0]);
   });
 });
