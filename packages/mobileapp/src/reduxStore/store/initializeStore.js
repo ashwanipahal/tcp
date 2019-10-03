c65ba598daaa4ba20b084a29b5bfe69a84d43dd0
Michael Edelman <michael@fabulas.io>
2019-10-03 13:32:28 -0400

    chore: add & wire-up redux-beacon

--- packages/mobileapp/src/reduxStore/store/initializeStore.js
+++ packages/mobileapp/src/reduxStore/store/initializeStore.js
@@ -1,32 +1,31 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+
 import Reactotron from 'reactotron-react-native';
 import { cacheEnhancerMiddleware } from '@tcp/core/src/utils/cache.util';
 import { setStoreRef } from '@tcp/core/src/utils/store.utils';
 
 import globalReducers from '../reducers';
 import rootSaga from '../sagas/sagas';
-
-let store;
+import createAnalyticsMiddleware from '../middlewares/analytics';
 
 export const initializeStore = initialState => {
   const sagaMiddleware = createSagaMiddleware();
 
+  const middlewares = [sagaMiddleware, createAnalyticsMiddleware()];
+
+  const enhancers = [
+    applyMiddleware(...middlewares),
+    __DEV__ && Reactotron.createEnhancer(),
+    cacheEnhancerMiddleware(),
+  ];
+  /* eslint-disable  */
+
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
   // eslint-disable-next-line no-underscore-dangle
-  const composeEnhancers = (__DEV__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-  if (__DEV__) {
-    store = createStore(
-      globalReducers,
-      initialState,
-      composeEnhancers(applyMiddleware(sagaMiddleware), Reactotron.createEnhancer())
-    );
-  } else {
-    store = createStore(
-      globalReducers,
-      initialState,
-      composeEnhancers(applyMiddleware(sagaMiddleware), cacheEnhancerMiddleware())
-    );
-  }
+
+  const store = createStore(globalReducers, initialState, composeEnhancers(...enhancers));
 
   store.sagaTask = sagaMiddleware.run(rootSaga);
 
