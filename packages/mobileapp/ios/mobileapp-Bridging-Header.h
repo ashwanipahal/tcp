fa1fb753e242d1aca85e5d3239f7179efda42fad
Michael Edelman <michael@fabulas.io>
2019-10-04 18:54:31 -0400

    feat: wire up iOS for native-side analytics init

--- packages/mobileapp/ios/mobileapp-Bridging-Header.h
+++ packages/mobileapp/ios/mobileapp-Bridging-Header.h
@@ -9,3 +9,8 @@
 #import <React/RCTUtils.h>
 #import <React/RCTConvert.h>
 #import <React/RCTBundleURLProvider.h>
+#import <RCTACPCore/ACPCore.h>
+#import <RCTACPCore/ACPLifecycle.h>
+#import <RCTACPCore/ACPIdentity.h>
+#import <RCTACPCore/ACPSignal.h>
+
