e6f496efa5ff30c9cd0e5ebdcab994e5b5f9e06d
Harmeet Singh <harmeet.singh@sapient.com>
2019-10-05 19:26:51 +0530

    test cases added

--- packages/core/src/components/features/CnC/LoyaltyBanner/molecules/LoyaltyBannerSection/__tests__/LoyaltyBannerSection-test.jsx
+++ packages/core/src/components/features/CnC/LoyaltyBanner/molecules/LoyaltyBannerSection/__tests__/LoyaltyBannerSection-test.jsx
@@ -11,9 +11,13 @@ describe('LoyaltyBanner View Component', () => {
     currentSubtotal: 21,
     estimatedSubtotal: 33,
     thresholdValue: 16.66,
+    isGuest: true,
+    earnedReward: 0,
+    isPlcc: false,
+    pointsToNextReward: false,
   };
 
-  it('OrderLedger should render correctly', () => {
+  it('not isPlcc should render correctly', () => {
     component = shallow(<LoyaltyBannerSectionVanilla {...Props} />);
     expect(component).toMatchSnapshot();
   });
@@ -25,10 +29,86 @@ describe('LoyaltyBanner View Component', () => {
     currentSubtotal: 21,
     estimatedSubtotal: 33,
     thresholdValue: 16.66,
+    isGuest: false,
+    earnedReward: 0,
+    isPlcc: false,
+    pointsToNextReward: false,
   };
 
-  it('OrderLedger should render correctly', () => {
+  it('isPlcc should render correctly', () => {
     component = shallow(<LoyaltyBannerSectionVanilla {...Props2} />);
+    expect(component).toMatchSnapshot();
+  });
+
+  const Props3 = {
+    className: '',
+    labels: {},
+    estimatedRewardsVal: 200,
+    currentSubtotal: 21,
+    estimatedSubtotal: 33,
+    thresholdValue: 16.66,
+    isGuest: false,
+    earnedReward: 0,
+    isPlcc: true,
+    pointsToNextReward: false,
+  };
+
+  it('earnedReward should render correctly', () => {
+    component = shallow(<LoyaltyBannerSectionVanilla {...Props3} />);
+    expect(component).toMatchSnapshot();
+  });
+
+  const Props4 = {
+    className: '',
+    labels: {},
+    estimatedRewardsVal: 200,
+    currentSubtotal: 21,
+    estimatedSubtotal: 33,
+    thresholdValue: 16.66,
+    isGuest: false,
+    earnedReward: 1,
+    isPlcc: true,
+    pointsToNextReward: false,
+  };
+
+  it('earnedReward isGuest should render correctly', () => {
+    component = shallow(<LoyaltyBannerSectionVanilla {...Props4} />);
+    expect(component).toMatchSnapshot();
+  });
+
+  const Props5 = {
+    className: '',
+    labels: {},
+    estimatedRewardsVal: 200,
+    currentSubtotal: 21,
+    estimatedSubtotal: 33,
+    thresholdValue: 16.66,
+    isGuest: true,
+    earnedReward: 1,
+    isPlcc: true,
+    pointsToNextReward: false,
+  };
+
+  it('isGuest should render correctly', () => {
+    component = shallow(<LoyaltyBannerSectionVanilla {...Props5} />);
+    expect(component).toMatchSnapshot();
+  });
+
+  const Props6 = {
+    className: '',
+    labels: {},
+    estimatedRewardsVal: 200,
+    currentSubtotal: 21,
+    estimatedSubtotal: 33,
+    thresholdValue: 16.66,
+    isGuest: false,
+    earnedReward: 1,
+    isPlcc: false,
+    pointsToNextReward: false,
+  };
+
+  it('isGuest isPlcc should render correctly', () => {
+    component = shallow(<LoyaltyBannerSectionVanilla {...Props6} />);
     expect(component).toMatchSnapshot();
   });
 });
