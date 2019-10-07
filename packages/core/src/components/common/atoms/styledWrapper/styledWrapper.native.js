9ee9dfebbb7060a8ec14c01e2aac4ebd83fae18c
Rajeev Jain <rajeev.jain@publicissapient.com>
2019-10-07 13:18:44 +0530

    Fixed: State should be in uppercase letter

--- packages/core/src/components/common/atoms/styledWrapper/styledWrapper.native.js
+++ packages/core/src/components/common/atoms/styledWrapper/styledWrapper.native.js
@@ -87,3 +87,8 @@ export const TextWithSpacing = styled.Text`
 export const BodyCopyWithTextTransform = styled(BodyCopy)`
   text-transform: ${props => (props.textTransform ? props.textTransform : 'capitalize')};
 `;
+export const Row = styled.View`
+  flex-direction: row;
+  display: flex;
+  justify-content: ${props => (props.justifyContent ? props.justifyContent : 'flex-start')};
+`;
