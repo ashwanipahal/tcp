b918f9ef9d9cfacf4a7b8bf49e25e29ef5069506
Sanjeev chauhan <sanjeev.chauhan@publicissapient.com>
2019-10-09 14:09:50 +0530

    Ribbon.jsx test cases updted

--- packages/core/src/components/common/molecules/Ribbon/__tests__/Ribbon-test.jsx
+++ packages/core/src/components/common/molecules/Ribbon/__tests__/Ribbon-test.jsx
@@ -5,8 +5,7 @@ import mock from '../../../../../services/abstractors/common/moduleS/mock-v2';
 
 describe('RibbonComponent', () => {
   let wrapper;
-
-  beforeEach(() => {
+  it('should match snapshot', () => {
     wrapper = shallow(
       <Ribbon
         ribbonBanner={mock.moduleS.composites.ribbonBanner}
@@ -16,9 +15,25 @@ describe('RibbonComponent', () => {
         locator="ribbon_test_locator"
       />
     );
+    expect(wrapper).toMatchSnapshot();
   });
-
-  it('should match snapshot', () => {
+  it('should call the getConfig getImgPath', () => {
+    wrapper = shallow(
+      <Ribbon
+        ribbonBanner={[
+          {
+            textItems: [[Object]],
+            link: { url: '/c/girls-clothes', text: 'Girls', title: 'Girls', target: '' },
+            ribbonPlacement: 'right',
+            ribbonClass: 'ribbon-color-cherry',
+          },
+        ]}
+        width="200"
+        height="70"
+        color="white"
+        locator="ribbon_test_locator"
+      />
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
