d9f38bf17312f7939b2ed6c9565a3bf279d34009
Gagan Bhardwaj <gbhardwaj4@sapient.com>
2019-10-09 22:58:54 -0400

    RWD-11420: Venmo Client token fetch and CTA

---
+++ packages/core/src/components/common/atoms/VenmoPaymentButton/views/VenmoPaymentButton.view.native.jsx
@@ -0,0 +1,181 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { string, func, bool, shape, oneOf } from 'prop-types';
+import Image from '../../Image/views/Image';
+import logger from '../../../../../utils/loggerInstance';
+import { modes, constants, VENMO_USER_STATES } from '../container/VenmoPaymentButton.util';
+import VenmoButton from '../styles/VenmoPaymentButton.style.native';
+
+const venmoIconBlue = require('../../../../../assets/venmo_logo_blue.png');
+
+export class VenmoPaymentButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasVenmoError: true,
+    };
+  }
+
+  /**
+   * @function fetchVenmoClientToken
+   * @description Fetch venmo token details from the backend api. This is used to create instance of venmo and for authorization
+   */
+  fetchVenmoClientToken = () => {
+    const { isGuest, orderId, enabled, isNonceNotExpired, getVenmoPaymentTokenAction } = this.props;
+    if (enabled && !isNonceNotExpired) {
+      let userState = '';
+      if (isGuest) {
+        userState = VENMO_USER_STATES.GUEST;
+      } else {
+        userState = VENMO_USER_STATES.REGISTERED;
+      }
+      getVenmoPaymentTokenAction({ userState, orderId });
+    }
+  };
+
+  /**
+   * This method is to validate if we can call the venmo client token api.
+   */
+  canCallVenmoApi = () => {
+    const { authorizationKey, mode, enabled } = this.props;
+    return enabled && authorizationKey && mode === modes.CLIENT_TOKEN;
+  };
+
+  // Logic will go here for in some cases, we may not want to display an error message
+  handleVenmoClickedError = e => logger.error('Venmo', 'Promises Error', e);
+
+  handleVenmoInstanceError = err => {
+    const { hasVenmoError } = this.state;
+    if (!hasVenmoError) {
+      this.setState({ hasVenmoError: true });
+    }
+    this.handleVenmoClickedError(err);
+  };
+
+  handleVenmoClick = () => {
+    const {
+      setVenmoData,
+      onVenmoPaymentButtonClick,
+      mode,
+      setVenmoPaymentInProgress,
+      isRemoveOOSItems,
+    } = this.props;
+    // Condition for OOS items in the bag, modal will open and proceed with regular checkout on continue cta trigger
+    if (isRemoveOOSItems) {
+      onVenmoPaymentButtonClick(mode);
+      setVenmoData({ loading: false });
+    }
+    setVenmoData({ loading: true, error: null });
+    setVenmoPaymentInProgress(true);
+  };
+
+  /**
+   * This callback method is called once venmo app authorization is successfull.
+   * @param {object} payload - response from the venmo authorization interface.
+   */
+  handleVenmoSuccess = payload => {
+    const { setVenmoData, mode, onVenmoPaymentButtonClick } = this.props;
+    const successData = { ...payload, error: null, timestamp: Date.now() };
+    setVenmoData(successData);
+    onVenmoPaymentButtonClick(mode);
+  };
+
+  /**
+   * This callback methos is called if we get any error or user is not authorized from the app.
+   * @param {object} code - error code from the venmo plugin
+   * @param {string} message - error message
+   * @param {string} name - name of the venmo user tring to authenticate
+   */
+  handleVenmoError = ({ code, message, name }) => {
+    const { setVenmoData, onVenmoPaymentButtonError } = this.props;
+    const errorData = { nonce: '', error: { code, message, name } };
+    if (code !== constants.VENMO_CANCELED) {
+      setVenmoData(errorData);
+      onVenmoPaymentButtonError(errorData);
+      // Suppress error message
+    }
+  };
+
+  componentDidUpdate = prevProps => {
+    const { isGuest } = this.props;
+    if (prevProps.isGuest !== isGuest) {
+      // Condition for bag page reload on registered user, and user logging in from bag page
+      this.fetchVenmoClientToken();
+    }
+  };
+
+  componentDidMount = () => {
+    const {
+      venmoData: { nonce },
+      setVenmoData,
+      isNonceNotExpired,
+    } = this.props;
+    if (nonce && isNonceNotExpired) {
+      this.setState({ hasVenmoError: false });
+      setVenmoData({ loading: false });
+    }
+    this.fetchVenmoClientToken();
+  };
+
+  render() {
+    const { mode, enabled, className } = this.props;
+    return (
+      <View className={className}>
+        {enabled && (this.canCallVenmoApi() || mode === modes.PAYMENT_TOKEN) && (
+          <VenmoButton accessibilityRole="none">
+            <Image source={venmoIconBlue} width={150} height={28} />
+          </VenmoButton>
+        )}
+      </View>
+    );
+  }
+}
+
+VenmoPaymentButton.propTypes = {
+  className: string,
+  enabled: bool,
+  authorizationKey: string,
+  mode: oneOf([modes.CLIENT_TOKEN, modes.PAYMENT_TOKEN]),
+  setVenmoData: func,
+  venmoData: shape({
+    deviceData: string,
+    supportedByBrowser: bool,
+    loading: bool,
+    details: shape({
+      username: string,
+    }),
+    nonce: string,
+    type: string,
+    error: shape({
+      code: string,
+      message: string,
+      name: string,
+    }),
+  }),
+  onVenmoPaymentButtonClick: func,
+  onVenmoPaymentButtonError: func,
+  setVenmoPaymentInProgress: func,
+  getVenmoPaymentTokenAction: func.isRequired,
+  isNonceNotExpired: bool,
+  isRemoveOOSItems: bool,
+  isGuest: bool.isRequired,
+  orderId: string.isRequired,
+};
+
+VenmoPaymentButton.defaultProps = {
+  className: '',
+  enabled: false,
+  authorizationKey: '',
+  setVenmoData: () => {},
+  venmoData: {
+    supportedByBrowser: false,
+  },
+  mode: modes.CLIENT_TOKEN,
+  onVenmoPaymentButtonClick: () => {},
+  onVenmoPaymentButtonError: () => {},
+  setVenmoPaymentInProgress: () => {},
+  isNonceNotExpired: false,
+  isRemoveOOSItems: false,
+};
+
+export default VenmoPaymentButton;
---
+++ packages/core/src/components/common/atoms/VenmoPaymentButton/views/__tests__/VenmoPaymentButton.view-test.native.jsx
@@ -0,0 +1,130 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Image } from '@tcp/core/src/components/common/atoms';
+import { VenmoPaymentButton } from '../VenmoPaymentButton.view.native';
+import { modes } from '../../container/VenmoPaymentButton.util';
+
+describe('Venmo Payment Button', () => {
+  const props = {
+    setVenmoData: jest.fn(),
+    onVenmoPaymentButtonClick: jest.fn(),
+    onVenmoPaymentButtonError: jest.fn(),
+    className: 'venmo-container sc-gqjmRU gvHLxz',
+    enabled: true,
+    isMobile: true,
+    mode: modes.PAYMENT_TOKEN,
+    authorizationKey: 'encrytptedauthorizationkey',
+    isNonceNotExpired: false,
+    isRemoveOOSItems: false,
+    venmoData: {
+      venmoClientTokenData: {
+        userState: 'R',
+        venmoCustomerIdAvailable: 'FALSE',
+        venmoIsDefaultPaymentType: 'FALSE',
+        venmoPaymentTokenAvailable: 'FALSE',
+        venmoSecurityToken: 'encrytptedauthorizationkey',
+      },
+      deviceData: '762a73c4175ca24f7b1436a440da5bd0',
+      supportedByBrowser: true,
+      loading: false,
+    },
+    venmoClientTokenData: {
+      userState: 'R',
+      venmoCustomerIdAvailable: 'FALSE',
+      venmoIsDefaultPaymentType: 'FALSE',
+      venmoPaymentTokenAvailable: 'FALSE',
+      venmoSecurityToken: 'encrytptedauthorizationkey',
+    },
+    allowNewBrowserTab: true,
+    isGuest: false,
+    orderId: 3000332630,
+    setVenmoPaymentInProgress: jest.fn(),
+    getVenmoPaymentTokenAction: jest.fn(),
+    setVenmoDataAction: jest.fn(),
+  };
+
+  const e = {
+    preventDefault: jest.fn(),
+  };
+
+  it('should render correctly', () => {
+    const tree = shallow(<VenmoPaymentButton {...props} />);
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render Image correctly', () => {
+    const tree = shallow(<VenmoPaymentButton {...props} />);
+    expect(tree.find(Image)).toHaveLength(1);
+  });
+
+  it('calling handleVenmoSuccess method', () => {
+    const tree = shallow(<VenmoPaymentButton {...props} />);
+    const componentInstance = tree.instance();
+    componentInstance.handleVenmoSuccess({ nonce: 'encryptedtext' });
+    expect(props.setVenmoData).toBeCalled();
+    expect(props.onVenmoPaymentButtonClick).toBeCalled();
+  });
+
+  it('calling handleVenmoError method', () => {
+    const tree = shallow(<VenmoPaymentButton {...props} />);
+    const componentInstance = tree.instance();
+    componentInstance.handleVenmoError({ code: 'error code 400' });
+    expect(props.onVenmoPaymentButtonError).toBeCalled();
+  });
+
+  it('calling handleVenmoInstanceError method', () => {
+    const tree = shallow(<VenmoPaymentButton {...props} />);
+    const componentInstance = tree.instance();
+    tree.setState({ hasVenmoError: true });
+    componentInstance.handleVenmoInstanceError({ code: 'error code 400' });
+    expect(tree.state('hasVenmoError')).toBeTruthy();
+  });
+
+  it('calling handleVenmoInstanceError method without error arg', () => {
+    const tree = shallow(<VenmoPaymentButton {...props} />);
+    const componentInstance = tree.instance();
+    tree.setState({ hasVenmoError: false });
+    const handleVenmoClickedError = jest.spyOn(componentInstance, 'handleVenmoClickedError');
+    componentInstance.handleVenmoInstanceError();
+    expect(tree.state('hasVenmoError')).toBe(true);
+    expect(handleVenmoClickedError).toHaveBeenCalled();
+  });
+
+  it('calling handleVenmoClick method', () => {
+    const tree = shallow(<VenmoPaymentButton {...props} />);
+    const componentInstance = tree.instance();
+    componentInstance.canCallVenmoApi = jest.fn();
+    componentInstance.venmoInstance = {};
+    componentInstance.handleVenmoClick(e);
+    expect(componentInstance.canCallVenmoApi).not.toBeCalled();
+    expect(props.setVenmoPaymentInProgress).toBeCalled();
+  });
+
+  it('calling handleVenmoClick method for OOS Items', () => {
+    const newProps = {
+      ...props,
+      isRemoveOOSItems: true,
+    };
+    const tree = shallow(<VenmoPaymentButton {...newProps} />);
+    const componentInstance = tree.instance();
+    componentInstance.canCallVenmoApi = jest.fn();
+    componentInstance.handleVenmoClick(e);
+    expect(componentInstance.canCallVenmoApi).not.toBeCalled();
+    expect(props.setVenmoPaymentInProgress).toBeCalled();
+    expect(props.onVenmoPaymentButtonClick).toBeCalled();
+  });
+
+  it('calling canCallVenmoApi method', () => {
+    const tree = shallow(<VenmoPaymentButton {...props} />);
+    const componentInstance = tree.instance();
+    expect(componentInstance.canCallVenmoApi()).toEqual(false);
+  });
+
+  it('calling fetchVenmoClientToken method', () => {
+    const getVenmoPaymentTokenAction = jest.fn();
+    const tree = shallow(<VenmoPaymentButton {...props} />);
+    const componentInstance = tree.instance();
+    componentInstance.fetchVenmoClientToken();
+    expect(getVenmoPaymentTokenAction).not.toHaveBeenCalled();
+  });
+});
