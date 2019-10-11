6e1396d99463e219fb37f4783c50f23fd905cea4
Ravee <sumer.patel@publicissapient.com>
2019-10-11 17:52:00 +0530

    RWD-6412 story implemented and  no content entered search button press handled

--- packages/core/src/components/common/organisms/SearchProduct/views/SearchProduct.view.native.jsx
+++ packages/core/src/components/common/organisms/SearchProduct/views/SearchProduct.view.native.jsx
@@ -136,7 +136,7 @@ class SearchProduct extends React.PureComponent {
           value={searchText}
           onSubmitEditing={() => {
             const { searchText: text } = this.state;
-            if (text.length > 0) this.searchProducts(text);
+            this.searchProducts(text);
           }}
         />
         {searchText.length > 0 ? (
@@ -160,7 +160,6 @@ class SearchProduct extends React.PureComponent {
         )}
         <Button
           buttonVariation="fixed-width"
-          width="40px"
           text="Cancel"
           fontSize="fs13"
           fontWeight="regular"
