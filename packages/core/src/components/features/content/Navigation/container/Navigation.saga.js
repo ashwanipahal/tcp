ebc5049546ee6cdd179b32e5aca50694814375ac
Mayank <mayank.rastogi@publicissapient.com>
2019-10-13 12:34:40 +0530

    temp fix - comment out navigation - as discussed with sanjeev

--- packages/core/src/components/features/content/Navigation/container/Navigation.saga.js
+++ packages/core/src/components/features/content/Navigation/container/Navigation.saga.js
@@ -1,16 +1,17 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
-import NavigationAbstractor from '../../../../../services/abstractors/bootstrap/navigation';
-import { loadNavigationData } from './Navigation.actions';
+// import { call, put, takeLatest } from 'redux-saga/effects';
+// import NavigationAbstractor from '../../../../../services/abstractors/bootstrap/navigation';
+// import { loadNavigationData } from './Navigation.actions';
 import { FETCH_NAVIGATION_DATA } from './Navigation.constants';
 import { validateReduxCache } from '../../../../../utils/cache.util';
 
 function* fetchNavigationData() {
-  const result = yield call(NavigationAbstractor.getData, 'navigation');
-  yield put(loadNavigationData(result));
+  // const result = yield call(NavigationAbstractor.getData, 'navigation');
+  // yield put(loadNavigationData(result));
 }
 
 function* NavigationSaga() {
   const cachedFetchNavigation = validateReduxCache(fetchNavigationData);
+  // eslint-disable-next-line no-undef
   yield takeLatest(FETCH_NAVIGATION_DATA, cachedFetchNavigation);
 }
 
