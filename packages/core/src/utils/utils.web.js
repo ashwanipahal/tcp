08ad9946453fcff6976508e9e58cf0994a3996d7
Ipsita Basak <ibasak@sapient.com>
2019-10-16 08:12:17 +0530

    Fetching the country from the country config instead of extracting it from siteId
    JIRA - RWD-12273
    
    - Fetching the country from the countryConfig object instead of extracting it from siteId since its already available in the countryConfig.

--- packages/core/src/utils/utils.web.js
+++ packages/core/src/utils/utils.web.js
@@ -340,10 +340,9 @@ export const handleGenericKeyDown = (event, key, method) => {
     method();
   }
 };
-const getAPIInfoFromEnv = (apiSiteInfo, processEnv, siteId) => {
-  const country = siteId && siteId.toUpperCase();
+const getAPIInfoFromEnv = (apiSiteInfo, processEnv, countryKey) => {
   const apiEndpoint = processEnv.RWD_WEB_API_DOMAIN || ''; // TO ensure relative URLs for MS APIs
-  const unbxdApiKey = processEnv[`RWD_WEB_UNBXD_API_KEY_${country}_EN`];
+  const unbxdApiKey = processEnv[`RWD_WEB_UNBXD_API_KEY${countryKey}_EN`];
   return {
     traceIdCount: 0,
     langId: processEnv.RWD_WEB_LANGID || apiSiteInfo.langId,
@@ -355,7 +354,7 @@ const getAPIInfoFromEnv = (apiSiteInfo, processEnv, siteId) => {
     unbxd: processEnv.RWD_WEB_UNBXD_DOMAIN || apiSiteInfo.unbxd,
     fbkey: processEnv.RWD_WEB_FACEBOOKKEY,
     instakey: processEnv.RWD_WEB_INSTAGRAM,
-    unboxKey: `${unbxdApiKey}/${processEnv[`RWD_WEB_UNBXD_SITE_KEY_${country}_EN`]}`,
+    unboxKey: `${unbxdApiKey}/${processEnv[`RWD_WEB_UNBXD_SITE_KEY${countryKey}_EN`]}`,
     unbxdApiKey,
     envId: processEnv.RWD_WEB_ENV_ID,
     previewEnvId: processEnv.RWD_WEB_STG_ENV_ID,
@@ -436,7 +435,11 @@ export const createAPIConfig = resLocals => {
   const apiSiteInfo = API_CONFIG.sitesInfo;
   const processEnv = process.env;
   const relHostname = apiSiteInfo.proto + apiSiteInfo.protoSeparator + hostname;
-  const basicConfig = getAPIInfoFromEnv(apiSiteInfo, processEnv, siteId);
+  const basicConfig = getAPIInfoFromEnv(
+    apiSiteInfo,
+    processEnv,
+    countryConfig && countryConfig.countryKey
+  );
   const graphQLConfig = getGraphQLApiFromEnv(apiSiteInfo, processEnv, relHostname);
   return {
     ...basicConfig,
