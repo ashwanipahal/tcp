b13d9d2e300ff11195debd913cb4fd8343c672fc
Rakesh Menon <rmenon3@sapient.com>
2019-10-18 01:49:59 +0530

    SEO Data - added NOSONAR for default params rule - 2

--- packages/core/src/reduxStore/reducers/seoData.js
+++ packages/core/src/reduxStore/reducers/seoData.js
@@ -3,8 +3,7 @@ import GLOBAL_CONSTANTS from '../constants';
 
 // TODO: Remove NOSONAR from line below and disable the default parameters rule
 // Remove eslint-disable after NOSONAR is removed
-const SEODataReducer = (state = {}, action) => {
-  //NOSONAR
+/* //NOSONAR */ const SEODataReducer = (state = {}, action) => {
   switch (action.type) {
     case GLOBAL_CONSTANTS.LOAD_SEO_DATA:
       return { ...state, ...action.payload };
