b867487721fb1956fabcff4605efc3d82ec0adc4
Rakesh Menon <rmenon3@sapient.com>
2019-10-18 01:19:28 +0530

    SEO Data - added processData test

--- packages/core/src/services/abstractors/bootstrap/seoData/__tests__/seoData-test.js
+++ packages/core/src/services/abstractors/bootstrap/seoData/__tests__/seoData-test.js
@@ -3,7 +3,7 @@ import mock from '../mock';
 
 jest.mock('../../../../handler/handler');
 
-it('SEO Data Abstractor | ', () => {
+it('SEO Data Abstractor | getData', () => {
   SEODataAbstractor.getData('seoData', {
     page: '/home',
     brand: 'TCP',
@@ -17,4 +17,17 @@ it('SEO Data Abstractor | ', () => {
 it('SEO Data Abstractor | getMock', () => {
   const data = SEODataAbstractor.getMock();
   expect(data).toMatchObject(mock);
+});
+
+it('SEO Data Abstractor | processData', () => {
+  const data = SEODataAbstractor.processData(mock);
+  expect(data.plp).toEqual({
+    pageTitle: 'PLP',
+    keywords: 'seo, tcp',
+    description: 'this is my plp seo desc',
+    robotsInfo: null,
+    canonicalUrl: null,
+    hrefLang: null,
+    thumbnailUrl: null,
+  });
 });
