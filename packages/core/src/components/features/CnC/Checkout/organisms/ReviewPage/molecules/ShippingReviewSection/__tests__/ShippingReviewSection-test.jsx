189d9ff6120fcd518ac8ca419ac0e131fe3612e9
Madan Joshi <mjoshi8@sapient.com>
2019-10-17 13:59:02 +0530

    Review comment

--- packages/core/src/components/features/CnC/Checkout/organisms/ReviewPage/molecules/ShippingReviewSection/__tests__/ShippingReviewSection-test.jsx
+++ packages/core/src/components/features/CnC/Checkout/organisms/ReviewPage/molecules/ShippingReviewSection/__tests__/ShippingReviewSection-test.jsx
@@ -33,4 +33,36 @@ describe('GiftWrappingDisplay component', () => {
     const component = shallow(<ShippingReviewSectionvanilla {...props} />);
     expect(component).toMatchSnapshot();
   });
+
+  it('calling componentDidUpdate method', () => {
+    const props = {
+      className: '',
+      labels: {
+        lbl_review_sectionShippingMethodTitle: 'title',
+      },
+      displayName: 'Free',
+      isExpressCheckout: true,
+      updateShippingMethodSelection: jest.fn(),
+      expressReviewShippingSectionId: {
+        shippingMethodId: '123',
+      },
+    };
+    const prevProps = {
+      className: '',
+      labels: {
+        lbl_review_sectionShippingMethodTitle: 'title',
+      },
+      displayName: 'Free',
+      isExpressCheckout: true,
+      updateShippingMethodSelection: jest.fn(),
+      expressReviewShippingSectionId: {
+        shippingMethodId: '354',
+      },
+    };
+    const tree = shallow(<ShippingReviewSectionvanilla {...props} />);
+    const componentInstance = tree.instance();
+    jest.spyOn(componentInstance, 'componentDidUpdate');
+    componentInstance.componentDidUpdate(prevProps);
+    expect(componentInstance.componentDidUpdate).toHaveBeenCalled();
+  });
 });
