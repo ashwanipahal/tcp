39640277872835a99dc52d2aea4d34a574f425a3
Rakesh Menon <rmenon3@sapient.com>
2019-10-19 14:44:05 +0530

    Moved seoData out of bootstrap as it's a page specific call

--- packages/core/src/services/abstractors/bootstrap/bootstrap.js
+++ packages/core/src/services/abstractors/bootstrap/bootstrap.js
@@ -1,7 +1,6 @@
 import logger from '@tcp/core/src/utils/loggerInstance';
 import layoutAbstractor from './layout';
 import labelsAbstractor from './labels';
-import seoDataAbstractor from './seoData';
 import headerAbstractor from './header';
 import footerAbstractor from './footer';
 import navigationAbstractor from './navigation';
@@ -186,9 +185,6 @@ const bootstrap = async (pageName = '', modules, cachedData) => {
       footerAbstractor.processData(retrieveCachedData({ ...fetchCachedDataParams, key: 'footer' }));
     response.labels = labelsAbstractor.processData(
       retrieveCachedData({ ...fetchCachedDataParams, key: 'labels' })
-    );
-    response.seoData = seoDataAbstractor.processData(
-      retrieveCachedData({ ...fetchCachedDataParams, key: 'seoData' })
     );
     response.navigation = navigationAbstractor.processData(
       retrieveCachedData({ ...fetchCachedDataParams, key: 'navigation' })
