8213f086fd71be8b5290b0a18465cdf1a5a84c42
Ipsita Basak <ibasak@sapient.com>
2019-10-22 17:12:41 +0530

    Adding the fix for showing capital title and scroll check for app.

--- packages/core/src/components/common/organisms/QuickViewModal/views/QuickViewModal.view.jsx
+++ packages/core/src/components/common/organisms/QuickViewModal/views/QuickViewModal.view.jsx
@@ -6,7 +6,7 @@ import styles, {
   quickViewColorSwatchesCss,
 } from '../styles/QuickViewModal.style';
 import FulfillmentSection from '../../FulfillmentSection';
-import { getLocator, enableBodyScroll } from '../../../../../utils';
+import { getLocator, enableBodyScroll, isMobileApp } from '../../../../../utils';
 import Modal from '../../../molecules/Modal';
 import { PRODUCT_INFO_PROP_TYPE_SHAPE } from '../../../../features/browse/ProductListing/molecules/ProductList/propTypes/productsAndItemsPropTypes';
 import ProductCustomizeFormPart from '../molecules/ProductCustomizeFormPart';
@@ -26,7 +26,9 @@ class QuickViewModal extends React.Component {
   };
 
   onCloseClick = () => {
-    enableBodyScroll();
+    if (!isMobileApp()) {
+      enableBodyScroll();
+    }
     const { closeQuickViewModal } = this.props;
     closeQuickViewModal();
   };
