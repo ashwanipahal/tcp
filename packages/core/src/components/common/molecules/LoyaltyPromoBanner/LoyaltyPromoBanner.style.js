4e53452823c4edb21a93f0ffd6fbe37667298839
Harminder Pal Singh <harminderpal.singh@publicissapient.com>
2019-10-22 17:15:33 +0530

    updated close button

--- packages/core/src/components/common/molecules/LoyaltyPromoBanner/LoyaltyPromoBanner.style.js
+++ packages/core/src/components/common/molecules/LoyaltyPromoBanner/LoyaltyPromoBanner.style.js
@@ -1,8 +1,35 @@
 import { css } from 'styled-components';
 
 const style = css`
-  margin: ${props => props.theme.spacing.ELEM_SPACING.LRG} auto;
+  margin: ${props => props.theme.spacing.ELEM_SPACING.SM} auto;
   display: block;
+  position: relative;
+
+  .loyalty-promo-close-btn {
+    padding: 0 8px;
+    margin-left: 10px;
+    cursor: pointer;
+    border: 0;
+    background: transparent;
+    position: absolute;
+    right: 6px;
+    top: 0;
+    height: 100%;
+  }
+
+  .loyalty-promo-close-btn-icon {
+    width: ${props => props.theme.spacing.ELEM_SPACING.XS};
+    height: ${props => props.theme.spacing.ELEM_SPACING.XS};
+  }
+
+  @media ${props => props.theme.mediaQuery.large} {
+    margin: ${props => props.theme.spacing.ELEM_SPACING.LRG} auto;
+
+    .loyalty-promo-close-btn-icon {
+      width: ${props => props.theme.spacing.ELEM_SPACING.MED};
+      height: ${props => props.theme.spacing.ELEM_SPACING.MED};
+    }
+  }
 `;
 
 export default style;
--- packages/core/src/components/common/molecules/LoyaltyPromoBanner/view/LoyaltyPromoBanner.jsx
+++ packages/core/src/components/common/molecules/LoyaltyPromoBanner/view/LoyaltyPromoBanner.jsx
@@ -6,42 +6,12 @@ import withStyles from '../../../hoc/withStyles';
 
 import style from '../LoyaltyPromoBanner.style';
 
-export const useLoyaltyBannerCloseStatus = () => {
-  const [bannerClosed, setBannerClosed] = useState(true);
-
-  useEffect(() => {
-    const getUUID = uuidCookieString => {
-      const UUID = readCookie(uuidCookieString);
-      return UUID ? UUID.split(',')[0] : '-1002';
-    };
-    const cookieName = `mprAboveHead_${getUUID('WC_USERACTIVITY_')}`;
-    const closeBtn = document.getElementById('loyaltyBannerCloseBtn');
-    const closeButtonHandler = e => {
-      e.stopPropagation();
-      const currentDate = new Date();
-      setCookie({
-        key: cookieName,
-        value: currentDate.toGMTString(),
-        daysAlive: 10,
-      });
-      setBannerClosed(true);
-    };
-
-    if (!readCookie(cookieName)) {
-      if (closeBtn) {
-        closeBtn.addEventListener('click', closeButtonHandler);
-      }
-      setBannerClosed(false);
-    }
-
-    return () => {
-      if (closeBtn) {
-        closeBtn.removeEventListener('click', closeButtonHandler);
-      }
-    };
-  }, [bannerClosed]);
-
-  return bannerClosed;
+/**
+ * Check if required UUID cookie available else return default.
+ */
+const getUUID = uuidCookieString => {
+  const UUID = readCookie(uuidCookieString);
+  return UUID ? UUID.split(',')[0] : '-1002';
 };
 
 /* istanbul ignore next */
@@ -51,8 +21,26 @@ const LoyaltyPromoBanner = props => {
     richTextList: [{ text, link }],
     dataLocator,
   } = props;
+  const cookieName = `mprAboveHead_${getUUID('WC_USERACTIVITY_')}`;
+  const [bannerClosed, setBannerClosed] = useState(true);
 
-  return !useLoyaltyBannerCloseStatus() ? (
+  useEffect(() => {
+    if (!readCookie(cookieName)) {
+      setBannerClosed(false);
+    }
+  }, [bannerClosed]);
+
+  const closeButtonHandler = () => {
+    const currentDate = new Date();
+    setCookie({
+      key: cookieName,
+      value: currentDate.toGMTString(),
+      daysAlive: 10,
+    });
+    setBannerClosed(true);
+  };
+
+  return bannerClosed ? null : (
     <div className={`${className} content-wrapper`}>
       <Row>
         <Col
@@ -71,10 +59,19 @@ const LoyaltyPromoBanner = props => {
           >
             <RichText richTextHtml={text} />
           </Anchor>
+          <button className="loyalty-promo-close-btn" onClick={closeButtonHandler}>
+            <svg className="loyalty-promo-close-btn-icon" viewBox="0 0 25 25">
+              <path
+                fill="#a0a0a0"
+                fillRule="nonzero"
+                d="M14.107 12.5l10.56-10.56A1.136 1.136 0 1 0 23.06.333L12.5 10.893 1.94.333A1.136 1.136 0 1 0 .333 1.94l10.56 10.56L.333 23.06a1.136 1.136 0 1 0 1.607 1.607l10.56-10.56 10.56 10.56c.222.222.513.333.804.333a1.136 1.136 0 0 0 .803-1.94L14.107 12.5z"
+              />
+            </svg>
+          </button>
         </Col>
       </Row>
     </div>
-  ) : null;
+  );
 };
 
 LoyaltyPromoBanner.propTypes = {
@@ -126,6 +123,7 @@ LoyaltyPromoBanner.defaultProps = {
   font-size: 10px;
   line-height: 1.04;
   text-align: left;
+  padding-right: 15px;
 }
 
 .loyalty-banner-wrapper__img {
@@ -134,19 +132,6 @@ LoyaltyPromoBanner.defaultProps = {
 }
 .loyalty-banner-wrapper__img img {
   height: 100%;
-}
-
-.loyalty-banner-wrapper__close-btn {
-  padding: 0 8px;
-  margin-left: 10px;
-  cursor: pointer;
-  border:0;
-  background: transparent;
-}
-
-.loyalty-banner-close-btn-icon {
-  width: 8px;
-  height: 8px;
 }
 
 @media (max-width: 767px) {
@@ -160,7 +145,7 @@ LoyaltyPromoBanner.defaultProps = {
     margin: 0 14px;
   }
   .the-my-place-rewards {
-    width: 310px;
+    width: 320px;
     font-size: 13px;
   }
   .loyalty-banner-wrapper__img {
@@ -188,17 +173,14 @@ LoyaltyPromoBanner.defaultProps = {
   }
 
   .the-my-place-rewards {
-    width: 590px;
+    width: 570px;
     font-size: 22px;
   }
   .loyalty-banner-wrapper__img {
     height: 44px;
     margin-right: 8px;
   }
-  .loyalty-banner-close-btn-icon {
-    width: 16px;
-    height: 16px;
-  }
+
 }
   </style>
 
@@ -232,11 +214,6 @@ ON ALL EASTER DRESS UP & MATCHING FAMILY STYLES
 
   </div>
 
-  <button id="loyaltyBannerCloseBtn" class="loyalty-banner-wrapper__item loyalty-banner-wrapper__close-btn">
-    <svg class="loyalty-banner-close-btn-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25">
-    <path fill="#a0a0a0" fill-rule="nonzero" d="M14.107 12.5l10.56-10.56A1.136 1.136 0 1 0 23.06.333L12.5 10.893 1.94.333A1.136 1.136 0 1 0 .333 1.94l10.56 10.56L.333 23.06a1.136 1.136 0 1 0 1.607 1.607l10.56-10.56 10.56 10.56c.222.222.513.333.804.333a1.136 1.136 0 0 0 .803-1.94L14.107 12.5z"/>
-</svg>
-  </button>
 </div>`,
       link: {
         url: '/banner/url',
