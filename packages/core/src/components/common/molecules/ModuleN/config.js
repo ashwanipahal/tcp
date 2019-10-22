603654f0d46ebadd53d0534646e33b81de687d51
Aman Bhardwaj <amabhard@publicisgroupe.net>
2019-10-23 02:17:06 +0530

    local testing and bugs fixed

--- packages/core/src/components/common/molecules/ModuleN/config.js
+++ packages/core/src/components/common/molecules/ModuleN/config.js
@@ -7,17 +7,6 @@ export const config = {
     stackedCTAButtonsExpandable: 'dropdownButtonCTA',
     CTAButtonCarouselExpandable: 'scrollCTAList',
   },
-  getColSize: moduleWidth => {
-    const colSize = {
-      small: 6,
-      medium: 8,
-      large: 12,
-    };
-    if (moduleWidth.toLowerCase() === 'half') {
-      colSize.large = 6;
-    }
-    return colSize;
-  },
 };
 
 export const ctaTypeProps = {
