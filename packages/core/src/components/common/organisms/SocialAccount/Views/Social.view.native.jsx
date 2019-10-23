db14530cb531d0e440339e242b9fa97c235de28f
Ajay Saini <ajay.saini@publicissapient.com>
2019-10-23 16:29:37 +0530

    Update Social.view.native.jsx

--- packages/core/src/components/common/organisms/SocialAccount/Views/Social.view.native.jsx
+++ packages/core/src/components/common/organisms/SocialAccount/Views/Social.view.native.jsx
@@ -305,7 +305,7 @@ class Socialview extends React.PureComponent {
       this.refactorSocialDetails(getSocialAcc);
     }
     const { assetHost, siteId, instakey } = getAPIConfig();
-    const redirectUrl = `${assetHost}/${siteId}/instagram?token=tcprwd`;
+    const redirectUrl = `${assetHost}/${siteId}/instagram`;
 
     return (
       <View>
