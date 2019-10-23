1003d9961e25372861af624a0fd3491770a2d01b
Chinmay Shimpi <chishimp@publicisgroupe.net>
2019-10-23 23:25:43 +0530

    testcases changed

--- packages/web/src/components/features/content/Header/molecules/GuestUserInfo/__tests__/GuestUserInfo-test.jsx
+++ packages/web/src/components/features/content/Header/molecules/GuestUserInfo/__tests__/GuestUserInfo-test.jsx
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import GuestUserInfo from '../view/GuestUserInfo';
 
 describe('GuestUserInfo component', () => {
-  it('GuestUserInfo component renders correctly to match snapshot', () => {
+  it('GuestUserInfo component renders correctly to match snapshot num1', () => {
     const props = {
       createAccount: 'createAccount',
       login: 'login',
@@ -15,7 +15,7 @@ describe('GuestUserInfo component', () => {
     const component = shallow(<GuestUserInfo {...props} />);
     expect(component).toMatchSnapshot();
   });
-  it('GuestUserInfo component renders correctly to match snapshot', () => {
+  it('GuestUserInfo component renders correctly to match snapshot num2', () => {
     const props = {
       createAccount: 'createAccount',
       login: 'login',
@@ -23,6 +23,30 @@ describe('GuestUserInfo component', () => {
       triggerLoginCreateAccount: false,
       onLinkClick: jest.fn(),
       userNameClick: true,
+    };
+    const component = shallow(<GuestUserInfo {...props} />);
+    expect(component).toMatchSnapshot();
+  });
+  it('GuestUserInfo component renders correctly to match snapshot num3', () => {
+    const props = {
+      createAccount: 'createAccount',
+      login: 'login',
+      openOverlay: jest.fn(),
+      triggerLoginCreateAccount: true,
+      onLinkClick: jest.fn(),
+      userNameClick: false,
+    };
+    const component = shallow(<GuestUserInfo {...props} />);
+    expect(component).toMatchSnapshot();
+  });
+  it('GuestUserInfo component renders correctly to match snapshot num4', () => {
+    const props = {
+      createAccount: 'createAccount',
+      login: 'login',
+      openOverlay: jest.fn(),
+      triggerLoginCreateAccount: false,
+      onLinkClick: jest.fn(),
+      userNameClick: false,
     };
     const component = shallow(<GuestUserInfo {...props} />);
     expect(component).toMatchSnapshot();
