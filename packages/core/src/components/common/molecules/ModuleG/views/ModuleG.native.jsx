af536b27534d4da51e37571e6ed424083e7f8a71
Prasoon <prasoon.singh@publicissapient.com>
2019-10-24 12:54:34 +0530

    update

--- packages/core/src/components/common/molecules/ModuleG/views/ModuleG.native.jsx
+++ packages/core/src/components/common/molecules/ModuleG/views/ModuleG.native.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View, Dimensions } from 'react-native';
-import { Button, Anchor, Image } from '../../../atoms';
+import { Button, Anchor, DamImage, Skeleton } from '../../../atoms';
 import { getLocator } from '../../../../../utils/index.native';
 import { Carousel } from '../..';
 import QuickViewModal from '../../../organisms/QuickViewModal/container/QuickViewModal.container';
@@ -31,15 +31,15 @@ import LinkText from '../../LinkText';
 
 const PRODUCT_IMAGE_WIDTH = 203;
 const PRODUCT_IMAGE_HEIGHT = 200;
-const PRODUCT_IMAGE_GUTTER = 16;
+const PRODUCT_IMAGE_GUTTER = 1;
 const PRODUCT_IMAGE_PER_SLIDE = 4;
-const MODULE_HEIGHT = 200;
-const MODULE_WIDTH = (PRODUCT_IMAGE_WIDTH + PRODUCT_IMAGE_GUTTER) * PRODUCT_IMAGE_PER_SLIDE;
+const MODULE_HEIGHT = 256;
+const MODULE_WIDTH = (PRODUCT_IMAGE_WIDTH + PRODUCT_IMAGE_GUTTER) * PRODUCT_IMAGE_PER_SLIDE + 60;
 const { TOTAL_IMAGES } = config;
 const { width: screenWidth } = Dimensions.get('window');
 
-const LOOP_CLONES_PER_SIDE = 2;
-const INACTIVE_SLIDE_SCALE = 0.81;
+const LOOP_CLONES_PER_SIDE = 7;
+const INACTIVE_SLIDE_SCALE = 0.7;
 const INACTIVE_SLIDE_OPACITY = 0.8;
 const ITEM_WIDTH = 235;
 
@@ -123,10 +123,115 @@ class ModuleG extends React.PureComponent {
             navigation={navigation}
             testID={`${getLocator('moduleG_product_image')}${productItemIndex}`}
           >
-            <Image url={imageUrl[0]} height={PRODUCT_IMAGE_HEIGHT} width={PRODUCT_IMAGE_WIDTH} />
+            <DamImage url={imageUrl[0]} height={PRODUCT_IMAGE_HEIGHT} width={PRODUCT_IMAGE_WIDTH} />
           </Anchor>
         </View>
       </ImageSlideWrapper>
+    );
+  };
+
+  getDataStatus = selectedCategoryId => {
+    const { productTabList = {} } = this.props;
+    let dataStatus = true;
+    if (productTabList && productTabList.completed) {
+      dataStatus = productTabList.completed[selectedCategoryId];
+    }
+    return dataStatus;
+  };
+
+  getMiddleContainer = (dataLength, type) => {
+    if (!dataLength) {
+      return null;
+    }
+    if (type === 'shadow') {
+      return (
+        <ShadowContainer>
+          <SHADOW />
+        </ShadowContainer>
+      );
+    }
+    return (
+      <MiddleContainer>
+        <Border />
+        <Circle />
+        <StyledCustomImage source={plusIcon} />
+      </MiddleContainer>
+    );
+  };
+
+  renderHeader = (headerText, promoBanner, navigation) => {
+    return (
+      <MessageContainer>
+        <Wrapper>
+          {headerText && headerText.length > 0 && (
+            <LinkText
+              navigation={navigation}
+              headerText={headerText}
+              locator={getLocator('moduleG_header_text')}
+              useStyle
+            />
+          )}
+        </Wrapper>
+        {promoBanner && (
+          <PromoContainer>
+            <PromoBanner
+              testID={getLocator('moduleG_promobanner_text')}
+              promoBanner={promoBanner}
+              navigation={navigation}
+            />
+          </PromoContainer>
+        )}
+      </MessageContainer>
+    );
+  };
+
+  renderFirstCarousel = (firstCarouseProductList, firstCarouselList) => {
+    return (
+      <View>
+        {firstCarouseProductList && firstCarouseProductList.length > 0 ? (
+          <Carousel
+            data={firstCarouselList}
+            renderItem={this.renderCarouselSlide}
+            height={MODULE_HEIGHT}
+            options={{
+              loopClonesPerSide: LOOP_CLONES_PER_SIDE,
+              inactiveSlideScale: INACTIVE_SLIDE_SCALE,
+              inactiveSlideOpacity: INACTIVE_SLIDE_OPACITY,
+              sliderWidth: MODULE_WIDTH,
+              itemWidth: ITEM_WIDTH,
+              autoplay: false,
+            }}
+            paginationProps={{
+              containerStyle: { paddingVertical: 5 },
+            }}
+          />
+        ) : null}
+      </View>
+    );
+  };
+
+  renderSecondCarousel = (secondCarouseProductList, secondCarouselList) => {
+    return (
+      <View>
+        {secondCarouseProductList && secondCarouseProductList.length ? (
+          <Carousel
+            data={secondCarouselList}
+            renderItem={this.renderCarouselSlide}
+            height={MODULE_HEIGHT}
+            options={{
+              loopClonesPerSide: LOOP_CLONES_PER_SIDE,
+              inactiveSlideScale: INACTIVE_SLIDE_SCALE,
+              inactiveSlideOpacity: INACTIVE_SLIDE_OPACITY,
+              sliderWidth: MODULE_WIDTH,
+              itemWidth: ITEM_WIDTH,
+              autoplay: false,
+            }}
+            paginationProps={{
+              containerStyle: { paddingVertical: 5 },
+            }}
+          />
+        ) : null}
+      </View>
     );
   };
 
@@ -134,6 +239,7 @@ class ModuleG extends React.PureComponent {
    * @param {object} props : Props for renderView multi type of banner list, button list, header text.
    * @desc This is Method return the complete View with CTA Button .
    */
+
   renderView = (
     selectedProductCarouselList,
     selectedProductList,
@@ -143,32 +249,18 @@ class ModuleG extends React.PureComponent {
     const { navigation, headerText, promoBanner, divTabs } = this.props;
     const firstCarouselList = selectedProductCarouselList[0];
     const secondCarouselList = selectedProductCarouselList[1];
-    const firstCarouseProductList = selectedProductList[0];
-    const secondCarouseProductList = selectedProductList[1];
+    const firstCarouseProductList = selectedProductList[0] || [];
+    const secondCarouseProductList = selectedProductList[1] || [];
+    const { selectedCategoryId } = this.state;
+    const dataStatus =
+      this.getDataStatus(selectedCategoryId[0]) || this.getDataStatus(selectedCategoryId[1]);
 
     return (
       <Container>
         <MessageContainer>
-          <Wrapper>
-            {headerText && headerText.length > 0 && (
-              <LinkText
-                navigation={navigation}
-                headerText={headerText}
-                locator={getLocator('moduleG_header_text')}
-                useStyle
-              />
-            )}
-          </Wrapper>
-          {promoBanner && (
-            <PromoContainer>
-              <PromoBanner
-                testID={getLocator('moduleG_promobanner_text')}
-                promoBanner={promoBanner}
-                navigation={navigation}
-              />
-            </PromoContainer>
-          )}
+          {this.renderHeader(headerText, promoBanner, navigation)}
         </MessageContainer>
+
         <ProductTabListContainer>
           <ProductTabList
             onProductTabChange={this.onProductTabChange}
@@ -177,56 +269,49 @@ class ModuleG extends React.PureComponent {
             testID={getLocator('moduleG_cta_link')}
           />
         </ProductTabListContainer>
-        <ShadowContainer>
-          <SHADOW />
-        </ShadowContainer>
+
+        {!dataStatus ? (
+          <ShadowContainer>
+            <SHADOW />
+          </ShadowContainer>
+        ) : null}
+
         <View>
+          {dataStatus ? (
+            <Skeleton
+              row={1}
+              col={3}
+              width={190}
+              height={170}
+              rowProps={{ justifyContent: 'center', marginTop: '20px' }}
+              showArrows
+            />
+          ) : null}
+
           <ImageSlidesWrapper>
-            {firstCarouseProductList && firstCarouseProductList.length > 0 ? (
-              <Carousel
-                data={firstCarouselList}
-                renderItem={this.renderCarouselSlide}
-                height={MODULE_HEIGHT}
-                options={{
-                  loopClonesPerSide: LOOP_CLONES_PER_SIDE,
-                  inactiveSlideScale: INACTIVE_SLIDE_SCALE,
-                  inactiveSlideOpacity: INACTIVE_SLIDE_OPACITY,
-                  sliderWidth: MODULE_WIDTH,
-                  itemWidth: ITEM_WIDTH,
-                  autoplay: false,
-                }}
-                paginationProps={{
-                  containerStyle: { paddingVertical: 5 },
-                }}
-              />
-            ) : null}
+            {this.renderFirstCarousel(firstCarouseProductList, firstCarouselList)}
           </ImageSlidesWrapper>
 
-          <MiddleContainer>
-            <Border />
-            <Circle />
-            <StyledCustomImage source={plusIcon} />
-          </MiddleContainer>
+          {!dataStatus ? (
+            <MiddleContainer>
+              <Border />
+              <Circle />
+              <StyledCustomImage source={plusIcon} />
+            </MiddleContainer>
+          ) : null}
+          {dataStatus ? (
+            <Skeleton
+              row={1}
+              col={3}
+              width={190}
+              height={170}
+              rowProps={{ justifyContent: 'center', marginTop: '20px' }}
+              showArrows
+            />
+          ) : null}
 
           <ImageSlidesWrapper>
-            {secondCarouseProductList && secondCarouseProductList.length ? (
-              <Carousel
-                data={secondCarouselList}
-                renderItem={this.renderCarouselSlide}
-                height={MODULE_HEIGHT}
-                options={{
-                  loopClonesPerSide: LOOP_CLONES_PER_SIDE,
-                  inactiveSlideScale: INACTIVE_SLIDE_SCALE,
-                  inactiveSlideOpacity: INACTIVE_SLIDE_OPACITY,
-                  sliderWidth: MODULE_WIDTH,
-                  itemWidth: ITEM_WIDTH,
-                  autoplay: false,
-                }}
-                paginationProps={{
-                  containerStyle: { paddingVertical: 5 },
-                }}
-              />
-            ) : null}
+            {this.renderSecondCarousel(secondCarouseProductList, secondCarouselList)}
           </ImageSlidesWrapper>
         </View>
 
