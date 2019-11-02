e1100001279cad492ea2b1cba2bba9cb397146db
Mohit Jain <mohit.jain2@publicissapient.com>
2019-11-02 21:25:51 +0530

    RWD-10672 | RWD-9788 | empty message implementaion form web

--- packages/core/src/components/features/CnC/Checkout/molecules/CheckoutPageEmptyBag/styles/CheckoutPageEmptyBag.style.js
+++ packages/core/src/components/features/CnC/Checkout/molecules/CheckoutPageEmptyBag/styles/CheckoutPageEmptyBag.style.js
@@ -3,16 +3,13 @@ import { css } from 'styled-components';
 const styles = css`
   flex: 1;
   flex-direction: column;
-  display: flex;
   .empty-bag-text {
-    display: flex;
     flex: 1;
     justify-content: center;
     margin-top: ${props => props.theme.spacing.LAYOUT_SPACING.XXL};
     font-size: ${props => props.theme.typography.fontSizes.fs22};
   }
   .empty-bag-subtext {
-    display: flex;
     flex: 1;
     justify-content: center;
     margin-top: ${props => props.theme.spacing.LAYOUT_SPACING.LRG};
