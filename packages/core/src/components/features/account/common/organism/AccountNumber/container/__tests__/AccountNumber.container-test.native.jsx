88b32a37da7fcbd88376bea9813cdbcfe8a80598
Ajay Saini <ajay.saini@publicissapient.com>
2019-11-04 12:34:56 +0530

    Update snapshot

--- packages/core/src/components/features/account/common/organism/AccountNumber/container/__tests__/AccountNumber.container-test.native.jsx
+++ packages/core/src/components/features/account/common/organism/AccountNumber/container/__tests__/AccountNumber.container-test.native.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { AccountNumberContainer, mapDispatchToProps } from '../AccountNumber.container.native';
+import { AccountNumberContainer } from '../AccountNumber.container.native';
 
 describe('AccountNumberContainer container', () => {
   const props = {
@@ -12,14 +12,5 @@ describe('AccountNumberContainer container', () => {
   it('should render AccountNumberContainer component', () => {
     const component = shallow(<AccountNumberContainer {...props} />);
     expect(component).toMatchSnapshot();
-  });
-
-  describe('#mapDispatchToProps', () => {
-    it('should return an action getAddressListAction which will call dispatch function on execution', () => {
-      const dispatch = jest.fn();
-      const dispatchProps = mapDispatchToProps(dispatch);
-      dispatchProps.fetchLabels();
-      expect(dispatch.mock.calls).toHaveLength(1);
-    });
   });
 });
