0a12b34b9a4fbead33803277c66b8e4bd6d0410d
Piyush Vish <piyush.vishwakarma008@gmail.com>
2019-11-04 17:49:59 +0530

    paypal test

--- packages/core/src/components/features/CnC/common/organism/PayPalButton/organism/PaypalButton/views/PayPalButton.view.native.jsx
+++ packages/core/src/components/features/CnC/common/organism/PayPalButton/organism/PaypalButton/views/PayPalButton.view.native.jsx
@@ -1,33 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { PropTypes } from 'prop-types';
 import { NavigationActions } from 'react-navigation';
 import { View, WebView, Platform } from 'react-native';
 
-const PayPalButton = props => {
-  const {
-    getPayPalSettings,
-    payPalWebViewHandle,
-    paypalAuthorizationHandle,
-    clearPaypalSettings,
-    navigation,
-    paypalEnv,
-    setVenmoState,
-    closeModal,
-    paypalStaticUrl,
-  } = props;
+class PayPalButton extends React.PureComponent {
+  state = { showAsModal: false };
 
-  const [showAsModal, setAsModal] = useState(false);
-  const handleWebViewEvents = event => {
+  handleWebViewEvents = event => {
+    const {
+      payPalWebViewHandle,
+      paypalAuthorizationHandle,
+      clearPaypalSettings,
+      navigation,
+      setVenmoState,
+      closeModal,
+    } = this.props;
+
     switch (event.nativeEvent.data) {
       case 'payment':
         payPalWebViewHandle(true);
-        setAsModal(true);
+        this.setState({ showAsModal: true });
         setVenmoState(false);
         break;
       case 'onAuthorize':
         paypalAuthorizationHandle({ navigation, navigationActions: NavigationActions });
         payPalWebViewHandle(false);
-        setAsModal(false);
+        this.setState({ showAsModal: false });
         setVenmoState(true);
         closeModal(true);
         break;
@@ -35,55 +33,60 @@ const PayPalButton = props => {
         clearPaypalSettings();
         setVenmoState(true);
         payPalWebViewHandle(false);
-        setAsModal(false);
+        this.setState({ showAsModal: false });
         break;
       default:
         payPalWebViewHandle(false);
-        setAsModal(false);
+        this.setState({ showAsModal: false });
         setVenmoState(true);
     }
   };
 
-  let styles = {
-    height: 42,
-    width: 150,
-    flex: 1,
-  };
+  render() {
+    const { getPayPalSettings, paypalEnv, paypalStaticUrl } = this.props;
 
-  if (showAsModal) {
-    styles = {
-      position: 'absolute',
-      top: 12,
-      width: '100%',
-      height: 800,
-      zIndex: 999,
+    let styles = {
+      height: 42,
+      width: 150,
+      flex: 1,
     };
-  }
 
-  const webURL = `${paypalStaticUrl}/static/paypal/index.html?key=${
-    getPayPalSettings.paypalInContextToken
-  }&paypalEnv=${paypalEnv}`;
-  return (
-    <View style={{ ...styles }}>
-      <WebView
-        scalesPageToFit
-        originWhitelist={['*']}
-        source={{
-          uri: webURL,
-        }}
-        mixedContentMode="always"
-        useWebKit={Platform.OS === 'ios'}
-        scrollEnabled
-        domStorageEnabled
-        thirdPartyCookiesEnabled
-        startInLoadingState
-        allowUniversalAccessFromFileURLs
-        javaScriptEnabled
-        onMessage={handleWebViewEvents}
-      />
-    </View>
-  );
-};
+    const { showAsModal } = this.state;
+    if (showAsModal) {
+      styles = {
+        position: 'absolute',
+        top: 12,
+        width: '100%',
+        height: 800,
+        zIndex: 999,
+      };
+    }
+
+    const webURL = `${paypalStaticUrl}/static/paypal/index.html?key=${
+      getPayPalSettings.paypalInContextToken
+    }&paypalEnv=${paypalEnv}`;
+    return (
+      <View style={{ ...styles }}>
+        <WebView
+          scalesPageToFit
+          originWhitelist={['*']}
+          source={{
+            uri: webURL,
+          }}
+          mixedContentMode="always"
+          useWebKit={Platform.OS === 'ios'}
+          scrollEnabled
+          domStorageEnabled
+          thirdPartyCookiesEnabled
+          startInLoadingState
+          allowUniversalAccessFromFileURLs
+          javaScriptEnabled
+          onMessage={this.handleWebViewEvents}
+        />
+      </View>
+    );
+  }
+}
 
 PayPalButton.propTypes = {
   getPayPalSettings: PropTypes.shape({}).isRequired,
