3a1843b6410279835405f0650ef3dbe0bda01b81
Prasoon <prasoon.singh@publicissapient.com>
2019-11-04 14:58:55 +0530

    update

--- packages/core/src/components/common/molecules/LoyaltyPromoBanner/view/LoyaltyPromoBanner.native.jsx
+++ packages/core/src/components/common/molecules/LoyaltyPromoBanner/view/LoyaltyPromoBanner.native.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { View, AsyncStorage } from 'react-native';
+import { View } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import PropTypes from 'prop-types';
 import { RichText, Anchor } from '../../../atoms';
 import { Wrapper, StyledImage, Touchable } from '../LoyaltyPromoBanner.style.native';
 
 const crossImage = require('../../../../../../src/assets/close.png');
 
-const cookieName = 'tcp-mobile';
-
+const cookieName = '@MySuperStore:key';
 const date = new Date();
 const currentDateValue = date.getTime();
 
@@ -20,34 +20,54 @@ class LoyaltyPromoBanner extends React.PureComponent {
     super(props);
     this.state = {
       bannerClosed: false,
-      bannerVisible: false,
     };
   }
 
+  componentDidMount() {
+    this.isDisplay();
+  }
+
+  /**
+   * To save the date
+   */
   setDate = () => {
     const daysAlive = 10;
     const expiteDateValue = date.setTime(date.getTime() + daysAlive * 24 * 60 * 60 * 1000);
+
     try {
-      AsyncStorage.setItem(cookieName, expiteDateValue);
+      AsyncStorage.setItem(cookieName, expiteDateValue.toString());
     } catch (error) {
       console.info('error', error);
     }
   };
 
-  retrieveDate = () => {
-    return AsyncStorage.getItem(cookieName);
+  /**
+   * To fetch the date
+   */
+  retrieveDate = async () => {
+    try {
+      return await AsyncStorage.getItem(cookieName);
+    } catch (error) {
+      // Error retrieving data
+      return null;
+    }
   };
 
+  /**
+   * To check the visiblity of the banner
+   */
   isDisplay = () => {
     this.retrieveDate().then(data => {
-      if (currentDateValue >= data) {
-        this.setState({
-          bannerVisible: true,
-        });
-      } else {
-        this.setState({
-          bannerVisible: false,
-        });
+      if (data !== null) {
+        if (currentDateValue <= parseInt(data, 10)) {
+          this.setState({
+            bannerClosed: true,
+          });
+        } else {
+          this.setState({
+            bannerClosed: false,
+          });
+        }
       }
     });
   };
@@ -55,50 +75,46 @@ class LoyaltyPromoBanner extends React.PureComponent {
   /**
    * This function validate the iconView.
    */
-  validateIcon = () => {
-    const { bannerClosed } = this.state;
-    this.setState({
-      bannerClosed: !bannerClosed,
-    });
+  validateView = () => {
     this.setDate();
+    this.setState({ bannerClosed: true });
   };
 
+  /**
+   * To render the view
+   */
   render() {
     const {
       richTextList: [{ text, link }],
       navigation,
     } = this.props;
 
-    this.isDisplay();
+    const { bannerClosed } = this.state;
 
-    const { bannerClosed, bannerVisible } = this.state;
-
-    if (bannerVisible) {
-      return (
-        <View>
-          {bannerClosed ? null : (
-            <Anchor navigation={navigation} url={link.url}>
-              <Wrapper>
-                <RichText
-                  source={{
-                    html: `<html><header><meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'> </header><body>${text}</body></html>`,
-                  }}
-                />
-
-                <Touchable
-                  accessibilityRole="button"
-                  accessibilityLabels="close"
-                  onPress={this.validateIcon}
-                >
-                  <StyledImage source={crossImage} />
-                </Touchable>
-              </Wrapper>
-            </Anchor>
-          )}
-        </View>
-      );
-    }
-    return null;
+    return (
+      <View>
+        {bannerClosed ? (
+          <View />
+        ) : (
+          <Anchor navigation={navigation} url={link.url}>
+            <Wrapper>
+              <RichText
+                source={{
+                  html: `<html><header><meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'> </header><body>${text}</body></html>`,
+                }}
+              />
+              <Touchable
+                accessibilityRole="button"
+                accessibilityLabels="close"
+                onPress={this.validateView}
+              >
+                <StyledImage source={crossImage} />
+              </Touchable>
+            </Wrapper>
+          </Anchor>
+        )}
+      </View>
+    );
   }
 }
 
