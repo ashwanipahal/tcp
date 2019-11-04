73dc0f12d83b0ea77dd1d79847ad506c47329f7d
Mohit Jain <mohit.jain2@publicissapient.com>
2019-11-04 10:42:34 +0530

    update

--- packages/core/src/components/features/CnC/Checkout/molecules/CheckoutPageEmptyBag/styles/CheckoutPageEmptyBag.style.js
+++ packages/core/src/components/features/CnC/Checkout/molecules/CheckoutPageEmptyBag/styles/CheckoutPageEmptyBag.style.js
@@ -8,7 +8,6 @@ const styles = css`
     display: flex;
     justify-content: center;
     margin-top: ${props => props.theme.spacing.LAYOUT_SPACING.XXL};
-    font-size: ${props => props.theme.typography.fontSizes.fs22};
   }
   .empty-bag-subtext {
     flex: 1;
--- packages/core/src/components/features/CnC/Checkout/molecules/CheckoutPageEmptyBag/views/CheckoutPageEmptyBag.view.native.jsx
+++ packages/core/src/components/features/CnC/Checkout/molecules/CheckoutPageEmptyBag/views/CheckoutPageEmptyBag.view.native.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {
   EmptyBagTextContainer,
-  EmptyBagSubText,
-  EmptyBagBoldText,
 } from '../styles/CheckoutPageEmptyBag.style.native';
+import { BodyCopyWithSpacing } from '../../../../../../common/atoms/styledWrapper';
 
 const CheckoutPageEmptyBag = ({ labels: { emptyBagText, emptyBagSubText } = {} }) => {
   return (
     <EmptyBagTextContainer>
-      <EmptyBagBoldText>{emptyBagText}</EmptyBagBoldText>
-      <EmptyBagSubText>{emptyBagSubText}</EmptyBagSubText>
+      <BodyCopyWithSpacing
+        text={emptyBagText}
+        fontSize="fs18"
+        fontWeight="extrabold"
+        fontFamily="secondary"
+        spacingStyles="margin-bottom-MED margin-left-MED margin-right-MED"
+      />
+      <BodyCopyWithSpacing text={emptyBagSubText} fontSize="fs14" fontFamily="secondary" />
     </EmptyBagTextContainer>
   );
 };
