dd2800db7e468b200e479dc7a149ca0db42bdbd8
Jarret Logan <jarret.logan@publicissapient.com>
2019-11-06 17:46:07 -0500

    Updated Nunito font file, gymboree typography constants, gymboree plist

--- packages/core/styles/themes/Gymboree/typography.constants.js
+++ packages/core/styles/themes/Gymboree/typography.constants.js
@@ -29,9 +29,21 @@ export const TEXT_ALIGNS = { left: 'left', center: 'center', right: 'right' };
 
 export const FONT_WEIGHTS = {
   regular: '400',
+  medium: '500',
   semibold: '600',
+  bold: '700',
   extrabold: '800',
   black: '900',
+};
+
+export const FONT_WEIGHT_NAMES = {
+  regular: 'Regular',
+  medium: 'Medium',
+  semibold: 'SemiBold',
+  semiBold: 'SemiBold',
+  extrabold: 'ExtraBold',
+  bold: 'Bold',
+  black: 'Black',
 };
 
 export const FONT_SIZES = {
