0f646aa3ca069734f4c9ba9008b60f43d7671ab5
Madan Joshi <madan.joshi@publicissapient.com>
2019-11-07 19:32:18 +0530

    snapshot

--- packages/core/src/components/features/CnC/Checkout/organisms/ShippingPage/molecules/GiftServices/container/__tests__/GiftServices.container.test.jsx
+++ packages/core/src/components/features/CnC/Checkout/organisms/ShippingPage/molecules/GiftServices/container/__tests__/GiftServices.container.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import GiftServices from '../GiftServices.view';
+import GiftServices from '../GiftServices.container';
 
 describe('GiftServices component', () => {
   it('renders correctly', () => {
