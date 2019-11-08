d13dd56b04f549302da09920c64b9c2837dfa0b5
Sachin <achin.rawal@publicissapient.com>
2019-11-08 16:10:47 +0530

    now story ready for review

---
+++ packages/core/src/components/features/account/PurchaseGiftsCard/views/PurchaseGiftsCard.view.native.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { getLabelValue } from '@tcp/core/src/utils';
+import { UrlHandler } from '@tcp/core/src/utils/utils.app';
+import ImageComp from '../../../../common/atoms/Image';
+import BodyCopy from '../../../../common/atoms/BodyCopy';
+import {
+  TouchabelContainer,
+  RightArrowImageContainer,
+} from '../styles/PurchaseGiftsCard.view.style.native';
+
+const rightIcon = require('../../../../../../../mobileapp/src/assets/images/carrot-small-right-gray.png');
+
+export const PurchaseGiftsCard = ({ labels, navigation }) => {
+  return (
+    <>
+      <TouchabelContainer
+        onPress={() => {
+          UrlHandler(
+            getLabelValue(labels, 'lbl_purchaseGiftsCard_animatedGiftCardsLinkUrl', 'common')
+          );
+        }}
+      >
+        <BodyCopy
+          fontFamily="primary"
+          fontSize="fs13"
+          fontWeight="regular"
+          text={getLabelValue(labels, 'lbl_purchaseGiftsCard_animatedGiftCards', 'common')}
+          color="gray.900"
+        />
+        <RightArrowImageContainer>
+          <ImageComp source={rightIcon} width={7} height={10} />
+        </RightArrowImageContainer>
+      </TouchabelContainer>
+
+      <TouchabelContainer
+        onPress={() => {
+          UrlHandler(getLabelValue(labels, 'lbl_purchaseGiftsCard_eGiftCardLinkUrl', 'common'));
+        }}
+      >
+        <BodyCopy
+          fontFamily="primary"
+          fontSize="fs13"
+          fontWeight="regular"
+          text={getLabelValue(labels, 'lbl_purchaseGiftsCard_eGiftCard', 'common')}
+          color="gray.900"
+        />
+        <RightArrowImageContainer>
+          <ImageComp source={rightIcon} width={7} height={10} />
+        </RightArrowImageContainer>
+      </TouchabelContainer>
+
+      <TouchabelContainer
+        onPress={() => {
+          navigation.navigate('ProductDetail', {
+            title: 'Gift Cards',
+            pdpUrl: 'Gift Card',
+          });
+        }}
+      >
+        <BodyCopy
+          fontFamily="primary"
+          fontSize="fs13"
+          fontWeight="regular"
+          text={getLabelValue(labels, 'lbl_purchaseGiftsCard_physicalGiftCard', 'common')}
+          color="gray.900"
+        />
+        <RightArrowImageContainer>
+          <ImageComp source={rightIcon} width={7} height={10} />
+        </RightArrowImageContainer>
+      </TouchabelContainer>
+    </>
+  );
+};
+
+PurchaseGiftsCard.propTypes = {
+  labels: PropTypes.shape({}),
+  navigation: PropTypes.func.isRequired,
+};
+
+PurchaseGiftsCard.defaultProps = {
+  labels: {},
+};
+
+export default PurchaseGiftsCard;
