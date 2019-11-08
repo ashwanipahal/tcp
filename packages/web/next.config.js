c3de89cefcfd67903c97999261b11d60132bc631
Rakesh Menon <rmenon3@sapient.com>
2019-11-08 22:50:02 +0530

    reverted old space size and added check for SOURCE_MAPS_ENABLED

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -5,75 +5,79 @@ const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 const withSourceMaps = require('@zeit/next-source-maps');
 const path = require('path');
 
-module.exports = withBundleAnalyzer(
-  withSourceMaps(
-    withTM({
-      devtool: process.env.NODE_ENV === 'production' ? 'hidden-source-map' : 'source-map',
-      analyzeServer: process.env.NODE_ENV === 'production',
-      analyzeBrowser: process.env.NODE_ENV === 'production',
-      bundleAnalyzerConfig: {
-        server: {
-          analyzerMode: 'static',
-          reportFilename: './bundle-sizes/bundles/server.html',
-          openAnalyzer: false,
-        },
-        browser: {
-          analyzerMode: 'static',
-          reportFilename: './bundle-sizes/bundles/client.html',
-          openAnalyzer: false,
-        },
-      },
-      transpileModules: ['@tcp', '../core/+/*.+.js'],
-      useFileSystemPublicRoutes: false,
-      // This is to supply build-time environment vars to both server and client files:
-      //    https://nextjs.org/docs#build-time-configuration
-      // We can also use something like this if we need entire *.env file usage:
-      //    https://github.com/zeit/next.js/blob/canary/examples/with-dotenv/next.config.js
-      env: {
-        // TODO: change to process.env.RWD_WEB_PERF_TIMING
-        PERF_TIMING: true,
-        // TODO: change to process.env.RWD_WEB_ANALYTICS
-        ANALYTICS: true,
-        // TODO: change to process.env.RWD_WEB_ANALYTICS_SCRIPT_URL
-        ANALYTICS_SCRIPT_URL:
-          '//assets.adobedtm.com/launch-EN2ad1a963f240455f9fff064520345530-development.min.js',
-      },
-      webpack(config, { isServer }) {
-        const newConfig = config;
-        newConfig.module.rules.push({
-          test: /\.svg$/,
-          use: ['@svgr/webpack'],
-        });
-        newConfig.module.rules.forEach(rule => {
-          const newRule = rule;
-          if (newRule.use && newRule.use.loader === 'next-babel-loader') {
-            newRule.use.options.configFile = path.resolve('./.babelrc');
-          }
-          return newRule;
-        });
-        if (!isServer) {
-          newConfig.resolve.alias.fs = path.resolve(__dirname, 'lib/fake/method.js');
-        }
-        newConfig.node = {
-          __dirname: false,
-        };
+const isProductionBuild = process.env.NODE_ENV === 'production';
+const isSourceMapsEnabled = process.env.SOURCE_MAPS_ENABLED === 'true';
 
-        /**
-         * Polyfills added as per
-         * https://nextjs.org/docs#browser-support
-         * https://github.com/zeit/next.js/tree/canary/examples/with-polyfills
-         */
-        const originalEntry = newConfig.entry;
-        newConfig.entry = async () => {
-          const entries = await originalEntry();
-          if (entries['main.js'] && !entries['main.js'].includes('./utils/polyfills.js')) {
-            entries['main.js'].unshift('./utils/polyfills.js');
-          }
-          return entries;
-        };
+let buildConfig = withTM({
+  analyzeServer: process.env.NODE_ENV === 'production',
+  analyzeBrowser: process.env.NODE_ENV === 'production',
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: './bundle-sizes/bundles/server.html',
+      openAnalyzer: false,
+    },
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: './bundle-sizes/bundles/client.html',
+      openAnalyzer: false,
+    },
+  },
+  transpileModules: ['@tcp', '../core/+/*.+.js'],
+  useFileSystemPublicRoutes: false,
+  // This is to supply build-time environment vars to both server and client files:
+  //    https://nextjs.org/docs#build-time-configuration
+  // We can also use something like this if we need entire *.env file usage:
+  //    https://github.com/zeit/next.js/blob/canary/examples/with-dotenv/next.config.js
+  env: {
+    // TODO: change to process.env.RWD_WEB_PERF_TIMING
+    PERF_TIMING: true,
+    // TODO: change to process.env.RWD_WEB_ANALYTICS
+    ANALYTICS: true,
+    // TODO: change to process.env.RWD_WEB_ANALYTICS_SCRIPT_URL
+    ANALYTICS_SCRIPT_URL:
+      '//assets.adobedtm.com/launch-EN2ad1a963f240455f9fff064520345530-development.min.js',
+  },
+  webpack(config, { isServer }) {
+    const newConfig = config;
+    newConfig.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    newConfig.module.rules.forEach(rule => {
+      const newRule = rule;
+      if (newRule.use && newRule.use.loader === 'next-babel-loader') {
+        newRule.use.options.configFile = path.resolve('./.babelrc');
+      }
+      return newRule;
+    });
+    if (!isServer) {
+      newConfig.resolve.alias.fs = path.resolve(__dirname, 'lib/fake/method.js');
+    }
+    newConfig.node = {
+      __dirname: false,
+    };
 
-        return newConfig;
-      },
-    })
-  )
-);
+    /**
+     * Polyfills added as per
+     * https://nextjs.org/docs#browser-support
+     * https://github.com/zeit/next.js/tree/canary/examples/with-polyfills
+     */
+    const originalEntry = newConfig.entry;
+    newConfig.entry = async () => {
+      const entries = await originalEntry();
+      if (entries['main.js'] && !entries['main.js'].includes('./utils/polyfills.js')) {
+        entries['main.js'].unshift('./utils/polyfills.js');
+      }
+      return entries;
+    };
+
+    return newConfig;
+  },
+});
+
+if (isProductionBuild && isSourceMapsEnabled) {
+  buildConfig = withSourceMaps(buildConfig);
+}
+
+module.exports = withBundleAnalyzer(buildConfig);
