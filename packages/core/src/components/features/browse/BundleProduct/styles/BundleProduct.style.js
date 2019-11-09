84d1af5ca724495e558aac9ec3e41e958b0de57c
Vijendra Mishra <vmishra8@sapient.com>
2019-11-09 20:40:44 +0530

    changes for bundle product PDP web

--- packages/core/src/components/features/browse/BundleProduct/styles/BundleProduct.style.js
+++ packages/core/src/components/features/browse/BundleProduct/styles/BundleProduct.style.js
@@ -1,3 +1,146 @@
 import { css } from 'styled-components';
 
-export default css``;
+export default css`
+  .placeholder {
+    background: #d8d8d8;
+    padding: 10px 0;
+    margin: 0 13px 5px;
+    text-align: center;
+    @media ${props => props.theme.mediaQuery.medium} {
+      margin: 0 13px ${props => props.theme.spacing.ELEM_SPACING.XXL};
+    }
+    @media ${props => props.theme.mediaQuery.large} {
+      margin: 0 13px ${props => props.theme.spacing.ELEM_SPACING.XL};
+    }
+  }
+
+  .breadcrum-wrapper {
+    @media ${props => props.theme.mediaQuery.medium} {
+      margin: 0 0 ${props => props.theme.spacing.ELEM_SPACING.LRG};
+    }
+    @media ${props => props.theme.mediaQuery.large} {
+      margin: 0 0 ${props => props.theme.spacing.ELEM_SPACING.XXL};
+    }
+  }
+
+  .product-detail-section {
+    flex: 1;
+    margin: 0;
+  }
+  .product-desc-row {
+    margin: 0 0 ${props => props.theme.spacing.ELEM_SPACING.XL};
+  }
+  .product-image-wrapper {
+    margin-right: 0;
+  }
+  .product-detail-image-wrapper {
+    margin-bottom: 8px;
+  }
+  .product-summary-wrapper {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    border-bottom: 1px solid ${props => props.theme.colors.PRIMARY.LIGHTGRAY};
+  }
+  .product-summary-section {
+    border-bottom: 1px solid ${props => props.theme.colors.PRIMARY.LIGHTGRAY};
+    padding-bottom: ${props => props.theme.spacing.ELEM_SPACING.XL};
+  }
+  .product-detail-footer {
+    flex-direction: column;
+  }
+  .product-price-container {
+    margin: 0 0 ${props => props.theme.spacing.ELEM_SPACING.XL};
+  }
+  .choose-child-btn {
+    max-width: 450px;
+    text-align: center;
+    font-family: ${props => props.theme.fonts.secondaryFontFamily};
+    background-color: ${props => props.theme.colors.PRIMARY.BLUE};
+    font-size: ${props => props.theme.fonts.fontSize.listmenu.large}px;
+    color: white;
+    font-weight: ${props => props.theme.fonts.fontWeight.extrabold};
+    font-stretch: normal;
+    line-height: normal;
+    font-style: normal;
+    letter-spacing: ${props => props.theme.typography.letterSpacings.ls1};
+    padding-top: ${props => props.theme.spacing.ELEM_SPACING.SM};
+    padding-left: 125px;
+    padding-right: 125px;
+    padding-bottom: ${props => props.theme.spacing.ELEM_SPACING.SM};
+    margin-bottom: ${props => props.theme.spacing.ELEM_SPACING.XL};
+
+    @media ${props => props.theme.mediaQuery.medium} {
+      ${props =>
+        !props.isQuickView
+          ? `padding-left: 90px;
+              padding-right: 89px;`
+          : `padding-left: 65px;
+                  padding-right: 65px;`};
+    }
+
+    @media ${props => props.theme.mediaQuery.smallOnly} {
+      padding-left: 90px;
+      padding-right: 89px;
+    }
+
+    @media ${props => props.theme.mediaQuery.mediumOnly} {
+      width: 100%;
+      padding-left: 10px;
+      padding-right: 10px;
+      padding-bottom: 15px;
+      padding-top: 15px;
+    }
+
+    &:hover {
+      background-color: ${props => props.theme.colors.BUTTON[props.fill || 'BLUE'].HOVER};
+    }
+  }
+  @media ${props => props.theme.mediaQuery.medium} {
+    .product-image-wrapper {
+      margin-right: 30px;
+    }
+    .product-detail-image-wrapper {
+      margin-bottom: 11px;
+    }
+    .product-summary-mobile-view {
+      display: none;
+    }
+    .product-summary-desktop-view {
+      display: flex;
+    }
+    .product-price-mobile-view {
+      display: none;
+    }
+    .product-price-desktop-view {
+      display: flex;
+      margin-top: ${props => props.theme.spacing.ELEM_SPACING.XS};
+      border-bottom: 1px solid ${props => props.theme.colors.PRIMARY.LIGHTGRAY};
+      padding-bottom: ${props => props.theme.spacing.ELEM_SPACING.MED};
+      margin-bottom: ${props => props.theme.spacing.ELEM_SPACING.MED};
+    }
+  }
+  @media ${props => props.theme.mediaQuery.large} {
+    .product-image-wrapper {
+      margin-right: 0;
+    }
+    .product-detail-image-wrapper {
+      margin-bottom: 27px;
+    }
+    .product-summary-mobile-view {
+      display: none;
+    }
+    .product-summary-desktop-view {
+      display: flex;
+    }
+    .product-price-mobile-view {
+      display: none;
+    }
+    .product-price-desktop-view {
+      display: flex;
+      border-bottom: 1px solid ${props => props.theme.colors.PRIMARY.LIGHTGRAY};
+      padding-bottom: ${props => props.theme.spacing.ELEM_SPACING.MED};
+      margin-bottom: ${props => props.theme.spacing.ELEM_SPACING.MED};
+    }
+  }
+`;
--- packages/core/src/components/features/browse/BundleProduct/views/BundleProduct.view.jsx
+++ packages/core/src/components/features/browse/BundleProduct/views/BundleProduct.view.jsx
@@ -1,50 +1,177 @@
 import React from 'react';
-import { Row, Col } from '../../../../common/atoms';
+import PropTypes from 'prop-types';
+import ExecutionEnvironment from 'exenv';
+import { Row, Col, Button } from '@tcp/core/src/components/common/atoms';
+import withStyles from '../../../../common/hoc/withStyles';
+import styles from '../styles/BundleProduct.style';
+import ProductDescription from '../../ProductDetail/molecules/ProductDescription/views';
+import FixedBreadCrumbs from '../../ProductListing/molecules/FixedBreadCrumbs/views';
+import ProductImagesWrapper from '../../ProductDetail/molecules/ProductImagesWrapper/views/ProductImagesWrapper.view';
+import Product from '../../ProductDetail/molecules/Product/views/Product.view';
+import { breakpoints } from '../../../../../../styles/themes/TCP/mediaQuery';
+import {
+  getImagesToDisplay,
+  getMapSliceForColorProductId,
+} from '../../ProductListing/molecules/ProductList/utils/productsCommonUtils';
 
-const BundleProduct = () => {
-  return (
-    <div>
-      <Row className="placeholder">
-        <Col colSize={{ small: 6, medium: 8, large: 12 }}>BREADCRUMBS</Col>
-      </Row>
-      <Row className="placeholder">
-        <Col colSize={{ small: 6, medium: 8, large: 12 }}>PROMO AREA 1</Col>
-      </Row>
-      <Row>
-        <Col hideCol={{ medium: true, large: true }} colSize={{ small: 6, medium: 8, large: 12 }}>
-          PRODUCT INFO
+class BundleProduct extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('----11111-----');
+  }
+
+  scrollToChildrens = () => {
+    console.log('------2222------');
+  };
+
+  renderChooseItemBtn = chooseItemBtnLbl => {
+    return (
+      <div className="button-container">
+        <Button
+          onClick={this.scrollToChildrens}
+          buttonVariation="fixed-width"
+          className="choose-child-btn"
+          data-locator=""
+        >
+          {chooseItemBtnLbl}
+        </Button>
+      </div>
+    );
+  };
+
+  getBreadCrumb = () => {
+    const { breadCrumbs } = this.props;
+    return breadCrumbs && <FixedBreadCrumbs crumbs={breadCrumbs} separationChar=">" />;
+  };
+
+  getProductSummary = currentColorEntry => {
+    const {
+      currentProduct,
+      productDetails,
+      pdpLabels,
+      currency,
+      currencyExchange,
+      ...otherProps
+    } = this.props;
+
+    const selectedColorProductId = currentColorEntry.colorProductId;
+
+    return (
+      <Row className="product-desc-row">
+        <Col colSize={{ small: 6, medium: 8, large: 12 }}>
+          <div className="product-summary-wrapper">
+            <Product
+              productDetails={productDetails}
+              isGiftCard={currentProduct.isGiftCard}
+              selectedColorProductId={selectedColorProductId}
+              currencySymbol={currency}
+              currencyExchange={currencyExchange}
+              isBundleProduct
+              {...otherProps}
+            />
+            {this.renderChooseItemBtn(pdpLabels.chooseItemBtnLbl)}
+          </div>
         </Col>
-        <Col colSize={{ small: 6, medium: 4, large: 7 }}>PRODUCT IMAGES</Col>
-        <Col colSize={{ small: 6, medium: 4, large: 4 }} offsetRight={{ large: 1 }}>
-          <Col hideCol={{ small: true }} colSize={{ small: 6, medium: 8, large: 12 }}>
-            PRODUCT INFO
-          </Col>
-          <Col colSize={{ small: 6, medium: 8, large: 12 }}>PRICE SECTION</Col>
-          <Col colSize={{ small: 6, medium: 8, large: 12 }}>DESCRIPTION</Col>
-        </Col>
       </Row>
-      <Row className="placeholder">
-        <Col colSize={{ small: 6, medium: 8, large: 12 }}>PROMO AREA 2</Col>
-      </Row>
-      <Row className="placeholder">
-        <Col hideCol={{ medium: true, large: true }} colSize={{ small: 6, medium: 8, large: 12 }}>
-          DESCRIPTION
-        </Col>
-      </Row>
-      <Row className="placeholder">
-        <Col colSize={{ small: 6, medium: 8, large: 12 }}>FILTER BY</Col>
-      </Row>
-      <Row className="placeholder">
-        <Col colSize={{ small: 6, medium: 8, large: 12 }}>PRODUCTS LIST</Col>
-      </Row>
-      <Row className="placeholder">
-        <Col colSize={{ small: 6, medium: 8, large: 12 }}>YOU MAY ALSO LIKE</Col>
-      </Row>
-      <Row className="placeholder">
-        <Col colSize={{ small: 6, medium: 8, large: 12 }}>YOU MAY ALSO LIKE</Col>
-      </Row>
-    </div>
-  );
+    );
+  };
+
+  isWebEnvironment = () => {
+    return ExecutionEnvironment.canUseDOM && document.body.offsetWidth >= breakpoints.values.lg;
+  };
+
+  render() {
+    const {
+      longDescription,
+      shortDescription,
+      itemPartNumber,
+      pdpLabels,
+      className,
+      currentProduct,
+    } = this.props;
+    if (currentProduct && JSON.stringify(currentProduct) !== '{}') {
+      let imagesToDisplay = [];
+      const { colorFitsSizesMap, generalProductId } = currentProduct;
+
+      const isWeb = this.isWebEnvironment();
+      const currentColorEntry = getMapSliceForColorProductId(colorFitsSizesMap, generalProductId);
+      if (colorFitsSizesMap) {
+        imagesToDisplay = getImagesToDisplay({
+          imagesByColor: currentProduct.imagesByColor,
+          curentColorEntry: getMapSliceForColorProductId(colorFitsSizesMap, generalProductId),
+          isAbTestActive: false,
+          isFullSet: true,
+        });
+      }
+
+      return (
+        <div className={className}>
+          <Row>
+            <Col colSize={{ small: 6, medium: 8, large: 12 }} className="breadcrum-wrapper">
+              {this.getBreadCrumb()}
+            </Col>
+          </Row>
+          <Row className="placeholder">
+            <Col colSize={{ small: 6, medium: 8, large: 12 }}>
+              <div className="promo-area-1">{pdpLabels.promoArea1}</div>
+            </Col>
+          </Row>
+          <Row className="product-summary-section">
+            <Col className="product-image-wrapper" colSize={{ small: 6, medium: 4, large: 7 }}>
+              <ProductImagesWrapper
+                isThumbnailListVisible={isWeb}
+                images={imagesToDisplay}
+                pdpLabels={pdpLabels}
+                currentProduct={currentProduct}
+              />
+            </Col>
+            <Col
+              id="productDetailsSection"
+              className="product-detail-section"
+              colSize={{ small: 6, medium: 4, large: 5 }}
+            >
+              <div>{this.getProductSummary(currentColorEntry)}</div>
+              <div>
+                <ProductDescription
+                  productId={itemPartNumber}
+                  isShowMore={false}
+                  pdpLabels={pdpLabels}
+                  shortDescription={shortDescription}
+                  longDescription={longDescription}
+                />
+              </div>
+            </Col>
+          </Row>
+        </div>
+      );
+    }
+    return '';
+  }
+}
+
+BundleProduct.propTypes = {
+  className: PropTypes.string,
+  shortDescription: PropTypes.string,
+  itemPartNumber: PropTypes.string,
+  longDescription: PropTypes.string,
+  pdpLabels: PropTypes.shape({}),
+  breadCrumbs: PropTypes.shape({}),
+  currentProduct: PropTypes.shape({}).isRequired,
+  productDetails: PropTypes.shape({}),
+  currency: PropTypes.string,
+  currencyExchange: PropTypes.string,
 };
 
-export default BundleProduct;
+BundleProduct.defaultProps = {
+  className: '',
+  longDescription: '',
+  shortDescription: '',
+  itemPartNumber: '',
+  pdpLabels: {},
+  breadCrumbs: [],
+  productDetails: {},
+  currency: 'USD',
+  currencyExchange: '',
+};
+
+export default withStyles(BundleProduct, styles);
