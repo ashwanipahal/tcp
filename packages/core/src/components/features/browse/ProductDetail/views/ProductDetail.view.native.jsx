e4f62819382aae70976bac1d68275cbf94729b74
SidNanda <siddhartha.nanda@publicissapient.com>
2019-11-09 10:49:23 +0530

    added navigation object in app

--- packages/core/src/components/features/browse/ProductDetail/views/ProductDetail.view.native.jsx
+++ packages/core/src/components/features/browse/ProductDetail/views/ProductDetail.view.native.jsx
@@ -204,7 +204,7 @@ class ProductDetailView extends React.PureComponent {
           {this.renderCarousel(imageUrls)}
           <AddedToBagContainer navigation={navigation} />
           <LoyaltyBannerView>
-            <LoyaltyBanner pageCategory="isProductDetailView" />
+            <LoyaltyBanner pageCategory="isProductDetailView" navigation={navigation} />
           </LoyaltyBannerView>
           <ProductDetailDescription
             shortDescription={shortDescription}
