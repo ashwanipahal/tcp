fcd40ff6af9bf7ccd44c214f123c654d22644b65
Mohit Jain <mohit.jain2@publicissapient.com>
2019-11-09 13:54:51 +0530

    update

--- packages/core/src/components/features/CnC/Checkout/container/CheckoutCommonContainer.jsx
+++ packages/core/src/components/features/CnC/Checkout/container/CheckoutCommonContainer.jsx
@@ -206,13 +206,9 @@ export class CheckoutContainer extends React.PureComponent<Props> {
       isHasPickUpAlternatePerson,
       pickUpContactPerson,
       pickUpContactAlternate,
-      toastMessage,
-      clearCheckoutServerError,
-      cartOrderItemsCount,
-      toggleCountrySelector,
-      checkoutPageEmptyBagLabels,
-      isBagLoaded,
     } = this.props;
+    const { toggleCountrySelector, checkoutPageEmptyBagLabels, isBagLoaded } = this.props;
+    const { toastMessage, clearCheckoutServerError, cartOrderItemsCount } = this.props;
     const availableStages = checkoutUtil.getAvailableStages(
       cartOrderItems,
       checkoutProgressBarLabels
