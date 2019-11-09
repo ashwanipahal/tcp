e22675e9f30e319bf9188af3a81959ef0467315d
Abhiraj <aku332@sapient.com>
2019-11-09 19:45:23 +0530

    partial

---
+++ packages/core/src/components/common/hoc/withIsomorphicRenderer/withIsomorphicRenderer.native.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable no-console */
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+
+export default function({ WrappedComponent, mapStateToProps, mapDispatchToProps }) {
+  class IsomorphicRenderer extends Component {
+    static async getInitialProps({ store, query, req, isServer }) {
+      const props = { ...mapStateToProps(store.getState()), ...mapDispatchToProps(store.dispatch) };
+      const intialProps = { props, query, isServer, req };
+      const deviceBot = req && req.device && req.device.type === 'bot';
+      if (isServer && !deviceBot) {
+        WrappedComponent.initiateApiCall(intialProps);
+      }
+    }
+
+    componentDidMount() {
+      const { deviceType, ...props } = this.props;
+      if (deviceType !== 'bot') {
+        WrappedComponent.initiateApiCall({ props });
+      }
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  }
+
+  IsomorphicRenderer.propTypes = {
+    deviceType: PropTypes.string.isRequired,
+  };
+
+  const selfMapStateToProps = state => {
+    const childProps = mapStateToProps(state);
+    return {
+      ...childProps,
+      deviceType: state.DeviceInfo && state.DeviceInfo.deviceType,
+      routerParam: state.routerParam || state.router,
+    };
+  };
+
+  return (
+    connect(
+      selfMapStateToProps,
+      mapDispatchToProps
+    )(IsomorphicRenderer)
+  );
+}
