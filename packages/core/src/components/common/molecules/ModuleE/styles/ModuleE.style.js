57586fdf10a57042e1047707f362800bd6740b10
Harminder Pal Singh <harminderpal.singh@publicissapient.com>
2019-11-11 16:53:55 +0530

    fixes alignment

--- packages/core/src/components/common/molecules/ModuleE/styles/ModuleE.style.js
+++ packages/core/src/components/common/molecules/ModuleE/styles/ModuleE.style.js
@@ -52,8 +52,11 @@ export default css`
   margin-bottom: ${props => props.theme.spacing.LAYOUT_SPACING.SM};
 
   .carousel-cta-button {
-    margin-top: ${props => props.theme.spacing.ELEM_SPACING.XS};
     margin-bottom: ${props => props.theme.spacing.ELEM_SPACING.SM};
+  }
+
+   .tcp_carousel_wrapper {
+    margin-bottom: ${props => props.theme.spacing.ELEM_SPACING.XS};
   }
 
   .carousel-cta-button-with-eybrow-img {
