768cc8d0b4476d3e58b2ae081b6d3439dc0c5c4d
Erik Jung <erik.jung@publicissapient.com>
2019-11-12 11:25:23 -0500

    RWD-5598 | Test both hotfix context providers

--- packages/core/src/components/common/context/HotfixContext.test.js
+++ packages/core/src/components/common/context/HotfixContext.test.js
@@ -1,12 +1,22 @@
 import { useContext } from 'react';
 import { renderHook } from '@testing-library/react-hooks';
-import HotfixContext from './HotfixContext';
+import { HotfixPropsContext, HotfixBrowserContext } from './HotfixContext';
 
 describe(__filename, () => {
-  it('has an object value type', () => {
-    const result = renderHook(() => useContext(HotfixContext));
-    const expected = 'object';
-    const actual = typeof result;
-    expect(actual).toBe(expected);
+  describe('HotfixPropsContext', () => {
+    it('has an object value type', () => {
+      const result = renderHook(() => useContext(HotfixPropsContext));
+      const expected = 'object';
+      const actual = typeof result;
+      expect(actual).toBe(expected);
+    });
+  });
+  describe('HotfixBrowserContext', () => {
+    it('has an object value type', () => {
+      const result = renderHook(() => useContext(HotfixBrowserContext));
+      const expected = 'object';
+      const actual = typeof result;
+      expect(actual).toBe(expected);
+    });
   });
 });
