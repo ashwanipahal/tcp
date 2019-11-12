b01f1fa0a515b30c53264fd993134a412fe8711a
Soumya Tiwari <soutiwar@publicisgroupe.net>
2019-11-12 15:54:17 +0530

    feature/RWD-12891 with mock

--- packages/core/src/components/common/molecules/HelpCenterModuleTwoCol/styles/HelpCenterModuleTwoCol.style.js
+++ packages/core/src/components/common/molecules/HelpCenterModuleTwoCol/styles/HelpCenterModuleTwoCol.style.js
@@ -1,5 +1,25 @@
 import { css } from 'styled-components';
 
 export default css`
-  margin: 0 15px;
+  margin: 0;
+  @media ${props => props.theme.mediaQuery.largeMax} {
+    width: 100%;
+    margin: 0;
+  }
+
+  .is-visible-nav {
+    display: none;
+  }
+  .is-hidden-nav {
+    display: block;
+    margin-bottom: ${props => props.theme.spacing.ELEM_SPACING.XL};
+  }
+  @media ${props => props.theme.mediaQuery.large} {
+    .is-hidden-nav {
+      display: none;
+    }
+    .is-visible-nav {
+      display: flex;
+    }
+  }
 `;
--- packages/core/src/components/common/molecules/LeftNavigation/views/LeftNavigation.jsx
+++ packages/core/src/components/common/molecules/LeftNavigation/views/LeftNavigation.jsx
@@ -1,22 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@tcp/core/src/components/common/hoc/withStyles';
+import Dropdown from '@tcp/core/src/components/common/molecules/Dropdown';
 import style from '../styles/LeftNavigation.style';
+import mockLeftSideNavigation from './mock';
+import Anchor from '../../../atoms/Anchor';
+import Row from '../../../atoms/Row';
+import Col from '../../../atoms/Col';
 
-const LeftNavigation = ({ className, children }) => (
-  <div className={className}>
-    LeftNavigation
-    {children}
-  </div>
-);
+/**
+ * @function getNavLink This function renders each link present in the left nav
+ * @param {data} data Each Link data is passed in nav Object
+ */
+const getNavLink = (data = {}) => {
+  const { navObj = {} } = data;
+  return (
+    <Anchor asPath={navObj.url} to={navObj.url} anchorVariation="primary" fontSizeVariation="large">
+      {navObj.displayName}
+    </Anchor>
+  );
+};
+
+/**
+ * @function LeftNavigation The AccountLayout component will provide a list of left
+ * navigationLinks and the component associated with it
+ * The main component will include this Layout and pass the component to render on the right panel
+ * @param {className} className Class name for the left navigation.
+ * @param {active} active name of active element from the list
+ * @param {data} data navigation data
+ */
+
+const LeftNavigation = ({ className }) => {
+  const {
+    data: { subnavigation: navData = [] },
+  } = mockLeftSideNavigation;
+  const navDropDown = navData.length && navData.map(nav => nav.leafLink);
+  const activeComponent = useState(navDropDown[0].component || null);
+
+  return (
+    <React.Fragment>
+      <Row className="is-hidden-nav">
+        <Col colSize={{ large: 12, medium: 8, small: 6 }}>
+          <Dropdown options={navDropDown} active={activeComponent[0]} isUpperCase />
+        </Col>
+      </Row>
+      <Row className="is-visible-nav">
+        <Col colSize={{ large: 12, medium: 8, small: 6 }} offsetLeft={{ medium: 2 }}>
+          <ul className={className}>
+            {navData.length &&
+              navData.map((nav, index) => {
+                const { leafLink: navObj = {} } = nav;
+                return (
+                  <li id={index} key={navObj.displayName} className="nav-link-wrapper">
+                    {getNavLink({ navObj })}
+                  </li>
+                );
+              })}
+          </ul>
+        </Col>
+      </Row>
+    </React.Fragment>
+  );
+};
 
 LeftNavigation.propTypes = {
   className: PropTypes.string.isRequired,
-  children: PropTypes.arrayOf(PropTypes.node),
-};
-
-LeftNavigation.defaultProps = {
-  children: null,
 };
 
 export default withStyles(LeftNavigation, style);
