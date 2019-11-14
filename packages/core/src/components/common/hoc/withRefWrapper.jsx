337a3207bffa81c5316ca3028f6dadaebcef0dec
Erik Jung <erik.jung@publicissapient.com>
2019-11-14 08:28:53 -0500

    RWD-5598 | Add JSDoc

--- packages/core/src/components/common/hoc/withRefWrapper.jsx
+++ packages/core/src/components/common/hoc/withRefWrapper.jsx
@@ -1,5 +1,13 @@
 import React, { forwardRef } from 'react';
 
+/**
+ * This HOC is only needed for components that are either class-based or have no
+ * convenient parent element to use forwardRef with. This should be used sparingly.
+ *
+ * @param {React.Component} Component base component
+ * @param {String} RootElement element or component to use as a wrapper
+ * @param {String} displayName HOC display name
+ */
 export default function withRefWrapper(Component, RootElement = 'div', displayName = '') {
   const HOC = forwardRef((props, ref) => (
     <RootElement ref={ref}>
