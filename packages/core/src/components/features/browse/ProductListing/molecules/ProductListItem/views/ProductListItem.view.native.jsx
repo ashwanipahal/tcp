fbb04f420eff7b215f2e692831229a620af5e451
Ravee <sumer.patel@publicissapient.com>
2019-11-14 11:33:39 +0530

    RWD-15357 collection pdp gymboree price range issue

--- packages/core/src/components/features/browse/ProductListing/molecules/ProductListItem/views/ProductListItem.view.native.jsx
+++ packages/core/src/components/features/browse/ProductListing/molecules/ProductListItem/views/ProductListItem.view.native.jsx
@@ -318,7 +318,7 @@ const renderPricePercentageDiscountLabel = value => {
   if (value) {
     return (
       <BodyCopy
-        margin="8px 0 0 0"
+        margin="0 0 0 8px"
         dataLocator="plp_filter_size_range"
         mobileFontFamily="secondary"
         fontSize="fs10"
@@ -343,7 +343,7 @@ const renderListPrice = (productInfo, currencySymbol, currencyExchange, badge3) 
     currencyExchange,
     null
   );
-  if (lowListPrice && lowOfferPrice && lowListPrice > lowOfferPrice) {
+  if (lowListPrice > lowOfferPrice || highListPrice > lowListPrice) {
     return (
       <OfferPriceAndBadge3Container>
         {renderListPriceLabels(listPriceValue)}
