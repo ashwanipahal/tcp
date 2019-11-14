e1b36d06366b2c12b71745aff1ad098a0a3efb3d
harmeet <harmeet.singh@publicissapient.com>
2019-11-14 19:15:05 +0530

    added spinner

---
+++ packages/core/src/components/features/CnC/AddedToBag/skeleton/AddedToBagSkeleton.style.native.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components/native';
+
+// const ParentWrapper = styled.View`
+//   background-color: ${props => props.theme.colorPalette.white};
+// `;
+
+const PricePointsWrapper = styled.View`
+  padding: 10px 0 10px 10px;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const PriceSubText = styled.View`
+  width: 160px;
+  height: 15px;
+`;
+
+const PriceSubValue = styled.View`
+  width: 80px;
+  height: 15px;
+`;
+
+const PriceSubText2 = styled.View`
+  width: 120px;
+  height: 15px;
+`;
+
+const PriceSubValue2 = styled.View`
+  width: 50px;
+  height: 15px;
+`;
+
+const PriceSubText3 = styled.View`
+  width: 160px;
+  height: 15px;
+`;
+
+const PriceSubValue3 = styled.View`
+  width: 80px;
+  height: 15px;
+`;
+
+export {
+  PricePointsWrapper,
+  PriceSubText,
+  PriceSubValue,
+  PriceSubText2,
+  PriceSubValue2,
+  PriceSubText3,
+  PriceSubValue3,
+};
