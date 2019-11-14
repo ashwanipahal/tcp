2f34600bb74795151dc29cab4f2c2ab09686ebae
Vasu Nagpal <vnagpal2@sapient.com>
2019-11-14 14:17:22 -0500

    increased margin for iphoneX

--- packages/core/src/components/common/molecules/ApplyNowPLCCModal/views/ApplyNowView/ApplyNowWrapper.native.jsx
+++ packages/core/src/components/common/molecules/ApplyNowPLCCModal/views/ApplyNowView/ApplyNowWrapper.native.jsx
@@ -82,7 +82,7 @@ class ApplyNowModalWrapper extends React.PureComponent {
           heading={getLabelValue(labels, 'lbl_PLCCForm_rewardsCardHeading')}
           onRequestClose={isPLCCModalOpen ? this.closePlccModal : this.closeModal}
           fontSize="fs14"
-          customHeaderMargin="55px 24px 0 24px"
+          customHeaderMargin="55px 25px 0 25px"
         />
         {isPLCCModalOpen && (
           <ApplyCardLayoutView
