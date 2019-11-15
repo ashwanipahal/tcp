4ff0bc1b3da7af237e2d477aabbb3824829380c1
Rajeev Jain <rajeev.jain@publicissapient.com>
2019-11-15 12:51:21 +0530

    Rollback

--- packages/core/src/components/features/account/OverlayModal/organisms/OverlayModalSection/views/OverlayModal.jsx
+++ packages/core/src/components/features/account/OverlayModal/organisms/OverlayModalSection/views/OverlayModal.jsx
@@ -79,7 +79,7 @@ class OverlayModal extends React.Component {
     this.isMobile = getViewportInfo().isMobile && isMobileWeb();
 
     if (!this.isMobile) {
-      modal.addEventListener('mousedown', this.keydownInOverlay);
+      modal.addEventListener('keydown', this.keydownInOverlay);
     }
 
     if (this.isMobile && nextTargetComponent === 'accountDrawer') {
@@ -102,7 +102,7 @@ class OverlayModal extends React.Component {
       this.body.style['overflow-y'] = '';
     }
     const modal = document.getElementById('dialogContent');
-    modal.removeEventListener('mousedown', this.keydownInOverlay);
+    modal.removeEventListener('keydown', this.keydownInOverlay);
     this.resetBodyScrollStyles();
     document
       .querySelectorAll('#overlayWrapper, .header-promo__container, footer')
