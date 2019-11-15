3fcaeec4c7ff067d1bb2653c3f02056d15af40e9
Raghav <raghasb@gmail.com>
2019-11-15 07:11:33 +0530

    Added icon condition

--- packages/core/src/components/common/atoms/TextItems/TextItems.jsx
+++ packages/core/src/components/common/atoms/TextItems/TextItems.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Image } from '..';
 import errorBoundary from '../../hoc/withErrorBoundary';
+import { getIconPath } from '../../../../utils';
 
-const TextItems = ({ textItems }) => {
+const TextItems = ({ textItems, icon: { placement, icon } }) => {
   if (!textItems) {
     return null;
   }
-  return textItems.map(({ style, text }, index) => (
-    <span key={index.toString()} className={style}>
-      {index ? ` ${text}` : text}
-    </span>
-  ));
+  return textItems.map(({ style, text }, index) => {
+    return (
+      <span key={index.toString()} className={style}>
+        {placement === 'left' ? <Image src={getIconPath(icon)} /> : null}
+        {index ? ` ${text}` : text}
+        {placement === 'right' ? <Image src={getIconPath(icon)} /> : null}
+      </span>
+    );
+  });
 };
 
 export default errorBoundary(TextItems);
