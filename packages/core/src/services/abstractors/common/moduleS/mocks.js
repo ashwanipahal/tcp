bcf11447d7c519a2494f89c84f66b2c12a621142
Kuldeep Singh <kuldeep.singh5@publicissapient.com>
2019-11-18 14:25:44 +0530

    duplicated code changes

---
+++ packages/core/src/services/abstractors/common/moduleS/mocks.js
@@ -0,0 +1,314 @@
+const mockV1 = {
+  moduleS: {
+    contentId: 'e499bac3-6770-4757-b924-55c837712dfcs',
+    name: 'moduleS',
+    type: 'module',
+    composites: {
+      headerText: [
+        {
+          textItems: [
+            {
+              text: 'Home Run Ready!1',
+              style: 'medium_text_regular',
+            },
+            {
+              text: 'Lets Play.',
+              style: 'medium_text_black',
+            },
+          ],
+          link: {
+            url: '/c/girls-clothess2',
+            text: 'Girlss',
+            title: 'Girlss',
+            target: '',
+            external: 0,
+            action: '',
+          },
+          icon: {
+            icon: '',
+            placement: '',
+          },
+        },
+      ],
+      linkedImage: [
+        {
+          image: {
+            url:
+              'https://dam-qa-ressh.cloudinary.com/image/upload/v1570708260/PlayB_Spring-1_2x_diwsan_pcs7srk.png',
+            alt: 'Pool',
+            title: 'Pool',
+            crop_d: 'c_crop,g_face:center,q_auto:best,w_610',
+            crop_t: 'c_crop,g_face:center,q_auto:best,w_768',
+            crop_m: 'c_crop,g_face:center,q_auto:best,w_305',
+            url_m: '',
+            url_t: '',
+          },
+          link: {
+            url: '/c/boys-clothess',
+            text: 'Pool',
+            title: 'Pool',
+            target: '',
+          },
+        },
+      ],
+      singleCTAButton: {
+        url: '/c/girls-clothess',
+        text: 'SHOP OPENINGG DAY',
+        title: 'SHOP OPENINNG DAY',
+        target: '',
+      },
+      moduleWidth: 'half',
+    },
+    set: [
+      {
+        key: 'moduleWidth',
+        val: 'half',
+      },
+    ],
+  },
+};
+
+const mockV2 = {
+  moduleS: {
+    contentId: 'e499bac3-6770-4757-b924-55c837712dfcs',
+    name: 'moduleS',
+    type: 'module',
+    composites: {
+      headerText: [
+        {
+          textItems: [
+            {
+              text: 'A Very Cherry Valentines!',
+              style: 'medium_text_black',
+            },
+          ],
+          link: {
+            // eslint-disable-next-line sonarjs/no-duplicate-string
+            url: '/c/girls-clothes/ac',
+            text: 'Girlss',
+            title: 'Girlss',
+            target: '',
+            external: 0,
+            action: '',
+          },
+          icon: {
+            icon: '',
+            placement: '',
+          },
+        },
+      ],
+      linkedImage: [
+        {
+          image: {
+            url:
+              'https://dam-qa-ressh.cloudinary.com/image/upload/v1570707425/Module-H_TCP_ybjasx_ahura1d.jpg',
+            alt: 'Pool',
+            title: 'Pool',
+            crop_d: 'c_crop,g_face:center,q_auto:best,w_440',
+            crop_t: 'c_crop,g_face:center,q_auto:best,w_530',
+            crop_m: 'c_crop,g_face:center,q_auto:best,w_327',
+            url_m: '',
+            url_t: '',
+          },
+          link: {
+            url: '/c/boys-clothess',
+            text: 'Pool',
+            title: 'Pool',
+            target: '',
+          },
+        },
+      ],
+      singleCTAButton: {
+        url: '/c/girls-clothess',
+        text: 'SHOP OPENING DAYD',
+        title: 'SHOP OPENING DAYD',
+        target: '',
+      },
+      ribbonBanner: [
+        {
+          textItems: [
+            {
+              text: '20% OF',
+              style: 'style10',
+            },
+          ],
+          link: {
+            url: '/c/girls-clothess1',
+            text: 'Girls',
+            title: 'Girls',
+            target: '',
+          },
+          ribbonPlacement: 'left',
+          ribbonClass: 'ribbon-color-cherry',
+        },
+      ],
+      moduleWidth: 'half',
+    },
+    set: [
+      {
+        key: 'moduleWidth',
+        val: 'half',
+      },
+    ],
+  },
+};
+
+const mockV3 = {
+  moduleS: {
+    contentId: 'e499bac3-6770-4757-b924-55c837712dfc',
+    name: 'moduleS',
+    type: 'module',
+    composites: {
+      headerText: [
+        {
+          textItems: [
+            {
+              text: 'SHOP ALL SUMMERR',
+              style: 'medium_text_black',
+            },
+            {
+              text: 'SURFS UPP',
+              style: 'small_text_normal',
+            },
+            {
+              text: 'POOL SIDEE',
+              style: 'small_text_normal',
+            },
+          ],
+          link: {
+            url: '/c/girls-clothesqw',
+            text: 'GirlsS',
+            title: 'Girls',
+            target: '',
+            external: 0,
+            action: '',
+          },
+          icon: {
+            icon: '',
+            placement: '',
+          },
+        },
+      ],
+      linkedImage: [
+        {
+          image: {
+            url:
+              'https://dam-qa-ressh.cloudinary.com/image/upload/v1570707425/Module-H_TCP_ybjasx_ahura1.jpg',
+            alt: 'Pool',
+            title: 'Pool',
+            crop_d: 'c_crop,g_face:center,q_auto:best,w_120',
+            crop_t: 'c_crop,g_face:center,q_auto:best,w_768',
+            crop_m: 'c_crop,g_face:center,q_auto:best,w_375,h_356',
+            url_m: '',
+            url_t: '',
+          },
+          link: {
+            url: '/c/boys-clothes',
+            text: 'Pool',
+            title: 'Pool',
+            target: '',
+          },
+        },
+      ],
+      singleCTAButton: {
+        url: '/c/girls-clothesw',
+        text: 'SHOP OPENING DAY11',
+        title: 'SHOP OPENING D1AY',
+        target: '',
+      },
+      moduleWidth: 'half',
+    },
+    set: [
+      {
+        key: 'moduleWidth',
+        val: 'half',
+      },
+    ],
+  },
+};
+const mockV4 = {
+  moduleS: {
+    contentId: 'e499bac3-6770-4757-b924-55c837712dfc',
+    name: 'moduleS',
+    type: 'module',
+    composites: {
+      headerText: [
+        {
+          textItems: [
+            {
+              text: 'A Very Cherry Valentines',
+              style: 'medium_text_black',
+            },
+          ],
+          link: {
+            // eslint-disable-next-line sonarjs/no-duplicate-string
+            url: '/c/girls-clothes',
+            text: 'Girls',
+            title: 'Girls',
+            target: '',
+            external: 0,
+            action: '',
+          },
+          icon: {
+            icon: '',
+            placement: '',
+          },
+        },
+      ],
+      linkedImage: [
+        {
+          image: {
+            url:
+              'https://dam-qa-ressh.cloudinary.com/image/upload/v1570707425/Module-H_TCP_ybjasx_ahura1.jpg',
+            alt: 'Pool',
+            title: 'Pool',
+            crop_d: 'c_crop,g_face:center,q_auto:best,w_450',
+            crop_t: 'c_crop,g_face:center,q_auto:best,w_500',
+            crop_m: 'c_crop,g_face:center,q_auto:best,w_347',
+            url_m: '',
+            url_t: '',
+          },
+          link: {
+            url: '/c/boys-clothes',
+            text: 'Pool',
+            title: 'Pool',
+            target: '',
+          },
+        },
+      ],
+      singleCTAButton: {
+        url: '/c/girls-clothes',
+        text: 'SHOP OPENING DAY',
+        title: 'SHOP OPENING DAY',
+        target: '',
+      },
+      ribbonBanner: [
+        {
+          textItems: [
+            {
+              text: '20% OFF',
+              style: 'style1',
+            },
+          ],
+          link: {
+            url: '/c/girls-clothes',
+            text: 'Girls',
+            title: 'Girls',
+            target: '',
+          },
+          ribbonPlacement: 'right',
+          ribbonClass: 'ribbon-color-cherry',
+        },
+      ],
+      moduleWidth: 'half',
+    },
+    set: [
+      {
+        key: 'moduleWidth',
+        val: 'half',
+      },
+    ],
+  },
+};
+
+export default { mockV1, mockV2, mockV3, mockV4 };
