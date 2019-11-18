82590acafffb06371b11fe993303cd0494a8c5d8
Raghav <raghasb@gmail.com>
2019-11-18 12:23:27 +0530

    revereted code to fix gated issue

--- packages/web/src/components/features/content/Header/container/Header.container.js
+++ packages/web/src/components/features/content/Header/container/Header.container.js
@@ -27,7 +27,7 @@ import HeaderView from '../views';
 
 const mapStateToProps = state => {
   const { Header } = state;
-
+  console.log(Header);
   return {
     loyaltyPromoBanner: Header.loyaltyPromoBanner,
     brandTabs: Header.brandTabs,
