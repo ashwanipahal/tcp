6ffcb35c1ccfe2dac1e328dba59848a7ff97cefa
Mohit Singh <mohsingh8@publicisgroupe.net>
2019-11-18 14:48:06 +0530

    RWD-14769: Incorporated review comments

--- packages/core/src/components/features/CnC/CartItemTile/container/CartItemTile.selectors.js
+++ packages/core/src/components/features/CnC/CartItemTile/container/CartItemTile.selectors.js
@@ -1,4 +1,5 @@
 import { getLabelValue } from '@tcp/core/src/utils';
+import { SESSIONCONFIG_REDUCER_KEY } from '@tcp/core/src/constants/reducer.constants';
 import CARTPAGE_CONSTANTS from '../CartItemTile.constants';
 
 export const getCartOrderList = state => {
@@ -435,5 +436,8 @@ export const getCartBossBopisToggleError = state => {
 };
 
 export const getIsDeleteConfirmationModalEnabled = state => {
-  return state.session.siteDetails.IS_DELETE_CONFIRMATION_MODAL_ENABLED === 'TRUE';
+  return (
+    state[SESSIONCONFIG_REDUCER_KEY] &&
+    state[SESSIONCONFIG_REDUCER_KEY].siteDetails.IS_DELETE_CONFIRMATION_MODAL_ENABLED === 'TRUE'
+  );
 };
