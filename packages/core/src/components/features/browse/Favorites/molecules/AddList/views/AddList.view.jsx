a27e383fe26965e75b826ad24a7d3c590b7353e8
Vijendra Mishra <vmishra8@sapient.com>
2019-11-19 18:09:57 +0530

    add list form code

---
+++ packages/core/src/components/features/browse/Favorites/molecules/AddList/views/AddList.view.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import { Field, reduxForm } from 'redux-form';
+import { getLabelValue } from '@tcp/core/src/utils/utils';
+import Row from '@tcp/core/src/components/common/atoms/Row';
+import TextBox from '@tcp/core/src/components/common/atoms/TextBox';
+import Col from '@tcp/core/src/components/common/atoms/Col';
+import withStyles from '@tcp/core/src/components/common/hoc/withStyles';
+import Button from '@tcp/core/src/components/common/atoms/Button';
+import createValidateMethod from '../../../../../../../utils/formValidation/createValidateMethod';
+import getStandardConfig from '../../../../../../../utils/formValidation/validatorStandardConfig';
+import styles from '../styles/AddList.style';
+
+class AddList extends React.PureComponent {
+  render() {
+    const { labels, className } = this.props;
+
+    return (
+      <>
+        <form className={className}>
+          <Row fullBleed>
+            <Col colSize={{ small: 6, medium: 8, large: 12 }}>
+              <Field
+                placeholder={getLabelValue(labels, 'lbl_add_child_save')}
+                name="listName"
+                id="listName"
+                type="text"
+                component={TextBox}
+                dataLocator="childNameField"
+              />
+            </Col>
+          </Row>
+          <Row fullBleed>
+            <Col colSize={{ small: 6, medium: 8, large: 12 }}>
+              <Button
+                buttonVariation="fixed-width"
+                type="submit"
+                fill="BLUE"
+                dataLocator="SaveListFormBtn"
+              >
+                {getLabelValue(labels, 'lbl_add_child_save')}
+              </Button>
+            </Col>
+          </Row>
+          <Row fullBleed>
+            <Col colSize={{ small: 6, medium: 8, large: 12 }}>
+              <Button buttonVariation="fixed-width" type="submit" dataLocator="CancelListFormBtn">
+                {getLabelValue(labels, 'lbl_add_child_save')}
+              </Button>
+            </Col>
+          </Row>
+        </form>
+      </>
+    );
+  }
+}
+
+const validateMethod = createValidateMethod(
+  getStandardConfig([
+    'acceptAddChildAgreement',
+    'childName',
+    'firstName',
+    'lastName',
+    'childBirthMonth',
+    'childBirthYear',
+    'gender',
+  ])
+);
+
+AddList.propTypes = {
+  labels: PropTypes.shape({}),
+  className: PropTypes.string,
+};
+AddList.defaultProps = {
+  labels: {},
+  className: '',
+};
+
+export default reduxForm({
+  form: 'AddListForm', // a unique identifier for this form
+  enableReinitialize: true,
+  ...validateMethod,
+})(withStyles(AddList, styles));
+
+export { AddList as AddListFormVanilla };
--- packages/core/src/components/features/browse/Favorites/views/Favorites.view.jsx
+++ packages/core/src/components/features/browse/Favorites/views/Favorites.view.jsx
@@ -9,93 +9,80 @@ import FavoritesViewStyle from '../styles/Favorites.style';
 import { getNonEmptyFiltersList, getSortsList, getVisibleWishlistItems } from '../Favorites.util';
 import SelectWishListDropdown from '../molecules/SelectWishListDropdown';
 import CustomSelect from '../../../../common/molecules/CustomSelect/views';
+import AddList from '../molecules/AddList/views';
 
-const FavoritesView = props => {
-  const {
-    className,
-    wishlistsSummaries,
-    activeWishList,
-    createNewWishListMoveItem,
-    getActiveWishlist,
-    createNewWishList,
-    setLastDeletedItemId,
-    labels,
-    slpLabels,
-    onQuickViewOpenClick,
-    selectedColorProductId,
-    // deleteWishList, @TODO will be used in the wish-list pop-up
-    filteredId,
-    sortId,
-    onFilterSelection,
-    onSortSelection,
-    selectBrandType,
-    gymSelected,
-    tcpSelected,
-    defaultWishList,
-  } = props;
+class FavoritesView extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    console.log('isModalOpen');
+  }
 
-  const shareClickHandler = value => {
+  shareClickHandler = value => {
     console.log(value);
   };
 
-  const filters = activeWishList ? getNonEmptyFiltersList(activeWishList.items, labels) : [];
+  renderProductList = () => {
+    const {
+      wishlistsSummaries,
+      activeWishList,
+      createNewWishListMoveItem,
+      createNewWishList,
+      setLastDeletedItemId,
+      labels,
+      onQuickViewOpenClick,
+      selectedColorProductId,
+      filteredId,
+      sortId,
+      gymSelected,
+      tcpSelected,
+    } = this.props;
 
-  let filteredItemsList =
-    !!activeWishList && getVisibleWishlistItems(activeWishList.items, filteredId, sortId);
-  if (filteredItemsList) {
-    if (gymSelected) {
-      filteredItemsList = filteredItemsList.filter(item => !item.itemInfo.isTCP);
-    } else if (tcpSelected) {
-      filteredItemsList = filteredItemsList.filter(item => item.itemInfo.isTCP);
+    let filteredItemsList =
+      !!activeWishList && getVisibleWishlistItems(activeWishList.items, filteredId, sortId);
+    if (filteredItemsList) {
+      if (gymSelected) {
+        filteredItemsList = filteredItemsList.filter(item => !item.itemInfo.isTCP);
+      } else if (tcpSelected) {
+        filteredItemsList = filteredItemsList.filter(item => item.itemInfo.isTCP);
+      }
     }
-  }
 
-  const productsList = !!filteredItemsList && (
-    <>
-      <ProductsGrid
-        products={filteredItemsList}
-        productsBlock={[filteredItemsList]}
-        labels={labels}
-        wishlistsSummaries={wishlistsSummaries}
-        createNewWishList={createNewWishList}
-        onQuickViewOpenClick={onQuickViewOpenClick}
-        isFavoriteView
-        removeFavItem={setLastDeletedItemId}
-        createNewWishListMoveItem={createNewWishListMoveItem}
-      />
-      <QuickViewModal selectedColorProductId={selectedColorProductId} />
-    </>
-  );
+    return (
+      !!filteredItemsList && (
+        <>
+          <ProductsGrid
+            products={filteredItemsList}
+            productsBlock={[filteredItemsList]}
+            labels={labels}
+            wishlistsSummaries={wishlistsSummaries}
+            createNewWishList={createNewWishList}
+            onQuickViewOpenClick={onQuickViewOpenClick}
+            isFavoriteView
+            removeFavItem={setLastDeletedItemId}
+            createNewWishListMoveItem={createNewWishListMoveItem}
+          />
+          <QuickViewModal selectedColorProductId={selectedColorProductId} />
+        </>
+      )
+    );
+  };
 
-  const brandOptions = [
-    {
-      name: 'gymboreeOption',
-      dataLocator: 'gymboree-option',
-      brandLabel: labels.lbl_fav_gym,
-      checked: gymSelected,
-    },
-    {
-      name: 'tcpOption',
-      dataLocator: 'tcp-option',
-      brandLabel: labels.lbl_fav_tcp,
-      checked: tcpSelected,
-    },
-  ];
-
-  const shareOptions = [
-    {
-      title: 'Email',
-      value: 'Email',
-      content: <span>Email</span>,
-    },
-    {
-      title: labels.lbl_fav_copyLink,
-      value: labels.lbl_fav_copyLink,
-      content: <span>{labels.lbl_fav_copyLink}</span>,
-    },
-  ];
-
-  const BrandFilterList = () => {
+  brandFilterList = () => {
+    const { labels, selectBrandType, gymSelected, tcpSelected } = this.props;
+    const brandOptions = [
+      {
+        name: 'gymboreeOption',
+        dataLocator: 'gymboree-option',
+        brandLabel: labels.lbl_fav_gym,
+        checked: gymSelected,
+      },
+      {
+        name: 'tcpOption',
+        dataLocator: 'tcp-option',
+        brandLabel: labels.lbl_fav_tcp,
+        checked: tcpSelected,
+      },
+    ];
     return (
       <>
         <div>
@@ -118,96 +105,128 @@ const FavoritesView = props => {
     );
   };
 
-  return (
-    <div className={className}>
-      <Row fullBleed>
-        <Col
-          colSize={{ small: 6, medium: 8, large: 12 }}
-          ignoreGutter={{ small: true, medium: true, large: true }}
-        >
-          <BodyCopy fontWeight="extrabold" fontSize="fs16" className="favorite-title">
-            {labels.lbl_fav_myFavorites}
-          </BodyCopy>
-        </Col>
-      </Row>
-      <Row fullBleed className="list-selection-row">
-        <Col colSize={{ small: 6, medium: 6, large: 8 }}>
-          <Row fullBleed>
-            <Col colSize={{ small: 6, medium: 5, large: 6 }} offsetLeft={{ medium: 3, large: 6 }}>
-              <SelectWishListDropdown
-                labels={labels}
-                wishlistsSummaries={wishlistsSummaries}
-                createNewWishList={createNewWishList}
-                getActiveWishlist={getActiveWishlist}
-                activeWishList={activeWishList}
-                defaultWishList={defaultWishList}
-              />
-            </Col>
-          </Row>
-        </Col>
-        <Col colSize={{ small: 6, medium: 2, large: 4 }}>
-          <Row fullBleed>
-            <Col
-              colSize={{ small: 2, medium: 6, large: 4 }}
-              offsetLeft={{ small: 4, medium: 2, large: 8 }}
-            >
-              <CustomSelect
-                options={shareOptions}
-                activeTitle={labels.lbl_fav_share}
-                clickHandler={(e, value) => shareClickHandler(value)}
-                customSelectClassName="social-share-fav-list"
-              />
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Row fullBleed>
-        <Col colSize={{ small: 6, medium: 8, large: 12 }}>
-          <ProductListingFiltersForm
-            filtersMaps={{
-              display_group_uFilter: filters,
-              unbxdDisplayName: {
-                display_group_uFilter: filters.length && filters[0].displayName,
-              },
-            }}
-            totalProductsCount={!!activeWishList && activeWishList.items.length}
-            initialValues={{}}
-            filtersLength={{}}
-            labels={labels}
-            slpLabels={slpLabels}
-            isFavoriteView
-            favoriteSortingParams={getSortsList(labels)}
-            onFilterSelection={onFilterSelection}
-            onSortSelection={onSortSelection}
-            defaultPlaceholder={getSortsList(labels)[0].displayName}
-          />
-        </Col>
-      </Row>
-      <Row className="brand-filter-section" fullBleed>
-        <Col colSize={{ large: 12, medium: 8, small: 6 }}>
-          <BrandFilterList />
-        </Col>
-      </Row>
-      <Row>
-        <Col
-          colSize={{ small: 6, medium: 8, large: 12 }}
-          className="product-list"
-          ignoreGutter={{ small: true, medium: true, large: true }}
-        >
-          {productsList}
-        </Col>
-        <Col
-          hideCol={{ small: true, medium: true }}
-          colSize={{ small: 6, medium: 8, large: 12 }}
-          className="recommendation"
-        >
-          {/* Placeholder for you may also like */}
-          <div>You may also like</div>
-        </Col>
-      </Row>
-    </div>
-  );
-};
+  render() {
+    const {
+      className,
+      wishlistsSummaries,
+      activeWishList,
+      getActiveWishlist,
+      createNewWishList,
+      labels,
+      slpLabels,
+      onFilterSelection,
+      onSortSelection,
+      defaultWishList,
+    } = this.props;
+
+    const shareOptions = [
+      {
+        title: labels.lbl_fav_facebook,
+        value: labels.lbl_fav_facebook,
+        content: <span>{labels.lbl_fav_facebook}</span>,
+      },
+      {
+        title: labels.lbl_fav_email,
+        value: labels.lbl_fav_email,
+        content: <span>{labels.lbl_fav_email}</span>,
+      },
+      {
+        title: labels.lbl_fav_copyLink,
+        value: labels.lbl_fav_copyLink,
+        content: <span>{labels.lbl_fav_copyLink}</span>,
+      },
+    ];
+    const filters = activeWishList ? getNonEmptyFiltersList(activeWishList.items, labels) : [];
+    return (
+      <div className={className}>
+        <Row fullBleed>
+          <Col
+            colSize={{ small: 6, medium: 8, large: 12 }}
+            ignoreGutter={{ small: true, medium: true, large: true }}
+          >
+            <BodyCopy fontWeight="extrabold" fontSize="fs16" className="favorite-title">
+              {labels.lbl_fav_myFavorites}
+            </BodyCopy>
+          </Col>
+        </Row>
+        <Row fullBleed className="list-selection-row">
+          <Col colSize={{ small: 6, medium: 6, large: 8 }}>
+            <Row fullBleed>
+              <Col colSize={{ small: 6, medium: 5, large: 6 }} offsetLeft={{ medium: 3, large: 6 }}>
+                <SelectWishListDropdown
+                  labels={labels}
+                  wishlistsSummaries={wishlistsSummaries}
+                  createNewWishList={createNewWishList}
+                  getActiveWishlist={getActiveWishlist}
+                  activeWishList={activeWishList}
+                  defaultWishList={defaultWishList}
+                />
+              </Col>
+            </Row>
+          </Col>
+          <Col colSize={{ small: 6, medium: 2, large: 4 }}>
+            <Row fullBleed>
+              <Col
+                colSize={{ small: 2, medium: 6, large: 4 }}
+                offsetLeft={{ small: 4, medium: 2, large: 8 }}
+              >
+                <CustomSelect
+                  options={shareOptions}
+                  activeTitle={labels.lbl_fav_share}
+                  clickHandler={(e, value) => this.shareClickHandler(value)}
+                  customSelectClassName="social-share-fav-list"
+                />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row fullBleed>
+          <Col colSize={{ small: 6, medium: 8, large: 12 }}>
+            <ProductListingFiltersForm
+              filtersMaps={{
+                display_group_uFilter: filters,
+                unbxdDisplayName: {
+                  display_group_uFilter: filters.length && filters[0].displayName,
+                },
+              }}
+              totalProductsCount={!!activeWishList && activeWishList.items.length}
+              initialValues={{}}
+              filtersLength={{}}
+              labels={labels}
+              slpLabels={slpLabels}
+              isFavoriteView
+              favoriteSortingParams={getSortsList(labels)}
+              onFilterSelection={onFilterSelection}
+              onSortSelection={onSortSelection}
+              defaultPlaceholder={getSortsList(labels)[0].displayName}
+            />
+          </Col>
+        </Row>
+        <Row className="brand-filter-section" fullBleed>
+          <Col colSize={{ large: 12, medium: 8, small: 6 }}>{this.brandFilterList()}</Col>
+        </Row>
+        <Row fullBleed>
+          <Col
+            colSize={{ small: 6, medium: 8, large: 12 }}
+            className="product-list"
+            ignoreGutter={{ small: true, medium: true, large: true }}
+          >
+            {this.renderProductList()}
+          </Col>
+          <Col
+            hideCol={{ small: true, medium: true }}
+            colSize={{ small: 6, medium: 8, large: 12 }}
+            className="recommendation"
+          >
+            {/* Placeholder for you may also like */}
+            <div>You may also like</div>
+          </Col>
+        </Row>
+        <AddList labels={labels} />
+      </div>
+    );
+  }
+}
 
 FavoritesView.propTypes = {
   className: PropTypes.string.isRequired,
