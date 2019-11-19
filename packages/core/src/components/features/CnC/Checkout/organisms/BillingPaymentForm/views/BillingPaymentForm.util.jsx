f12b0ab0e91a258f7dd20c32e7183c2821286744
Gagan Bhardwaj <gbhardwaj4@sapient.com>
2019-11-19 18:44:23 -0500

    RWD-10843: Venmo CTA on billing page

--- packages/core/src/components/features/CnC/Checkout/organisms/BillingPaymentForm/views/BillingPaymentForm.util.jsx
+++ packages/core/src/components/features/CnC/Checkout/organisms/BillingPaymentForm/views/BillingPaymentForm.util.jsx
@@ -48,10 +48,45 @@ const onAddNewCreditCardClick = scope => {
   onAddNewCreditCardUpdate(dispatch);
 };
 
+const getPaymentMethods = labels => [
+  { id: constants.PAYMENT_METHOD_CREDIT_CARD, displayName: labels.creditCard },
+  { id: constants.PAYMENT_METHOD_PAY_PAL, displayName: labels.payPal },
+  { id: constants.PAYMENT_METHOD_VENMO, displayName: labels.venmo },
+];
+
+/**
+ * @function onAddCreditCardClick
+ * @description sets the add new credit card state as true
+ */
+const onAddCreditCardClick = scope => {
+  const { dispatch } = scope.props;
+  scope.setState({ addNewCCState: true });
+  dispatch(change(constants.FORM_NAME, 'cardNumber', ''));
+  dispatch(change(constants.FORM_NAME, 'expMonth', ''));
+  dispatch(change(constants.FORM_NAME, 'expYear', ''));
+  dispatch(change(constants.FORM_NAME, 'cvvCode', ''));
+};
+
+/**
+ * @function onCCDropDownChange
+ * @description sets the add new credit card state to false if it is true
+ */
+const onCCDropDownChange = scope => {
+  const { addNewCCState } = scope.state;
+  if (addNewCCState) {
+    scope.setState({ addNewCCState: false });
+  }
+  const { dispatch } = scope.props;
+  dispatch(change(constants.FORM_NAME, 'cvvCode', ''));
+};
+
 export {
   onEditCardFocus,
   setFormToEditState,
   unsetPaymentFormEditState,
   handleBillingFormSubmit,
   onAddNewCreditCardClick,
+  getPaymentMethods,
+  onAddCreditCardClick,
+  onCCDropDownChange,
 };
