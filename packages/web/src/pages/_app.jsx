e5c1850e95c554b95a3b29fc24c13b67a1bc87f0
Raghav <raghasb@gmail.com>
2019-11-20 19:10:10 +0530

    removed headers from redux

--- packages/web/src/pages/_app.jsx
+++ packages/web/src/pages/_app.jsx
@@ -218,9 +218,6 @@ class TCPWebApp extends App {
       apiConfig.isPreviewEnv = req.headers.preview || req.query.preview || '';
       // preview date if any from the query param
       apiConfig.previewDate = req.query.preview_date || '';
-      // response headers
-      apiConfig.resHeaders = res.getHeaders();
-      apiConfig.reqHeaders = req.headers;
       // optimizely headers
       const optimizelyHeadersObject = {};
       const setCookieHeaderList = setCookie.parse(res).map(TCPWebApp.parseCookieResponse);
