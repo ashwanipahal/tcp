ed8ab38ca294088338846832bae431199ebc3b75
Vijendra Mishra <vmishra8@sapient.com>
2019-11-22 10:53:24 +0530

    updated style

--- packages/core/src/components/features/browse/Favorites/molecules/ShareList/styles/ShareList.style.js
+++ packages/core/src/components/features/browse/Favorites/molecules/ShareList/styles/ShareList.style.js
@@ -5,9 +5,10 @@ export default css`
     width: 100%;
     min-height: 200px;
     border: 1px solid ${props => props.theme.colors.BUTTON.WHITE.BORDER};
+    font-size: ${props => props.theme.typography.fontSizes.fs16};
   }
   .share-list-form {
-    && .subject-field input {
+    input.subject-field input {
       font-size: ${props => props.theme.typography.fontSizes.fs14};
     }
   }
