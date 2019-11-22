b3588d2558bb57e99aaf178786c49dce86203287
Devesh Sharma <devesh.sharma@publicissapient.com>
2019-11-22 11:19:40 +0530

    Fix merge conflicts

--- packages/mobileapp/android/app/src/main/java/com/mobileapp/MainApplication.java
+++ packages/mobileapp/android/app/src/main/java/com/mobileapp/MainApplication.java
@@ -8,6 +8,10 @@ import com.facebook.react.ReactApplication;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
+
 import com.facebook.soloader.SoLoader;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -15,6 +19,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 
+import com.google.firebase.FirebaseApp;
 import com.microsoft.codepush.react.CodePush;
 import com.microsoft.codepush.react.ReactInstanceHolder;
 
@@ -62,6 +67,8 @@ public class MainApplication extends Application implements ReactApplication {
           // packages.add(new FBSDKPackage());
           packages.add(new VenmoPackage());
           packages.add(new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), MainApplication.this, BuildConfig.DEBUG));
+          packages.add(new RNFirebaseMessagingPackage());
+          packages.add(new RNFirebaseNotificationsPackage());
           return packages;
         }
 
@@ -82,6 +89,7 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     // initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+    FirebaseApp.initializeApp(this);
   }
 
   /**
