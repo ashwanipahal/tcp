0c020949558882f4553a6d43f529670631f92b89
Aman Bhardwaj <amabhard@publicisgroupe.net>
2019-11-26 22:09:15 +0530

    fixed height problem

--- packages/core/src/components/common/molecules/ModuleP/ModuleP.style.js
+++ packages/core/src/components/common/molecules/ModuleP/ModuleP.style.js
@@ -2,18 +2,19 @@ import { css } from 'styled-components';
 
 export default css`
   .item-container-inner {
-    height: ${props => (props.isPromoAvailable ? '418px' : '')};
+    height: ${props => (props.isPromoAvailable ? '391px' : '')};
     @media ${props => props.theme.mediaQuery.medium} {
-      height: ${props => (props.isPromoAvailable ? '485.23px' : '')};
+      height: ${props => (props.isPromoAvailable ? '512px' : '')};
     }
     @media ${props => props.theme.mediaQuery.large} {
-      height: ${props => (props.isPromoAvailable ? '550.44px' : '')};
+      height: ${props => (props.isPromoAvailable ? '604px' : '')};
     }
   }
 
   margin: 0 10px;
   .product-image-container {
     height: 186px;
+    width: 149px;
     a {
       min-height: 185px;
       display: block;
@@ -36,6 +37,7 @@ export default css`
   @media ${props => props.theme.mediaQuery.medium} {
     .product-image-container {
       height: 267px;
+      width: 214px;
     }
   }
 
@@ -43,6 +45,7 @@ export default css`
     margin: 0 ${props => props.theme.spacing.APP_LAYOUT_SPACING.XS};
     .product-image-container {
       height: 325px;
+      width: 261px;
       a {
         min-height: 259px;
       }
