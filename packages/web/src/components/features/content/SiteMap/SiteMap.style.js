f47c02d00682536b97d794c81ef79c4513f7ca06
Aman Bhardwaj <amabhard@publicisgroupe.net>
2019-11-27 15:44:52 +0530

    added functionality

--- packages/web/src/components/features/content/SiteMap/SiteMap.style.js
+++ packages/web/src/components/features/content/SiteMap/SiteMap.style.js
@@ -1,55 +1,36 @@
 import { css } from 'styled-components';
 
 export default css`
-  .title {
-    text-align: center;
-    margin-bottom: 14px;
+
+  .site_map_container {
+
   }
 
-  &.seo-text {
-    padding: 24px 14px 0px 14px;
-    @media ${props => props.theme.mediaQuery.medium} {
-      padding: 24px 15px 0px 19px;
-    }
-    @media ${props => props.theme.mediaQuery.large} {
-      padding: 24px 40px 0px 50px;
-    }
+  .site_map_heading_container {
+
   }
 
-  .body-copy {
-    padding-bottom: ${props => props.theme.spacing.ELEM_SPACING.SM};
-    text-align: left;
-    margin-bottom: ${props => props.theme.spacing.ELEM_SPACING.XS};
+  .site_map_heading {
+
   }
 
-  .read-more-state {
-    display: none;
+  .heading_link {
+
   }
 
-  .read-less {
-    display: none;
-  }
-  .read-more-target {
-    display: none;
+  .categories_container {
+
   }
 
-  &.read-more-expanded .read-more-target {
-    display: block;
-    font-size: ${props => props.theme.typography.fontSizes.fs14};
-    font-weight: ${props => props.theme.typography.fontWeights.regular};
+  .Link_level_1 {
+
   }
 
-  .read-more-trigger {
-    font-size: ${props => props.theme.typography.fontSizes.fs18};
-    font-weight: ${props => props.theme.typography.fontWeights.semibold};
-    cursor: pointer;
+  .Link_level_2 {
+
   }
 
-  .read-more-state:checked ~ .read-more {
-    display: none;
-  }
+  .Link_level_3 {
 
-  .read-more-state:checked ~ .read-less {
-    display: block;
   }
 `;
--- packages/web/src/components/features/content/SiteMap/views/SiteMap.view.jsx
+++ packages/web/src/components/features/content/SiteMap/views/SiteMap.view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import BodyCopy from '@tcp/core/src/components/common/atoms/BodyCopy';
 import { PropTypes } from 'prop-types';
 import errorBoundary from '@tcp/core/src/components/common/hoc/withErrorBoundary';
 import withStyles from '@tcp/core/src/components/common/hoc/withStyles';
@@ -6,43 +7,65 @@ import style from '../SiteMap.style';
 import { Heading, Anchor } from '@tcp/core/src/components/common/atoms';
 
 class SiteMap extends React.PureComponent {
-
-  createLink(items, level) {
-      return (
-        <Anchor
+  printCurrentLink = (items, level) => {
+    return (
+      <Anchor
         className={`Link_level_${level}`}
         to={items.href.replace('/c/', '/c?cid=')}
         asPath={items.href}
       >
         {items.name}
       </Anchor>
-      if(items.category && items.category.length > 0)
-      {
-        return items.map(item => createLink(items.category, level+1));
-      }
-      )
-  }
+    );
+  };
+
+  createLink = (items, level) => {
+    return this.printCurrentLink(items, level);
+    // return items.category && items.category.length > 0 ? items.map(item => this.createLink(items.category, level + 1)) : null;
+  };
 
   render() {
-    const { siteMapData } = this.props;
+    // const { siteMapData } = this.props;
+    const siteMapData = {content: "The Children Place - Kids Clothes",
+    url: "us/c/toddler-boy-clothes",
+    categories: [
+      {
+        href: "us/c/toddler-boy-clothes",
+        name: "TODDLER BOY",
+        category: []
+      },
+      {
+        href: "us/c/toddler-girl-clothes",
+        name: "TODDLER GIRL",
+        category: []
+      },
+      {
+        href: "us/c/boys-clothing",
+        name: "BOY",
+        category: []
+      },
+      {
+        href: "us/c/baby-clothes",
+        name: "BABY",
+        category: []
+      },
+      {
+        href: "us/c/girls-clothing",
+        name: "GIRL",
+        category: []
+      }]}
     const { content, url, categories } = siteMapData;
 
     return (
       <div className="site_map_container">
         <div className="site_map_heading_container">
           <Heading component="h5" variant="h5" className="site_map_heading">
-            <Anchor
-              className="heading_link"
-              to={url.replace('/c/', '/c?cid=')}
-              asPath={url}
-            >
+            <Anchor className="heading_link" to={url.replace('/c/', '/c?cid=')} asPath={url}>
               {content}
             </Anchor>
           </Heading>
         </div>
-        <div className="categories_container">
-          {categories.map(item = createLink(item, 1))}
-        </div>
+        <div className="categories_container">{categories.map((item => this.createLink(item, 1)))}</div>
       </div>
     );
   }
