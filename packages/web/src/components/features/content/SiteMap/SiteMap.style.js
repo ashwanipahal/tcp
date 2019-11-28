383d24677bad97235920793ec7d3b6bc5e111715
Raghav <raghasb@gmail.com>
2019-11-28 15:17:02 +0530

    render data in view

--- packages/web/src/components/features/content/SiteMap/SiteMap.style.js
+++ packages/web/src/components/features/content/SiteMap/SiteMap.style.js
@@ -2,6 +2,30 @@ import { css } from 'styled-components';
 
 export default css`
   .siteMap_heading-container {
-    margin: 0 ${props => props.theme.spacing.ELEM_SPACING.LRG};
+    margin: 0 auto ${props => props.theme.spacing.LAYOUT_SPACING.LRG};
+  }
+  .siteMap_heading {
+    text-transform: uppercase;
+  }
+
+  .categories_container {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+  }
+
+  ol {
+    margin: 0;
+    padding: 0;
+    width: 33%;
+
+    h4 {
+      color: ${props => props.theme.colors.PRIMARY.GRAY['900']};
+      margin: 0 0 ${props => props.theme.spacing.ELEM_SPACING.MED};
+
+      a {
+        color: ${props => props.theme.colors.PRIMARY.GRAY['900']};
+      }
+    }
   }
 `;
--- packages/web/src/components/features/content/SiteMap/views/SiteMap.view.jsx
+++ packages/web/src/components/features/content/SiteMap/views/SiteMap.view.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
-import { Anchor, Heading, Col, Row } from '@tcp/core/src/components/common/atoms';
+import { Anchor, BodyCopy, Col, Row } from '@tcp/core/src/components/common/atoms';
 import errorBoundary from '@tcp/core/src/components/common/hoc/withErrorBoundary';
 import withStyles from '@tcp/core/src/components/common/hoc/withStyles';
+import mock from '@tcp/core/src/services/abstractors/common/sitemap/mock';
 import style from '../SiteMap.style';
 
 class SiteMap extends React.PureComponent {
@@ -25,20 +26,100 @@ class SiteMap extends React.PureComponent {
   };
 
   render() {
-    const { className, siteMapData } = this.props;
-    const { categories, content, url } = siteMapData;
+    const { className } = this.props;
+    const { siteMap: siteMapData } = mock;
+    const { categories, content } = siteMapData;
+    console.log(categories);
     return (
       <Row className={`${className} siteMap`}>
-        <Col colSize={{ small: 6, medium: 8, large: 10 }} offsetLeft={{ large: 1 }}>
+        <Col
+          colSize={{ small: 6, medium: 8, large: 10 }}
+          offsetLeft={{ large: 1 }}
+          offsetRight={{ large: 1 }}
+        >
           <div className="siteMap_heading-container">
-            <Heading component="h5" variant="h5" className="site_map_heading">
-              <Anchor className="heading_link" to={url} asPath={url}>
-                {content}
-              </Anchor>
-            </Heading>
+            <BodyCopy
+              component="h3"
+              className="siteMap_heading"
+              fontWeight="medium"
+              fontSize="fs24"
+              textAlign="center"
+              letterSpacing="normal"
+            >
+              {content}
+            </BodyCopy>
           </div>
           <div className="categories_container">
-            {categories.map(item => this.createLink(item, 1))}
+            {categories &&
+              categories.map(({ name, href, category }) => {
+                return (
+                  name && (
+                    <ol className="level-two-container">
+                      <BodyCopy
+                        component="h4"
+                        className="level-two-title"
+                        color="gray.900"
+                        fontWeight="medium"
+                        fontSize="fs16"
+                        textAlign="left"
+                        letterSpacing="normal"
+                      >
+                        {href ? (
+                          <Anchor to={href} asPath={href}>
+                            {name}
+                          </Anchor>
+                        ) : (
+                          name
+                        )}
+                      </BodyCopy>
+                      {category &&
+                        category.map(({ name: nameL2, href: hrefL2, category: categoriesL3 }) => {
+                          return (
+                            nameL2 && (
+                              <li>
+                                <ul className="level-three-container">
+                                  <BodyCopy
+                                    component="h5"
+                                    className="level-three-title"
+                                    color="gray.900"
+                                    fontWeight="medium"
+                                    fontSize="fs14"
+                                    textAlign="left"
+                                    letterSpacing="normal"
+                                  >
+                                    {href ? (
+                                      <Anchor to={hrefL2} asPath={hrefL2}>
+                                        {nameL2}
+                                      </Anchor>
+                                    ) : (
+                                      nameL2
+                                    )}
+                                  </BodyCopy>
+                                  {categoriesL3 &&
+                                    categoriesL3.map(({ name: nameL3, href: hrefL3 }) => {
+                                      return (
+                                        nameL3 && (
+                                          <li>
+                                            {href ? (
+                                              <Anchor to={hrefL3} asPath={hrefL3}>
+                                                {nameL3}
+                                              </Anchor>
+                                            ) : (
+                                              nameL3
+                                            )}
+                                          </li>
+                                        )
+                                      );
+                                    })}
+                                </ul>
+                              </li>
+                            )
+                          );
+                        })}
+                    </ol>
+                  )
+                );
+              })}
           </div>
         </Col>
       </Row>
