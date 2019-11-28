c06d73d48800d3bc50abaf8fc1951dadc360ff4e
Abhinesh Yadav <abhinesh.yadav@publicissapient.com>
2019-11-28 16:28:01 +0530

    Promo Desktop visual chnages

--- packages/core/src/components/common/molecules/GridPromo/styles/GridPromo.style.js
+++ packages/core/src/components/common/molecules/GridPromo/styles/GridPromo.style.js
@@ -12,6 +12,12 @@ const stylesGridPromo = css`
     .highlighted-text {
       background: linear-gradient(to top, yellow 70%, transparent 70%);
       padding: 0 15px;
+      @media ${props => props.theme.mediaQuery.smallOnly} {
+        font-size: ${props => props.theme.typography.fontSizes.fs28};
+      }
+      @media ${props => props.theme.mediaQuery.mediumOnly} {
+        font-size: ${props => props.theme.typography.fontSizes.fs32};
+      }
     }
     .headline-wrapper {
       padding-bottom: 12px;
@@ -19,8 +25,37 @@ const stylesGridPromo = css`
     .middle-text-wrapper {
       padding-bottom: 30px;
     }
+
+    .middle-text-wrapper > p {
+      @media ${props => props.theme.mediaQuery.smallOnly} {
+        font-size: ${props => props.theme.typography.fontSizes.fs18};
+        font-weight: ${props => props.theme.typography.fontWeights.black};
+      }
+      @media ${props => props.theme.mediaQuery.mediumOnly} {
+        font-size: ${props => props.theme.typography.fontSizes.fs20};
+        font-weight: ${props => props.theme.typography.fontWeights.black};
+      }
+    }
     .description-wrapper {
       padding-bottom: 24px;
+    }
+
+    .description-wrapper > p {
+      @media ${props => props.theme.mediaQuery.smallOnly} {
+        font-size: ${props => props.theme.typography.fontSizes.fs10};
+      }
+      @media ${props => props.theme.mediaQuery.mediumOnly} {
+        font-size: ${props => props.theme.typography.fontSizes.fs12};
+      }
+    }
+
+    .cta-wrapper > a {
+      @media ${props => props.theme.mediaQuery.smallOnly} {
+        font-size: ${props => props.theme.typography.fontSizes.fs10};
+      }
+      @media ${props => props.theme.mediaQuery.mediumOnly} {
+        font-size: ${props => props.theme.typography.fontSizes.fs10};
+      }
     }
   }
 `;
