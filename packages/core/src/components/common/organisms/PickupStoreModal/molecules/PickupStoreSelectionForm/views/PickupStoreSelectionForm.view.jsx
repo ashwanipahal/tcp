0ff13210e3a3d177714bff557af3b79a87591562
harmeet <harmeet.singh@publicissapient.com>
2019-11-28 13:54:52 +0530

    modal height fixed

--- packages/core/src/components/common/organisms/PickupStoreModal/molecules/PickupStoreSelectionForm/views/PickupStoreSelectionForm.view.jsx
+++ packages/core/src/components/common/organisms/PickupStoreModal/molecules/PickupStoreSelectionForm/views/PickupStoreSelectionForm.view.jsx
@@ -335,14 +335,16 @@ class _PickupStoreSelectionForm extends React.Component {
       <form onSubmit={handleSubmit(onSearch)}>
         {isPickUpWarningModal && (
           <div className={className}>
-            <BodyCopy
-              className="item-unavailable"
-              fontFamily="secondary"
-              fontWeight="semibold"
-              fontSize="fs14"
-            >
-              {PICKUP_LABELS.ITEM_UNAVAILABLE}
-            </BodyCopy>
+            <div className="warning-cancel-wrapper">
+              <BodyCopy
+                className="item-unavailable"
+                fontFamily="secondary"
+                fontWeight="semibold"
+                fontSize="fs14"
+              >
+                {PICKUP_LABELS.ITEM_UNAVAILABLE}
+              </BodyCopy>
+            </div>
             <div className="warning-cancel-wrapper">
               <Button
                 onClick={onCloseClick}
