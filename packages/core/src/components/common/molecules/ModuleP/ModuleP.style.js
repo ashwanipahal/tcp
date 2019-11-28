a9e766cc57f9bec56bcfd7cb67222cd5fcc2004f
Aman Bhardwaj <amabhard@publicisgroupe.net>
2019-11-28 18:44:30 +0530

    bug fixes

--- packages/core/src/components/common/molecules/ModuleP/ModuleP.style.js
+++ packages/core/src/components/common/molecules/ModuleP/ModuleP.style.js
@@ -1,19 +1,8 @@
 import { css } from 'styled-components';
 
 export default css`
-  .item-container-inner {
-    height: ${props => (props.isPromoAvailable ? '405px' : '385px')};
-    @media ${props => props.theme.mediaQuery.medium} {
-      height: ${props => (props.isPromoAvailable ? '505px' : '485px')};
-    }
-    @media ${props => props.theme.mediaQuery.large} {
-      height: ${props => (props.isPromoAvailable ? '577px' : '557px')};
-    }
-  }
-
   margin: 0 10px;
   .product-image-container {
-    height: auto;
     a {
       min-height: 185px;
       display: block;
@@ -33,16 +22,9 @@ export default css`
     }
   }
 
-  @media ${props => props.theme.mediaQuery.medium} {
-    .product-image-container {
-      height: auto;
-    }
-  }
-
   @media ${props => props.theme.mediaQuery.large} {
     margin: 0 ${props => props.theme.spacing.APP_LAYOUT_SPACING.XS};
     .product-image-container {
-      height: auto;
       a {
         min-height: 259px;
       }
