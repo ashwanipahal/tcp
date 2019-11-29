06de1f2c4e4f28fc64c676341c40c54958c8789a
Aman Bhardwaj <amabhard@publicisgroupe.net>
2019-11-29 11:02:12 +0530

    sandbox

--- packages/web/src/components/common/molecules/Recommendations/config.js
+++ packages/web/src/components/common/molecules/Recommendations/config.js
@@ -2,7 +2,6 @@ import { breakpoints } from '@tcp/core/styles/themes/TCP/mediaQuery';
 
 export default {
   CAROUSEL_OPTIONS: {
-    infinite: true,
     autoplay: true,
     arrows: true,
     autoplaySpeed: 4000,
