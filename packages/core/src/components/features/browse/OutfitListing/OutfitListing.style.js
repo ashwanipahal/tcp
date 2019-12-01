1902201fa4d19c48ab809b8bbedda594014f508c
SidNanda <siddhartha.nanda@publicissapient.com>
2019-12-01 19:17:56 +0530

    RWD-16842 ui issues on outfit breadcrumbs fixed

--- packages/core/src/components/features/browse/OutfitListing/OutfitListing.style.js
+++ packages/core/src/components/features/browse/OutfitListing/OutfitListing.style.js
@@ -7,4 +7,14 @@ export default css`
     margin-bottom: 5px;
     text-align: center;
   }
+  .bread-crumb {
+    margin: ${props => props.theme.spacing.ELEM_SPACING.MED} 0
+      ${props => props.theme.spacing.ELEM_SPACING.LRG};
+    @media ${props => props.theme.mediaQuery.medium} {
+      margin-bottom: ${props => props.theme.spacing.ELEM_SPACING.XXL};
+    }
+    @media ${props => props.theme.mediaQuery.large} {
+      margin-bottom: ${props => props.theme.spacing.ELEM_SPACING.LRG};
+    }
+  }
 `;
