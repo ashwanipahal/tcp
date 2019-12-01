a3c1bd53b702c519455e00b1dc9a8b77d5daf609
SidNanda <siddhartha.nanda@publicissapient.com>
2019-12-01 19:55:18 +0530

    RWD-16842 removed dummt template and styling

--- packages/core/src/components/features/browse/OutfitListing/OutfitListing.style.js
+++ packages/core/src/components/features/browse/OutfitListing/OutfitListing.style.js
@@ -1,12 +1,6 @@
 import { css } from 'styled-components';
 
 export default css`
-  .placeholder div {
-    background: #d8d8d8;
-    padding: 10px 0;
-    margin-bottom: 5px;
-    text-align: center;
-  }
   .bread-crumb {
     margin: ${props => props.theme.spacing.ELEM_SPACING.MED} 0
       ${props => props.theme.spacing.ELEM_SPACING.LRG};
