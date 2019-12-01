f9e940c745e7bf2fa7419235e9b7dcc06af688ae
Gagan Bhardwaj <gbhardwaj4@sapient.com>
2019-11-30 22:53:23 -0500

    RWD-14874: Adding unit test cases for coverage for app

--- packages/core/src/components/features/CnC/common/organism/CnCTemplate/__tests__/CnCTemplate.view-test.native.jsx
+++ packages/core/src/components/features/CnC/common/organism/CnCTemplate/__tests__/CnCTemplate.view-test.native.jsx
@@ -48,6 +48,51 @@ describe('CnCCommonTemplate Page', () => {
       },
       showPayPalButton: true,
       showVenmoSubmit: true,
+      isPayPalWebViewEnable: true,
+    };
+    const tree = shallow(<CnCCommonTemplate {...props} />);
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render paypal button correctly if on confirmation page for registered user', () => {
+    const props = {
+      isConfirmationPage: true,
+      isGuest: false,
+      isVenmoPaymentInProgress: true,
+      venmoPayment: {
+        ccBrand: 'VENMO',
+        ccType: 'VENMO',
+        defaultInd: true,
+        userName: 'test-user',
+      },
+      showPayPalButton: true,
+      showVenmoSubmit: true,
+      isPayPalWebViewEnable: true,
+    };
+    const tree = shallow(<CnCCommonTemplate {...props} />);
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render payment buttons correctly if on confirmation page for registered user', () => {
+    const props = {
+      isConfirmationPage: true,
+      isGuest: false,
+      isVenmoPaymentInProgress: true,
+      venmoPayment: {
+        ccBrand: 'VENMO',
+        ccType: 'VENMO',
+        defaultInd: true,
+        userName: 'test-user',
+      },
+      showPayPalButton: true,
+      showVenmoSubmit: true,
+      isPayPalWebViewEnable: true,
+      getPayPalSettings: jest.fn(),
+      onVenmoSubmit: jest.fn(),
+      onVenmoError: jest.fn(),
+      btnText: 'Review',
+      onPress: jest.fn(),
+      backLinkText: 'Billing',
+      onBackLinkPress: jest.fn(),
+      footerBody: null,
     };
     const tree = shallow(<CnCCommonTemplate {...props} />);
     expect(tree).toMatchSnapshot();
