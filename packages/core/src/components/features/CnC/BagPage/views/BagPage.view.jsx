318ac080c7ea595f91ab5f8ccb2fb7dbc217a790
Madan Joshi <madan.joshi@publicissapient.com>
2019-12-02 14:12:14 +0530

    Bag page load event

--- packages/core/src/components/features/CnC/BagPage/views/BagPage.view.jsx
+++ packages/core/src/components/features/CnC/BagPage/views/BagPage.view.jsx
@@ -22,7 +22,6 @@ import BagPageUtils from './Bagpage.utils';
 import QuickViewModal from '../../../../common/organisms/QuickViewModal/container/QuickViewModal.container';
 import InformationHeader from '../../common/molecules/InformationHeader';
 import { isClient } from '../../../../../utils';
-import BagPageAnalytics from './BagPageAnalytics.view';
 
 class BagPageView extends React.PureComponent {
   constructor(props) {
@@ -240,7 +239,6 @@ class BagPageView extends React.PureComponent {
             orderItemsCount
           )}
         {this.renderRecommendations()}
-        <BagPageAnalytics {...this.props} />
       </React.Fragment>
     );
   };
