0e357b4229c6c48df3efa8db374249bde882db4e
Himalaya <himalaya.batra@publicissapient.com>
2019-12-02 18:54:07 +0530

    RWD-17151: APP|WEB cross brand PDP

--- packages/core/src/components/common/molecules/PickupPromotionBanner/container/PromotionBanner.selectors.jsx
+++ packages/core/src/components/common/molecules/PickupPromotionBanner/container/PromotionBanner.selectors.jsx
@@ -1,5 +1,3 @@
-import { getLabelValue } from '@tcp/core/src/utils/utils';
-
 const getPickupPromotionBannerLabels = state => {
   const labelValue = state.Labels && state.Labels.global && state.Labels.global.bossPromotions;
   return labelValue || {};
