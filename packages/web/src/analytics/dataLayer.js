995585d8e81d4e1656523792bf6493c2577901c3
Sakshi Jain <sakshi.jain@publicissapient.com>
2019-12-02 21:01:17 +0530

    DTN-17113 | Analytics | My Account | - Defects

--- packages/web/src/analytics/dataLayer.js
+++ packages/web/src/analytics/dataLayer.js
@@ -188,12 +188,17 @@ export default function create(store) {
 
     checkoutType: {
       get() {
-        return store
-          .getState()
-          .User.get('personalData')
-          .get('isGuest')
-          ? 'guest'
-          : 'registered';
+        const { pageType = '' } = store.getState().pageData;
+        let userType = '';
+        if (pageType === 'checkout') {
+          userType = store
+            .getState()
+            .User.get('personalData')
+            .get('isGuest')
+            ? 'guest'
+            : 'registered';
+        }
+        return userType;
       },
     },
 
@@ -253,18 +258,22 @@ export default function create(store) {
     },
     cartType: {
       get() {
-        const orderDetails = store.getState().CartPageReducer.get('orderDetails');
-        let typeCart = 'standard';
-        const isBopisOrder = orderDetails.get('isBopisOrder');
-        const isBossOrder = orderDetails.get('isBossOrder');
-        const isPickupOrder = orderDetails.get('isPickupOrder');
-        const isShippingOrder = orderDetails.get('isShippingOrder');
-        if (isShippingOrder && (isBopisOrder || isBossOrder || isPickupOrder)) {
-          typeCart = 'mix';
-        } else if (isBopisOrder && !isBossOrder) {
-          typeCart = 'bopis';
-        } else if (isBossOrder && !isBopisOrder) {
-          typeCart = 'boss';
+        const { pageType = '' } = store.getState().pageData;
+        let typeCart = '';
+        if (pageType === 'checkout') {
+          const orderDetails = store.getState().CartPageReducer.get('orderDetails');
+          typeCart = 'standard';
+          const isBopisOrder = orderDetails.get('isBopisOrder');
+          const isBossOrder = orderDetails.get('isBossOrder');
+          const isPickupOrder = orderDetails.get('isPickupOrder');
+          const isShippingOrder = orderDetails.get('isShippingOrder');
+          if (isShippingOrder && (isBopisOrder || isBossOrder || isPickupOrder)) {
+            typeCart = 'mix';
+          } else if (isBopisOrder && !isBossOrder) {
+            typeCart = 'bopis';
+          } else if (isBossOrder && !isBopisOrder) {
+            typeCart = 'boss';
+          }
         }
         return typeCart;
       },
