34e8f108c6fc886cd25be7054e59c951f30c860b
furkan <furkan.ali@publicissapient.com>
2019-12-03 15:42:13 +0530

    review comment fixed

--- packages/core/src/components/common/molecules/Barcode/views/Barcode.view.native.jsx
+++ packages/core/src/components/common/molecules/Barcode/views/Barcode.view.native.jsx
@@ -2,8 +2,7 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { View } from 'react-native';
 import Barcode from 'react-native-barcode-builder';
-import BodyCopy from '@tcp/core/src/components/common/atoms/BodyCopy';
-import { ViewWithSpacing } from '@tcp/core/src/components/common/atoms/styledWrapper';
+import { BodyCopyWithSpacing } from '@tcp/core/src/components/common/atoms/styledWrapper';
 
 class BarcodeView extends PureComponent {
   render() {
@@ -12,9 +11,13 @@ class BarcodeView extends PureComponent {
       <View>
         <Barcode value={value} width={width} height={height} format={format} />
         {displayValue && (
-          <ViewWithSpacing spacingStyles="margin-bottom-XXS">
-            <BodyCopy textAlign="center" fontFamily="secondary" fontSize={fontSize} text={value} />
-          </ViewWithSpacing>
+          <BodyCopyWithSpacing
+            textAlign="center"
+            fontFamily="secondary"
+            fontSize={fontSize}
+            text={value}
+            spacingStyles="margin-bottom-XXS"
+          />
         )}
       </View>
     );
