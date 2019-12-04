86d8f6a202b4d603ee89020df70142ce297c54a6
Mohit Jain <mohit.jain2@publicissapient.com>
2019-12-04 18:01:47 +0530

    RWD-14755 | only SMALL DamImage support

--- packages/core/src/components/common/atoms/DamImage/views/DamImage.jsx
+++ packages/core/src/components/common/atoms/DamImage/views/DamImage.jsx
@@ -99,6 +99,7 @@ const RenderImage = forwardRef((imgProps, ref) => {
     itemBrand,
     showPlaceHolder,
     isProductImage,
+    xsOnly,
     ...other
   } = imgProps;
 
@@ -106,15 +107,19 @@ const RenderImage = forwardRef((imgProps, ref) => {
 
   return (
     <picture>
-      <source
-        media={`(min-width: ${breakpoints.values.lg}px)`}
-        data-srcset={getBreakpointImgUrl('lg', imgProps)}
-      />
+      {!xsOnly && (
+        <source
+          media={`(min-width: ${breakpoints.values.lg}px)`}
+          data-srcset={getBreakpointImgUrl('lg', imgProps)}
+        />
+      )}
 
-      <source
-        media={`(min-width: ${breakpoints.values.sm}px)`}
-        data-srcset={getBreakpointImgUrl('sm', imgProps)}
-      />
+      {!xsOnly && (
+        <source
+          media={`(min-width: ${breakpoints.values.sm}px)`}
+          data-srcset={getBreakpointImgUrl('sm', imgProps)}
+        />
+      )}
 
       {lazyLoad ? (
         <LazyLoadImage
@@ -155,6 +160,7 @@ const DamImage = props => {
     showPlaceHolder,
     videoData,
     isProductImage,
+    xsOnly,
     ...other
   } = props;
 
@@ -173,6 +179,7 @@ const DamImage = props => {
     itemBrand,
     showPlaceHolder,
     isProductImage,
+    xsOnly,
     ...other,
   };
 
@@ -214,6 +221,7 @@ const DamImage = props => {
 
 DamImage.defaultProps = {
   lazyLoad: true,
+  xsOnly: false,
   theme: {},
   imgConfigs: [],
   imgData: {
@@ -236,6 +244,8 @@ DamImage.defaultProps = {
 DamImage.propTypes = {
   /* Load the image laziliy or not */
   lazyLoad: PropTypes.bool,
+  /* Flag to load only small image */
+  xsOnly: PropTypes.bool,
   /* StyleComponent theme, will come from context */
   theme: PropTypes.shape({ breakpoints: PropTypes.object }),
 
--- packages/core/src/components/features/browse/ProductListing/molecules/ProductList/views/ProductColorChip.jsx
+++ packages/core/src/components/features/browse/ProductListing/molecules/ProductList/views/ProductColorChip.jsx
@@ -77,6 +77,7 @@ export default class ProductColorChip extends React.Component {
           imgData={imgData}
           imgConfigs={imgDataConfig}
           isProductImage
+          xsOnly
         />
       </button>
     );
