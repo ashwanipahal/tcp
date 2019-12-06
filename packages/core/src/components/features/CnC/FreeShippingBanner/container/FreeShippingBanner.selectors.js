5b33eb638c75d10b829e875dac84af99e5342975
Sakshi Jain <sakshi.jain@publicissapient.com>
2019-12-06 10:20:52 +0530

    Updated selector with GetLabelValue function to fetch label value

--- packages/core/src/components/features/CnC/FreeShippingBanner/container/FreeShippingBanner.selectors.js
+++ packages/core/src/components/features/CnC/FreeShippingBanner/container/FreeShippingBanner.selectors.js
@@ -1,9 +1,13 @@
+import { getLabelValue } from '@tcp/core/src/utils/utils';
+
 export const getLabels = state => {
   return state.Labels.checkout.bagPage;
 };
 
 export const getAccessibilityLabels = state => {
-  return state.Labels.global.accessibility;
+  return {
+    lbl_fast_shipping: getLabelValue(state.Labels, 'lbl_fast_shipping', 'accessibility', 'global'),
+  };
 };
 
 export default {
